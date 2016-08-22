13dd455192e99323e6750902318c822e2c10ebff
Oscar Lazaro <elazaro@sigcomt.com>
2016-08-22 16:44:30 -0500

    CatalogoPersonalizadoController

---
+++ app/Portal.Consultoras.Web/Controllers/CatalogoPersonalizadoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class CatalogoPersonalizadoController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
