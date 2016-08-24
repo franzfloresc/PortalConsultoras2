735631985ed1a6595522ea26544a1dede0e681ae
rdelarosab <srdelarosab@outlook.com>
2016-08-24 16:42:22 -0500

    Video Mobile y Desktop

--- app/Portal.Consultoras.Web/Models/UsuarioModel.cs
+++ app/Portal.Consultoras.Web/Models/UsuarioModel.cs
@@ -105,6 +105,8 @@ namespace Portal.Consultoras.Web.Models
 
         public DateTime FechaLimPago { get; set; }
 
+        public int VioVideoModelo { get; set; }
+
         public int PedidoID { get; set; }
         public string CodigorRegion { get; set; }
         public string CodigoZona { get; set; }
