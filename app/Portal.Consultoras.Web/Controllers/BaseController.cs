bf903785f8e2999260f733cb421794c99de883e4
Gabriel Alva <galva@sigcomt.com>
2016-08-24 16:54:26 -0500

    1. Google Tag Manager
    2. Dimensiones personalizadas - Queda pendiente obtencion de semana.

--- app/Portal.Consultoras.Web/Controllers/BaseController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseController.cs
@@ -495,7 +495,8 @@ namespace Portal.Consultoras.Web.Controllers
                 DateTime fechaHoy = DateTime.Now.AddHours(model.ZonaHoraria).Date;
                 ViewBag.FechaActualPais = fechaHoy.ToShortDateString();
                 ViewBag.Dias = fechaHoy >= model.FechaInicioCampania.Date && fechaHoy <= model.FechaFinCampania.Date ? 0 : (model.FechaInicioCampania.Subtract(DateTime.Now.AddHours(model.ZonaHoraria)).Days + 1);
-                ViewBag.PeriodoAnalitycs = fechaHoy >= model.FechaInicioCampania.Date && fechaHoy <= model.FechaFinCampania.Date ? "Facturacion" : "Venta";
+                ViewBag.PeriodoAnalytics = fechaHoy >= model.FechaInicioCampania.Date && fechaHoy <= model.FechaFinCampania.Date ? "Facturacion" : "Venta";
+                ViewBag.SemanaAnalytics = ObtenerSemanaAnalytics();
 
                 DateTime FechaHoraActual = DateTime.Now.AddHours(model.ZonaHoraria);
                 TimeSpan HoraCierrePortal = model.EsZonaDemAnti == 0 ? model.HoraCierreZonaNormal : model.HoraCierreZonaDemAnti;
@@ -762,6 +763,11 @@ namespace Portal.Consultoras.Web.Controllers
 
                 #endregion
             }
+        }
+
+        private string ObtenerSemanaAnalytics()
+        {
+            return "Semana 1";
         }
 
         private string GetFormatDecimalPais(string isoPais)
