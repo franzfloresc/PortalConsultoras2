9ef4337e4d0419089544dcc6983a8da1770ab7c8
Alan AuPe <alaninforme@gmail.com>
2016-08-26 16:47:51 -0500

    Bug Barra

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Pedido/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Pedido/Index.js
@@ -573,6 +573,8 @@ function CargarDetallePedido(page, rows) {
             mostrarSimplificacionCUV();
             //if (tieneMicroefecto)
             //MostrarMicroEfecto();
+
+            MostrarBarra(response);
         },
         error: function (error) {
             //alert(error);
