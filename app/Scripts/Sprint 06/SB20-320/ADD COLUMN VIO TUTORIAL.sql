7b74f45d557487764805e569d957a6db5a36cf21
Oscar Lazaro <elazaro@sigcomt.com>
2016-08-29 09:31:44 -0500

    Se elimino los archivos scripts de base datos

--- app/Scripts/Sprint 06/SB20-320/ADD COLUMN VIO TUTORIAL.sql
+++
@@ -1,8 +0,0 @@
-if (select COUNT(*) from dbo.sysobjects inner join dbo.syscolumns on SYSOBJECTS.ID = SYSCOLUMNS.ID 
-	where sysobjects.id = object_id('dbo.Usuario') and SYSCOLUMNS.NAME = N'VioTutorial') = 0
-	ALTER TABLE dbo.Usuario ADD VioTutorial bit
-go
-
-UPDATE dbo.Usuario set VioTutorial = 0
-
-SELECT TOP 10 * FROM USUARIO
