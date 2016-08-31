7a1f278497354548e532d16ecb771e3e7b2f78ef
Carlos Jave <jave.carlos@gmail.com>
2016-08-31 13:02:15 -0500

    agregando scripts del 1 al 5

---
+++ app/Scripts/Sprint 05/SB20-353/AWNTS59/Rollback/1. Alter Table TipoEstrategia.sql
@@ -0,0 +1,23 @@
+
+
+USE BelcorpPeru_SB2
+GO
+
+ALTER TABLE dbo.TipoEstrategia DROP COLUMN FlagMostrarImg
+GO
+
+/*end*/
+
+USE BelcorpMexico_SB2
+GO
+
+ALTER TABLE dbo.TipoEstrategia DROP COLUMN FlagMostrarImg
+GO
+
+/*end*/
+
+USE BelcorpColombia_SB2
+GO
+
+ALTER TABLE dbo.TipoEstrategia DROP COLUMN FlagMostrarImg
+GO
