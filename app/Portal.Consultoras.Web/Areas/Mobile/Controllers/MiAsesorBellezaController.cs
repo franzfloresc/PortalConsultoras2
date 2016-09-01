7d70ceb980f9a2a5d2302130bd705ff3b9acc694
Kike <lead123456@hotmail.com>
2016-09-01 17:24:52 -0500

    SB20-466: Funcionamiento de página Mobile/MiAsesorBelleza

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/MiAsesorBellezaController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/MiAsesorBellezaController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Portal.Consultoras.Web.Areas.Mobile.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +10,35 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 {
     public class MiAsesorBellezaController : BaseMobileController
     {
-        //
-        // GET: /Mobile/MiAsesorBelleza/
-
         public ActionResult Index()
         {
-            return View();
+            var userData = UserData();
+            MiAsesorBellezaModel model = new MiAsesorBellezaModel();
+
+            if (userData.CodigoISO == "PA")
+            {
+                model.OrdenMaximo = 2;
+                model.OrdenLbel = 1;
+                model.OrdenCyzone = 2;
+            }
+            else
+            {
+                model.OrdenMaximo = 3;
+                model.OrdenCyzone = 3;
+
+                if (ConfigurationManager.AppSettings.Get("PaisesEsika").Contains(userData.CodigoISO))
+                {
+                    model.OrdenEsika = 1;
+                    model.OrdenLbel = 2;
+                }
+                else
+                {
+                    model.OrdenLbel = 1;
+                    model.OrdenEsika = 2;
+                }
+            }
+
+            return View(model);
         }
 
     }
