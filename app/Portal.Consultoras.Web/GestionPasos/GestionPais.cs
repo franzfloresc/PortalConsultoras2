4c5ec8545ece0ec91313192260141a432b8430ec
CamiCasus <mcarbajal@sigcomt.com>
2016-09-06 11:59:04 -0500

    Ajustes Unete

---
+++ app/Portal.Consultoras.Web/GestionPasos/GestionPais.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.GestionPasos.EvaluacionCrediticia;
+
+namespace Portal.Consultoras.Web.GestionPasos
+{
+    public class GestionPais
+    {
+        public static Dictionary<string, IEvaluacionCrediticia> EvaluacionCrediticia = new Dictionary<string, IEvaluacionCrediticia>();
+
+        public GestionPais()
+        {
+            EvaluacionCrediticia = new Dictionary<string, IEvaluacionCrediticia>
+            {
+                {Constantes.CodigosISOPais.Chile, new EvaluacionCrediticiaChile()},
+                {Constantes.CodigosISOPais.Colombia, new EvaluacionCrediticiaColombia()},
+                {Constantes.CodigosISOPais.Mexico, new EvaluacionCrediticiaMexico()},
+                {Constantes.CodigosISOPais.Peru, new EvaluacionCrediticiaPeru()}
+            };
+        }
+    }
+}
