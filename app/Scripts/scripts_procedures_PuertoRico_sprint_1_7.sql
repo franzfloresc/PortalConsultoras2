0641aac730b5d78378e46dee64c86e9236da550d
Alan AuPe <alaninforme@gmail.com>
2016-09-08 19:09:58 -0500

    InsPedidoWebDetalle_SB2 y GetPedidosIngresadoFacturado_SB2

--- app/Scripts/scripts_procedures_PuertoRico_sprint_1_7.sql
+++ app/Scripts/scripts_procedures_PuertoRico_sprint_1_7.sql
@@ -2414,6 +2414,21 @@ CREATE PROCEDURE [dbo].InsPedidoWebDetalle_SB2
 	@EskitNueva bit = 0
 AS	
 BEGIN
+	
+	declare @existe int = 0
+	if @EskitNueva = 1
+	begin
+		select @existe = count(1)
+		from dbo.PedidoWebDetalle
+		where ConsultoraID = @ConsultoraID and CampaniaID = @CampaniaID and eskitNueva = 1
+
+		set @existe = isnull(@existe, 0)
+	end
+	
+	if (@existe = 0 and @EskitNueva = 1) or @EskitNueva = 0
+	begin
+		 
+	
 	declare @orden int = 0
 
 	SELECT @PedidoDetalleID = MAX(ISNULL(PedidoDetalleID,0))
@@ -2435,6 +2450,8 @@ BEGIN
 	@Cantidad*@PrecioUnidad, @CUV, @OfertaWeb, 0, @ConfiguracionOfertaID, @TipoOfertaSisID, 
 	@CodigoUsuarioCreacion, dbo.fnObtenerFechaHoraPais(),@SubTipoOfertaSisID, @EsSugerido, @EskitNueva, @orden)
 
+	end
+	
 END
 
 go
@@ -2662,6 +2679,11 @@ FROM @T1 T
 ) AS R
 ON R.CampaniaID = T.CampaniaID AND T.EstadoPedido <> 'F'
 
+-- solo los 4 correlativos anteriores de la campaña actual
+declare @CampaniaAnterior int = 0
+set @CampaniaAnterior = ffvv.fnGetCampaniaAnterior(@CampaniaID, @top)
+set @CampaniaAnterior = isnull(@CampaniaAnterior, 0)
+
 SELECT top (@top)
  CampaniaID  
 , ImporteTotal 
@@ -2676,7 +2698,9 @@ SELECT top (@top)
 , FechaRegistro
 , CanalIngreso 
 , CantidadProductos 
-FROM @T1 order by CampaniaID desc
+FROM @T1 
+where CampaniaID >= @CampaniaAnterior
+order by CampaniaID desc
 
 
 end
