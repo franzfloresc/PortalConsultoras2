5465f42e2d18f8066c9f1a90e5e84998fa356bbb
Alan AuPe <alaninforme@gmail.com>
2016-09-14 18:07:02 -0500

    ObtenerEstrategiasOfertaParaTi_SB2

--- app/Scripts/Sprint 08/ObtenerEstrategiasOfertaParaTi_SB2.sql
+++ app/Scripts/Sprint 08/ObtenerEstrategiasOfertaParaTi_SB2.sql
@@ -6,50 +6,32 @@ GO
 
 CREATE PROCEDURE [dbo].[ObtenerEstrategiasOfertaParaTi_SB2]
 	@CampaniaID INT,
-	@CodigoConsultora VARCHAR(30)
+	@CodigoConsultora VARCHAR(30) = ''
 AS
 /*
-dbo.ObtenerEstrategiasOfertaParaTi_SB2 201614,'000758833'
+dbo.ObtenerEstrategiasOfertaParaTi_SB2 201612,''
 */
 BEGIN
 
--- declare @CampaniaID INT = 201613
--- declare @CodigoConsultora VARCHAR(30) = '000758833'
+-- declare @CampaniaID INT = 201612
+-- declare @CodigoConsultora VARCHAR(30) = ''
 
-	SELECT
-		EstrategiaID,
-		CUV2 as CUV,
-		DescripcionCUV2 as NombreComercial,
-		dbo.ObtenerDescripcionEtiqueta(EtiquetaID) EtiquetaDescripcion,
-		e.Precio as PrecioCatalogo,
-		dbo.ObtenerDescripcionEtiqueta(EtiquetaID2) EtiquetaDescripcion2,
-		Precio2,
-		TextoLibre,
-		FlagEstrella,
-		ColorFondo,
-		e.TipoEstrategiaID,
-		e.ImagenURL as Imagen,
-		--te.ImagenEstrategia ImagenURL,
-		e.LimiteVenta,    
-		pc.MarcaID,
-		te.Orden Orden1,
-		op.Orden Orden2,
-		pc.IndicadorMontoMinimo,
-		pc.CodigoProducto,
-		0 AS FlagNueva, 
-		3 as TipoEstrategiaImagenMostrar	--Oferta para Ti
-		, E.EtiquetaID		
-		, E.EtiquetaID2	
-		, c.codigo as CodigoConsultora
+ SET @CodigoConsultora = ISNULL(@CodigoConsultora, '')
+
+	SELECT distinct
+		e.CUV2 as CUV,
+		e.DescripcionCUV2 as NombreComercial,
+		E.ImagenURL AS Imagen
 	FROM Estrategia E
 	INNER JOIN TipoEstrategia TE ON E.TipoEstrategiaID = TE.TipoEstrategiaID AND TE.FlagActivo = 1 AND TE.flagRecoPerfil = 1
 	INNER JOIN ods.Campania ca ON CA.Codigo = e.campaniaid
 	INNER JOIN ods.OfertasPersonalizadas op ON E.CUV2 = op.CUV and op.AnioCampanaVenta = CA.Codigo and op.TipoPersonalizacion = 'OPT'
-	INNER JOIN ods.Consultora c ON op.CodConsultora = c.Codigo and c.codigo = @CodigoConsultora
+	INNER JOIN ods.Consultora c ON op.CodConsultora = c.Codigo and ( c.codigo = @CodigoConsultora OR @CodigoConsultora = '')
 	INNER JOIN ods.ProductoComercial PC ON PC.CampaniaID = CA.CampaniaID AND PC.CUV = E.CUV2
 	WHERE
 		E.Activo = 1		
-		AND ca.Codigo = @CampaniaID
-	ORDER BY CASE WHEN ISNULL(op.Orden,0) = 0 THEN te.Orden ELSE op.Orden END ASC
+		AND e.campaniaid = @CampaniaID
+	--ORDER BY CASE WHEN ISNULL(op.Orden,0) = 0 THEN te.Orden ELSE op.Orden END ASC
+	group by e.CUV2, e.DescripcionCUV2, E.ImagenURL
 
 end
