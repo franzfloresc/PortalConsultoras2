85e6f86577125d72e9da1d867539b9450d9a95a8
Carlos Jave <jave.carlos@gmail.com>
2016-09-19 11:50:25 -0500

    correccion bugs

--- app/Portal.Consultoras.Web/Controllers/PedidoController.cs
+++ app/Portal.Consultoras.Web/Controllers/PedidoController.cs
@@ -4697,6 +4697,9 @@ namespace Portal.Consultoras.Web.Controllers
 
                         if (olstProducto.Count != 0)
                         {
+                            if (!olstProducto[0].TieneStock)
+                                continue;
+
                             string descripcion = producto.NombreComercial;
                             string imagenUrl = Util.SubStr(producto.Imagen, 0);
 
