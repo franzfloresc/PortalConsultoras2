566b56f1a84f5e256e726f313707787ac66c5768
Alan AuPe <alaninforme@gmail.com>
2016-09-23 12:29:11 -0500

    Cambio de EsPedidoRechazado_SB2 y donde se utiliza

--- app/Scripts/Sprint 08/EsPedidoRechazado_SB2.sql
+++ app/Scripts/Sprint 08/EsPedidoRechazado_SB2.sql
@@ -1,6 +1,6 @@
 
 GO
-CREATE FUNCTION dbo.EsPedidoRechazado_SB2
+ALTER FUNCTION dbo.EsPedidoRechazado_SB2
 (
 	@ConsultoraID bigint
 	,@CampaniaID int
@@ -8,54 +8,87 @@ CREATE FUNCTION dbo.EsPedidoRechazado_SB2
 )
 RETURNS @T TABLE(
 	esRechazado INT
+	,IndicadorEnviado BIT
 )
 as 
 /*
-@esRechazado => 0: Proceso (sin respuesta), 1: Rechazado, 2: No Rechazado
+@esRechazado => -1: Sin enviar, 0: Proceso (sin respuesta), 1: Rechazado, 2: No Rechazado
 */
 begin
-	SET @esRechazado = 0
+	SET @esRechazado = -1
 
 	declare @fecha datetime
 	declare @estado varchar(25)
 	declare @CodigoConsultora varchar(25)
 	declare @IdProceso int
 
-	select @fecha = FechaProceso, @CodigoConsultora = c.Codigo
-	from pedidoweb p
-		inner join ods.consultora c on c.ConsultoraID = p.ConsultoraID
-	where IndicadorEnviado = 1
-	and p.CampaniaID = @CampaniaID
-	and p.ConsultoraID = @ConsultoraID
+	declare @IndicadorEnviado bit = 0  
+	declare @fechaFin datetime
 	
-	select @IdProceso = p.IdProcesoPedidoRechazado, @estado = P.Estado
-	from GPR.ProcesoPedidoRechazado p
-	where P.Fecha > @fecha
-	order by p.Fecha asc
+	Select @IndicadorEnviado  = IndicadorEnviado    
+	From pedidoweb (nolock)  
+	Where CampaniaID = @CampaniaID and consultoraid = @consultoraid    
 
-	if @IdProceso > 0
-	begin
+	If(@IndicadorEnviado = 1)
+	BEGIN
+				set @esRechazado = 0
 		
-		SET @esRechazado = 2
-
-		if @Estado = 'OK'
-		begin
-				select @IdProceso = count(1)
+				select @fecha = FechaProceso, @CodigoConsultora = c.Codigo
+				from pedidoweb p
+					inner join ods.consultora c on c.ConsultoraID = p.ConsultoraID
+				where IndicadorEnviado = 1
+				and p.CampaniaID = @CampaniaID
+				and p.ConsultoraID = @ConsultoraID
+	
+				select @IdProceso = p.IdProcesoPedidoRechazado, @estado = P.Estado
 				from GPR.ProcesoPedidoRechazado p
-					inner join GPR.PedidoRechazado r
-						on r.IdProcesoPedidoRechazado = p.IdProcesoPedidoRechazado
-							and r.RequiereGestion = 1
-							and r.Campania = @CampaniaID 
-							and r.CodigoConsultora = @CodigoConsultora
-				where p.IdProcesoPedidoRechazado = @IdProceso
+				where P.Fecha > @fecha
+				order by p.Fecha asc
 
 				if @IdProceso > 0
-					SET @esRechazado = 1
-		end
-	
-	end
+				begin
+		
+					SET @esRechazado = 2
 
-	INSERT @T(esRechazado)
-	VALUES(@esRechazado)
+					if @Estado = 'OK' OR @Estado = 'OK'
+					begin
+							select @IdProceso = count(1)
+							from GPR.ProcesoPedidoRechazado p
+								inner join GPR.PedidoRechazado r
+									on r.IdProcesoPedidoRechazado = p.IdProcesoPedidoRechazado
+										and r.RequiereGestion = 1
+										and r.Campania = @CampaniaID 
+										and r.CodigoConsultora = @CodigoConsultora
+							where p.IdProcesoPedidoRechazado = @IdProceso
+
+							if @IdProceso > 0
+								SET @esRechazado = 1
+					end
+	
+				end
+
+
+				if @esRechazado = 1 -- esta rechazado
+				begin
+					
+					DECLARE @ZonaID INT, @RegionID INT
+					SELECT @ZonaID = ZonaID, @RegionID = RegionID
+					FROM ods.consultora
+					WHERE ConsultoraID = @ConsultoraID
+
+					select @fechaFin = c.FechaInicioReFacturacion
+					FROM [ods].[Cronograma] c (nolock)
+					INNER JOIN [ods].[Campania] ca (nolock) ON c.CampaniaID = ca.CampaniaID
+					WHERE c.ZonaID = @ZonaID AND c.RegionID = @RegionID  AND ca.Codigo = @CampaniaID
+
+					if convert(date, @fechaFin) < convert(date, getdate()) -- si llego el ultimo dia de la ReFacturacion
+						set @esRechazado = 2 -- continua con el proceso
+				end
+
+	END
+
+
+	INSERT @T(esRechazado, IndicadorEnviado)
+	VALUES(@esRechazado, @IndicadorEnviado)
 	RETURN
 end 
