a19e85cfa757912a41d38a444e264bb757cc6e78
pcabrerahundred <paul.cabrera@hundred.com.pe>
2016-09-30 14:55:05 -0500

    Scripts para Consultora Online SB-398-2

---
+++ app/Scripts/Sprint 08/SB20-398-2/Rollback/5. GetSolicitudesPedidoDetalle_SB2.sql
@@ -0,0 +1,51 @@
+USE BelcorpPeru
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
+
+/*end*/
+
+USE BelcorpColombia
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
+
+/*end*/
+
+USE BelcorpChile
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
+
+/*end*/
+
+USE BelcorpMexico
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
+
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
+
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+IF OBJECT_ID('dbo.GetSolicitudesPedidoDetalle_SB2', 'P') IS NOT NULL
+  DROP PROCEDURE dbo.GetSolicitudesPedidoDetalle_SB2
+GO
---
+++ app/Scripts/Sprint 08/SB20-398-2/Script/7. UpdSolicitudClienteDetalle_SB2.sql
@@ -0,0 +1,169 @@
+USE BelcorpPeru
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudId
+
+END
+
+/*end*/
+
+USE BelcorpColombia
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudId
+
+END
+
+/*end*/
+
+USE BelcorpChile
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudId
+
+END
+
+/*end*/
+
+USE BelcorpMexico
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudIdId
+
+END
+
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudIdd
+
+END
+
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+CREATE PROCEDURE [dbo].[UpdSolicitudClienteDetalle_SB2]
+(
+	@SolicitudDetalleId BIGINT,
+	@TipoAtencion INT,
+	@PedidoWebID INT,
+	@PedidoWebDetalleID INT
+)
+as
+BEGIN
+
+DECLARE @SolicitudId INT
+
+UPDATE SolicitudClienteDetalle SET TipoAtencion = @TipoAtencion, 
+PedidoWebID = @PedidoWebID, 
+PedidoWebDetalleID = @PedidoWebDetalleID 
+WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+SELECT @SolicitudId = SolicitudClienteID FROM SolicitudClienteDetalle WHERE SolicitudClienteDetalleID = @SolicitudDetalleId
+
+UPDATE SolicitudCliente set PedidoWebID = @PedidoWebID WHERE SolicitudClienteID = @SolicitudId
+
+END
+
+
+
