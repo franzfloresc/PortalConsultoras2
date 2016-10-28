0d4cf7c359c911bbd37f720b3608d2cb261ca720
Oscar Lazaro <elazaro@sigcomt.com>
2016-10-28 15:11:49 -0500

    Avance DAHInformacionOnlineConsultora

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/TerminosYCondicionesController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/TerminosYCondicionesController.cs
@@ -7,10 +7,10 @@ using System.Web.Mvc;
 namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 {
     public class TerminosYCondicionesController : BaseMobileController
+    {
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
