a5e08722cf9d5ff286d90ddea9b44e41fd692b9c
Nohely <ederlazaro@outlook.com>
2016-10-28 12:33:25 -0500

    terminos y condiciones / ajuste responsive a 1024

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/TerminosYCondicionesController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/TerminosYCondicionesController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 {
     public class TerminosYCondicionesController : BaseMobileController
+    {
         public ActionResult Index()
         {
             return View();
