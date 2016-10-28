c79ba574a6fc073bdde52dcaee8965f3ae4579b3
Nohely <ederlazaro@outlook.com>
2016-10-28 10:26:30 -0500

    TERMINOS Y CONDICIONES

---
+++ app/Portal.Consultoras.Web/Controllers/TerminosYCondicionesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class TerminosYCondicionesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
