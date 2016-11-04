e35926c1f7fa2672dd2126af62b697c8621c0d4e
Carlos Jave <jave.carlos@gmail.com>
2016-11-04 18:02:00 -0500

    mis reclamos parte 1

--- app/Portal.Consultoras.Web/Models/MisReclamosModel.cs
+++ app/Portal.Consultoras.Web/Models/MisReclamosModel.cs
@@ -29,5 +29,7 @@ namespace Portal.Consultoras.Web.Models
         
         public string Email { get; set; }
         public string Telefono { get; set; }
+
+        public List<CDRWebModel> ListaCDRWeb { get; set; } 
     }
 }
