f9714f0a14be548d9c9301361ada484a584406ad
luisruidias <luis.ruidias@hundred.com.pe>
2016-11-21 17:31:31 -0500

    (merge) valores de LoginController al BaseController

--- app/Portal.Consultoras.Web/Controllers/LoginController.cs
+++ app/Portal.Consultoras.Web/Controllers/LoginController.cs
@@ -412,7 +412,6 @@ namespace Portal.Consultoras.Web.Controllers
                     model.CodigoConsultora = oBEUsuario.CodigoConsultora;
                     model.NombreConsultora = oBEUsuario.Nombre;
                     model.RolID = oBEUsuario.RolID;
-                    model.EMail = oBEUsuario.EMail;
                     model.CampaniaID = oBEUsuario.CampaniaID;
                     model.BanderaImagen = oBEUsuario.BanderaImagen;
                     model.CambioClave = Convert.ToInt32(oBEUsuario.CambioClave);
@@ -453,10 +452,7 @@ namespace Portal.Consultoras.Web.Controllers
                     {
                         model.DiaPROL = false;
                         model.FechaFacturacion = oBEUsuario.FechaInicioFacturacion.AddDays(-oBEUsuario.DiasAntes);
-                        if (oBEUsuario.DiasAntes == 0)
-                            model.HoraFacturacion = oBEUsuario.HoraInicio;
-                        else
-                            model.HoraFacturacion = oBEUsuario.HoraInicioNoFacturable;
+                        model.HoraFacturacion = oBEUsuario.DiasAntes == 0 ? oBEUsuario.HoraInicio : oBEUsuario.HoraInicioNoFacturable;
                     }
                     else
                     {
@@ -526,9 +522,7 @@ namespace Portal.Consultoras.Web.Controllers
                     model.FechaFinFIC = oBEUsuario.FechaFinFIC;
                     model.MenuNotificaciones = 1;
                     if (model.MenuNotificaciones == 1)
-                    {
                         model.TieneNotificaciones = TieneNotificaciones(oBEUsuario);
-                    }
                     model.NuevoPROL = oBEUsuario.NuevoPROL;
                     model.ZonaNuevoPROL = oBEUsuario.ZonaNuevoPROL;
 
@@ -561,6 +555,14 @@ namespace Portal.Consultoras.Web.Controllers
                     model.EsquemaDAConsultora = oBEUsuario.EsquemaDAConsultora;
                     model.ValidacionInteractiva = oBEUsuario.ValidacionInteractiva;
                     model.MensajeValidacionInteractiva = oBEUsuario.MensajeValidacionInteractiva;
+
+                    // Pago Online CO - CL - PR
+                    model.IndicadorPagoOnline = model.PaisID == 4 || model.PaisID == 3 || model.PaisID == 12 ? 1 : 0;
+                    model.UrlPagoOnline = model.PaisID == 4 ? "https://www.zonapagos.com/pagosn2/LoginCliente"
+                        : model.PaisID == 3 ? "https://www.belcorpchile.cl/BotonesPagoRedireccion/PagoConsultora.aspx"
+                        : model.PaisID == 12 ? "https://www.somosbelcorp.com/Paypal"
+                        : "";
+
                     model.OfertaFinal = oBEUsuario.OfertaFinal;
                     model.EsOfertaFinalZonaValida = oBEUsuario.EsOfertaFinalZonaValida;
                     model.CatalogoPersonalizado = oBEUsuario.CatalogoPersonalizado;
@@ -571,7 +573,7 @@ namespace Portal.Consultoras.Web.Controllers
                     model.FechaActualPais = oBEUsuario.FechaActualPais;
                     model.IndicadorBloqueoCDR = oBEUsuario.IndicadorBloqueoCDR;
 
-                    if (model.RolID == Portal.Consultoras.Common.Constantes.Rol.Consultora)
+                    if (model.RolID == Constantes.Rol.Consultora)
                     {
                         if (model.TieneHana == 1)
                         {
