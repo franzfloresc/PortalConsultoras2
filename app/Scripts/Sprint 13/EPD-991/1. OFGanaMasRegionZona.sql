fc44d5a68c2f2f78ad0f563315e6b4785968408f
pcabrerahundred <paul.cabrera@hundred.com.pe>
2016-11-25 16:04:32 -0500

    EPD-991, Scripts

---
+++ app/Scripts/Sprint 13/EPD-991/1. OFGanaMasRegionZona.sql
@@ -0,0 +1,166 @@
+USE BelcorpBolivia
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpChile
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpColombia
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpCostaRica
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpDominicana
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpEcuador
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpGuatemala
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpMexico
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpPanama
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpPeru
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpPuertoRico
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpSalvador
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
+
+GO
+
+USE BelcorpVenezuela
+GO
+
+create table OFGanaMasRegionZona
+(
+	OFGanaMasRegionZonaID int not null identity(1,1) primary key,
+	CodigoRegion varchar(4),
+	CodigoZona varchar(4),
+	Estado bit
+)
