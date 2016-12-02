ab20361f3e05162678b8bd3b232b492b1e8f7c5e
pcabrerahundred <paul.cabrera@hundred.com.pe>
2016-12-02 14:23:26 -0500

    Ajustes Scripts para EPD-991 Y EPD-1552

---
+++ app/Scripts/Sprint 13/EPD-991/2. Update Pais.sql
@@ -0,0 +1,126 @@
+USE BelcorpBolivia
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+ALTER TABLE Pais ADD OFGanaMas TINYINT
+
+INSERT INTO OfertaFinalParametria (Tipo,PrecioMinimo) VALUES('GM', 25)
+
+GO
