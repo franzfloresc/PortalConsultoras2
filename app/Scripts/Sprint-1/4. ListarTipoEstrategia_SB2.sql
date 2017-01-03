cf9af0aa2a15cffd84df5e96cd1d145595f5d5bf
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-01-03 09:46:29 -0500

    Ajustes PL20 Review

--- app/Scripts/Sprint-1/4. ListarTipoEstrategia_SB2.sql
+++ app/Scripts/Sprint-1/4. ListarTipoEstrategia_SB2.sql
@@ -1,7 +1,9 @@
-USE BelcorpPeru
+
+USE BelcorpBolivia
 GO
 
-ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2] 
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
  @TipoEstrategiaID INT  
 AS  
 BEGIN  
@@ -12,25 +14,25 @@ BEGIN
   SELECT   
    TipoEstrategiaID,   
    DescripcionEstrategia,   
-   isnull(dbo.ObtenerDescripcionOferta(TipoEstrategiaID),'') DescripcionOferta,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
    Orden,   
    FlagActivo,   
    CodigoPrograma,
-   isnull(dbo.ObtenerOfertaID(TipoEstrategiaID),'') OfertaID,  
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
    ImagenEstrategia,  
    FlagNueva,  
    FlagRecoPerfil,  
    FlagRecoProduc
    , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
-   , CASE TipoEstrategiaID
-	WHEN 10 THEN 1
-	WHEN 1009 THEN 2
-	WHEN 2009 THEN 3
-	WHEN 3009 THEN 4
-	WHEN 3011 THEN 5
-	WHEN 3012 THEN 6
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 1002 then 3
+	when 2002 then 4
+	when 2003 then 5
+	when 2004 then 6
 	WHEN 3014 THEN 7
-   END AS CodigoGeneral
+   end as CodigoGeneral
   FROM   
    TipoEstrategia  
   WHERE  
@@ -38,5 +40,552 @@ BEGIN
   ORDER BY Orden, DescripcionEstrategia ASC  
  SET NOCOUNT OFF  
 END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpChile
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 1002 then 3
+	when 2002 then 4
+	when 2003 then 5
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpColombia
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 3
+	when 1003 then 4
+	when 1004 then 5
+	when 1005 then 6
+   end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END
+
+
+GO
+
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 4 then 1
+	when 1004 then 2
+	when 1005 then 4
+	when 1006 then 5
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+GO
+
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 1002 then 3
+	when 2002 then 4
+	when 2003 then 5
+	when 2004 then 6
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 5
+	when 4 then 4
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 5
+	when 4 then 4
+	when 6 then 6
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpMexico
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 3
+	when 1003 then 4
+	when 1004 then 5
+   end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpPanama
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 4
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpPeru
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 10 then 1
+	when 1009 then 2
+	when 2009 then 3
+	when 3009 then 4
+	when 3011 then 5
+	when 3012 then 6
+   end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 5
+	when 4 then 6
+	when 5 then 4
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 5
+	when 4 then 4
+	when 5 then 6
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
+
+GO
+
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+
+ALTER PROCEDURE [dbo].[ListarTipoEstrategia_SB2]  
+ @TipoEstrategiaID INT  
+AS  
+BEGIN  
+/*  
+ EXEC ListarTipoEstrategia_SB2 0  
+*/  
+ SET NOCOUNT ON  
+  SELECT   
+   TipoEstrategiaID,   
+   DescripcionEstrategia,   
+   dbo.ObtenerDescripcionOferta(TipoEstrategiaID) DescripcionOferta,   
+   Orden,   
+   FlagActivo,   
+   CodigoPrograma,
+   dbo.ObtenerOfertaID(TipoEstrategiaID) OfertaID,  
+   ImagenEstrategia,  
+   FlagNueva,  
+   FlagRecoPerfil,  
+   FlagRecoProduc
+   , ISNULL(FlagMostrarImg,0) AS FlagMostrarImg 		/* SB20-353 */
+   , case TipoEstrategiaID
+	when 1 then 1
+	when 2 then 2
+	when 3 then 4
+	when 4 then 3
+	WHEN 3014 THEN 7
+	end as CodigoGeneral
+  FROM   
+   TipoEstrategia  
+  WHERE  
+   TipoEstrategiaID = @TipoEstrategiaID OR 0 = @TipoEstrategiaID  
+  ORDER BY Orden, DescripcionEstrategia ASC  
+ SET NOCOUNT OFF  
+END 
+
 
 GO
