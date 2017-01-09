66cd48dab1b5616bdbdefa9897b69a47968ffc7e
Jose <jose.ponte@hundred.com.pe>
2017-01-09 10:28:08 -0500

    PL20-1237 - Pagina Publica - Cls Entidad BEProductoCompartido

---
+++ app/Portal.Consultoras.Entities/BEProductoCompartido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Portal.Consultoras.Entities
+{
+    public class BEProductoCompartido
+    {
+        [DataMember]
+        public int PcCampaniaID { get; set; }
+
+        [DataMember]
+        public string PcCuv { get; set; }
+
+        [DataMember]
+        public string PcPalanca { get; set; }
+
+        [DataMember]
+        public int PcDetalle { get; set; }
+
+        [DataMember]
+        public int PcApp { get; set; }
+
+        [DataMember]
+        public int PcID { get; set; }
+
+        [DataMember]
+        public int PaisID { get; set; }
+    }
+}
