7307b12c857d644dfa7eaa11e0468c1a9756ad54
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-01-17 11:30:41 -0500

    se termino el ticket PL20-1269

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/CatalogoPersonalizadoController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/CatalogoPersonalizadoController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Configuration;
 
 using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServiceODS;
+using Portal.Consultoras.Web.ServiceProductoCatalogoPersonalizado;
 
 namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 {
@@ -31,27 +33,87 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
                 return RedirectToAction("Index");
             }
 
-            ProductoModel model2 = new ProductoModel();
+            //ProductoModel model2 = new ProductoModel();
+            var productoModel = new ProductoModel();
+
             if (Session["ProductosCatalogoPersonalizado"] != null)
             {
                 var listaProductoModel = new List<ProductoModel>();
                 listaProductoModel = (List<ProductoModel>)Session["ProductosCatalogoPersonalizado"] ?? new List<ProductoModel>();
+
                 if (listaProductoModel.Any())
                 {
-                    var xitem = listaProductoModel.Where(x => x.CUV.ToLower() == model.CUVFP).FirstOrDefault();
-                    if (xitem != null)
+                    //var xitem = listaProductoModel.Where(x => x.CUV.ToLower() == model.CUVFP).FirstOrDefault();
+                    //if (xitem != null)
+                    //{
+                    //    model2 = xitem;
+                    //}
+
+                    //var productoModel = new ProductoModel();
+                    productoModel = listaProductoModel.Where(x => x.CUV == model.CUVFP).FirstOrDefault();
+
+                    if (productoModel != null)
                     {
-                        model2 = xitem;
+                        if (productoModel.EsMaquillaje)
+                        {
+                            if (productoModel.Hermanos == null)
+                            {
+                                var listaHermanos = new List<BEProducto>();
+                                using (ODSServiceClient svc = new ODSServiceClient())
+                                {
+                                    listaHermanos = svc.GetListBrothersByCUV(userData.PaisID, userData.CampaniaID, model.CUVFP).ToList();
+                                }
+
+                                if (listaHermanos.Any())
+                                {
+                                    string joinCuv = string.Empty;
+                                    foreach (var item in listaHermanos)
+                                    {
+                                        joinCuv += item.CUV + ",";
+                                    }
+
+                                    joinCuv = joinCuv.Substring(0, joinCuv.Length - 1);
+
+                                    var listaAppCatalogo = new List<Producto>();
+                                    using (ProductoServiceClient svc = new ProductoServiceClient())
+                                    {
+                                        listaAppCatalogo = svc.ObtenerProductosAppCatalogoByListaCUV(userData.CodigoISO, userData.CampaniaID, joinCuv).ToList();
+                                    }
+
+                                    if (listaAppCatalogo.Any())
+                                    {
+                                        productoModel.Hermanos = new List<ProductoModel>();
+
+                                        foreach (var item in listaAppCatalogo)
+                                        {
+                                            productoModel.Hermanos.Add(new ProductoModel
+                                            {
+                                                CUV = item.Cuv,
+                                                CodigoProducto = item.CodigoSap,
+                                                Descripcion = item.NombreComercial,
+                                                DescripcionComercial = item.DescripcionComercial,
+                                                NombreBulk = item.NombreBulk,
+                                                ImagenProductoSugerido = item.Imagen,
+                                                ImagenBulk = item.ImagenBulk
+                                            });
+                                        }
+                                    }
+
+                                    Session["ProductosCatalogoPersonalizadoFilter"] = listaProductoModel;
+                                }
+                            }
+
+                        }// EsMaquillaje
                     }
                 }
             }
 
-            if (string.IsNullOrEmpty(model2.CUV))
+            if (string.IsNullOrEmpty(productoModel.CUV))
             {
                 return RedirectToAction("Index");
             }
 
-            return View(model2);
+            return View(productoModel);
         }
     }
 }
