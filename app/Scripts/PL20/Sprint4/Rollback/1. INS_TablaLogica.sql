cedb1b5c85ef0fa4bdea794aa6e3e3e36f2b3727
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-01-23 11:09:38 -0500

    integracion de scripts

---
+++ app/Scripts/PL20/Sprint4/Rollback/1. INS_TablaLogica.sql
@@ -0,0 +1,210 @@
+
+
+USE BelcorpBolivia
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+DELETE FROM TablaLogica WHERE TablaLogicaID = 94
+DELETE FROM TablaLogica WHERE TablaLogicaID = 95
+DELETE FROM TablaLogica WHERE TablaLogicaID = 96
+DELETE FROM TablaLogica WHERE TablaLogicaID = 97
+
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 94
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 95
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 96
+DELETE FROM TablaLogicaDatos WHERE TablaLogicaID = 97
+
+GO
+
---
+++ app/Scripts/PL20/Sprint4/Scripts/1. INS_TablaLogica.sql
@@ -0,0 +1,441 @@
+
+USE BelcorpBolivia
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+--TablaLogica
+INSERT INTO [dbo].[TablaLogica] VALUES (94,'Ordernamiento FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (95,'Filtro Categorias FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (96,'Filtro Marcas FAV')
+INSERT INTO [dbo].[TablaLogica] VALUES (97,'Filtro Tipo Publicación FAV')
+
+--TablaLogicaDatos
+-- Ordenamiento
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9303,94,'01','PRECIO | menor a mayor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9304,94,'02','PRECIO | mayor a menor')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9305,94,'03','RELEVANCIA')
+
+-- Categorias
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9306,95,'01','FRAGANCIAS')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9307,95,'02','MAQUILLAJE')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9308,95,'04','CUIDADO PERSONAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9309,95,'09','TRATAMIENTO CORPORAL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9310,95,'10','TRATAMIENTO FACIAL')
+
+-- Marca
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9311,96,'L','L´BEL')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9312,96,'E','ÉSIKA')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9313,96,'C','CYZONE')
+
+-- Tipo Publicacion
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9314,97,'SC','SOLO CATALOGO')
+INSERT INTO [dbo].[TablaLogicaDatos] VALUES (9315,97,'SN','SOLO GUIA DE NEGOCIO')
+
+GO
