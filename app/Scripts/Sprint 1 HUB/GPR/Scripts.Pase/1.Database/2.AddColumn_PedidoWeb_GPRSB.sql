7ae9ccebb393b921fbc578170733c00458d4aedb
luisruidias <luis.ruidias@hundred.com.pe>
2017-01-26 16:23:28 -0500

    Scripts de Pase

--- app/Scripts/Sprint 1 HUB/GPR/Scripts.Pase/1.Database/2.AddColumn_PedidoWeb_GPRSB.sql
+++ app/Scripts/Sprint 1 HUB/GPR/Scripts.Pase/1.Database/2.AddColumn_PedidoWeb_GPRSB.sql
@@ -90,4 +90,31 @@ IF (SELECT COUNT(*) FROM dbo.sysobjects inner join dbo.syscolumns ON SYSOBJECTS.
 
 GO
 
+-----------------------------------------------------------------------------------------
+USE BelcorpColombia
+go
+
+IF (SELECT COUNT(*) FROM dbo.sysobjects inner join dbo.syscolumns ON SYSOBJECTS.ID = SYSCOLUMNS.ID 
+	WHERE sysobjects.id = object_id('dbo.PedidoWeb') and SYSCOLUMNS.NAME = N'GPRSB') = 0
+	ALTER TABLE dbo.PedidoWeb ADD GPRSB tinyint not null CONSTRAINT DF_PedidoWeb_GPRSB DEFAULT (0)
+GO
+
+USE BelcorpMexico
+go
+
+IF (SELECT COUNT(*) FROM dbo.sysobjects inner join dbo.syscolumns ON SYSOBJECTS.ID = SYSCOLUMNS.ID 
+	WHERE sysobjects.id = object_id('dbo.PedidoWeb') and SYSCOLUMNS.NAME = N'GPRSB') = 0
+	ALTER TABLE dbo.PedidoWeb ADD GPRSB tinyint not null CONSTRAINT DF_PedidoWeb_GPRSB DEFAULT (0)
+
+
+GO
+
+USE BelcorpPeru
+go
+
+IF (SELECT COUNT(*) FROM dbo.sysobjects inner join dbo.syscolumns ON SYSOBJECTS.ID = SYSCOLUMNS.ID 
+	WHERE sysobjects.id = object_id('dbo.PedidoWeb') and SYSCOLUMNS.NAME = N'GPRSB') = 0
+	ALTER TABLE dbo.PedidoWeb ADD GPRSB tinyint not null CONSTRAINT DF_PedidoWeb_GPRSB DEFAULT (0)
+
+GO
 
