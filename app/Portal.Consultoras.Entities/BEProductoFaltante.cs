ffe8492f65a662c8699acbf24c808f8f11d4e64a
Kike <lead123456@hotmail.com>
2017-02-06 14:14:09 -0500

    EPD-944: Se agregron los campos nuevos del requerimiento a la entidad de servicio BEProductoFaltante.

--- app/Portal.Consultoras.Entities/BEProductoFaltante.cs
+++ app/Portal.Consultoras.Entities/BEProductoFaltante.cs
@@ -53,6 +53,18 @@ namespace Portal.Consultoras.Entities
         [ViewProperty] // 1957
         public string rowID { set; get; }
 
+        [DataMember]
+        [ViewProperty]
+        public string Categoria { set; get; }
+
+        [DataMember]
+        [ViewProperty]
+        public string Catalogo { set; get; }
+
+        [DataMember]
+        [ViewProperty]
+        public int NumeroPagina { set; get; }
+
         public BEProductoFaltante(IDataRecord datarec)
         {
             if (DataRecord.HasColumn(datarec, "RowNumber")) //R1957
@@ -61,33 +73,20 @@ namespace Portal.Consultoras.Entities
                 CampaniaID = Convert.ToInt32(datarec["CampaniaID"]);
             if (DataRecord.HasColumn(datarec, "CUV"))
                 CUV = Convert.ToString(datarec["CUV"]);
-            if (DataRecord.HasColumn(datarec, "ZonaID"))    
-                ZonaID = Convert.ToInt32(datarec["ZonaID"]);
-            if (DataRecord.HasColumn(datarec, "Zona"))    
-                Zona = Convert.ToString(datarec["Zona"]);
-            if (DataRecord.HasColumn(datarec, "Descripcion"))    
-                Descripcion = Convert.ToString(datarec["Descripcion"]);
-            if (DataRecord.HasColumn(datarec, "List_TotalNumeroPagina"))    
-                TotalPages = Convert.ToInt32(datarec["List_TotalNumeroPagina"]);
-            if (DataRecord.HasColumn(datarec, "List_TotalRegistros"))    
-                RowsCount = Convert.ToInt32(datarec["List_TotalRegistros"]);
-            if (DataRecord.HasColumn(datarec, "Estado"))
-                Estado = Convert.ToInt32(datarec["Estado"]);
-            if (DataRecord.HasColumn(datarec, "fecha"))
-            {
-                if (!Convert.IsDBNull(datarec["fecha"])) // 1957
-                    Fecha = datarec["fecha"].ToString();
-                else
-                    Fecha = "";
-            }
-            else
-            {
-                Fecha = "";
-            }
-            if (DataRecord.HasColumn(datarec, "Codigo"))
-                Codigo = Convert.ToString(datarec["Codigo"]);
-            if (DataRecord.HasColumn(datarec, "FaltanteUltimoMinuto"))
-                FaltanteUltimoMinuto = Convert.ToBoolean(datarec["FaltanteUltimoMinuto"]);
+            if (DataRecord.HasColumn(datarec, "ZonaID")) ZonaID = Convert.ToInt32(datarec["ZonaID"]);
+            if (DataRecord.HasColumn(datarec, "Zona")) Zona = Convert.ToString(datarec["Zona"]);
+            if (DataRecord.HasColumn(datarec, "Descripcion")) Descripcion = Convert.ToString(datarec["Descripcion"]);
+            if (DataRecord.HasColumn(datarec, "List_TotalNumeroPagina")) TotalPages = Convert.ToInt32(datarec["List_TotalNumeroPagina"]);
+            if (DataRecord.HasColumn(datarec, "List_TotalRegistros")) RowsCount = Convert.ToInt32(datarec["List_TotalRegistros"]);
+            if (DataRecord.HasColumn(datarec, "Estado")) Estado = Convert.ToInt32(datarec["Estado"]);
+            if (DataRecord.HasColumn(datarec, "fecha")) Fecha = Convert.IsDBNull(datarec["fecha"]) ? "" : datarec["fecha"].ToString();
+            else Fecha = "";
+            if (DataRecord.HasColumn(datarec, "Codigo")) Codigo = Convert.ToString(datarec["Codigo"]);
+            if (DataRecord.HasColumn(datarec, "FaltanteUltimoMinuto")) FaltanteUltimoMinuto = Convert.ToBoolean(datarec["FaltanteUltimoMinuto"]);
+
+            if (DataRecord.HasColumn(datarec, "Categoria")) Categoria = Convert.IsDBNull(datarec["Categoria"]) ? "" : datarec["Categoria"].ToString();
+            if (DataRecord.HasColumn(datarec, "Catalogo")) Catalogo = Convert.IsDBNull(datarec["Catalogo"]) ? "" : datarec["Catalogo"].ToString();
+            if (DataRecord.HasColumn(datarec, "NumeroPagina")) NumeroPagina = Convert.IsDBNull(datarec["NumeroPagina"]) ? 0 : Convert.ToInt32(datarec["NumeroPagina"]);
         }
 
         public BEProductoFaltante()
