4de9b07901c64acef2e97b5c071ecbe6c7fe9f5d
leonardogonzalo <leonarddgl@gmail.com>
2017-02-23 14:13:14 -0500

    EDP1774 - MailDesigner nuevo

--- app/Portal.Consultoras.Web/WebPages/MailConfirmation.aspx.designer.cs
+++ app/Portal.Consultoras.Web/WebPages/MailConfirmation.aspx.designer.cs
@@ -13,13 +13,22 @@ namespace Portal.Consultoras.Web.WebPages {
     public partial class MailConfirmation {
         
         /// <summary>
-        /// Control form1.
+        /// Control frmractivarcorreo.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm frmractivarcorreo;
+        
+        /// <summary>
+        /// Control txtmarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtmarca;
         
         /// <summary>
         /// Control lblConfirmacion.
@@ -31,13 +40,12 @@ namespace Portal.Consultoras.Web.WebPages {
         protected global::System.Web.UI.WebControls.Label lblConfirmacion;
         
         /// <summary>
-        /// Control lnkClienteCC.
+        /// Control linkregresarasomosbelcorp.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink lnkClienteCC;
-
+        protected global::System.Web.UI.WebControls.HyperLink linkregresarasomosbelcorp;
     }
 }
