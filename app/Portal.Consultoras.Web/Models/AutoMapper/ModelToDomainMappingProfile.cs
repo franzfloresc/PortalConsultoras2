2c98b06242db6b69dfd9f57f5d363328482b122a
alan.aurora <alan.aurora@ZOLUXLP053>
2017-03-01 15:29:30 -0500

    PL20-1310 : Comprar desde Página de Oferta

--- app/Portal.Consultoras.Web/Models/AutoMapper/ModelToDomainMappingProfile.cs
+++ app/Portal.Consultoras.Web/Models/AutoMapper/ModelToDomainMappingProfile.cs
@@ -4,6 +4,7 @@ using Portal.Consultoras.Web.ServiceSAC;
 using Portal.Consultoras.Web.ServicePedidoRechazado;
 using Portal.Consultoras.Web.ServiceUsuario;
 using System.Collections.Generic;
+using Portal.Consultoras.Web.ServiceProductoCatalogoPersonalizado;
 
 namespace Portal.Consultoras.Web.Models.AutoMapper
 {
@@ -21,6 +22,11 @@ namespace Portal.Consultoras.Web.Models.AutoMapper
                 .ForMember(t => t.Nombre, f => f.MapFrom(c => c.Nombre));
 
             Mapper.CreateMap<MatrizComercialModel, BEMatrizComercial>();
+
+            Mapper.CreateMap<ProductoModel, Producto>()
+                .ForMember(t => t.IdMarca, f => f.MapFrom(c => c.MarcaID))
+                .ForMember(t => t.Cuv, f => f.MapFrom(c => c.CUV))
+                .ForMember(t => t.NombreMarca, f => f.MapFrom(c => c.DescripcionMarca));
         }
     }
 }
