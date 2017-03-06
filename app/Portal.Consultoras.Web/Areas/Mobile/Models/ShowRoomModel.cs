23823ddce28297c11bb0eeaf6ce28cd022a83d97
magnosanchez <antonio.sql@gmail.com>
2017-03-06 17:28:48 -0500

    PL 20 SPRINT 7 SHOWROOM VENTAS

--- app/Portal.Consultoras.Web/Areas/Mobile/Models/ShowRoomModel.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Models/ShowRoomModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Portal.Consultoras.Web.Areas.Mobile.Models
 {
+    [Serializable()]
     public class ShowRoomModel
     {
         public string EstadoActivo { get; set; }
