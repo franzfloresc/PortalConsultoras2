68e778fa4dc3c5b442045b5c81c981f977331f6c
Carlos Jave <jave.carlos@gmail.com>
2017-03-06 19:55:41 -0500

    correcciones y ajustes

--- app/Portal.Consultoras.Web/Controllers/BaseShowRoomController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseShowRoomController.cs
@@ -348,7 +348,7 @@ namespace Portal.Consultoras.Web.Controllers
 
                 if (esIntriga)
                 {
-                    if (!(fechaHoy >= userData.FechaInicioCampania.AddDays(-showRoomEvento.DiasAntes).Date
+                    if ((fechaHoy >= userData.FechaInicioCampania.AddDays(-showRoomEvento.DiasAntes).Date
                     && fechaHoy <= userData.FechaInicioCampania.AddDays(showRoomEvento.DiasDespues).Date))
                     {
                         TimeSpan ts = userData.FechaInicioCampania.AddDays(-showRoomEvento.DiasAntes) - fechaHoy;
