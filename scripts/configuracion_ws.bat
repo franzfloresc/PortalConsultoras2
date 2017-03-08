316e4e05bcf753c09eda8f1a4a36cbe85f7ed378
fiorellagoyzueta <fiorella.goyzueta@gmail.com>
2017-03-08 18:08:00 -0500

    Update configuracion_ws.bat

--- scripts/configuracion_ws.bat
+++ scripts/configuracion_ws.bat
@@ -1,1 +1,1 @@
-aws s3 cp s3://belcorp-codedeploy/PortalConsultorasWCF2/PPR/Config/Web.config C:\inetpub\wwwroot\SB2WCF 
+aws s3 cp s3://belcorp-codedeploy/PortalConsultorasWCF2/PRD/Config/Web.config C:\inetpub\wwwroot\SB2WCF 
