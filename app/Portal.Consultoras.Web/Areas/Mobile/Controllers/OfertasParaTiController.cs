621188b713065c36fc74142496b9ba3e372b40dc
Nohely <nohely.246@gmail.com>
2017-03-16 18:48:47 -0500

    HOMOLOGAR OPT SIMPLE

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/OfertasParaTiController.cs
@@ -0,0 +1,22 @@
+﻿using Portal.Consultoras.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class OfertasParaTiController : BaseController
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+    }
+}
