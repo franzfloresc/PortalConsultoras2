e47804e6cfe4f4c50b982c3a59711b9765476293
alan.aurora <alan.aurora@ZOLUXLP053>
2017-03-17 12:09:36 -0500

    Homologar Tonos

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/OfertasParaTi/OfertasParaTi.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/OfertasParaTi/OfertasParaTi.js
@@ -10,5 +10,14 @@ var conPopup = conPopup || "";
 
 $(document).ready(function () {
 
+    $(".texto_sin_tono").on("click", function () {
+        $('.content_tonos_select').show(); //muestro mediante id  
+        $(".texto_sin_tono").parent().addClass("tono_por_elegir");
+    });
+    $(".content_tono_elegido").on("click", function () {
+        $('.content_tonos_select').hide();
+        $(".texto_sin_tono").parent().removeClass("tono_por_elegir");
+
+    });
 
 });
