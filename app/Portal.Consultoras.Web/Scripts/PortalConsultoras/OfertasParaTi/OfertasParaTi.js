b310869a4e2288b01cf91391b95018e28fa4d022
alan.aurora <alan.aurora@ZOLUXLP053>
2017-03-21 11:17:59 -0500

    Homologar Tonos

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/OfertasParaTi/OfertasParaTi.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/OfertasParaTi/OfertasParaTi.js
@@ -10,36 +10,47 @@ var conPopup = conPopup || "";
 
 $(document).ready(function () {
 
-    $(".texto_sin_tono").on("click", function () {
-        $('.content_tonos_select').show(); //muestro mediante id  
-        $(".texto_sin_tono").parent().addClass("tono_por_elegir");
-    });
-    $(".content_tonos_select .content_tono_elegido").on("click", function () {
-        $('.content_tonos_select').hide();
-        $(".texto_sin_tono").parent().removeClass("tono_por_elegir");
-
-    });
+    //$(".texto_sin_tono").on("click", function () {
+    //    $('.content_tonos_select').show(); //muestro mediante id  
+    //    $(".texto_sin_tono").parent().addClass("tono_por_elegir");
+    //});
+    //$(".content_tonos_select .content_tono_elegido").on("click", function () {
+    //    $('.content_tonos_select').hide();
+    //    $(".texto_sin_tono").parent().removeClass("tono_por_elegir");
+    //});
 
     $(document).on('click', '[data-tono-change]', function (e) {
         var accion = $(this).attr("data-tono-change");
         if (accion == 1) {
-            $('.content_tonos_select').show();
+            $(this).parents("[data-tono]").find('.content_tonos_select').attr("data-visible", "1");
+            $(this).parents("[data-tono]").find('.content_tonos_select').show();
             $(this).parent().addClass("tono_por_elegir");
             return true;
         }
+        
+        var hideSelect = $(this).parents("[data-tono]").find('.content_tonos_select').attr("data-visible");
+        if (hideSelect == "1") {
+            $(this).parents("[data-tono]").find('.content_tonos_select').hide();
+            $(this).parents("[data-tono]").find('.content_tonos_select').attr("data-visible", "0");
+            $(this).parents("[data-tono]").find("[data-tono-change='1']").parent().removeClass("tono_por_elegir");
+        }
 
-        var cuv = $(this).attr("data-tono-cuv");
-        var objSet = $("[data-tono-change='1']");
+        var cuv = $.trim($(this).attr("data-tono-cuv"));
+        var prod = $(this).parents("[data-tono]");
+        var objSet = prod.find("[data-tono-change='1']");
         objSet.find("img").attr("src", $(this).find("img").attr("src"));
         objSet.find(".tono_seleccionado").show();
         objSet.find(".texto_tono_seleccionado").html($(this).attr("data-tono-nombre"));
 
         objSet.parent().addClass("tono_escogido");
-        objSet.parents("[data-set-componente]").find("[data-tono-nombrecomercial]").html($(this).attr("data-tono-descripcion"));
+        prod.find("[data-tono-select-nombrecomercial]").html($(this).attr("data-tono-descripcion"));
+        prod.attr("data-tono-select", cuv);
 
-        var objCompartir = objSet.parents("[data-item]").find("[data-compartir-campos]");
+        var objCompartir = prod.find("[data-item]").find("[data-compartir-campos]");
         objCompartir.find(".CUV").val(cuv);
         objCompartir.find(".Nombre").val($(this).attr("data-tono-descripcion"));
+
+        prod.parents("[data-item]").find("#tbnAgregarProducto").removeClass("btn_desactivado_general");
 
         //objSet.parent().attr("class", "");
         //objSet.attr("class", "tono_escogido");
