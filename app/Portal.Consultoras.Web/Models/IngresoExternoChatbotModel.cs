603e074ad84379bb2f48735a5a921636f3646fd2
Eder Lazaro <elazaro@belcorp.biz>
2017-03-22 12:08:13 -0500

    Ajustes para ingreso a SB de Chatbot mediante token

---
+++ app/Portal.Consultoras.Web/Models/IngresoExternoChatbotModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Portal.Consultoras.Web.Models
+{
+    public class IngresoExternoChatbotModel
+    {
+        public string Pais { get; set; }
+
+        public string CodigoConsultora { get; set; }
+
+        public string Pagina { get; set; }
+
+        public string Version { get; set; }
+    }
+}
