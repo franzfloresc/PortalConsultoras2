148b275c136d7d776edf4ef9e663b2ae1d408e82
alan.aurora <alan.aurora@ZOLUXLP053>
2017-04-10 14:58:03 -0500

    Mail Confirmation Email

--- app/Portal.Consultoras.Web/WebPages/MailConfirmation.aspx.cs
+++ app/Portal.Consultoras.Web/WebPages/MailConfirmation.aspx.cs
@@ -55,7 +55,7 @@ namespace Portal.Consultoras.Web.WebPages
                         var opcional = query.Length > 4 ? query[4].Trim() : "";
                         if (opcional != "")
                         {
-                            var opcionalLista = opcional.Split('=');
+                            var opcionalLista = opcional.Split(',');
                             if (opcionalLista.Length > 1)
                             {
                                 if (opcionalLista[0].ToLower() == "urlreturn")
