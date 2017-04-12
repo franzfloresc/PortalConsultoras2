c9b1ec19bb79565eef50a399f786868d911a40bf
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-04-12 18:15:53 -0500

    Scripts PL20-1398

---
+++ app/Scripts/PL20/PL20-1339_Sprint9/Database/5. StockPrecioOfertaShowRoomType.sql
@@ -0,0 +1,312 @@
+
+USE BelcorpBolivia
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+IF TYPE_ID(N'ShowRoom.StockPrecioOfertaShowRoomType') IS NOT NULL
+BEGIN
+	DROP TYPE ShowRoom.StockPrecioOfertaShowRoomType
+END
+GO
+
+CREATE TYPE [ShowRoom].[StockPrecioOfertaShowRoomType] AS TABLE(
+	[TipoOfertaSisID] [int] NULL,
+	[CampaniaID] [int] NULL,
+	[CUV] [varchar](20) NULL,
+	[Stock] [int] NULL,
+	[PrecioOferta] [decimal](18, 2) NULL,
+	[UnidadesPermitidas] [int] NULL,
+	[Descripcion] [varchar](250) NULL,
+	[CodigoCategoria] [varchar](100) NULL,
+	[TipNegocio] [varchar](500) NULL,
+	[PrecioOferta2] [decimal](18, 2) NULL
+)
+GO
+
---
+++ app/Scripts/PL20/PL20-1339_Sprint9/Database/6. UpdStockOfertaShowRoomMasivo.sql
@@ -0,0 +1,377 @@
+
+USE BelcorpBolivia
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+/*end*/
+
+USE BelcorpVenezuela
+GO
+
+ALTER procedure [ShowRoom].[UpdStockOfertaShowRoomMasivo]
+@StockPrecioOfertaShowRoom ShowRoom.StockPrecioOfertaShowRoomType readonly
+as
+begin
+
+UPDATE ShowRoom.OfertaShowRoom
+SET Stock = t.Stock,
+	StockInicial = t.Stock,
+	PrecioOferta = t.PrecioOferta,
+	UnidadesPermitidas = t.UnidadesPermitidas,
+	Descripcion = t.Descripcion,
+	CodigoCategoria = t.CodigoCategoria,
+	TipNegocio = t.TipNegocio,
+	PrecioOferta2 = t.PrecioOferta2
+FROM ShowRoom.OfertaShowRoom o
+INNER JOIN ods.Campania c on 
+	c.CampaniaID = o.CampaniaID
+JOIN @StockPrecioOfertaShowRoom t ON 
+	c.Codigo = t.CampaniaID
+	AND o.CUV = t.CUV
+	AND o.TipoOfertaSisID = t.TipoOfertaSisID
+
+end
+GO
+
