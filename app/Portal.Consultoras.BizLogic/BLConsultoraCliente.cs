3a91e6f3dff25b54952e3bd56cd6f3f484231b6f
victor <victor.marino@hundred.com.pe>
2017-05-09 16:19:44 -0500

    Creacion del servicio Cliente

---
+++ app/Portal.Consultoras.BizLogic/BLConsultoraCliente.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Portal.Consultoras.Entities;
+using Portal.Consultoras.Data;
+
+namespace Portal.Consultoras.BizLogic
+{
+    public class BLConsultoraCliente
+    {
+        public bool InsertConsultoraCliente(int paisID, BEConsultoraCliente consultoraCliente)
+        {
+            var cronogramas = new List<BEConsultoraCliente>();
+            var DAConsultoraCliente = new DAConsultoraCliente(paisID);
+
+            return DAConsultoraCliente.InsertConsultoraCliente(consultoraCliente);
+        }
+
+        public bool DeleteConsultoraCliente(int paisID, long ConsultoraID, long ClienteID)
+        {
+            var cronogramas = new List<BEConsultoraCliente>();
+            var DAConsultoraCliente = new DAConsultoraCliente(paisID);
+
+            return DAConsultoraCliente.DeleteConsultoraCliente(ConsultoraID, ClienteID);
+        }
+
+        public List<BEConsultoraCliente> GetConsultoraCliente(int paisID, long ConsultoraID)
+        {
+            var lst = new List<BEConsultoraCliente>();
+            var DAConsultoraCliente = new DAConsultoraCliente(paisID);
+
+            using (IDataReader reader = DAConsultoraCliente.GetConsultoraCliente(ConsultoraID))
+            {
+                while (reader.Read())
+                {
+                    var consultoraCliente = new BEConsultoraCliente(reader);
+                    lst.Add(consultoraCliente);
+                }
+            }
+
+            return lst;
+        }
+    }
+}
---
+++ app/Portal.Consultoras.Entities/BEConsultoraCliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Runtime.Serialization;
+using Portal.Consultoras.Common;
+
+namespace Portal.Consultoras.Entities
+{
+    [DataContract]
+    public class BEConsultoraCliente
+    {
+        [DataMember]
+        public long ConsultoraClienteID { get; set; }
+
+        [DataMember]
+        public long ConsultoraID { get; set; }
+
+        [DataMember]
+        public long ClienteID { get; set; }
+
+        public BEConsultoraCliente(IDataRecord row)
+        {
+            if (DataRecord.HasColumn(row, "ConsultoraClienteID"))
+                ConsultoraClienteID = Convert.ToInt64(row["ConsultoraClienteID"]);
+
+            if (DataRecord.HasColumn(row, "ConsultoraID"))
+                ConsultoraID = Convert.ToInt64(row["ConsultoraID"]);
+
+            if (DataRecord.HasColumn(row, "ClienteID"))
+                ClienteID = Convert.ToInt64(row["ClienteID"]);
+        }
+    }
+}
