da95f5c930527e4a5751e383cc9b5f77fb27d723
jaime <jaime.zamora@avantica.net>
2017-05-10 15:24:42 -0500

    Temp Cupon (1441)

--- app/Portal.Consultoras.Web/Controllers/CuponController.cs
+++ app/Portal.Consultoras.Web/Controllers/CuponController.cs
@@ -1,4 +1,5 @@
 ﻿using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServicePedido;
 using System;
 using System.Web.Mvc;
 
@@ -14,10 +15,63 @@ namespace Portal.Consultoras.Web.Controllers
                 var success = true;
                 return Json(new { success = success });
             }
-            catch (Exception ex)
+            catch (Exception ex) { return Json(new { success = false, message = "Ocurrió un error al ejecutar la operación. " + ex.Message }); }
+        }
+
+        [HttpGet]
+        public JsonResult ObtenerDatosCupo()
+        {
+            try
             {
-                return Json(new { success = false, message = "Ocurrió un error al ejecutar la operación. " + ex.Message });
+                CuponModel cupon = ObtenerDatosCupon();
+                return Json(new { success = true, data = cupon });
             }
+            catch (Exception ex) { return Json(new { success = false, message = "Ocurrió un error al ejecutar la operación. " + ex.Message }); }
+        }
+
+        private CuponModel ObtenerDatosCupon()
+        {
+            CuponModel cuponModel;
+            BECuponConsultora cuponResult = ObtenerCuponDesdeServicio();
+            
+            if (cuponResult != null)
+                cuponModel = MapearBECuponACuponModel(cuponResult);
+            else
+                throw new Exception();
+
+            return cuponModel;
+        }
+
+        private BECuponConsultora ObtenerCuponDesdeServicio()
+        {
+            using (PedidoServiceClient svClient = new PedidoServiceClient())
+            {
+                int paisId = userData.PaisID;
+                BECuponConsultora cuponBE = new BECuponConsultora();
+                cuponBE.CodigoConsultora = userData.CodigoConsultora;
+                cuponBE.CampaniaId = userData.CampaniaID;
+
+                var cuponResult = svClient.GetCuponConsultoraByCodigoConsultoraCampaniaId(paisId, cuponBE);
+                return cuponResult;
+            }
+        }
+
+        private CuponModel MapearBECuponACuponModel(BECuponConsultora cuponBE)
+        {
+            return new CuponModel() {
+                CuponConsultoraId = cuponBE.CuponConsultoraId,
+                CodigoConsultora = cuponBE.CodigoConsultora,
+                CampaniaId = cuponBE.CampaniaId,
+                CuponId = cuponBE.CuponId,
+                ValorAsociado = cuponBE.ValorAsociado,
+                EstadoCupon = cuponBE.EstadoCupon,
+                EnvioCorreo = cuponBE.EnvioCorreo,
+                FechaCreacion = cuponBE.FechaCreacion,
+                FechaModificacion = cuponBE.FechaModificacion,
+                UsuarioCreacion = cuponBE.UsuarioCreacion,
+                UsuarioModificacion = cuponBE.UsuarioModificacion,
+                TipoCupon = cuponBE.TipoCupon
+            };
         }
     }
 }
--- app/Portal.Consultoras.Web/Models/CuponModel.cs
+++ app/Portal.Consultoras.Web/Models/CuponModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Portal.Consultoras.Web.Models
+﻿using System;
+
+namespace Portal.Consultoras.Web.Models
 {
     public class CuponModel
     {
-        public int Estado { get; set; }
+        public int CuponConsultoraId { get; set; }
+        public string CodigoConsultora { get; set; }
+        public int CampaniaId { get; set; }
+        public int CuponId { get; set; }
+        public decimal ValorAsociado { get; set; }
+        public int EstadoCupon { get; set; }
+        public bool EnvioCorreo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+        public string TipoCupon { get; set; }
     }
 }
