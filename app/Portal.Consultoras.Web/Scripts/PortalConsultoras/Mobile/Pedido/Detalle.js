2cdba20245e1b5a076635596cdbd0702c9f43fa7
Jose <jose.ponte@hundred.com.pe>
2017-05-11 17:08:13 -0500

    EPD-2278 Elimino comentario

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/Pedido/Detalle.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/Pedido/Detalle.js
@@ -1001,7 +1001,6 @@ function RespuestaEjecutarServicioPROL(response, inicio) {
                     return true;
                 }
                 
-                //messageInfoBueno('<h3>Tu pedido fue validado con éxito</h3><p>Tus productos fueron reservados.</p>');
                 messageInfoBueno('<h3>Tu pedido fue reservado con éxito.</h3>'); //EPD-2278 POPUP
                 if (estaRechazado == "2") {
                     cerrarMensajeEstadoPedido();
@@ -1359,7 +1358,6 @@ function MostrarMensajeProl(data) {
                 return true;
             }
 
-            //messageInfoBueno('<h3>Tu pedido fue validado con éxito</h3><p>Tus productos fueron reservados.</p>');
             messageInfoBueno('<h3>Tu pedido fue reservado con éxito.</h3>'); //EPD-2278
             setTimeout(function () {
                 location.href = urlPedidoValidado;
