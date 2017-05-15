00218bfcf457044d3bf30c32b2cd7e98f2dcc92b
Kike <lead123456@hotmail.com>
2017-05-15 11:51:42 -0500

    CHATBOT-110: Corrección en login de usuario a atraves de FB cuando ya tenía asociado un código de consultora

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/LoginChatbot/Facebook.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/LoginChatbot/Facebook.js
@@ -26,7 +26,7 @@ function LoginFB() {
 }
 
 function ValidarUsuarioFBAsociado(responseFB) {
-    var exists = ExistsExternalUser('Facebook', responseFB.id, function (exists) {
+    var exists = ExistsExternalUser('Facebook', responseFB.authResponse.userID, function (exists) {
         if (exists == null) MessageInfoError('Ocurrió un problema al intentar validar si tiene una consultora asociada.');
         else if (!exists) MostrarLoginNormal(responseFB);
         else ResponderBotmakerFB(responseFB.authResponse.userID, responseFB.authResponse.accessToken);
