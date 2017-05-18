97d41fb2e314db9ed74b96b614d5d0211b3017a0
luisruidias <luis.ruidias@hundred.com.pe>
2017-05-18 14:29:54 -0500

    EPD2250 - Corrección de las observaciones Analytics.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Index.js
@@ -2506,7 +2506,7 @@ function TagManagerCarruselInicio(arrayItems) {
     }
 }
 
-function TagManagerClickAgregarProducto() {
+function TagManagerClickAgregarProducto() {  
     dataLayer.push({
         'event': 'addToCart',
         'ecommerce': {
@@ -2625,6 +2625,30 @@ function TagManagerCarruselLiquidacionesInicio(arrayItems) {
         });
     }
 }
+
+function TagManagerClickAgregarProductoOfertaParaTI(item) {
+    dataLayer.push({
+        'event': 'addToCart',
+        'ecommerce': {
+            'add': {
+                'actionField': { 'list': 'Ofertas para ti – Home' },
+                'products': [
+                    {
+                        'name': item.DescripcionCUV2,
+                        'price': item.Precio2,
+                        'brand': item.DescripcionMarca,
+                        'id': item.CUV2,
+                        'category': 'NO DISPONIBLE',
+                        'variant': item.DescripcionEstrategia,
+                        'quantity': parseInt(item.Cantidad),
+                        'position': parseInt(item.posicionItem)
+                    }
+                ]
+            }
+        }
+    });
+}
+
 function TagManagerClickAgregarProductoLiquidacion(item) {
     dataLayer.push({
         'event': 'addToCart',
