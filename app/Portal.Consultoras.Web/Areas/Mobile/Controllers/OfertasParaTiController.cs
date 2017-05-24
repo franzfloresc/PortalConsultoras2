8fd817b1082641ea8bf8aea14b37dc6da73f27f8
alan.aurora <alan.aurora@ZOLUXLP053>
2017-05-24 18:04:29 -0500

    Mobile => Oferta Para Ti  => Detalle

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/OfertasParaTiController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/OfertasParaTiController.cs
@@ -14,8 +14,34 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
         public ActionResult Detalle(int id, int origen)
         {
             var modelo = EstrategiaGetDetalle(id);
+            var origenPantalla = Util.Trim(origen.ToString()).Substring(1, 1);
+            if (modelo.EstrategiaID <= 0)
+            {
+                if (origenPantalla == "1") // Home
+                {
+                    return RedirectToAction("Index", "Bienvenida", new { area = "Mobile" });
+                }
+                if (origenPantalla == "2") // pedido
+                {
+                    return RedirectToAction("Index", "Pedido", new { area = "Mobile" });
+                }
+                if (origenPantalla == "7") // RevistaDigital
+                {
+                    return RedirectToAction("Index", "RevistaDigital", new { area = "Mobile" });
+                }
+            }
+
             modelo.Origen = origen;
-            if (Util.Trim(origen.ToString()).Substring(1, 1) == "7")
+            modelo.OrigenUrl = Url.Action("Index", "Bienvenida", new { area = "Mobile" });
+            if (origenPantalla == "1") // Home
+            {
+                modelo.OrigenUrl = Url.Action("Index", "Bienvenida", new { area = "Mobile" });
+            }
+            else if (origenPantalla == "2") // pedido
+            {
+                modelo.OrigenUrl = Url.Action("Index", "Pedido", new { area = "Mobile" });
+            }
+            else if (origenPantalla == "7") // RevistaDigital
             {
                 modelo.OrigenUrl = Url.Action("Index", "RevistaDigital", new { area = IsMobile() ? "Mobile" : "" });
             }
