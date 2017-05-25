335b16b65f495d842e6df05ab8a0acd871186a7c
Eder Lazaro <elazaro@belcorp.biz>
2017-05-25 17:33:40 -0500

    Ajuste de refrencias de Newtonsoft.Json

--- app/Portal.Consultoras.Web/Global.asax.cs
+++ app/Portal.Consultoras.Web/Global.asax.cs
@@ -16,8 +16,7 @@ namespace Portal.Consultoras.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
