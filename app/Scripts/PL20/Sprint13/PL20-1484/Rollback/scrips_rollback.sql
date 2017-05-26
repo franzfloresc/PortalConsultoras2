4b3344753ed8e02c1fdd66ef356a1942d1ded883
jaime <jaime.zamora@avantica.net>
2017-05-26 14:58:26 -0500

    Script SQL para la insercion del registro "Cupon". Pendiente el backup

---
+++ app/Scripts/PL20/Sprint13/PL20-1484/Rollback/scrips_rollback.sql
@@ -0,0 +1,38 @@
+USE BelcorpBolivia
+go
+
+USE BelcorpChile
+go
+
+USE BelcorpColombia
+go
+
+USE BelcorpCostaRica
+go
+
+USE BelcorpDominicana
+go
+
+USE BelcorpEcuador
+go
+
+USE BelcorpGuatemala
+go
+
+USE BelcorpMexico
+go
+
+USE BelcorpPanama
+go
+
+USE BelcorpPeru
+go
+
+USE BelcorpPuertoRico
+go
+
+USE BelcorpSalvador
+go
+
+USE BelcorpVenezuela
+go
