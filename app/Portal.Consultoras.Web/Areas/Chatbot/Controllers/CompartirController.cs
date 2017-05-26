143c05f520e43cde2d8802793ed9dad42ac6687a
German Nina <german1311@outlook.com>
2017-05-26 15:53:13 -0500

    Refactor Clean code

--- app/Portal.Consultoras.Web/Areas/Chatbot/Controllers/CompartirController.cs
+++ app/Portal.Consultoras.Web/Areas/Chatbot/Controllers/CompartirController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Portal.Consultoras.Web.Areas.Chatbot.Models;
-using Portal.Consultoras.Web.Areas.Mobile.Controllers;
 using Portal.Consultoras.Web.Controllers;
 
 namespace Portal.Consultoras.Web.Areas.Chatbot.Controllers
