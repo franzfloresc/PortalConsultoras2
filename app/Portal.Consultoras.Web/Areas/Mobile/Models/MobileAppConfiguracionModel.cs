acef27866d66db844272f493b69267d76556c44b
German Nina <german1311@outlook.com>
2017-06-06 10:42:31 -0500

    Ocultar boton atras

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Models/MobileAppConfiguracionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Models
+{
+    public class MobileAppConfiguracionModel
+    {
+        /// <summary>
+        /// Establece si se mostrara o no el boton atras en las vistas
+        /// </summary>
+        public bool MostrarBotonAtras { get; set; }
+    }
+}
