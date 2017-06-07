86e9f32f71f92701a1c33e3a6930107066e313a3
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-06-07 11:01:41 -0500

    Fix mostrar cupon

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
@@ -116,19 +116,8 @@ function ArmarCarouselEstrategias(data) {
     data.Lista = EstructurarDataCarousel(data.Lista);
     tieneOPT = true;
     arrayOfertasParaTi = data.Lista;
-    var obj = new Object();
-    obj.CodigoEstrategia = $("#hdCodigoEstrategia").val() || "";
-    //obj.CodigoEstrategia = "101";
-    obj.Lista = data;
-    obj.Consultora = usuarioNombre.toUpperCase();
-    obj.Titulo = obj.Consultora + ", LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA";
-    obj.TituloDescripcion = tipoOrigenEstrategia == 1
-        ? "ENCUENTRA MÁS OFERTAS, MÁS BONIFICACIONES Y LANZAMIENTOS DE LAS 3 MARCAS Y AUMENTA TUS GANANCIAS"
-        : tipoOrigenEstrategia ==2  
-            ? "ENCUENTRA OFERTAS, BONIFICACIONES Y LANZAMIENTOS DE LAS 3 MARCAS"
-            : "ENCUENTRA LOS PRODUCTOS QUE TUS CLIENTES BUSCAN HASTA 65% DE DSCTO.";
-
-    SetHandlebars("#template-estrategia-header", data, '#divListaEstrategias');
+    
+    SetHandlebars("#template-estrategia-header", data, '#contenedor_template_estrategia_cabecera');
     $('#divListaEstrategias').show();
     $("#divListaEstrategias").attr("data-OrigenPedidoWeb", data.OrigenPedidoWeb);
     SetHandlebars("#estrategia-template", data, '#divListadoEstrategia');
