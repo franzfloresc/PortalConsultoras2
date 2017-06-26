2b4e7e0fe7e62fe52d3dace30e93e88922888576
Nohely <nohely.246@gmail.com>
2017-06-26 10:56:31 -0500

    avance mobile product page best seller

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/EstrategiaProductoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class EstrategiaProductoController : BaseMobileController
+    {
+        // GET: Mobile/EstrategiaProducto
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DetalleProducto()
+        {
+            return View();
+        }
+    }
+}
