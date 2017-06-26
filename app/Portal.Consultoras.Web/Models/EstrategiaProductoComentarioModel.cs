89ca9499faf659d6adfe8b9aa2d8a9dcbddb2478
jaime <jaime.zamora@avantica.net>
2017-06-26 12:00:06 -0500

    PL20-1566: Temp

---
+++ app/Portal.Consultoras.Web/Models/EstrategiaProductoComentarioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Portal.Consultoras.Web.Models
+{
+    public class EstrategiaProductoComentarioModel
+    {
+        public long ProdComentarioDetalleId { get; set; }
+        public int ProdComentarioId { get; set; }
+        public Int16 Valorizado { get; set; }
+        public bool Recomendado { get; set; }
+        public string Comentario { get; set; }
+        public string CodigoConsultora { get; set; }
+        public int CampaniaID { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaAprobacion { get; set; }
+        public int CodTipoOrigen { get; set; }
+        public Int16 Estado { get; set; }
+        public string CodigoSAP { get; set; }
+        public string CodigoGenerico { get; set; }
+    }
+}
