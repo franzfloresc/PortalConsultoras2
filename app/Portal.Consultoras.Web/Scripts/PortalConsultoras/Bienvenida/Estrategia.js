e2f89f9108f76e33829181187e225e71f097216f
MIJAEL <mijael888@gmail.com>
2017-06-27 11:03:31 -0500

    Correccion bugs...

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
@@ -125,7 +125,10 @@ function ArmarCarouselEstrategias(data) {
     //SetHandlebars("#template-estrategia-header", data, '#contenedor_template_estrategia_cabecera');
     $("#divListaEstrategias").attr("data-OrigenPedidoWeb", data.OrigenPedidoWeb);
     SetHandlebars("#estrategia-template", data, '#divListadoEstrategia');
-    //ResizeBoxContnet();
+    //try {
+    //    ResizeBoxContnet();
+    //}catch(e){ console.log(e)}
+    
 
     if (tipoOrigenEstrategia == 11) { 
         $('#cierreCarousel').hide();
@@ -606,7 +609,7 @@ function CargarProductoDestacado(objParameter, objInput, popup, limite) {
     var flagNueva = objParameter.FlagNueva;
 
     var cantidadIngresada = (limite > 0) ? limite : $(".btn_agregar_ficha_producto ").parents("[data-item]").find("input.liquidacion_rango_cantidad_pedido").val()
-                                                  || $(objInput).parents(".content_btn_agregar_detalle").find("input.rango_cantidad_pedido").val()
+                                                  || $(objInput).parents("[data-item]").find("input.rango_cantidad_pedido").val()
                                                   || $(objInput).parents("[data-item]").find("[data-input='cantidad']").val();
     origenPedidoWebEstrategia =
         $(objInput).parents("[data-item]").find("input.OrigenPedidoWeb").val()
@@ -1010,3 +1013,4 @@ function ProcesarActualizacionMostrarContenedorCupon() {
         }
     }
 }
+
