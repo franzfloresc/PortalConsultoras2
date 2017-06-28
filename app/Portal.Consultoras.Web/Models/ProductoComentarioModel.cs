d6bfe94343842ac2be7ada8f21cb19030f392f23
Victor Vilela <vilela_victor@hotmail.com>
2017-06-28 11:10:43 -0500

    PL20-1567 : Add Busqueda ProductoComentario

--- app/Portal.Consultoras.Web/Models/ProductoComentarioModel.cs
+++ app/Portal.Consultoras.Web/Models/ProductoComentarioModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Portal.Consultoras.Web.Models
 {
     public class ProductoComentarioModel
     {
+        [Required]
         public int PaisID { get; set; }
 
         public IEnumerable<PaisModel> Paises { set; get; }
