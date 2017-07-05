033a8b7d0145868b5608e3b9fbba9eef08433805
Victor Vilela <vilela_victor@hotmail.com>
2017-07-05 14:05:12 -0500

    FRZ-6: Add Unit tests project Portal.Contultoras
    Add Portal.Consultoras.Common.UnitTest project.
    add Portal.Consultoras.Web.UnitTest.
    Add Moq library reference to projects.

---
+++ app/Portal.Consultoras.Common.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Portal.Consultoras.Common.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/LoginControllerUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Portal.Consultoras.Web.UnitTest.Controllers
+{
+    [TestClass]
+    public class LoginControllerUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
