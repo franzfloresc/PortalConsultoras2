8a72060fb437ce043678c4339076be57c70253e2
Kike <lead123456@hotmail.com>
2017-07-06 11:01:25 -0500

    FRZ-7: Avances de UnitTest de PedidoController.Delete

---
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/PedidoControllerUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Portal.Consultoras.Web.Controllers;
+using Portal.Consultoras.Common;
+
+namespace Portal.Consultoras.Web.UnitTest.Controllers
+{
+    [TestClass]
+    public class PedidoControllerUnitTest
+    {
+        [TestMethod]
+        public void DeleteCuvNoExistente()
+        {
+            var mensaje = string.Empty;
+            try
+            {
+                var controller = new PedidoController();
+                var response = controller.Delete(201711, 5097367, 2, 0, "NoExiste", 1, null, "", false);
+            }
+            catch (Exception) { }
+            Assert.AreEqual(Constantes.MensajesError.DeletePedido_CuvNoExiste, mensaje);
+        }
+    }
+}
