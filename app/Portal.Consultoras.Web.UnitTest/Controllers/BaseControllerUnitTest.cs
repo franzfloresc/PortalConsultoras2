7c82b3f0a5af137b66cc0561d125ea29fae62e8d
AlessandroMartinCh <alessandro.chipulina@zoluxiones.com>
2017-07-06 16:41:41 -0500

    // FREEZE

---
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/BaseControllerUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Portal.Consultoras.Web.Controllers;
+using System.Web;
+using MvcContrib.TestHelper;
+
+namespace Portal.Consultoras.Web.UnitTest.Controllers
+{
+    [TestClass]
+    public class BaseControllerUnitTest
+    {
+        [TestMethod]
+        public void BaseController_OnActionExecuting_UnitTest()
+        {
+            // Arrange
+            BaseController oB = new BaseController();
+            TestControllerBuilder oTB = new TestControllerBuilder();
+
+            // Act
+            oTB.InitializeController(oB);
+
+            // Assert
+            Assert.AreEqual(oB.OnActionExecutingTest(), 7);
+            Assert.AreEqual(oB.OnActionExecutingTest(), 0);
+        }
+    }
+}
