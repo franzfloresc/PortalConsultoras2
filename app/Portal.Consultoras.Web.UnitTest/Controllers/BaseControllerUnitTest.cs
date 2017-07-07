e1dafe539479acf7a7d31a42e708547746a691ac
AlessandroMartinCh <alessandro.chipulina@zoluxiones.com>
2017-07-07 10:40:59 -0500

    vvv

--- app/Portal.Consultoras.Web.UnitTest/Controllers/BaseControllerUnitTest.cs
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/BaseControllerUnitTest.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Portal.Consultoras.Web.Controllers;
 using System.Web;
 using MvcContrib.TestHelper;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Portal.Consultoras.Web.UnitTest.Controllers
 {
@@ -13,15 +15,43 @@ namespace Portal.Consultoras.Web.UnitTest.Controllers
         public void BaseController_OnActionExecuting_UnitTest()
         {
             // Arrange
+            int Iteration = 10;
+            // BaseController[] oBB = new BaseController[Iteration];
+            // TestControllerBuilder[] oTT = new TestControllerBuilder[Iteration];
+
+            // Single
+            /*
             BaseController oB = new BaseController();
-            TestControllerBuilder oTB = new TestControllerBuilder();
-
-            // Act
-            oTB.InitializeController(oB);
-
-            // Assert
+            TestControllerBuilder oT = new TestControllerBuilder();
+            oT.InitializeController(oB);
             Assert.AreEqual(oB.OnActionExecutingTest(), 7);
             Assert.AreEqual(oB.OnActionExecutingTest(), 0);
+            */
+            int totalSize = 0;
+
+            Parallel.For(0, Iteration - 1,
+                   index => {
+                       // Arrange
+                       try
+                       {
+                           BaseController oBB;
+                           TestControllerBuilder oTT;
+                           oBB = new BaseController();
+                           oTT = new TestControllerBuilder();
+                           oTT.InitializeController(oBB);
+
+                           // Act // Assert
+                           Interlocked.Add(ref totalSize, oBB.OnActionExecutingTest());
+                           Interlocked.Add(ref totalSize, oBB.OnActionExecutingTest());
+                       }
+                       catch(Exception)
+                       {
+                       }
+                       // Interlocked.Add(ref totalSize, size);
+                   });
+
+            Assert.AreEqual(7 * Iteration, totalSize);
+            // Assert.AreEqual(oBB.OnActionExecutingTest(), 0);
         }
     }
 }
