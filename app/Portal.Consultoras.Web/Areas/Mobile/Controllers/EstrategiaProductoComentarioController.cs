be4054e21e6e9475603d4cb342c6af0c58df4716
miguel-otero-calderon@hotmail.com <miguel-otero-calderon@hotmail.com>
2017-07-07 16:52:43 -0500

    PL20-1604 Mostrar Comentarios del Producto - Mobile 10%

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/EstrategiaProductoComentarioController.cs
@@ -0,0 +1,33 @@
+﻿using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServicePedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class EstrategiaProductoComentarioController : Controller
+    {
+        Web.Controllers.EstrategiaProductoComentarioController controllerDesktop = 
+            new Web.Controllers.EstrategiaProductoComentarioController();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult RegistrarComentario(EstrategiaProductoComentarioModel model)
+        {
+            return controllerDesktop.RegistrarComentario(model);
+        }
+
+        [HttpGet]
+        public JsonResult ListarComentarios(string codigoSAP, int cantidadMostrar, int orden)
+        {
+            return controllerDesktop.ListarComentarios(codigoSAP, cantidadMostrar, orden);
+        }
+    }
+}
