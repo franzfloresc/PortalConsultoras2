09547608a4ba88f617dc2dcc7cfe32a1190bb5ef
Victor Vilela <vilela_victor@hotmail.com>
2017-07-12 17:24:05 -0500

    FRZ-15: Add SessionManager to Portal.Consultoras.Web

---
+++ app/Portal.Consultoras.Web/SessionManager/SessionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.SessionManager
+{
+    public class SessionManager : ISessionManager
+    {
+        private static ISessionManager _instance;
+
+        public static ISessionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SessionManager();
+
+                return _instance;
+            }
+        }
+
+
+    }
+}
