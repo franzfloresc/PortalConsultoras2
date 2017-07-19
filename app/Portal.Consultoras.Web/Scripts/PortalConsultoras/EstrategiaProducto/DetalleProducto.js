8a51cfe20c351b0f2b3bc18d9e13cf8ffbe59ab4
miguel-otero-calderon@hotmail.com <miguel-otero-calderon@hotmail.com>
2017-07-19 12:50:30 -0500

    PL20-1669 -100%

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaProducto/DetalleProducto.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaProducto/DetalleProducto.js
@@ -35,7 +35,7 @@
             if (tipoOrigenPantalla === 1) { inicializarDivMasVendidos('desktop');}
             if (tipoOrigenPantalla === 2) { inicializarDivMasVendidosCarruselSinFlechas('mobile'); }            
             _validarDivTituloMasVendidos();
-            if (tipoOrigenPantalla === 2) { $('.header_slider').hide(); OcultarBannerTop(); }
+            if (tipoOrigenPantalla === 2) { $('.header_slider').hide(); LayoutHeader(); }
         });
     }
 
