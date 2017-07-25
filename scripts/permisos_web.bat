ddafea65e61dd3bc182594815c31f8e26e2c0d22
German Nina <german1311@outlook.com>
2017-07-25 11:12:57 -0500

    Restore scripts

--- scripts/permisos_web.bat
+++ scripts/permisos_web.bat
@@ -5,3 +5,4 @@ icacls "C:\inetpub\wwwroot\SBAPPQA\Content\FileConsultoras" /grant .\IIS_IUSRS:(
 icacls "C:\inetpub\wwwroot\SBAPPQA\Content\Ofertas" /grant .\IIS_IUSRS:(OI)(CI)F /t
 icacls "C:\inetpub\wwwroot\SBAPPQA\Content\Temporales" /grant .\IIS_IUSRS:(OI)(CI)F /t
 icacls "C:\inetpub\wwwroot\SBAPPQA\Content\FileShowRoomCargaConsultora" /grant .\IIS_IUSRS:(OI)(CI)F
+
