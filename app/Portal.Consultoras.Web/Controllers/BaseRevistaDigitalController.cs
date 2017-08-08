8d51df207eb312d4431f7448e07fa4be5b514709
alan.aurora <alan.aurora@ZOLUXLP053>
2017-08-08 18:27:12 -0500

    Revista Digital => Mejoras

--- app/Portal.Consultoras.Web/Controllers/BaseRevistaDigitalController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseRevistaDigitalController.cs
@@ -69,7 +69,8 @@ namespace Portal.Consultoras.Web.Controllers
             model.Success = true;
             ViewBag.TieneProductosPerdio = TieneProductosPerdio(model.CampaniaID);
             ViewBag.NombreConsultora = userData.Sobrenombre;
-            var campaniaX2 = userData.RevistaDigital.SuscripcionAnterior1Model.CampaniaID > 0 ? userData.RevistaDigital.SuscripcionAnterior1Model.CampaniaID : userData.CampaniaID;
+            var campaniaX2 = userData.RevistaDigital.SuscripcionAnterior1Model.CampaniaID > 0 && userData.RevistaDigital.SuscripcionAnterior1Model.EstadoRegistro == Constantes.EstadoRDSuscripcion.Activo
+                ? userData.RevistaDigital.SuscripcionAnterior1Model.CampaniaID : userData.CampaniaID;
             ViewBag.CampaniaMasDos = AddCampaniaAndNumero(campaniaX2, 2) % 100;
             ViewBag.EstadoSuscripcion = userData.RevistaDigital.SuscripcionModel.EstadoRegistro;
             return PartialView("template-Landing", model);
