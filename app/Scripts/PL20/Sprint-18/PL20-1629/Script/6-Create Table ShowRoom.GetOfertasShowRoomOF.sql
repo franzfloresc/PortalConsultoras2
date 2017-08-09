68c4c87aaf71a3116f5ab1f3f0bc98ae69481b11
miguel-otero-calderon@hotmail.com <miguel-otero-calderon@hotmail.com>
2017-08-09 14:52:11 -0500

    PL20-1630
    Avance

--- app/Scripts/PL20/Sprint-18/PL20-1629/Script/6-Create Table ShowRoom.GetOfertasShowRoomOF.sql
+++ app/Scripts/PL20/Sprint-18/PL20-1629/Script/6-Create Table ShowRoom.GetOfertasShowRoomOF.sql
@@ -6,73 +6,18 @@ if Exists(Select 1 From sysobjects Where name = 'GetOfertasShowRoomOF' and xtype
 go
 Create procedure [ShowRoom].[GetOfertasShowRoomOF]
 @CampaniaID int
---@CodigoConsultora varchar(20)
 as
 /*
-ShowRoom.GetShowRoomOfertasConsultora 201706,'0012954'
-ShowRoom.GetShowRoomOfertasConsultora 201706,'1615995'
+ShowRoom.GetShowRoomOfertasConsultora 201709
 */
 begin
-	declare @tablaEventoConsultora table 
-	(
-		EventoConsultoraID int,
-		EventoID int,
-		CampaniaID int,
-		CodigoConsultora varchar(20),
-		Segmento varchar(50),
-		MostrarPopup bit,
-		MostrarPopupVenta bit,
-		FechaCreacion datetime,
-		UsuarioCreacion varchar(20), 
-		FechaModificacion datetime,
-		UsuarioModificacion varchar(20)
-	)
-
-	insert into @tablaEventoConsultora
-	select top 1
-	EventoConsultoraID, EventoID, CampaniaID, 
-	CodigoConsultora, Segmento, MostrarPopup, 
-	MostrarPopupVenta, FechaCreacion, UsuarioCreacion, 
-	FechaModificacion, UsuarioModificacion
-	from ShowRoom.EventoConsultora
-	where CampaniaID = @CampaniaID --and CodigoConsultora = @CodigoConsultora
-	order by EventoConsultoraID desc
-
-	/*Validacion FaltanteAnunciado y ProductoFaltante*/
-	--declare @ZonaID int
-	--declare @CodigoRegion varchar(2)
-	--declare @CodigoZona varchar(4)
-
-	/*
-	select top 1 
-	@ZonaID = z.ZonaID, @CodigoRegion = r.Codigo, @CodigoZona = z.Codigo
-	from ods.Consultora c
-	inner join ods.Zona z on
-		c.ZonaID = z.ZonaID
-	inner join ods.Region r on
-		c.RegionID = r.RegionID
-	where c.Codigo = @CodigoConsultora
-	*/
-
 	declare @tablaFaltante table (CUV varchar(6))
-
 	insert into @tablaFaltante
-	select 
-		distinct ltrim(rtrim(CUV))
-	from dbo.ProductoFaltante nolock
-	where 
-		CampaniaID = @CampaniaID --and Zonaid = @ZonaID
-	UNION ALL
-	select 
-		distinct ltrim(rtrim(fa.CodigoVenta)) AS CUV
-	from ods.FaltanteAnunciado fa (nolock)
-	inner join ods.Campania c (nolock) on 
-		fa.CampaniaID = c.CampaniaID
-	where 
-		c.Codigo = @CampaniaID 
-		--and (rtrim(fa.CodigoRegion) = @CodigoRegion or fa.CodigoRegion IS NULL) 
-		--and (rtrim(fa.CodigoZona) = @CodigoZona or fa.CodigoZona IS NULL)
-	/*Fin Validacion FaltanteAnunciado y ProductoFaltante*/
+	select distinct ltrim(rtrim(CUV)) from dbo.ProductoFaltante nolock where CampaniaID = @CampaniaID 
+	Union All
+	select distinct ltrim(rtrim(fa.CodigoVenta)) AS CUV from ods.FaltanteAnunciado fa (nolock) 
+		inner join ods.Campania c (nolock) on fa.CampaniaID = c.CampaniaID
+	where c.Codigo = @CampaniaID 
 
 	select
 	--o.OfertaShowRoomID,
@@ -105,22 +50,19 @@ begin
 	inner join ods.ProductoComercial pc on o.CampaniaID = pc.CampaniaID 
 		and o.CUV = pc.CUV
 	inner join ods.Campania c on o.CampaniaID = c.CampaniaID
-	inner join @tablaEventoConsultora ec on c.Codigo = ec.CampaniaID
-	inner join ShowRoom.Perfil p on ec.Segmento = p.PerfilDescripcion
-		and ec.EventoID = p.EventoID
-	inner join ShowRoom.PerfilOfertaShowRoom pos on p.PerfilID = pos.PerfilID
-		and c.Codigo = pos.CampaniaID
-		and p.EventoID = pos.EventoID	
-		and o.CUV = pos.CUV
-	left join ShowRoom.Categoria cat on ec.EventoID = cat.EventoID
-		and o.CodigoCategoria = cat.Codigo
+	--inner join @tablaEventoConsultora ec on c.Codigo = ec.CampaniaID
+	--inner join ShowRoom.Perfil p on ec.Segmento = p.PerfilDescripcion
+	--	and ec.EventoID = p.EventoID
+	--inner join ShowRoom.PerfilOfertaShowRoom pos on p.PerfilID = pos.PerfilID
+	--	and c.Codigo = pos.CampaniaID
+	--	and p.EventoID = pos.EventoID	
+	--	and o.CUV = pos.CUV
+	--left join ShowRoom.Categoria cat on ec.EventoID = cat.EventoID
+	--	and o.CodigoCategoria = cat.Codigo
 	where 
 		c.Codigo = @CampaniaID
 		--and ec.CodigoConsultora = @CodigoConsultora
 		and o.FlagHabilitarProducto = 1
-		and o.CUV not in (
-			select CUV from @tablaFaltante
-		)
-	order by pos.Orden
-
+		and o.CUV not in (select CUV from @tablaFaltante)
+	--order by pos.Orden
 end
