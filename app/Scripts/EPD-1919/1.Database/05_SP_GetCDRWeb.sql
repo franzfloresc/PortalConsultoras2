9e032225165833ca58491567e00e2624b0089cc5
Kike <lead123456@hotmail.com>
2017-08-10 15:23:58 -0500

    EPD-1919: Ajuste en script GetCDRWEb.

--- app/Scripts/EPD-1919/1.Database/05_SP_GetCDRWeb.sql
+++ app/Scripts/EPD-1919/1.Database/05_SP_GetCDRWeb.sql
@@ -1,18 +1,11 @@
 GO
-alter procedure [dbo].[GetCDRWeb]
+ALTER PROCEDURE [dbo].[GetCDRWeb]
 	@ConsultoraID BIGINT,
 	@PedidoID INT = 0,
 	@CampaniaID INT = 0,
 	@CDRWebID INT = 0
-as
---GetCDRWeb 2
+AS
 BEGIN
-	--INI: EPD-1919
-	declare @TieneCDRExpress bit = isnull((select top 1 Estado from ConfiguracionPais where Codigo = 'CDR-EXP'),0);
-	declare @MensajeCDRExpress varchar(200) = isnull((select top 1 Descripcion from TablaLogicaDatos where TablaLogicaID = 104 and Codigo = 'Mensaje'),'');
-	declare @MensajeCDRExpressNueva varchar(200) = isnull((select top 1 Descripcion from TablaLogicaDatos where TablaLogicaID = 104 and Codigo = 'MensajeNew'),'');
-	--FIN: EPD-1919
-
 	SELECT top 20
 		C.CDRWebID,
 		C.PedidoID,
@@ -31,9 +24,9 @@ BEGIN
 			WHERE CDRWebId = C.CDRWebId AND Estado = 2
 			ORDER BY FechaRegistro DESC)
 		,0) AS ConsultoraSaldo,
-		@TieneCDRExpress as TieneCDRExpress, --EPD-1919
-		@MensajeCDRExpress as MensajeCDRExpress, --EPD-1919
-		@MensajeCDRExpressNueva as MensajeCDRExpressNueva --EPD-1919
+		TipoDespacho, --EPD-1919
+		FleteDespacho, --EPD-1919
+		MensajeDespacho --EPD-1919
 	FROM CDRWeb C
 	WHERE C.ConsultoraID = @ConsultoraID
 		AND (C.PedidoID = @PedidoID OR @PedidoID = 0)
