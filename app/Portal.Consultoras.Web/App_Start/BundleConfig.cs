c1571a598c7927fdd4eb579df13c391471e1bde5
Giovanni <giovanni.benvenutto@zoluxionessac.onmicrosoft.com>
2017-08-17 18:17:29 -0500

    Vista Asesora Online y Popup de Inscripción Asesora Online

--- app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
+++ app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
@@ -139,7 +139,8 @@ namespace Portal.Consultoras.Web
             bundles.Add(new StyleBundle("~/Content/Css/Site/Esika/CssSB2").Include(
                "~/Content/Css/Site/Esika/reset.css",
                "~/Content/Css/Site/Esika/style.css",
-               "~/Content/Css/ui.jquery/Esika/jquery-ui.css"
+               "~/Content/Css/ui.jquery/Esika/jquery-ui.css",
+               "~/Content/Css/Site/asesora-online.css"
                ));
 
             bundles.Add(new StyleBundle("~/Content/Css/Site/Lbel/CssSB2").Include(
---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/Index.js
@@ -0,0 +1,72 @@
+﻿var vista;
+
+$(document).ready(function () {
+
+    var LandingAsesoraOnline;
+
+    LandingAsesoraOnline = function () {
+
+        var me = this;
+
+        me.Eventos = {
+
+            irASeccionFormulario: function (e) {
+
+                e.preventDefault();
+
+                var atributoEnlaceQuieroInscribirme = $(".cta_inscripcion").attr("href");
+
+                $('html,body').animate({
+                    scrollTop: $(atributoEnlaceQuieroInscribirme).offset().top
+                }, 650, "easeOutSine");
+
+            }
+
+        };
+
+        me.Funciones = {
+
+            removerEnlace: function () {
+
+                setTimeout(function () {
+                    $('a[href*="//apps.elfsight.com/panel/"]').remove();
+                }, 1500);
+
+            },
+
+            cambiarTexto: function () {
+
+                if (window.matchMedia("(max-width:767px)").matches) {
+
+                    $(".descripcion_asesora_online").html("<span>Recibe por correo y mensaje de texto la mejor asesoría para ganar más.</span><span>¡Sólo debes confirmar tus datos de contacto!</span>");
+
+                } else {
+
+                    $(".descripcion_asesora_online").html("<span>Recibe por correo y mensaje de texto la mejor asesoría para ganar más, gestionar tus clientes, comprar ofertas exclusivas y mucho más.</span><span>¡Sólo debes confirmar tus datos de contacto!</span>");
+                }
+
+            },
+
+            inicializarEventos: function () {
+
+                $("body").on("click", ".cta_inscripcion", me.Eventos.irASeccionFormulario);
+
+            }
+
+        };
+
+        me.Inicializar = function () {
+
+            me.Funciones.removerEnlace();
+            me.Funciones.cambiarTexto();
+            me.Funciones.inicializarEventos();
+            $(window).resize(me.Funciones.cambiarTexto);
+
+        };
+
+    };
+
+    vista = new LandingAsesoraOnline();
+    vista.Inicializar();
+
+});
