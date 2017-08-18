90deb53dc01c8db61c5c412eaba3b3765c733741
Giovanni <giovanni.benvenutto@zoluxionessac.onmicrosoft.com>
2017-08-18 18:02:01 -0500

    Se quitó versión mobile aparte de landing asesora online

--- app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
+++ app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
@@ -153,8 +153,7 @@ namespace Portal.Consultoras.Web
                "~/Content/Css/Mobile/esika/icomon.css",
                "~/Content/Css/Mobile/esika/menu.css",
                "~/Content/Css/Mobile/esika/style.css",
-               "~/Content/Css/Mobile/esika/theme.css",
-               "~/Content/Css/Site/asesora-online.css"
+               "~/Content/Css/Mobile/esika/theme.css"
                ));
 
             bundles.Add(new StyleBundle("~/Content/Css/Mobile/lbel/CssSB2Mobile").Include(
