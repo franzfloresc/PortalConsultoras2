e3691e7f36ad8f83c002dcc396e79edbda11a51e
AlonsoUchidaZol <javier.uchida@zoluxiones.com>
2017-08-22 15:46:37 -0500

    https://jira.belcorp.biz/browse/VC-1

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/AsesoraOnline.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/AsesoraOnline.js
@@ -16,30 +16,34 @@
             }
             if ($("#ddlTipoEstrategia").val() == "") {
                 alert("Debe seleccionar el tipo de estrategia, verifique.");
-                return false;
+                return false; 
             }*/
+        var tipsVentas = $("#tips-ventas:checked").val();
+        var tipsGestionClientes = $("#tips-gestion-clientes:checked").val();
+        var masCatalogos = $("#mas-catalogos:checked").val();
+        var tipsMasClientes = $("#tips-mas-clientes:checked").val();
 
         var params = {
-            TipsVentas: tipsVentas,
-            TipsGestionClientes: tipsGestionClientes,
-            TasCatalogos: masCatalogos,
-            TipsMasClientes: tipsMasClientes
+            TipsVentas: typeof tipsVentas === "undefined" ? 0 : tipsVentas,
+            TipsGestionClientes: typeof tipsGestionClientes === "undefined" ? 0 : tipsGestionClientes,
+            MasCatalogos: typeof masCatalogos === "undefined" ? 0 : masCatalogos,
+            TipsMasClientes: typeof tipsMasClientes === "undefined" ? 0 : tipsMasClientes
             };
 
-            jQuery.ajax({
-                type: 'POST',
-                url: _config.enviarFormularioUrl,
-                dataType: 'json',
-                contentType: 'application/json; charset=utf-8',
-                data: JSON.stringify(params),
-                async: true,
-                success: function (data) {
-                    alert(data.message);
-                },
-                error: function (data, error) {
-                    alert(data.message);
-                }
-            });
+        jQuery.ajax({
+            type: 'POST',
+            url: _config.enviarFormularioUrl,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(params),
+            async: true,
+            success: function (data) {
+                alert(data.message);
+            },
+            error: function (data, error) {
+                alert(data.message);
+            }
+        });
 
     };
 
