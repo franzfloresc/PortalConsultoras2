86d80ea0ff256aa2b47ea8b58791dbd9ac3c0e67
JuanLago <juan.lago@zoluxiones.com>
2017-08-25 17:22:36 -0500

    MC-HD976 Avance en mostrar y editar datos de la consultora en mobiles

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/MisDatosController.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Portal.Consultoras.Web.Areas.Mobile.Models;
+using Portal.Consultoras.Web.ServiceCliente;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.ServiceUsuario;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServiceUsuario;
+using System.Configuration;
+using Portal.Consultoras.Web.ServiceZonificacion;
+using Portal.Consultoras.Web.ServiceSAC;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class MisDatosController : BaseMobileController
+    {
+        //
+        // GET: /MisDatos/
+
+       
+
+        public ActionResult Index()
+        {
+            BEUsuario beusuario = new BEUsuario();
+            ViewBag.DatosIniciales = new BienvenidaHomeModel();
+
+            var model = new MisDatosModel();
+
+            //EPD-1089
+            if (userData.PaisID == 9)
+            {
+                ViewBag.limiteMinimoTelef = 5;
+                ViewBag.limiteMaximoTelef = 15;
+            }
+            else if (userData.PaisID == 11)
+            {
+                ViewBag.limiteMinimoTelef = 7;
+                ViewBag.limiteMaximoTelef = 9;
+            }
+            else if (userData.PaisID == 4)
+            {
+                ViewBag.limiteMinimoTelef = 10;
+                ViewBag.limiteMaximoTelef = 10;
+            }
+            else if (userData.PaisID == 8 || userData.PaisID == 7 || userData.PaisID == 10 || userData.PaisID == 5)
+            {
+                ViewBag.limiteMinimoTelef = 8;
+                ViewBag.limiteMaximoTelef = 8;
+            }
+            else if (userData.PaisID == 6)
+            {
+                ViewBag.limiteMinimoTelef = 9;
+                ViewBag.limiteMaximoTelef = 10;
+            }
+            else
+            {
+                ViewBag.limiteMinimoTelef = 0;
+                ViewBag.limiteMaximoTelef = 15;
+            }
+             
+
+            using (UsuarioServiceClient sv = new UsuarioServiceClient())
+            {
+                beusuario = sv.Select(userData.PaisID, userData.CodigoUsuario);
+            }
+
+            if (beusuario != null)
+            {
+                model.PaisISO = userData.CodigoISO;
+                ViewBag.PaisId = userData.PaisID;
+                model.NombreCompleto = beusuario.Nombre;
+                model.NombreGerenteZonal = userData.NombreGerenteZonal;     //SB20-907
+                model.EMail = beusuario.EMail;
+                model.NombreGerenteZonal = userData.NombreGerenteZonal;     //SB20-907
+                model.Telefono = beusuario.Telefono;
+                model.TelefonoTrabajo = beusuario.TelefonoTrabajo;
+                model.Celular = beusuario.Celular;
+                model.Sobrenombre = beusuario.Sobrenombre;
+                model.CompartirDatos = beusuario.CompartirDatos;
+                model.AceptoContrato = beusuario.AceptoContrato;
+                model.UsuarioPrueba = userData.UsuarioPrueba;
+                model.NombreArchivoContrato = ConfigurationManager.AppSettings["Contrato_ActualizarDatos_" + userData.CodigoISO].ToString();
+
+                BEZona[] bezona;
+                using (ZonificacionServiceClient sv = new ZonificacionServiceClient())
+                {
+                    bezona = sv.SelectZonaById(userData.PaisID, userData.ZonaID);
+                }
+                model.NombreGerenteZonal = bezona.ToList().Count == 0 ? "" : bezona[0].NombreGerenteZona;
+
+                if (beusuario.EMailActivo) model.CorreoAlerta = "";
+                if (!beusuario.EMailActivo && beusuario.EMail != "") model.CorreoAlerta = "Su correo aun no ha sido activado";
+
+                if (model.UsuarioPrueba == 1)
+                {
+                    using (SACServiceClient sv = new SACServiceClient())
+                    {
+                        model.NombreConsultoraAsociada = sv.GetNombreConsultoraAsociada(userData.PaisID, userData.CodigoUsuario) + " (" + sv.GetCodigoConsultoraAsociada(userData.PaisID, userData.CodigoUsuario) + ")";
+                    }
+                }
+                /*EPD-1068*/
+                model.CodigoUsuario = userData.CodigoUsuario + " (Zona: " + userData.CodigoZona + ")";
+                string PaisesDigitoControl = ConfigurationManager.AppSettings["PaisesDigitoControl"].ToString();
+                model.DigitoVerificador = string.Empty;
+                if (PaisesDigitoControl.Contains(model.PaisISO))
+                {
+                    if (!String.IsNullOrEmpty(beusuario.DigitoVerificador))
+                    {
+                        model.CodigoUsuario = string.Format("{0} - {1} (Zona:{2})", userData.CodigoUsuario, beusuario.DigitoVerificador, userData.CodigoZona);
+                    }
+                }
+            }
+          
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult CambiarContrasenia(string OldPassword, string NewPassword)
+        {
+            int rslt = 0;
+            try
+            {
+                using (UsuarioServiceClient sv = new UsuarioServiceClient())
+                {
+                    int resultExiste;
+                    bool result;
+
+                    resultExiste = sv.ExisteUsuario(userData.PaisID, userData.CodigoUsuario, OldPassword);
+
+                    if (resultExiste == Constantes.ValidacionExisteUsuario.Existe)
+                    {
+                        result = sv.CambiarClaveUsuario(userData.PaisID, userData.CodigoISO, userData.CodigoUsuario,
+                            NewPassword, "", userData.CodigoUsuario, EAplicacionOrigen.MisDatosConsultora);
+
+                        rslt = result ? 2 : 1;
+                    }
+                    else
+                    {
+                        if (resultExiste == Constantes.ValidacionExisteUsuario.ExisteDiferenteClave)
+                            rslt = 0;
+                    }
+                }
+
+                return Json(new
+                {
+                    success = true,
+                    message = rslt
+                });
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    success = false,
+                    message = "Ocurrió un erro al Cambiar la Contraseña, Intente nuevamente.",
+                    extra = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesBus(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    success = false,
+                    message = "Ocurrió un erro al Cambiar la Contraseña, Intente nuevamente.",
+                    extra = ""
+                });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ActualizarDatos(MisDatosModel model)
+        {
+            try
+            {
+                Mapper.CreateMap<MisDatosModel, BEUsuario>()
+                    .ForMember(t => t.CodigoUsuario, f => f.MapFrom(c => c.CodigoUsuario))
+                    .ForMember(t => t.EMail, f => f.MapFrom(c => c.EMail))
+                    .ForMember(t => t.Telefono, f => f.MapFrom(c => c.Telefono))
+                    .ForMember(t => t.TelefonoTrabajo, f => f.MapFrom(c => c.TelefonoTrabajo))
+                    .ForMember(t => t.Celular, f => f.MapFrom(c => c.Celular))
+                    .ForMember(t => t.Sobrenombre, f => f.MapFrom(c => c.Sobrenombre))
+                    .ForMember(t => t.Nombre, f => f.MapFrom(c => c.NombreCompleto))
+                    .ForMember(t => t.CompartirDatos, f => f.MapFrom(c => c.CompartirDatos))
+                    .ForMember(t => t.AceptoContrato, f => f.MapFrom(c => c.AceptoContrato));
+
+                BEUsuario entidad = Mapper.Map<MisDatosModel, BEUsuario>(model);
+                string CorreoAnterior = model.CorreoAnterior;
+
+                entidad.CodigoUsuario = (entidad.CodigoUsuario == null) ? "" : UserData().CodigoUsuario;
+                entidad.EMail = (entidad.EMail == null) ? "" : entidad.EMail;
+                entidad.Telefono = (entidad.Telefono == null) ? "" : entidad.Telefono;
+                entidad.TelefonoTrabajo = (entidad.TelefonoTrabajo == null) ? "" : entidad.TelefonoTrabajo;
+                entidad.Celular = (entidad.Celular == null) ? "" : entidad.Celular;
+                entidad.Sobrenombre = (entidad.Sobrenombre == null) ? "" : entidad.Sobrenombre;
+                entidad.ZonaID = UserData().ZonaID;
+                entidad.RegionID = UserData().RegionID;
+                entidad.ConsultoraID = UserData().ConsultoraID;
+                entidad.PaisID = UserData().PaisID;
+                entidad.PrimerNombre = userData.PrimerNombre;
+                entidad.CodigoISO = UserData().CodigoISO;
+
+                using (UsuarioServiceClient svr = new UsuarioServiceClient())
+                {
+                    string resultado = svr.ActualizarMisDatos(entidad, CorreoAnterior);
+                    string[] lst = resultado.Split('|');
+
+                    if (lst[0] == "0")
+                    {
+                        return Json(new
+                        {
+                            Cantidad = lst[3],
+                            success = false,
+                            message = lst[2],
+                            extra = ""
+                        });
+                    }
+                    else
+                    {
+                        return Json(new
+                        {
+                            Cantidad = 0,
+                            success = true,
+                            message = lst[2],
+                            extra = ""
+                        });
+                    }
+                }
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    Cantidad = 0,
+                    success = false,
+                    message = "Ocurrió un error al acceder al servicio, intente nuevamente.",
+                    extra = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesBus(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    Cantidad = 0,
+                    success = false,
+                    message = "Ocurrió un error al acceder al servicio, intente nuevamente.",
+                    extra = ""
+                });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult AceptarContrato(MisDatosModel model)
+        {
+            try
+            {
+                Mapper.CreateMap<MisDatosModel, BEUsuario>()
+                    .ForMember(t => t.AceptoContrato, f => f.MapFrom(c => c.AceptoContrato));
+
+                BEUsuario entidad = Mapper.Map<MisDatosModel, BEUsuario>(model);
+                string CorreoAnterior = model.CorreoAnterior;
+
+                entidad.CodigoUsuario = (entidad.CodigoUsuario == null) ? "" : UserData().CodigoUsuario;
+                entidad.ZonaID = UserData().ZonaID;
+                entidad.RegionID = UserData().RegionID;
+                entidad.ConsultoraID = UserData().ConsultoraID;
+                entidad.PaisID = UserData().PaisID;
+                entidad.PrimerNombre = userData.PrimerNombre;
+                entidad.CodigoISO = UserData().CodigoISO;
+
+                using (UsuarioServiceClient svr = new UsuarioServiceClient())
+                {
+                    string resultado = svr.AceptarContrato(entidad);
+                    string[] lst = resultado.Split('|');
+
+                    if (lst[0] == "0")
+                    {
+                        return Json(new
+                        {
+                            Cantidad = lst[3],
+                            success = false,
+                            message = lst[2],
+                            extra = ""
+                        });
+                    }
+                    else
+                    {
+                        return Json(new
+                        {
+                            Cantidad = 0,
+                            success = true,
+                            message = lst[2],
+                            extra = ""
+                        });
+                    }
+                }
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    Cantidad = 0,
+                    success = false,
+                    message = "Ocurrió un error al acceder al servicio, intente nuevamente.",
+                    extra = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesBus(ex, UserData().CodigoConsultora, UserData().CodigoISO);
+                return Json(new
+                {
+                    Cantidad = 0,
+                    success = false,
+                    message = "Ocurrió un error al acceder al servicio, intente nuevamente.",
+                    extra = ""
+                });
+            }
+        }
+
+  
+    }
+}
