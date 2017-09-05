0a2cf23f1404b09cd36423595089b91e638490a0
Kike <lead123456@hotmail.com>
2017-09-05 14:41:12 -0500

    VC-1: Ajustes en textos de descripción en pantalla AsesoraOnline

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/Index.js
@@ -46,18 +46,6 @@ $(document).ready(function () {
                     $('a[href*="//apps.elfsight.com/panel/"]').remove();
                 }, 2000);
             },
-            cambiarTexto: function () {
-                if (window.matchMedia("(max-width:767px)").matches) {
-                    $(".texto_titulo span").first().html("¿Cuál es tu método de venta preferido con tus clientes?");
-                    $(".textoOpcionDejarCatalogo label").html("<span></span>Dejo el catálogo a mis clientes");
-                    $(".textoOpcionTienda label").html("<span></span>Tengo un negocio");
-                }
-                else {
-                    $(".texto_titulo span").first().html("¿Cuál es tu forma de vender ganadora?");
-                    $(".textoOpcionDejarCatalogo label").html("<span></span>Dejo el catálogo a mis clientes");
-                    $(".textoOpcionTienda label").html("<span></span>Mis clientes vienen a mi tienda");
-                }
-            },
             inicializarEventos: function () {
                 $("body").on("click", ".cta_inscripcion", me.Eventos.irASeccionFormulario);
                 $("body").on("click", ".cerrar_popup_inscripcion", me.Eventos.cerrarPopupsAsesoraOnline);
@@ -67,9 +55,7 @@ $(document).ready(function () {
 
         me.Inicializar = function () {
             me.Funciones.removerEnlace();
-            me.Funciones.cambiarTexto();
             me.Funciones.inicializarEventos();
-            $(window).resize(me.Funciones.cambiarTexto);
 
             $("#enviar-form").on("click", asesoraOnlineObj.enviarFormulario);
             $("#modificar_mis_datos").on("click", asesoraOnlineObj.irAModificarMisDatos);
