b286d7d662f6670fea63abe2d63f3808497d82e3
Alberto Oshiro <alberto.oshiro@zoluxiones.com>
2017-09-11 18:12:21 -0500

    corrección de mail (links) y popup (cerrar) de Asesora Online

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AsesoraOnline/Index.js
@@ -26,11 +26,14 @@ $(document).ready(function () {
                 e.preventDefault();
 
                 $(this).parents(".contenedor_fondo_popup").fadeOut(400);
-                $(this).parent().fadeOut(400);            
+                //$(this).parent().fadeOut(400);            
             },
             mostrarBotonSuscripcionMobile: function () {
                 if(window.matchMedia("(max-width:767px)").matches) {
-                    if ($(window).scrollTop() > $("#cta-suscribirme").offset().top + 45) {
+                    if ($(window).scrollTop() > $("#formulario-inscripcion").offset().top - 200) {
+                        $(".cta_inscripcion_fixed").removeClass("mostrar_cta_inscripcion_mobile_fijo");
+                    }
+                    else if ($(window).scrollTop() > $("#cta-suscribirme").offset().top + 45) {
                         $(".cta_inscripcion_fixed").addClass("mostrar_cta_inscripcion_mobile_fijo");
                     }
                     else {
