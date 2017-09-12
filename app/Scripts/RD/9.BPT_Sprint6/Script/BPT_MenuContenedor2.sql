00975dd6c19d557e9d1256c54d8b65851b16f162
alan.aurora <alan.aurora@ZOLUXLP053>
2017-09-12 18:03:40 -0500

    Contenedor de Ofertas => Mobile

--- app/Scripts/RD/9.BPT_Sprint6/Script/BPT_MenuContenedor2.sql
+++ app/Scripts/RD/9.BPT_Sprint6/Script/BPT_MenuContenedor2.sql
@@ -6,3 +6,11 @@ if not Exists (select * from ConfiguracionPais where Codigo = 'InicioContenedor'
 	values('InicioContenedor', 1, 'Configuracion por defecto para el menú del contenedor de ofertas', 1, 1)
 
 go
+
+go
+
+if not Exists (select * from ConfiguracionPais where Codigo = 'OPT')
+	insert into ConfiguracionPais(Codigo,Excluyente,Descripcion,Estado,TienePerfil)
+	values('OPT', 1, 'Oferta Para Ti', 1, 1)
+
+go
