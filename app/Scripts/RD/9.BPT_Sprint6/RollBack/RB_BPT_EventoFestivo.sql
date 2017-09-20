61867a008e8c194da4f162b305f7bf2251669845
Carlos Jave <jave.carlos@gmail.com>
2017-09-20 16:51:28 -0500

    correcciones en scripts y creacion de rollback parte 6

--- app/Scripts/RD/9.BPT_Sprint6/RollBack/RB_BPT_EventoFestivo.sql
+++ app/Scripts/RD/9.BPT_Sprint6/RollBack/RB_BPT_EventoFestivo.sql
@@ -1,42 +1,130 @@
 USE BelcorpBolivia
 go
 
-USE BelcorpChile_BPT
-go
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
 
-USE BelcorpColombia
-go
-
-USE BelcorpCostaRica
-go
-
-USE BelcorpDominicana
-go
-
-USE BelcorpEcuador
-go
-
-USE BelcorpGuatemala
-go
-
-USE BelcorpMexico_Plan20
-go
+GO
 
 IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
 	DROP TABLE [dbo].[EventoFestivo]
 GO
+
+USE BelcorpChile
+go
 
 IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
 BEGIN
 	DROP PROC [dbo].[GetEventoFestivo]
 END
 
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpColombia
 go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpCostaRica
+go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpDominicana
+go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpEcuador
+go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpGuatemala
+go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpMexico
+go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
 
 USE BelcorpPanama
 go
 
-USE BelcorpPeru_BPT
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
+USE BelcorpPeru
 go
 
 IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
@@ -53,8 +141,41 @@ go
 USE BelcorpPuertoRico
 go
 
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
 USE BelcorpSalvador
 go
 
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
+
 USE BelcorpVenezuela
 go
+
+IF EXISTS (select * from sys.objects where type = 'P' and name like '%GetEventoFestivo%')
+BEGIN
+	DROP PROC [dbo].[GetEventoFestivo]
+END
+
+GO
+
+IF EXISTS (select * from sys.objects where type = 'U' and name like '%EventoFestivo%')
+	DROP TABLE [dbo].[EventoFestivo]
+GO
