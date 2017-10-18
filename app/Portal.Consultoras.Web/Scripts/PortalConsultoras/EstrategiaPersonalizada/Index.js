25ebd3488c6a3701be7537a2d10309d06ef702a1
jose-ponte <jose.ponte@hundred.com.pe>
2017-10-18 15:32:35 -0500

    Se quito el "delete Link"

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaPersonalizada/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaPersonalizada/Index.js
@@ -350,14 +350,15 @@ function Descargables(Filename) {
                     var nombre = NombreArchivo;
 
                     var link = document.createElement("a");
-                    link.download = nombre;
-                    link.href = url;
+                    link.setAttribute("id", "dwnarchivo")
                     link.setAttribute("target", "_blank");
+                    link.setAttribute('style', 'display:none;');
+                    link.download = nombre;
+                    link.href = url;                    
                     document.body.appendChild(link);
                     link.click();
 
                     document.body.removeChild(link);
-                    delete link;
                 }
             }            
         },
