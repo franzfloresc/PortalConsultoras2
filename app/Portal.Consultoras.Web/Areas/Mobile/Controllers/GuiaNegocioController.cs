067f9d891e5a0f29ce306ae690991fcf2588f13b
Victor Vilela <vilela_victor@hotmail.com>
2017-10-19 10:53:22 -0500

    Add Mobile\GuiaNegocioController

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/GuiaNegocioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class GuiaNegocioController : Controller
+    {
+        // GET: Mobile/GuiaNegocio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
