526069ea479151165033181fecaf8c054fd1fe95
alan.aurora <alan.aurora@ZOLUXLP053>
2017-10-20 17:00:26 -0500

    SQL => Script para el pase

---
+++ app/Scripts/RD/11.BPT_Sprint11/RollBack/4.3.RB_BPT_ConfiguracionPais_GetAll.sql
@@ -0,0 +1,55 @@
+﻿GO
+
+ALTER PROCEDURE [dbo].[ConfiguracionPais_GetAll]
+	(
+	@DesdeCampania int = 0
+	,@Codigo varchar(100) = ''
+	,@CodigoRegionvarchar(100) = ''
+	,@CodigoZonavarchar(100) = ''
+	,@CodigoSeccionvarchar(100) = ''
+	,@CodigoConsultoravarchar(100) = ''
+	)
+	AS
+	/*
+	dbo.ConfiguracionPais_GetAll 0,'','','','',''
+	*/
+	BEGIN
+	SET NOCOUNT ON;
+	
+	SET @DesdeCampania = ISNULL(@DesdeCampania, 0)
+	
+	select
+	
+	c.ConfiguracionPaisID
+	,c.Codigo
+	,c.Excluyente
+	,c.Descripcion
+	,c.Estado
+	,C.TienePerfil
+	,C.DesdeCampania
+	,C.MobileTituloMenu
+	,C.DesktopTituloMenu
+	,C.Logo
+	,C.Orden
+	,C.DesktopTituloBanner
+	,C.MobileTituloBanner
+	,C.DesktopSubTituloBanner
+	,C.MobileSubTituloBanner
+	,C.DesktopFondoBanner
+	,C.MobileFondoBanner
+	,C.DesktopLogoBanner
+	,C.MobileLogoBanner
+	,C.UrlMenu
+	,C.OrdenBpt
+	from dbo.fnConfiguracionPais_GetAll(
+	@Codigo,
+	@CodigoRegion,
+	@CodigoZona,
+	@CodigoSeccion,
+	@CodigoConsultora
+	) as c
+	WHERE C.DesdeCampania <= @DesdeCampania OR @DesdeCampania = 0
+	
+	END
+
+GO
