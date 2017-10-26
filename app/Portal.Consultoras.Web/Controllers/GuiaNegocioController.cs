7ff7edb9c6afa9794c58ec3ffd5fe000fc876c8f
alan.aurora <alan.aurora@ZOLUXLP053>
2017-10-26 15:12:04 -0500

    GND => Quitando Metodos No Utilizados

--- app/Portal.Consultoras.Web/Controllers/GuiaNegocioController.cs
+++ app/Portal.Consultoras.Web/Controllers/GuiaNegocioController.cs
@@ -9,7 +9,6 @@ using System.Web.Mvc;
 
 namespace Portal.Consultoras.Web.Controllers
 {
-    //BPT - 387
     public class GuiaNegocioController : BaseGuiaNegocioController
     {
         public ActionResult Index()
@@ -57,48 +56,6 @@ namespace Portal.Consultoras.Web.Controllers
                 });
             }
         }
-
-        public ActionResult Comprar()
-        {
-            try
-            {
-                //return ViewLanding(1);
-                return ViewLanding();
-            }
-            catch (Exception ex)
-            {
-                LogManager.LogManager.LogErrorWebServicesBus(ex, userData.CodigoConsultora, userData.CodigoISO);
-            }
-
-            return RedirectToAction("Index", "Bienvenida");
-        }
-
-        [HttpPost]
-        public JsonResult GuardarProductoTemporal(EstrategiaPersonalizadaProductoModel modelo)
-        {
-            if (modelo != null)
-            {
-                modelo.ClaseBloqueada = Util.Trim(modelo.ClaseBloqueada);
-                modelo.ClaseEstrategia = Util.Trim(modelo.ClaseEstrategia);
-                modelo.CodigoEstrategia = Util.Trim(modelo.CodigoEstrategia);
-                modelo.DescripcionResumen = Util.Trim(modelo.DescripcionResumen);
-                modelo.DescripcionDetalle = Util.Trim(modelo.DescripcionDetalle);
-                modelo.DescripcionCompleta = Util.Trim(modelo.DescripcionCompleta);
-                modelo.PrecioTachado = Util.Trim(modelo.PrecioTachado);
-                modelo.CodigoVariante = Util.Trim(modelo.CodigoVariante);
-                modelo.TextoLibre = Util.Trim(modelo.TextoLibre);
-                modelo.UrlCompartir = Util.Trim(modelo.UrlCompartir);
-            }
-
-            Session[Constantes.ConstSession.ProductoTemporal] = modelo;
-
-            return Json(new
-            {
-                success = true
-            }, JsonRequestBehavior.AllowGet);
-        }
-
-
-
+        
     }
 }
