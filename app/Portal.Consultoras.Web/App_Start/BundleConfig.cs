f5f713dde030039a2752ec459568765ef729e821
alan.aurora <alan.aurora@ZOLUXLP053>
2017-10-30 10:54:45 -0500

    Menu Contenedor => Mejora Click Menu

--- app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
+++ app/Portal.Consultoras.Web/App_Start/BundleConfig.cs
@@ -29,7 +29,8 @@ namespace Portal.Consultoras.Web
                "~/Scripts/jquery.unobtrusive-ajax.js",
                "~/Scripts/jquery-ui-1.9.2.custom.js",
                "~/Scripts/HojaInscripcion/validations.js",
-               "~/Scripts/donetyping.js"
+               "~/Scripts/donetyping.js",
+                "~/Scripts/PortalConsultoras/RevistaDigital/RevistaDigital-DataLayer.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/JQGridJs").Include(
