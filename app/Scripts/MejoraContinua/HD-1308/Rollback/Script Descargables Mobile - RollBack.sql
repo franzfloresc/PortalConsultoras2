6b84b2290366de3b225fb5e5cfbc21be4a440ed2
jose-ponte <jose.ponte@hundred.com.pe>
2017-11-03 11:56:03 -0500

    HD-1308 - Script

---
+++ app/Scripts/MejoraContinua/HD-1308/Rollback/Script Descargables Mobile - RollBack.sql
@@ -0,0 +1,130 @@
+USE BelcorpPeru
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpMexico
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpSalvador
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpPanama
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpGuatemala
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpEcuador
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpDominicana
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpCostaRica
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
+USE BelcorpBolivia
+GO
+
+IF EXISTS (SELECT 1 FROM ConfiguracionPais WHERE Codigo = 'DES-NAV')
+BEGIN
+	DECLARE @ConfiguracionPaisID INT = 0
+	SELECT @ConfiguracionPaisID = ConfiguracionPaisID FROM ConfiguracionPais WHERE Codigo = 'DES-NAV'
+
+	UPDATE [dbo].[ConfiguracionPais] SET MobileTituloMenu = NULL, MobileOrden = 7, MobileOrdenBPT = 7 where Codigo = 'DES-NAV'
+	UPDATE [dbo].[ConfiguracionOfertasHome] SET MobileTitulo = NULL, MobileTipoPresentacion = 0, MobileActivo = 0, MobileOrdenBpt = 0 where ConfiguracionPaisID = @ConfiguracionPaisID
+END
+GO
+
