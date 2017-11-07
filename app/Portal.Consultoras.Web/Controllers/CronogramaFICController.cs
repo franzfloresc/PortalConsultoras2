968609a0e7f47f7551a06fd657dc26d2258154a9
Victor Vilela <vilela_victor@hotmail.com>
2017-11-07 16:15:14 -0500

    Refactor : Move Mapping from BEPais to PaisModel to DomainToModelMappingProfile

--- app/Portal.Consultoras.Web/Controllers/CronogramaFICController.cs
+++ app/Portal.Consultoras.Web/Controllers/CronogramaFICController.cs
@@ -477,10 +477,6 @@ namespace Portal.Consultoras.Web.Controllers
                     lst.Add(sv.SelectPais(UserData().PaisID));
                 }
             }
-            Mapper.CreateMap<BEPais, PaisModel>()
-                    .ForMember(t => t.PaisID, f => f.MapFrom(c => c.PaisID))
-                    .ForMember(t => t.Nombre, f => f.MapFrom(c => c.Nombre))
-                    .ForMember(t => t.NombreCorto, f => f.MapFrom(c => c.NombreCorto));
 
             return Mapper.Map<IList<BEPais>, IEnumerable<PaisModel>>(lst);
         }
