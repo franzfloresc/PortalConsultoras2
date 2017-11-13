0d2e2698f32b0aac09dce88e1bb3c8d687492a34
Carlos Jave <jave.carlos@gmail.com>
2017-11-13 17:05:38 -0500

    agregando vistas y controlador de Certificado Comercial

---
+++ app/Portal.Consultoras.Web/Controllers/CertificadoComercialController.cs
@@ -0,0 +1,28 @@
+﻿using Portal.Consultoras.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class CertificadoComercialController : BaseController
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                if (!UsuarioModel.HasAcces(ViewBag.Permiso, "CertificadoComercial/Index"))
+                    return RedirectToAction("Index", "Bienvenida");
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, userData.CodigoConsultora, userData.CodigoISO);
+            }
+
+            return View();
+        }
+    }
+}
