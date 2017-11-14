06ebca90e5d4fb2517578c85e43cd78488ac1d0f
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-11-14 11:38:02 -0500

    HD-801, se agrego la libreria itextsharp para generar PDF

--- app/Portal.Consultoras.Web/Controllers/CertificadoComercialController.cs
+++ app/Portal.Consultoras.Web/Controllers/CertificadoComercialController.cs
@@ -6,6 +6,12 @@ using System.ServiceModel;
 using System.Web;
 using System.Web.Mvc;
 
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
+
 namespace Portal.Consultoras.Web.Controllers
 {
     public class CertificadoComercialController : BaseController
@@ -24,5 +30,40 @@ namespace Portal.Consultoras.Web.Controllers
 
             return View();
         }
+
+        private string RenderViewToString(ControllerContext context,
+                            string viewPath,
+                            object model = null,
+                            bool partial = false)
+        {
+            // first find the ViewEngine for this view
+            ViewEngineResult viewEngineResult = null;
+            if (partial)
+                viewEngineResult = ViewEngines.Engines.FindPartialView(context, viewPath);
+            else
+                viewEngineResult = ViewEngines.Engines.FindView(context, viewPath, null);
+
+            if (viewEngineResult == null)
+                throw new FileNotFoundException("View cannot be found.");
+
+            // get the view and attach the model to view data
+            var view = viewEngineResult.View;
+            context.Controller.ViewData.Model = model;
+
+            string result = null;
+
+            using (var sw = new StringWriter())
+            {
+                var ctx = new ViewContext(context, view,
+                                            context.Controller.ViewData,
+                                            context.Controller.TempData,
+                                            sw);
+                view.Render(ctx, sw);
+                result = sw.ToString();
+            }
+
+            return result;
+        }
     }
+}
 }
