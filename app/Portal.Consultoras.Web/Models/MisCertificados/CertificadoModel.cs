97dd089cb5df5423817d2aa201a1097e7fd5d1eb
Carlos Jave <jave.carlos@gmail.com>
2017-11-15 10:48:41 -0500

    agregado propiedades generar pdf

--- app/Portal.Consultoras.Web/Models/MisCertificados/CertificadoModel.cs
+++ app/Portal.Consultoras.Web/Models/MisCertificados/CertificadoModel.cs
@@ -11,6 +11,27 @@ namespace Portal.Consultoras.Web.Models.MisCertificados
         public int CertificadoComercialId { get; set; }
         public string Nombre { get; set; }
         public string MensajeError { get; set; }
-        public string NombreVista { get; set; }       
+        public string NombreVista { get; set; }
+        
+        #region Propiedades PDF
+
+        public string NumeroSecuencial { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Ciudad { get; set; }
+        public string Asunto { get; set; }
+        public string NombreCompleto { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NumeroDocumento { get; set; }
+        public string RazonSocial { get; set; }
+        public string Ruc { get; set; }
+        public string DescripcionEstado { get; set; }
+        public string FechaCreacionTexto { get; set; }
+        public string UrlFirma { get; set; }
+        public string Responsable { get; set; }
+        public string Cargo { get; set; }        
+        public decimal PromedioVentas { get; set; }    
+        public string Telefono { get; set; }
+
+        #endregion
     }
 }
