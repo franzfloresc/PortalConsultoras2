10d77dc114d7d56daea1643fe049b4ff8556643a
MIJAEL888 <mijael888@gmail.com>
2017-11-15 14:44:37 -0500

    Scripts creacion tablas

---
+++ app/Scripts/RD/14.BPT_Sprint14/Scripts/1.1.BPT_ConfiguracionPaisDatos.sql
@@ -0,0 +1,16 @@
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ConfiguracionPaisDatos]') AND type in (N'U'))
+	DROP TABLE [dbo].[ConfiguracionPaisDatos]
+GO
+
+CREATE TABLE [dbo].[ConfiguracionPaisDatos](
+	[ConfiguracionPaisID] [int] NOT NULL,
+	[Codigo] [varchar](50) NOT NULL,
+	[CampaniaID] [int] NOT NULL,
+	[Valor1] [varchar](800),
+	[Valor2] [varchar](800),
+	[Valor3] [varchar](800),
+	[Descripcion] [varchar](800),
+	[Estado] [bit] NOT NULL,
+)
+GO
