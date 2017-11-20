e0a47a43e0ae842ed8e9ff9f91c82ae8c0c13d5b
MIJAEL888 <mijael888@gmail.com>
2017-11-20 14:39:23 -0500

    EPM-453 Redireccion Ofertas.

--- app/Scripts/RD/14.BPT_Sprint14/Scripts/2.1.BPT_ConfiguracionPaisDatos.sql
+++ app/Scripts/RD/14.BPT_Sprint14/Scripts/2.1.BPT_ConfiguracionPaisDatos.sql
@@ -26,7 +26,8 @@ begin
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Descripcion)
 	values (@ConfiguracionPaisID, 1, 0, 'LogoComercialNoActiva', 'GanaMas.jpg', 'Logo comercial cuando el estado es no activa que se asignara en todo sb2')
 
-	-- Datos desktop Bienvenida 
+	-- RD Textos desktop Bienvenida 
+	BEGIN 
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
 	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaInscritaActiva', 
 	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
@@ -34,24 +35,26 @@ begin
 	'black', 'Textos de bienvenida para un consultora Inscrita activa')
 
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
-	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaInscritaNoActiva', '
-	#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaInscritaNoActiva', 
+	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
 	'ESTÁS INSCRITA EN ÉSIKA PARA MÍ, AHORA PUEDES PEDIR TUS OFERTAS DE CAMPAÑA #Cx Y VER LAS DE CAMPAÑA #Cx1', 
-	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	'black', 'Textos de bienvenida para un consultora Inscrita no activa')
 
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
 	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaNoInscritaActiva', 
 	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
 	'AHORA PUEDES COMPRAR TODAS TUS OFERTAS <br/>DE CAMPAÑA #Cx Y VER LAS DE CAMPAÑA #Cx1', 
-	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	'black', 'Textos de bienvenida para un consultora No Inscrita activa')
 
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
 	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaNoInscritaNoActiva', 
 	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
 	'ENTRA Y CONOCE TODO LO QUE TIENE PARA TI. <br /> HASTA 65% DE DESCUENTO EN OFERTAS EXCLUSIVAS.', 
-	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	'black', 'Textos de bienvenida para un consultora No Inscrita No activa')
+	END 
 
-	-- Datos mobile Bienvenida 
+	-- RD Textos mobile Bienvenida 
+	BEGIN 
 	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
 	values (@ConfiguracionPaisID, 1, 0, 'MBienvenidaInscritaActiva', 
 	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
@@ -75,6 +78,156 @@ begin
 	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
 	'#Nombre, ¿YA VISTE TUS <b>OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx</b>?', 
 	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	END
 
+	-- RD Textos desktop Pedido 
+	BEGIN
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DPedidoInscritaActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx</b>?', 
+	'black', 'Textos de Pedido para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DPedidoInscritaNoActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx1</b>?', 
+	'black', 'Textos de Pedido para un consultora Inscrita no activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DPedidoNoInscritaActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx</b>?', 
+	'black', 'Textos de Pedido para un consultora No Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DPedidoNoInscritaNoActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, CONOCE LAS OFERTAS CON HASTA 65% DE DESCUENTO', 
+	'black', 'Textos de Pedido para un consultora No Inscrita No activa')
+	END
+
+	-- RD Textos mobile Pedido (Completamente igual a Bienvenida mobile)
+	BEGIN 
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MPedidoInscritaActiva', 
+	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'AHORA PUEDES COMPRAR TODAS TUS OFERTAS <br/>DE CAMPAÑA #Cx Y VER LAS DE CAMPAÑA #Cx1', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MPedidoInscritaNoActiva', 
+	'LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, YA PUEDES VER TODAS TUS <b>OFERTAS PERSONALIZADAS DE LA #Cx1', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MPedidoNoInscritaActiva', 
+	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'AHORA PUEDES COMPRAR TODAS TUS OFERTAS <br/>DE CAMPAÑA #Cx Y VER LAS DE CAMPAÑA #Cx1', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MPedidoNoInscritaNoActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx</b>?', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	END
+
+	-- RD Textos desktop Catálogo 
+	BEGIN
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DCatalogoInscritaActiva', 
+	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'¿YA VISTE TUS OFERTAS DE LA C-#Cx EN ÉSIKA PARA MÍ? ', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DCatalogoInscritaNoActiva', 
+	'#Nombre, YA LLEGÓ TU NUEVA REVISTA ONLINE', 
+	'¿YA VISTE TUS <b>OFERTAS EN ÉSIKA PARA MÍ</b>?', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DCatalogoNoInscritaActiva', 
+	'#Nombre, LLEGÓ TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'¿YA VISTE TUS OFERTAS DE LA C-#Cx EN ÉSIKA PARA MÍ? ', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DCatalogoNoInscritaNoActiva', 
+	'#Nombre, YA LLEGÓ TU NUEVA REVISTA ONLINE', 
+	'¿YA VISTE TUS <b>OFERTAS EN ÉSIKA PARA MÍ</b>?', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	END
+
+	-- RD Textos mobile Catálogo 
+	BEGIN
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MCatalogoInscritaActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS DE LA C-#Cx EN ÉSIKA PARA MÍ?</b>', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MCatalogoInscritaNoActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS EN ÉSIKA PARA MÍ?</b>', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MCatalogoNoInscritaActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS DE LA C-#Cx EN ÉSIKA PARA MÍ?</b>', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MCatalogoNoInscritaNoActiva', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS <b>OFERTAS DE LA C-#Cx EN ÉSIKA PARA MÍ?</b>', 
+	'black', 'Textos de bienvenida para un consultora Inscrita activa')
+	END
+
+	-- RDR Textos desktop
+	BEGIN
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DBienvenidaRdr', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'ENCUENTRA MÁS OFERTAS, MÁS BONIFICACIONES Y LANZAMIENTOS DE LAS 3 MARCAS Y AUMENTA TUS GANANCIAS', 
+	'black', 'Textos de Bienvenida ara un consultora plan 20+')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DPedidoRdr', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'ENCUENTRA LOS PRODUCTOS QUE TUS CLIENTES BUSCAN HASTA 65% DE DSCTO.', 
+	'black', 'Textos de Pedido ara un consultora plan 20+')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'DCatalogoRdr', 
+	'TU REVISTA ONLINE PERSONALIZADA', 
+	'Conoce ahora tus ofertas y aumenta tus ganancias', 
+	'black', 'Textos de Catálogo ara un consultora plan 20+')
+	END
+
+	-- RDR Textos Mobile
+	BEGIN
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MBienvenidaRdr', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx?', 
+	'black', 'Textos de Bienvenida para un consultora plan 20+')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MPedidoRdr', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'#Nombre, ¿YA VISTE TUS OFERTAS PERSONALIZADAS DE CAMPAÑA #Cx?', 
+	'black', 'Textos de Pedido para un consultora plan 20+')
+
+	insert into ConfiguracionPaisDatos (ConfiguracionPaisID, Estado, CampaniaID, Codigo, Valor1, Valor2, Valor3, Descripcion)
+	values (@ConfiguracionPaisID, 1, 0, 'MCatalogoRdr', 
+	'TU NUEVA REVISTA ONLINE PERSONALIZADA', 
+	'Conoce ahora tus ofertas y aumenta tus ganancias', 
+	'black', 'Textos de Ctalogo para un consultora plan 20+')
+	END
 
 end
