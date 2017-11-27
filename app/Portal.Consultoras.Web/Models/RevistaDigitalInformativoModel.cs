ee4aefa73b178efa2b50e866eac922f5228727d2
Alan Manuel Aurora Pesantes <alan.aurora@zoluxiones.com>
2017-11-27 18:23:34 -0500

    Jira EPM-453 y EPM-454 => Nueva Funcionalidad de Pagina Informativa

---
+++ app/Portal.Consultoras.Web/Models/RevistaDigitalInformativoModel.cs
@@ -0,0 +1,15 @@
+﻿using Portal.Consultoras.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Portal.Consultoras.Web.Models
+{
+    [Serializable]
+    public class RevistaDigitalInformativoModel
+    {
+        public bool EsSuscrita { get; set; }
+        public int EstadoSuscripcion { get; set; }
+        public string Video { get; set; }
+        
+    }
+}
