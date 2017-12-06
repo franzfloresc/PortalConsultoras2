af60c827a274369115e4f868470f8859bcdf22fd
Alan Manuel Aurora Pesantes <alan.aurora@zoluxiones.com>
2017-12-06 14:46:42 -0500

    Script SQL => Limpiar imagenes de banner

--- app/Scripts/RD/14.BPT_Sprint14/Scripts/2.5.BPT_ConfiguracionPais.sql
+++ app/Scripts/RD/14.BPT_Sprint14/Scripts/2.5.BPT_ConfiguracionPais.sql
@@ -6,6 +6,10 @@ go
 update ConfiguracionPais
 set Estado = 1
 , Excluyente = 1
+, DesktopFondoBanner = ''
+, DesktopLogoBanner = ''
+, MobileFondoBanner = ''
+, MobileLogoBanner = ''
 where Codigo in ('RD', 'RDS', 'LAN', 'INICIORD')
 
 go
