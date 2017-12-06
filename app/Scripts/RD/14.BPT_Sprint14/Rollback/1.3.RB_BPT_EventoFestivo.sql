5bd592018512d16789c7a88249967f3d4b25b9ab
MIJAEL888 <mijael888@gmail.com>
2017-12-06 11:25:38 -0500

    Rollback Scripts

--- app/Scripts/RD/14.BPT_Sprint14/Rollback/1.3.RB_BPT_EventoFestivo.sql
+++ app/Scripts/RD/14.BPT_Sprint14/Rollback/1.3.RB_BPT_EventoFestivo.sql
@@ -1,4 +1,4 @@
-USE BelcorpPeru_BPT
+USE BelcorpPeru
 GO
 
 IF EXISTS(
@@ -14,3 +14,208 @@ BEGIN
 	ALTER TABLE EventoFestivo
 	ALTER COLUMN Nombre VARCHAR(30);
 END
+
+USE BelcorpMexico
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpColombia
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpVenezuela
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpSalvador
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpPuertoRico
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpPanama
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpGuatemala
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpEcuador
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpDominicana
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpCostaRica
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpChile
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
+USE BelcorpBolivia
+GO
+
+IF EXISTS(
+SELECT *
+FROM SYS.OBJECTS O 
+	JOIN SYS.COLUMNS C ON O.[OBJECT_ID] = C.[OBJECT_ID]
+	AND O.TYPE = 'U'
+	AND O.NAME = 'EventoFestivo'
+	AND C.NAME = 'Nombre'
+)
+BEGIN
+	PRINT ('Rollback Cambio tamaño de columna ''EventoFestivo.Nombre'' de 50 a 30 caracteres.')
+	ALTER TABLE EventoFestivo
+	ALTER COLUMN Nombre VARCHAR(30);
+END
+
