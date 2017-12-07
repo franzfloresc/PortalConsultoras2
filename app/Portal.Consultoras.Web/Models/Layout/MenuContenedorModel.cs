2bb1939e9596fe7b794e709c835f2039894f05ab
Victor Vilela <vilela_victor@hotmail.com>
2017-12-07 17:30:21 -0500

    Change the format of NroCampania at Tabs in ContenedorOfertas to completes with left zero when NroCampania  has one digit

--- app/Portal.Consultoras.Web/Models/Layout/MenuContenedorModel.cs
+++ app/Portal.Consultoras.Web/Models/Layout/MenuContenedorModel.cs
@@ -9,6 +9,20 @@ namespace Portal.Consultoras.Web.Models.Layout
         public int CampaniaId { get; set; }
         public int CampaniaX0 { get; set; }
         public int CampaniaX1 { get; set; }
+        public string CampaniaX0ConFormato
+        {
+            get
+            {
+                return (CampaniaX0 % 100).ToString("00");
+            }
+        }
+        public string CampaniaX1ConFormato
+        {
+            get
+            {
+                return (CampaniaX1 % 100).ToString("00");
+            }
+        }
         public ConfiguracionPaisModel ConfiguracionPais { get; set; }
     }
 }
