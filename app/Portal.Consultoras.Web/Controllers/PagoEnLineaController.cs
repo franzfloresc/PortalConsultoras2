d611ecf1efcff256df7c52c88471b5bb2dfc7af9
luisruidias <luis.ruidias@hundred.com.pe>
2017-12-07 09:41:25 -0500

    HD1295: Vista de pago linea.

---
+++ app/Portal.Consultoras.Web/Controllers/PagoEnLineaController.cs
@@ -0,0 +1,19 @@
+﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class PagoEnLineaController : BaseController
+    {
+        // GET: PagoEnLinea
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
