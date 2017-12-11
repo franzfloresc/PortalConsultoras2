59918c092f6efb689f61de9dace3885ee0c846ba
luisruidias <luis.ruidias@hundred.com.pe>
2017-12-11 09:50:15 -0500

    HD1295: obtener saldo de la consultora.

--- app/Portal.Consultoras.Web/Controllers/PagoEnLineaController.cs
+++ app/Portal.Consultoras.Web/Controllers/PagoEnLineaController.cs
@@ -13,7 +13,34 @@ namespace Portal.Consultoras.Web.Controllers
         // GET: PagoEnLinea
         public ActionResult Index()
         {
+            //ViewBag.CodigoConsultoraDL
+            //ViewBag.UsuarioNombre
+            ViewBag.SaldoActual = ObtenerSaldoActualConsultora();
             return View();
         }
+
+        public decimal ObtenerSaldoActualConsultora()
+        {
+            decimal Saldo = 0;
+            try
+            {
+                using (ServiceODS.ODSServiceClient sv = new ServiceODS.ODSServiceClient())
+                {
+                    Saldo = sv.GetSaldoActualConsultora(userData.PaisID, userData.CodigoConsultora);
+                }
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, userData.CodigoConsultora, userData.CodigoISO);
+            }
+            catch (Exception ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesBus(ex, userData.CodigoConsultora, userData.CodigoISO);
+            }
+            return Saldo;
+        }
+
+
+
     }
 }
