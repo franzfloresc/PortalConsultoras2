27679f5ed0389b79573a379447ed1037dfa8590f
Soporte <Soporte@ZOLUXLP088>
2017-12-19 11:27:29 -0500

    RD cofirmar datos

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/RevistaDigital/RevistaDigital-ConfirmarDatos.js
@@ -0,0 +1,30 @@
+﻿function RDConfirmarDatos() {
+    AbrirLoad();
+    $.ajax({
+        type: 'POST',
+        url: baseUrl + 'RevistaDigital/Desuscripcion',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            CerrarLoad();
+            if (!checkTimeout(data))
+                return false;
+
+            if (data.success != true) {
+                AbrirMensaje(data.message);
+                return false;
+            }
+
+            window.location.href = (isMobile() ? "/Mobile" : "") + "/Ofertas";
+        },
+        error: function (data, error) {
+            CerrarLoad();
+        }
+    });
+}
+
+function RedireccionarVista(Vista) {
+
+    vista = $.trim(Vista);
+    window.location = (isMobile() ? "/Mobile" : "") + "/" + vista;
+}
