f13dda5b913a04add5f74a3ff79c0e17c38adfa3
Giovanni <giovanni.benvenutto@zoluxionessac.onmicrosoft.com>
2017-12-28 16:27:08 -0500

    Vista Sin Pagos Pendientes, Vista Pagos Pendientes finalizada

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/PagoEnLinea/Index.js
@@ -0,0 +1,39 @@
+﻿var PedidoEnLinea;
+
+$(document).ready(function () {
+    'use strict';
+
+    var mainPL;
+
+    mainPL = function () {
+        var me = this;
+
+        me.Funciones = {
+            InicializarEventos: function () {
+                $(document).on('click', '.barra_activacion_edicion_monto_a_pagar', me.Eventos.ActivarOpcion);
+                $(document).on('click', '.barra_activacion.barra_activacion_terminos_condiciones', me.Eventos.ActivarOpcion);
+            }
+        },
+        me.Eventos = {
+            ActivarOpcion: function () {
+                $(this).toggleClass('activado');
+                if ($(this).is('.activado')) {
+                    $(this).attr('data-estado', 1);
+                    // $('.tooltip_terminos_y_condiciones').fadeOut();
+                    console.log('Se activó opción');
+                } else {
+                    $(this).attr('data-estado', 0);
+                    console.log('Se desactivó opción');
+                }
+            }
+        },
+        me.Inicializar = function () {
+            me.Funciones.InicializarEventos();
+        }
+    }
+
+    PedidoEnLinea = new mainPL();
+
+    PedidoEnLinea.Inicializar();
+
+});
