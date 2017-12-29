77290514d65faa6a58f19efd16b139ca2acefae7
Giovanni <giovanni.benvenutto@zoluxionessac.onmicrosoft.com>
2017-12-29 13:03:13 -0500

    Se crea vista para Pago En Linea Mobile y se migra estilos y estructura html y se deja como estaba antes la versión Desktop de Pago En Línea.

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/PagoEnLineaController.cs
@@ -0,0 +1,18 @@
+﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class PagoEnLineaController : BaseMobileController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
