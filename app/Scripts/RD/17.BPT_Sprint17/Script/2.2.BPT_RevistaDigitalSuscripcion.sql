ccaf252ece52d32c8853489c0cc4e83d2c7e4321
MIJAEL888 <mijael888@gmail.com>
2018-01-11 16:04:51 -0500

    Fix Scripts Sprint17

--- app/Scripts/RD/17.BPT_Sprint17/Script/2.2.BPT_RevistaDigitalSuscripcion.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE RevistaDigitalSuscripcion DROP CONSTRAINT COST_RevistaDigiralSuscripcion_Origen
-ALTER TABLE RevistaDigitalSuscripcion ADD CONSTRAINT COST_RevistaDigiralSuscripcion_Origen DEFAULT 'RD' FOR Origen;
-UPDATE RevistaDigitalSuscripcion SET Origen = 'RD' WHERE Origen is null or Origen = ''
