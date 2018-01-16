14e133b9e0517cd434a531c15b008056eb8c0239
Victor Vilela <vilela_victor@hotmail.com>
2018-01-16 12:31:23 -0500

    Add GuiaNegocio-Landing.js to desktop and mobile

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/GuiaNegocio/GuiaNegocio-Landing.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(main);
+
+var contador = 0;
+
+function main() {
+    $('.cerrar-vineta .sbcont span').click(function () {
+        // $('nav').toggle();
+
+        if (contador == 1) {
+            $('.cont-vineta').animate({
+                right: '0'
+            });
+            $('.cerrar-vineta').animate({
+                right: '150px'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('.cont-vineta').animate({
+                right: '-100%'
+            });
+            $('.cerrar-vineta').animate({
+                right: '0%'
+            });
+        }
+
+    });
+};
