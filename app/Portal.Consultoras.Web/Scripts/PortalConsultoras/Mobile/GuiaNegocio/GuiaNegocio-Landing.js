618ecf13cb969c8a2e07784c5edc0689e4a0eb51
Victor Vilela <vilela_victor@hotmail.com>
2018-01-17 12:32:44 -0500

    EPM-517 : Incluir sección para visualizar la revista física - Mobile

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/GuiaNegocio/GuiaNegocio-Landing.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/GuiaNegocio/GuiaNegocio-Landing.js
@@ -16,4 +16,41 @@
         }
 
     });
+
+    mostrarImagenPortadaRevista(campaniaId);
 });
+
+
+function mostrarImagenPortadaRevista(codigoCampania) {
+    var promise = getUrlImagenPortadaRevistaPromise(codigoCampania);
+    $.when(promise).done(function (promiseResult) {
+        if (checkTimeout(promiseResult)) {
+            var urlImagen = promiseResult || defaultImageRevista;
+            $("#imgPortadaRevista").attr("src", urlImagen);
+        }
+    });
+}
+
+function getUrlImagenPortadaRevistaPromise(codigoCampania) {
+
+    var defered = jQuery.Deferred();
+
+    var data = JSON.stringify({
+        codigoRevista: RevistaCodigoIssuu[codigoCampania]
+    });
+    jQuery.ajax({
+        type: 'POST',
+        url: urlObtenerPortadaRevista,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: data,
+        success: function (response) {
+            defered.resolve(response);
+        },
+        error: function () {
+            defered.resolve(defaultImageRevista);
+        }
+    });
+
+    return defered.promise();
+}
