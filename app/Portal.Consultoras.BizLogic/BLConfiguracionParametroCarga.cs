d1e4a5de0337ad7a86f4154ad7d578a8fab54425
Alan <Aurora>
2018-01-22 10:21:31 -0500

    Capa Logic - BLConfiguracionParametroCarga.cs => Mejoras de Codigo

--- app/Portal.Consultoras.BizLogic/BLConfiguracionParametroCarga.cs
+++ app/Portal.Consultoras.BizLogic/BLConfiguracionParametroCarga.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
+﻿using Portal.Consultoras.Data;
 using Portal.Consultoras.Entities;
-using Portal.Consultoras.Data;
+using System.Collections.Generic;
+using System.Data;
 using System.Transactions;
 
 namespace Portal.Consultoras.BizLogic
@@ -15,13 +11,12 @@ namespace Portal.Consultoras.BizLogic
         public IList<BEConfiguracionParametroCarga> GetCampaniasActivasConfiguracionParametroCarga(int paisID, int CampaniaID)
         {
             var lista = new List<BEConfiguracionParametroCarga>();
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
 
-            using (IDataReader reader = DAConfiguracionParametroCarga.GetCampaniasActivasConfiguracionParametroCarga(CampaniaID))
+            using (IDataReader reader = daConfiguracionParametroCarga.GetCampaniasActivasConfiguracionParametroCarga(CampaniaID))
                 while (reader.Read())
                 {
-                    var entidad = new BEConfiguracionParametroCarga(reader);
-                    entidad.PaisID = paisID;
+                    var entidad = new BEConfiguracionParametroCarga(reader) {PaisID = paisID};
                     lista.Add(entidad);
                 }
 
@@ -30,26 +25,27 @@ namespace Portal.Consultoras.BizLogic
 
         public void InsConfiguracionParametroCarga(BEConfiguracionValidacion ent, List<BEConfiguracionParametroCarga> lista)
         {
-            BEConfiguracionParametroCarga entidad;
-            TransactionOptions transactionOptions = new TransactionOptions();
-            transactionOptions.IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted;
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(ent.PaisID);
-            var DAConfiguracionValidacion = new DAConfiguracionValidacion(ent.PaisID);
+            TransactionOptions transactionOptions =
+                new TransactionOptions {IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted};
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(ent.PaisID);
+            var daConfiguracionValidacion = new DAConfiguracionValidacion(ent.PaisID);
 
             using (TransactionScope transaction = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
             {
-                DAConfiguracionParametroCarga.DelConfiguracionParametroCarga(ent.CampaniaID);
-                DAConfiguracionValidacion.Insert(ent);
+                daConfiguracionParametroCarga.DelConfiguracionParametroCarga(ent.CampaniaID);
+                daConfiguracionValidacion.Insert(ent);
 
                 foreach (var item in lista)
                 {
-                    entidad = new BEConfiguracionParametroCarga();
-                    entidad.PaisID = item.PaisID;
-                    entidad.CampaniaID = ent.CampaniaID;
-                    entidad.ZonaID = item.ZonaID;
-                    entidad.ValidacionActiva = item.ValidacionActiva;
-                    entidad.DiasParametroCarga = item.DiasParametroCarga;
-                    DAConfiguracionParametroCarga.InsConfiguracionParametroCarga(entidad);
+                    var entidad = new BEConfiguracionParametroCarga
+                    {
+                        PaisID = item.PaisID,
+                        CampaniaID = ent.CampaniaID,
+                        ZonaID = item.ZonaID,
+                        ValidacionActiva = item.ValidacionActiva,
+                        DiasParametroCarga = item.DiasParametroCarga
+                    };
+                    daConfiguracionParametroCarga.InsConfiguracionParametroCarga(entidad);
                 }
                 transaction.Complete();
             }
@@ -57,26 +53,27 @@ namespace Portal.Consultoras.BizLogic
 
         public void UpdConfiguracionParametroCarga(BEConfiguracionValidacion ent, List<BEConfiguracionParametroCarga> lista)
         {
-            BEConfiguracionParametroCarga entidad;
-            TransactionOptions transactionOptions = new TransactionOptions();
-            transactionOptions.IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted;
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(ent.PaisID);
-            var DAConfiguracionValidacion = new DAConfiguracionValidacion(ent.PaisID);
+            TransactionOptions transactionOptions =
+                new TransactionOptions {IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted};
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(ent.PaisID);
+            var daConfiguracionValidacion = new DAConfiguracionValidacion(ent.PaisID);
 
             using (TransactionScope transaction = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
             {
-                DAConfiguracionParametroCarga.DelConfiguracionParametroCarga(ent.CampaniaID);
-                DAConfiguracionValidacion.Update(ent);
+                daConfiguracionParametroCarga.DelConfiguracionParametroCarga(ent.CampaniaID);
+                daConfiguracionValidacion.Update(ent);
 
                 foreach (var item in lista)
                 {
-                    entidad = new BEConfiguracionParametroCarga();
-                    entidad.PaisID = item.PaisID;
-                    entidad.CampaniaID = ent.CampaniaID;
-                    entidad.ZonaID = item.ZonaID;
-                    entidad.ValidacionActiva = item.ValidacionActiva;
-                    entidad.DiasParametroCarga = item.DiasParametroCarga;
-                    DAConfiguracionParametroCarga.InsConfiguracionParametroCarga(entidad);
+                    var entidad = new BEConfiguracionParametroCarga
+                    {
+                        PaisID = item.PaisID,
+                        CampaniaID = ent.CampaniaID,
+                        ZonaID = item.ZonaID,
+                        ValidacionActiva = item.ValidacionActiva,
+                        DiasParametroCarga = item.DiasParametroCarga
+                    };
+                    daConfiguracionParametroCarga.InsConfiguracionParametroCarga(entidad);
                 }
                 transaction.Complete();
             }
@@ -84,16 +81,16 @@ namespace Portal.Consultoras.BizLogic
 
         public void DelConfiguracionParametroCarga(int paisID, int CampaniaID)
         {
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
-            DAConfiguracionParametroCarga.DelConfiguracionParametroCarga(CampaniaID);
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
+            daConfiguracionParametroCarga.DelConfiguracionParametroCarga(CampaniaID);
         }
 
         public IList<BEConfiguracionParametroCarga> GetRegionZonaDiasParametroCarga(int PaisID, int RegionID, int ZonaID)
         {
             var lista = new List<BEConfiguracionParametroCarga>();
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(PaisID);
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(PaisID);
 
-            using (IDataReader reader = DAConfiguracionParametroCarga.GetRegionZonaDiasParametroCarga(RegionID, ZonaID))
+            using (IDataReader reader = daConfiguracionParametroCarga.GetRegionZonaDiasParametroCarga(RegionID, ZonaID))
                 while (reader.Read())
                 {
                     var entidad = new BEConfiguracionParametroCarga(reader);
@@ -105,15 +102,15 @@ namespace Portal.Consultoras.BizLogic
 
         public void UpdConfiguracionParametroCargaPedido(int PaisID, List<BEConfiguracionParametroCarga> listaEntidades)
         {
-            TransactionOptions transactionOptions = new TransactionOptions();
-            transactionOptions.IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted;
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(PaisID);
+            TransactionOptions transactionOptions =
+                new TransactionOptions {IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted};
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(PaisID);
 
             using (TransactionScope transaction = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
             {
                 foreach (var entidad in listaEntidades)
                 {
-                    DAConfiguracionParametroCarga.UpdConfiguracionParametroCargaPedido(entidad);
+                    daConfiguracionParametroCarga.UpdConfiguracionParametroCargaPedido(entidad);
                 }
                 transaction.Complete();
             }
@@ -122,14 +119,13 @@ namespace Portal.Consultoras.BizLogic
         public BEConfiguracionParametroCarga GetConfiguracionParametroCarga(int paisID, int campaniaID, int zonaID)
         {
             BEConfiguracionParametroCarga entidad = null;
-            var DAConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
+            var daConfiguracionParametroCarga = new DAConfiguracionParametroCarga(paisID);
 
-            using (IDataReader reader = DAConfiguracionParametroCarga.GetConfiguracionParametroCarga(campaniaID, zonaID))
+            using (IDataReader reader = daConfiguracionParametroCarga.GetConfiguracionParametroCarga(campaniaID, zonaID))
             {
                 if (reader.Read())
                 {
-                    entidad = new BEConfiguracionParametroCarga(reader);
-                    entidad.PaisID = paisID;
+                    entidad = new BEConfiguracionParametroCarga(reader) {PaisID = paisID};
                 }
             }
             return entidad;
