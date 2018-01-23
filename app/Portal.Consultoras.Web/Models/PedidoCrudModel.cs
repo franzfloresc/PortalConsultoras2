f80a6c01cd347297dc751a732f49e82f7e089542
Alan <Aurora>
2018-01-23 18:19:52 -0500

    Jira EPM-552 => Cargar mismo CUV en varios tipos de estrategia

---
+++ app/Portal.Consultoras.Web/Models/PedidoCrudModel.cs
@@ -0,0 +1,34 @@
+﻿using Portal.Consultoras.Common;
+
+namespace Portal.Consultoras.Web.Models
+{
+    public class PedidoCrudModel
+    {
+        public string ClienteID { set; get; }
+
+        public int TipoOfertaSisID { get; set; }
+
+        public int ConfiguracionOfertaID { get; set; }
+
+        public bool OfertaWeb { get; set; }
+
+        public string IndicadorMontoMinimo { get; set; }
+
+        public bool EsSugerido { get; set; }
+
+        public bool EsKitNueva { get; set; }
+
+        public int MarcaID { get; set; }
+
+        public string Cantidad { get; set; }
+
+        public decimal PrecioUnidad { get; set; }
+
+        public string CUV { get; set; }
+
+        public int OrigenPedidoWeb { get; set; }
+
+        public string DescripcionProd { get; set; }
+
+    }
+}
