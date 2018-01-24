4d9da7c3cd3595efecab140ce53ef2ba412e2376
Giovanni <giovanni.benvenutto@zoluxionessac.onmicrosoft.com>
2018-01-24 17:13:41 -0500

    Vistas Descarga La App (Ésika Conmigo y L'bel Conmigo)

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/DescargarAppController.cs
@@ -0,0 +1,17 @@
+﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Areas.Mobile.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class DescargarAppController : BaseMobileController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.EsPaisEsika = System.Configuration.ConfigurationManager.AppSettings.Get("PaisesEsika").Contains(userData.CodigoISO) ? "1" : "0";
+            return View();
+        }
+
+    }
+}
