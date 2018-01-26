29b1551503275d289f8456d6e363d111820befa2
Alan <Aurora>
2018-01-26 15:53:51 -0500

    Jira EPM-570 => Catalogo - Sin RD

--- app/Portal.Consultoras.Web/Controllers/MisCatalogosRevistasController.cs
+++ app/Portal.Consultoras.Web/Controllers/MisCatalogosRevistasController.cs
@@ -27,7 +27,7 @@ namespace Portal.Consultoras.Web.Controllers
                 CampaniaAnterior = AddCampaniaAndNumero(userData.CampaniaID, -1).ToString(),
                 CampaniaSiguiente = AddCampaniaAndNumero(userData.CampaniaID, 1).ToString(),
                 TieneSeccionRD = revistaDigital.TieneRDC && (!userData.TieneGND || (userData.TieneGND && revistaDigital.EsActiva)),
-                TieneSeccionRevista = revistaDigital.TieneRDC && !revistaDigital.EsActiva,
+                TieneSeccionRevista = !revistaDigital.TieneRDC || (revistaDigital.TieneRDC && !revistaDigital.EsActiva),
                 TieneGND = userData.TieneGND
             };
 
