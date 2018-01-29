b278c054beb52a17fbdaf2d0968e0e6035b6a782
Alan <Aurora>
2018-01-29 10:37:15 -0500

    BPT Sprint 17 - Script SQL Pase

--- app/Scripts/RD/17.BPT_Sprint17/Script/2.1.BPT_TipoEstrategia.sql
+++ app/Scripts/RD/17.BPT_Sprint17/Script/2.1.BPT_TipoEstrategia.sql
@@ -3,10 +3,10 @@ GO
 
 update TipoEstrategia set NombreComercial = DescripcionEstrategia
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '001'
-update TipoEstrategia set NombreComercial = 'LO NUEVO ¡NUEVO! DE GANA+' WHERE Codigo = '005'
+update TipoEstrategia set NombreComercial = 'LO NUEVO Â¡NUEVO! DE GANA+' WHERE Codigo = '005'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '007'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '008'
-update TipoEstrategia set NombreComercial = 'OFERTA ¡SOLO HOY! GANA+' WHERE Codigo = '009'
+update TipoEstrategia set NombreComercial = 'OFERTA Â¡SOLO HOY! GANA+' WHERE Codigo = '009'
 update TipoEstrategia set NombreComercial = '' WHERE Codigo = '010'
 GO
 
@@ -15,10 +15,10 @@ GO
 
 update TipoEstrategia set NombreComercial = DescripcionEstrategia
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '001'
-update TipoEstrategia set NombreComercial = 'LO NUEVO ¡NUEVO! DE GANA+' WHERE Codigo = '005'
+update TipoEstrategia set NombreComercial = 'LO NUEVO Â¡NUEVO! DE GANA+' WHERE Codigo = '005'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '007'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '008'
-update TipoEstrategia set NombreComercial = 'OFERTA ¡SOLO HOY! GANA+' WHERE Codigo = '009'
+update TipoEstrategia set NombreComercial = 'OFERTA Â¡SOLO HOY! GANA+' WHERE Codigo = '009'
 update TipoEstrategia set NombreComercial = '' WHERE Codigo = '010'
 GO
 
@@ -27,10 +27,10 @@ GO
 
 update TipoEstrategia set NombreComercial = DescripcionEstrategia
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '001'
-update TipoEstrategia set NombreComercial = 'LO NUEVO ¡NUEVO! DE GANA+' WHERE Codigo = '005'
+update TipoEstrategia set NombreComercial = 'LO NUEVO Â¡NUEVO! DE GANA+' WHERE Codigo = '005'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '007'
 update TipoEstrategia set NombreComercial = 'OFERTAS PARA TI GANA+' WHERE Codigo = '008'
-update TipoEstrategia set NombreComercial = 'OFERTA ¡SOLO HOY! GANA+' WHERE Codigo = '009'
+update TipoEstrategia set NombreComercial = 'OFERTA Â¡SOLO HOY! GANA+' WHERE Codigo = '009'
 update TipoEstrategia set NombreComercial = '' WHERE Codigo = '010'
 GO
 USE BelcorpMexico
