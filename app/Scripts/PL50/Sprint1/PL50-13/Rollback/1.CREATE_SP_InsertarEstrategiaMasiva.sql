e54f41794e50afaf060aef8ff4ad14f1080daa50
jhonattanalarcon <jhonattan_alarcon@hotmail.com>
2018-02-07 18:41:20 -0500

    Agregar paises a Scripts de BD

--- app/Scripts/PL50/Sprint1/PL50-13/Rollback/1.CREATE_SP_InsertarEstrategiaMasiva.sql
+++ app/Scripts/PL50/Sprint1/PL50-13/Rollback/1.CREATE_SP_InsertarEstrategiaMasiva.sql
@@ -1,3 +1,132 @@
-USE BelcorpPeru_PL50
+﻿USE BelcorpPeru 
 GO
-DROP PROCEDURE dbo.InsertarEstrategiaMasiva
+ 
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpBolivia 
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpChile
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpColombia
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpCostaRica 
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpDominicana 
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpEcuador
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpGuatemala 
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpMexico
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpPanama
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpPuertoRico
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpSalvador
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+USE BelcorpVenezuela
+GO
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures  WHERE Name = 'InsertarEstrategiaMasiva')
+BEGIN 
+  DROP PROCEDURE dbo.InsertarEstrategiaMasiva 
+END
+
+go
+
+ 
--- app/Scripts/PL50/Sprint1/PL50-64/Scripts/1. CREATE_SP_EliminarEstrategia.sql
+++ app/Scripts/PL50/Sprint1/PL50-64/Scripts/1. CREATE_SP_EliminarEstrategia.sql
@@ -1,5 +1,17 @@
-USE BelcorpPeru_PL50
+﻿USE BelcorpPeru
 GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
 CREATE PROCEDURE dbo.EliminarEstrategia  
  @EstrategiaID INT
 AS  
@@ -9,3 +21,329 @@ BEGIN
   DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
  SET NOCOUNT OFF  
 END  
+
+GO
+
+USE BelcorpBolivia
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpChile
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpColombia
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpCostaRica
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpDominicana
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpEcuador
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpGuatemala
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpMexico
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpPanama
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpPuertoRico
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpSalvador
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
+
+USE BelcorpVenezuela
+GO
+
+-- check exists SP
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'EliminarEstrategia')
+BEGIN
+    PRINT 'Stored Procedure Exists'
+  
+  DROP PROCEDURE dbo.EliminarEstrategia 
+END
+GO
+
+
+CREATE PROCEDURE dbo.EliminarEstrategia  
+ @EstrategiaID INT
+AS  
+BEGIN  
+ SET NOCOUNT ON  
+  DELETE FROM dbo.EstrategiaProducto WHERE EstrategiaID = @EstrategiaID
+  DELETE FROM dbo.Estrategia WHERE EstrategiaID = @EstrategiaID
+ SET NOCOUNT OFF  
+END  
+
+GO
+
