4ec2115dd2af7ee92a4e17bc7185b2c2fea0f12a
german <german1311@outlook.com>
2018-02-14 18:14:48 -0500

    Mapeo de entidades

---
+++ app/Portal.Consultoras.Data/Estrategia/UpSelling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Consultoras.Data.Estrategia
+{
+    public class UpSelling : DataAccess
+    {
+        public UpSelling(int paisId) : base(paisId, EDbSource.Portal)
+        {}
+
+
+    }
+}
---
+++ app/Portal.Consultoras.Entities/Estrategia/UpSellingEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Consultoras.Entities.Estrategia
+{
+    public class UpSellingEntity
+    {
+        public int UpSellingId { get; set; }
+        public int CodigoCampana { get; set; }
+
+        public decimal MontoMeta { get; set; }
+
+        [Column("TextoMeta1")]
+        public string TextoMeta { get; set; }
+
+        [Column("TextoMeta2")]
+        public string TextoMetaSecundario { get; set; }
+
+        [Column("TextoGanaste1")]
+        public string TextoGanaste { get; set; }
+
+        [Column("TextoGanaste2")]
+        public string TextoGanasteSecundario { get; set; }
+
+
+    }
+}
