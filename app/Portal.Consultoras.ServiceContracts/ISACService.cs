c6978c2c2539cf1c5033305c6efb68b355f847fa
german <german1311@outlook.com>
2018-02-19 15:53:51 -0500

    Merge branch 'PL50/PL50-117_Upselling_adm' into PL50/PL50-127_upselling_admin_crud

--- app/Portal.Consultoras.ServiceContracts/ISACService.cs
+++ app/Portal.Consultoras.ServiceContracts/ISACService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.ServiceModel;
+using Portal.Consultoras.Entities.Estrategia;
 
 namespace Portal.Consultoras.ServiceContracts
 {
@@ -427,7 +428,7 @@ namespace Portal.Consultoras.ServiceContracts
 
         [OperationContract]
         void InsertarComunicadoVisualizado(int PaisID, string CodigoConsultora, int ComunicadoID);
-        
+
         [OperationContract]
         void InsertarDonacionConsultora(int PaisId, string CodigoISO, string CodigoConsultora, string Campania, string IPUsuario);
         #endregion
@@ -573,7 +574,7 @@ namespace Portal.Consultoras.ServiceContracts
 
         [OperationContract]
         void CancelarSolicitudCliente(int paisID, long solicitudId, int opcionCancelacion, string razonMotivoCancelacion);
-        
+
         [OperationContract]
         void CancelarSolicitudClienteYRemoverPedido(int paisID, int campaniaID, long consultoraID, string codigoUsuario, long solicitudId, int opcionCancelacion, string razonMotivoCancelacion);
 
@@ -637,7 +638,7 @@ namespace Portal.Consultoras.ServiceContracts
 
         [OperationContract]
         List<BEProveedorDespachoCobranza> GetProveedorDespachoCobranzaBYiD(int paisID, BEProveedorDespachoCobranza entity);
-        
+
         [OperationContract]
         bool EnviarProactivaChatbot(string paisISO, string urlRelativa, List<BEChatbotProactivaMensaje> listMensajeProactiva);
 
@@ -649,7 +650,7 @@ namespace Portal.Consultoras.ServiceContracts
 
         [OperationContract]
         string GetCampaniaActualAndSiguientePais(int paisID, string codigoIso);
-        
+
         [OperationContract]
         IList<BEApp> ListarApps(int paisID);
 
@@ -680,11 +681,25 @@ namespace Portal.Consultoras.ServiceContracts
 
         #region Estrategia
         [OperationContract]
-        List<BEDescripcionEstrategia> ActualizarDescripcionEstrategia(int paisId, int campaniaId, 
+        List<BEDescripcionEstrategia> ActualizarDescripcionEstrategia(int paisId, int campaniaId,
                 int tipoEstrategiaId, List<BEDescripcionEstrategia> listaDescripcionEstrategias);
 
         [OperationContract]
         int ActualizarTonoEstrategia(int paisId, int estrategiaId, string codigoEstrategia, int tieneVariedad);
+        #endregion
+
+        #region UpSelling
+        [OperationContract]
+        IEnumerable<UpSelling> UpSellingObtener(int paisId, string codigoCampana, bool incluirDetalle = false);
+
+        [OperationContract]
+        UpSelling UpSellingInsertarActualizar(int paisId, UpSelling upSelling);
+
+        [OperationContract]
+        void UpSellingEliminar(int paisId, int upSellingId);
+
+        [OperationContract]
+        UpSellingDetalle UpSellingObtenerDetalle(int paisId, int upSellingDetalleId);
         #endregion
     }
 }
