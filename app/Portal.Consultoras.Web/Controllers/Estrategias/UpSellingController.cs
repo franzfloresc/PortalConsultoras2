5cc4e3a70121d1bd13fac60665c4d0fe43c344f6
german <german1311@outlook.com>
2018-02-19 18:21:25 -0500

    Avances en grilla

--- app/Portal.Consultoras.Web/Controllers/Estrategias/UpSellingController.cs
+++ app/Portal.Consultoras.Web/Controllers/Estrategias/UpSellingController.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Portal.Consultoras.Web.Infraestructure;
+using Portal.Consultoras.Web.Models.Estrategia;
+using Portal.Consultoras.Web.Providers;
 
 namespace Portal.Consultoras.Web.Controllers.Estrategias
 {
-    public class UpSellingController
+    public class UpSellingController : BaseController
     {
-        
+        private readonly ZonificacionProvider _zonificacionProvider;
+
+        public UpSellingController()
+        {
+            _zonificacionProvider = new ZonificacionProvider();
+        }
+
+        public ActionResult Index()
+        {
+            var campanas = _zonificacionProvider.ObtenerCampanias(userData.PaisID);
+            var model = new UpSellingAdminModel()
+            {
+                PaisIso = userData.CodigoISO,
+                Campanas = campanas,
+                PaisNombre = userData.NombrePais,
+                EsPaisEsika = Settings.Instance.PaisesEsika.Contains(userData.CodigoISO)
+            };
+
+            return View(model);
+        }
+
+        public JsonResult Obtener(string codigoCampana)
+        {
+            return Json(new { a = 'b' });
+        }
     }
 }
---
+++ app/Portal.Consultoras.Web/Providers/ZonificacionProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServiceZonificacion;
+
+namespace Portal.Consultoras.Web.Providers
+{
+    public class ZonificacionProvider
+    {
+        public IEnumerable<CampaniaModel> ObtenerCampanias(int paisId)
+        {
+            IList<BECampania> campanias;
+            using (var sv = new ZonificacionServiceClient())
+            {
+                campanias = sv.SelectCampanias(paisId);
+            }
+
+            return Mapper.Map<IList<BECampania>, IEnumerable<CampaniaModel>>(campanias);
+        }
+    }
+}
