9799bb7f7b6c8aabb1dc93bc62ff422657673f0e
Victor Vilela <vilela_victor@hotmail.com>
2018-02-20 11:12:36 -0500

    update scripts configuracionPaisDatos

--- app/Scripts/RD/19.BPT_Sprint19/HV/RollBack/2.1.RB_BPT_ConfiguracionPaisDatos.sql
+++ app/Scripts/RD/19.BPT_Sprint19/HV/RollBack/2.1.RB_BPT_ConfiguracionPaisDatos.sql
@@ -1,6 +1,8 @@
 USE BelcorpPeru
 GO
 
+print DB_NAME()
+
 declare @HerramientasVentaId int = 0
 declare @HerramientasVentaCodigo varchar(30) = 'HV'
 
@@ -10,18 +12,509 @@ from ConfiguracionPais cp
 where cp.Codigo = @HerramientasVentaCodigo;
 
 
-IF EXISTS(SELECT 1 FROM ConfiguracionPaisDatos CPD
-	WHERE ConfiguracionPaisID = @HerramientasVentaId AND  Codigo = 'MPopupBloqueado'
-	)
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
 BEGIN
-	DELETE FROM ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 18 AND  Codigo = 'MPopupBloqueado'
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
 END
 GO
 
-IF EXISTS(SELECT 1 FROM ConfiguracionPaisDatos CPD
-	WHERE ConfiguracionPaisID = 18 AND  Codigo = 'DPopupBloqueado'
-	)
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
 BEGIN
-	DELETE FROM ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 18 AND  Codigo = 'DPopupBloqueado'
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
 END
 GO
+
+USE BelcorpMexico
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpColombia
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpVenezuela
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpSalvador
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpPanama
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpGuatemala
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpEcuador
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpDominicana
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpCostaRica
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpChile
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
+USE BelcorpBolivia
+GO
+
+print DB_NAME()
+
+declare @HerramientasVentaId int = 0
+declare @HerramientasVentaCodigo varchar(30) = 'HV'
+
+select 
+@HerramientasVentaId = cp.ConfiguracionPaisID
+from ConfiguracionPais cp
+where cp.Codigo = @HerramientasVentaCodigo;
+
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'MPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando MPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'MPopupBloqueado'
+END
+GO
+
+IF EXISTS(	SELECT 1 
+			FROM ConfiguracionPaisDatos CPD
+			WHERE ConfiguracionPaisID = @HerramientasVentaId 
+			AND  Codigo = 'DPopupBloqueado')
+BEGIN
+	PRINT 'Eliminando DPopupBloqueado de ConfiguracionPaisDatos'
+	DELETE 
+	FROM ConfiguracionPaisDatos 
+	WHERE ConfiguracionPaisID = @HerramientasVentaId 
+	AND  Codigo = 'DPopupBloqueado'
+END
+GO
+
