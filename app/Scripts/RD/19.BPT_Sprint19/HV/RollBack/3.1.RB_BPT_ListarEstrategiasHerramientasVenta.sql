5a41078d681ad7032e99b019a0bbe062688252dc
Victor Vilela <vilela_victor@hotmail.com>
2018-02-20 15:11:15 -0500

    update scripts bd  for  sp ListarEstrategiasHerramientasVenta

--- app/Scripts/RD/19.BPT_Sprint19/HV/RollBack/3.1.RB_BPT_ListarEstrategiasHerramientasVenta.sql
+++ app/Scripts/RD/19.BPT_Sprint19/HV/RollBack/3.1.RB_BPT_ListarEstrategiasHerramientasVenta.sql
@@ -1,12 +1,182 @@
-USE [BelcorpPeru_BPT]
+USE BelcorpPeru
 GO
 
-GO
+PRINT DB_NAME()
 IF EXISTS (
-	SELECT * FROM sys.objects
-	WHERE object_id =
-	OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
 	AND Type in (N'P',N'PC')
 )
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
 	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
 GO
+
+USE BelcorpMexico
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpColombia
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpVenezuela
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpSalvador
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpPuertoRico
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpPanama
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpGuatemala
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpEcuador
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpDominicana
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpCostaRica
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpChile
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
+USE BelcorpBolivia
+GO
+
+PRINT DB_NAME()
+IF EXISTS (
+	SELECT 1
+	FROM sys.objects
+	WHERE object_id = OBJECT_ID(N'[dbo].[ListarEstrategiasHerramientasVenta]')
+	AND Type in (N'P',N'PC')
+)
+	PRINT 'Eliminando procedure : ListarEstrategiasHerramientasVenta'
+	DROP PROCEDURE [dbo].[ListarEstrategiasHerramientasVenta]
+GO
+
