6d8029c757db767eca0f10690801e0c0a17d4bf6
german <german1311@outlook.com>
2018-02-21 18:53:54 -0500

    Displaying correctly, left saving data

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Estrategia/UpSelling/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Estrategia/UpSelling/Index.js
@@ -20,22 +20,31 @@ belcorp.estrategias.upselling.initialize = function (config) {
         cmbCampanas: config.cmbCampanas,
         idTabs: config.idTabs,
         idTargetDiv: config.idTargetDiv,
-        campanasPais: config.campanasPais
+        campanasPais: config.campanasPais,
+        idDivPopUpRegalo: config.idDivPopUpRegalo
     };
 
     registerEvent.call(this, "onUpSellingEdit");
     registerEvent.call(this, "onUpSellingDelete");
     registerEvent.call(this, "onUpSellingDesactivar");
 
+    registerEvent.call(this, "onUpSellingRegaloEdit");
+    registerEvent.call(this, "onUpSellingRegaloDelete");
+    registerEvent.call(this, "onUpSellingRegaloDesactivar");
+
     var self = this;
     function initBindings() {
         $("#" + settings.buttonBuscarId).on("click", document.body, buscar);
         $("#" + settings.buttonNuevoId).on("click", document.body, self.upSellingViewModel.nuevo);
 
-        
+
         self.subscribe("onUpSellingEdit", editar);
         self.subscribe("onUpSellingDelete", eliminar);
         self.subscribe("onUpSellingDesactivar", desactivar);
+
+        self.subscribe("onUpSellingRegaloEdit", self.upSellingViewModel.regaloEditar);
+        self.subscribe("onUpSellingRegaloDelete", self.upSellingViewModel.regaloDelete);
+        self.subscribe("onUpSellingRegaloDesactivar", self.upSellingViewModel.regaloDesactivar);
 
         cargarGrilla();
     }
@@ -86,15 +95,81 @@ belcorp.estrategias.upselling.initialize = function (config) {
         jQuery("#" + settings.idGrilla).jqGrid("navGrid", "#" + settings.idPager, { edit: false, add: false, refresh: false, del: false, search: false });
     }
 
+    function cargarGrillaRegalos(data) {
+        var grilla = $("#" + settings.idGrillaRegalos);
+        grilla.jqGrid("GridUnload");
+
+        grilla.jqGrid({
+            hidegrid: false,
+            datatype: "local",
+            postData: ({
+                codigoCampana: settings.cmbCampanas.val()
+            }),
+            mtype: "GET",
+            contentType: "application/json; charset=utf-8",
+            multiselect: false,
+            colNames: ["#", "Nombre", "CUV", "Stock", "StockActual", "Orden", "Activo", "Opciones"],
+            colModel: [
+                { name: "UpSellingRegaloId", index: "UpSellingRegaloId", width: 50, sortable: false, align: "center" },
+                { name: "Nombre", index: "Nombre", width: 80, sortable: false, align: "center" },
+                { name: "CUV", index: "CUV", width: 80, sortable: false, align: "center" },
+                { name: "Stock", index: "Stock", width: 80, sortable: false, align: "center" },
+                { name: "StockActual", index: "StockActual", width: 100, sortable: false, align: "center" },
+                { name: "Orden", index: "Orden", width: 100, sortable: false, align: "center" },
+                { name: "Activo", index: "Activo", width: 100, sortable: false, align: "center" },
+                { name: "Options", width: 60, editable: true, sortable: false, align: "center", resizable: false, formatter: optionButtonsRegalos },
+            ],
+            //jsonReader:
+            //{
+            //    root: "Data",
+            //    repeatitems: false,
+            //    id: "UpSellingId"
+            //},
+            pager: jQuery("#" + settings.idPager),
+            loadtext: "Cargando datos...",
+            recordtext: "{0} - {1} de {2} Registros",
+            emptyrecords: "No hay resultados",
+            rowNum: 10,
+            scrollOffset: 0,
+            rowList: [10, 20, 30, 40, 50],
+            sortname: "UpSellingId",
+            sortorder: "asc",
+            viewrecords: true,
+            height: "auto",
+            width: 930,
+            pgtext: "Pág: {0} de {1}",
+            altRows: false
+        });
+
+        grilla.jqGrid("navGrid", "#" + settings.idPager, { edit: false, add: false, refresh: false, del: false, search: false });
+
+        for (var i = 0; i <= data.length; i++)
+            grilla.jqGrid('addRowData', i + 1, data[i]);
+    }
+
     function buscar(e, s) {
         e.preventDefault();
         cargarGrilla();
     }
-    
+
     function optionButtons(cellvalue, options, rowObject) {
         var edit = "&nbsp;<a href='javascript:;' onclick=\"belcorp.estrategias.upselling.applyChanges('onUpSellingEdit', " + options.rowId + "); return false;\">" + "<img src='" + settings.rutaImagenEdit + "' alt='Editar UpSelling' title='Editar UpSelling' border='0' /></a>";
         var del = "&nbsp;<a href='javascript:;' onclick=\"return belcorp.estrategias.upselling.onUpSellingDelete.emit(" + options.rowId + "); return false;\"><img src='" + settings.rutaImagenDesactivar + "' alt='Deshabilitar UpSelling' title='Deshabilitar UpSelling' border='0' /></a>";
         var desactivar = "&nbsp;<a href='javascript:;' onclick=\"return belcorp.estrategias.upselling.applyChanges('onUpSellingDesactivar', " + options.rowId + "); return false;\" > " + "<img src='" + settings.rutaImagenDelete + "' alt='Eliminar UpSelling' title='Eliminar UpSelling' border='0' /></a>";
+
+        var resultado = edit;
+        resultado += del;
+
+        if (rowObject.Activo !== true)
+            resultado += desactivar;
+
+        return resultado;
+    }
+
+    function optionButtonsRegalos(cellvalue, options, rowObject) {
+        var edit = "&nbsp;<a href='javascript:;' onclick=\"belcorp.estrategias.upselling.applyChanges('onUpSellingRegaloEdit', " + options.rowId + "); return false;\">" + "<img src='" + settings.rutaImagenEdit + "' alt='Editar Regalo' title='Editar Regalo' border='0' /></a>";
+        var del = "&nbsp;<a href='javascript:;' onclick=\"return belcorp.estrategias.upselling.applyChanges('onUpSellingRegaloDesactivar'," + options.rowId + "); return false;\"><img src='" + settings.rutaImagenDesactivar + "' alt='Deshabilitar Regalo' title='Deshabilitar Regalo' border='0' /></a>";
+        var desactivar = "&nbsp;<a href='javascript:;' onclick=\"return belcorp.estrategias.upselling.applyChanges('onUpSellingRegaloDelete', " + options.rowId + "); return false;\" > " + "<img src='" + settings.rutaImagenDelete + "' alt='Eliminar Regalo' title='Eliminar Regalo' border='0' /></a>";
 
         var resultado = edit;
         resultado += del;
@@ -111,7 +186,6 @@ belcorp.estrategias.upselling.initialize = function (config) {
 
     function editar(upSellingId) {
         var rowData = getRowData(settings.idGrilla, upSellingId);
-
         self.upSellingViewModel.edit(rowData);
     }
 
@@ -167,6 +241,21 @@ belcorp.estrategias.upselling.initialize = function (config) {
         console.error(err);
     }
 
+    function showDialog(divId) {
+        var opt = {
+            autoOpen: false,
+            modal: true,
+            width: 950,
+            height: 650,
+            title: 'Details'
+        };
+
+
+        $("#" + divId).dialog(opt).dialog("open");
+        $("#ui-datepicker-div").css("z-index", "9999");
+        return false;
+    }
+
     function upSellingRegalosObtenerPromise(upsellingId) {
         return jQuery.ajax({
             type: "GET",
@@ -205,7 +294,7 @@ belcorp.estrategias.upselling.initialize = function (config) {
     function getRowData(idGrid, idRow) {
         return $("#" + idGrid).jqGrid('getRowData', idRow);
     }
-    
+
     function UpSellingModel(data) {
         this.UpSellingId = ko.observable(data.UpSellingId);
         this.CodigoCampana = ko.observable(data.CodigoCampana);
@@ -228,18 +317,23 @@ belcorp.estrategias.upselling.initialize = function (config) {
         this.Descripcion = ko.observable(data.Descripcion);
         this.Imagen = ko.observable(data.Imagen);
         this.Stock = ko.observable(data.Stock);
+        this.StockActual = ko.observable(data.StockActual);
+        this.Orden = ko.observable(data.Orden);
+        this.Activo = ko.observable(data.Activo);
     }
 
     function UpSellingViewModel() {
         var selfvm = this;
 
         selfvm.upSelling = ko.observable({});
-        selfvm.campanasPais = settings.campanasPais.map(function(campana) {
-            return new { Id: campana.CampaniaID, Codigo: campana.Codigo };
+        selfvm.campanasPais = settings.campanasPais.map(function (campana) {
+            return { Id: campana.CampaniaID, Codigo: campana.Codigo };
         });
-        selfvm.mostrarFormulario = ko.observable(false);
-        selfvm.nuevo = function () {
 
+        selfvm.mostrarFormulario = ko.observable(false);
+
+        selfvm.nuevo = function () {
+            //todo:
         }
 
         selfvm.edit = function (upSellingRow) {
@@ -249,14 +343,15 @@ belcorp.estrategias.upselling.initialize = function (config) {
             waitingDialog({});
             upSellingRegalosObtenerPromise(upSellingRow.UpSellingId)
                 .then(function (result) {
-                    if (!result.Success) {
+                    if (!result.Success)
                         fail(result.Message);
-                    }
 
                     upSellingRow.Regalos = result.Data;
                     selfvm.upSelling = new UpSellingModel(upSellingRow);
                     selfvm.mostrarFormulario(true);
                     enableTabs(settings.idTabs);
+                    //todo: eval length
+                    cargarGrillaRegalos(ko.toJS(selfvm.upSelling.Regalos));
                 }, fail)
                 .done(function () {
                     closeWaitingDialog();
@@ -267,13 +362,28 @@ belcorp.estrategias.upselling.initialize = function (config) {
             //ajax
         }
 
-        selfvm.agregarRegalo = function () {
+        selfvm.regalo = ko.observable();
+        selfvm.mostrarFormularioRegalo = ko.observable(false);
+        selfvm.regaloAgregar = function () {
             //show popUp
+            showDialog("DialogDatosShowRoom");
         }
 
-        selfvm.cargarGrillaRegalos = function(data) {
+        selfvm.regaloEditar = function (regaloId) {
+            showDialog(settings.idDivPopUpRegalo);
+            var regalos = selfvm.upSelling.Regalos().filter(function (regalo) {
+                return regalo.UpSellingRegaloId() === regaloId;
+            });
+            if (!!regalos && regalos.length > 0) {
+                selfvm.regalo = regalos[0];
+                selfvm.mostrarFormularioRegalo(true);
+            }
 
+            closeWaitingDialog();
         }
+
+        selfvm.regaloDelete = function (regaloId) { }
+        selfvm.regaloDesactivar = function (regaloId) { }
     }
 
     self.upSellingViewModel = new UpSellingViewModel();
