4cef5c82d107c72e682cefee47cdd1b554be8deb
Alan <alanaupe.trabajo@gmail.com>
2018-03-20 09:56:40 -0500

    BPT-Sprint-20 => Menu HV

--- app/Portal.Consultoras.Web/Controllers/LoginController.cs
+++ app/Portal.Consultoras.Web/Controllers/LoginController.cs
@@ -1507,6 +1507,8 @@ namespace Portal.Consultoras.Web.Controllers
                     sessionManager.SetTieneOpm(true);
                     sessionManager.SetTieneOpmX1(true);
                     sessionManager.SetTieneRdr(true);
+                    sessionManager.SetTieneHv(true);
+                    sessionManager.SetTieneHvX1(true);
 
                     usuarioModel.FotoPerfil = usuario.FotoPerfil;
                 }
