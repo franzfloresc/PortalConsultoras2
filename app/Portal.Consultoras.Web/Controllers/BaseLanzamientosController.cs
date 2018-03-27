21a1a442d74d34efedc030c8709734f4d8263039
Victor Vilela <vilela_victor@hotmail.com>
2018-03-27 10:36:26 -0500

    Add LanzamientosController

---
+++ app/Portal.Consultoras.Web/Controllers/BaseLanzamientosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class BaseLanzamientosController : BaseEstrategiaController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ app/Portal.Consultoras.Web/Controllers/LanzamientosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class LanzamientosController : BaseLanzamientosController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
