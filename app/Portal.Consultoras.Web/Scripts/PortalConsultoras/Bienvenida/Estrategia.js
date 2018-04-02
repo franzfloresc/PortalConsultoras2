c674ffb4e0a5058e79953e42d04146d7543784dd
Hugo Roca <hugo.rock20@hotmail.com>
2018-04-02 11:37:29 -0500

    IPhone - touchStart

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Estrategia.js
@@ -13,7 +13,8 @@ var origenPedidoWebEstrategia = origenPedidoWebEstrategia || "";
 var divAgregado = null;
 
 $(document).ready(function () {
-    $('[data-tono-change]').click(function (e) {
+    $('body').on('touchstart click', '[data-tono-change]', function (e) {
+        e.preventDefault();
         var accion = $(this).attr("data-tono-change");
 
         var hideSelect = $(this).parents("[data-tono]").find('.content_tonos_select').attr("data-visible");
