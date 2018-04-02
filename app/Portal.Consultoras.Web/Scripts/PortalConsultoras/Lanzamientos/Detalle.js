ff276ce88f061aaabd49a44cae9ba05893372cf5
Victor Vilela <vilela_victor@hotmail.com>
2018-04-02 16:13:03 -0500

    EPM-784 : Set relacionados

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/Detalle.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/Detalle.js
@@ -4,9 +4,10 @@ var detalleLanzamiento = (function () {
     var _player;
     var _elements = {
         idPlantillaProductoLanding: "#producto-landing-template",
-        idDivSetsProductosRelacionados: "#divOfertaProductos",
+        divCarruselSetsProductosRelacionados: "#divOfertaProductos",
         verDetalleButtons: "[data-item-tag='verdetalle']",
-        eligeTuOpcionButtons: "[data-item-tag='eligetuopcion']"
+        eligeTuOpcionButtons: "[data-item-tag='eligetuopcion']",
+        divSetsProductosRelacionados: "#set_relacionados",
     };
     var _params = {
         videoId: "",
@@ -41,30 +42,78 @@ var detalleLanzamiento = (function () {
     };
 
     var _mostrarSetRelacionados = function () {
+        $(_elements.divSetsProductosRelacionados).fadeOut();
+
         var cuv = _getParamValueFromQueryString("cuv");
         var campaniaId = _getParamValueFromQueryString("campaniaid");
         
-
         if (cuv == "" || campaniaId == "" || campaniaId == "0") {
             return false;
         }
 
-        var prod = GetProductoStorage(cuv, campaniaId);
-        if (prod == null || prod == undefined || prod.CUV2 == undefined) {
+        var str = LocalStorageListado(lsListaRD + campaniaId, '', 1) ||'';
+
+        if (str === '') {
             return false;
         }
 
-        var data = new Object();
-        data.CampaniaID = prod.CampaniaID;
-        data.lista = new Array();
-        data.lista.push(prod);
-
+        var data = {
+            lista: JSON.parse(str).response.listaLan
+        };
         $.each(data.lista, function (index, item) {
             item.ClaseBloqueada = $.trim(item.ClaseBloqueada);
             item.Posicion = index + 1;
         });
 
-        SetHandlebars(_elements.idPlantillaProductoLanding, data, _elements.idDivSetsProductosRelacionados);
+        SetHandlebars(_elements.idPlantillaProductoLanding, data, _elements.divCarruselSetsProductosRelacionados);
+        EstablecerAccionLazyImagen("img[data-lazy-seccion-revista-digital]");
+
+        var platform = 'desktop';
+        if (isMobile()) {
+            platform = 'mobile';
+        }
+
+        var slickArrows = {
+            'mobile': {
+                prev: '<a class="previous_ofertas_mobile" href="javascript:void(0);" style="margin-left:-12%; text-align:left;"><img src="' + baseUrl + 'Content/Images/mobile/Esika/previous_ofertas_home.png")" alt="" /></a>'
+                , next: '<a class="previous_ofertas_mobile" href="javascript:void(0);" style="margin-right:-12%; text-align:right; right:0"><img src="' + baseUrl + 'Content/Images/mobile/Esika/next.png")" alt="" /></a>'
+            },
+            'desktop': {
+                prev: '<a class="previous_ofertas" style="left:-5%; text-align:left;"><img src="' + baseUrl + 'Content/Images/Esika/previous_ofertas_home.png")" alt="" /></a>'
+                , next: '<a class="previous_ofertas" style="display: block; right:-5%; text-align:right;"><img src="' + baseUrl + 'Content/Images/Esika/next.png")" alt="" /></a>'
+            }
+        };
+
+        $(_elements.divCarruselSetsProductosRelacionados + '.slick-initialized').slick('unslick');
+        $(_elements.divCarruselSetsProductosRelacionados).not('.slick-initialized').slick({
+            infinite: true,
+            vertical: false,
+            centerMode: false,
+            centerPadding: '0px',
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: false,
+            speed: 270,
+            pantallaPedido: false,
+            prevArrow: slickArrows[platform].prev,
+            nextArrow: slickArrows[platform].next,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: { slidesToShow: 3, slidesToScroll: 1 }
+                },
+                {
+                    breakpoint: 900,
+                    settings: { slidesToShow: 2, slidesToScroll: 1 }
+                },
+                {
+                    breakpoint: 600,
+                    settings: { slidesToShow: 1, slidesToScroll: 1 }
+                }
+            ]
+        });
+
+        $(_elements.divSetsProductosRelacionados).fadeIn();
     }
 
     var _redigirAVerDetallaLanzamiento = function (event) {
