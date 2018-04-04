3edb2ad091eb6102c70574cd668af00883f00b5d
Alan <alanaupe.trabajo@gmail.com>
2018-04-04 16:26:30 -0500

    Soportec => Script GetOfertasParaTiByTipoConfiguradoTemporal

---
+++ app/Scripts/PPRFix/SoporTec-PprFix/Script/4.3.PPRFIX_GetOfertasParaTiByTipoConfiguradoTemporal.sql
@@ -0,0 +1,76 @@
+GO
+ALTER PROCEDURE dbo.GetOfertasParaTiByTipoConfiguradoTemporal
+@CampaniaID int,
+@TipoConfigurado int,
+@nroLote int = 0
+as
+/*
+exec GetOfertasParaTiByTipoConfiguradoTemporal 201618,0
+exec GetOfertasParaTiByTipoConfiguradoTemporal 201618,1
+exec GetOfertasParaTiByTipoConfiguradoTemporal 201618,2
+*/
+begin
+
+	declare @tablaResultado table (
+		CUV2 varchar(5), 
+		DescripcionCUV2 varchar(100),
+		Precio2 decimal(18,2),
+		Precio decimal(18,2),
+		CodigoProducto varchar(20),
+		OfertaUltimoMinuto int,
+		LimiteVenta int,
+		ImagenURL varchar(150),
+		CodigoEstrategia varchar(150), 
+		TieneVariedad INT,
+		PrecioPublico decimal(18,2),
+		Ganancia decimal(18,2),
+		Niveles varchar(255)
+	)
+
+	SET @nroLote = ISNULL(@nroLote, 0)
+
+	declare @NumeroLote int = 0
+	IF @nroLote <= 0
+		select @NumeroLote = max(NumeroLote) from EstrategiaTemporal
+	ELSE
+		SET @NumeroLote = @nroLote
+
+	if @TipoConfigurado = 0
+	begin
+		insert into @tablaResultado
+		select CUV, Descripcion, PrecioOferta, PrecioTachado, CodigoSap, OfertaUltimoMinuto, LimiteVenta, FotoProducto01,
+			CodigoEstrategia, TieneVariedad, PrecioPublico, Ganancia, Niveles
+		from EstrategiaTemporal
+		where 
+			CampaniaId = @CampaniaID
+			and NumeroLote = @NumeroLote
+	end
+	else
+	begin
+		if @TipoConfigurado = 1
+		begin
+			insert into @tablaResultado
+			select CUV, Descripcion, PrecioOferta, PrecioTachado, CodigoSap, OfertaUltimoMinuto, LimiteVenta, FotoProducto01,
+				CodigoEstrategia, TieneVariedad, PrecioPublico, Ganancia, Niveles
+			from EstrategiaTemporal
+			where 
+				CampaniaId = @CampaniaID
+				and Descripcion != ''
+				and NumeroLote = @NumeroLote
+		end
+		else
+		begin
+			insert into @tablaResultado
+			select CUV, Descripcion, PrecioOferta, PrecioTachado, CodigoSap, OfertaUltimoMinuto, LimiteVenta, FotoProducto01,
+				CodigoEstrategia, TieneVariedad, PrecioPublico, Ganancia, Niveles
+			from EstrategiaTemporal
+			where 
+				CampaniaId = @CampaniaID
+				and Descripcion = ''
+				and NumeroLote = @NumeroLote
+		end
+	end
+
+	select * from @tablaResultado
+end
+GO
