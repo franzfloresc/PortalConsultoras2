07c422c7c1016c7b8fd715f0b920a2c0311d640f
Kike <lead123456@hotmail.com>
2018-04-09 12:02:39 -0500

    Prol3-Sprint1.1: Actualización de scripts de BD.

--- app/Scripts/PROL3/Sprint1/1.Database/11_ALT_TablaLogicaDatos.sql
+++ app/Scripts/PROL3/Sprint1/1.Database/11_ALT_TablaLogicaDatos.sql
@@ -5,3 +5,9 @@ GO
 ALTER TABLE dbo.TablaLogicaDatos
 ALTER COLUMN Descripcion VARCHAR(180) NOT NULL;
 GO
+IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE name='IX_TablaLogicaDatos_TablaLogicaID')
+BEGIN
+	CREATE INDEX IX_TablaLogicaDatos_TablaLogicaID ON dbo.TablaLogicaDatos(TablaLogicaID);
+END
+GO
+
