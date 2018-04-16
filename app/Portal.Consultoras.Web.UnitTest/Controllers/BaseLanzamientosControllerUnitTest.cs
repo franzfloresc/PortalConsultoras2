8b28277445b7211a406fb3b26345deada3f2710a
Victor Vilela <victor.vilela@outlook.com>
2018-04-16 15:03:14 -0500

    Add Unit Tests to BaseLanzamientosController.Detalle
    Add test to verify error log  when revistaDigital is null
    Add test to verify exception throw   when revistaDigital is null
    Add test to verify redirect to Bienvenida when consutora doesn´t have  RevistaDigital and is no Activa

---
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/BaseLanzamientosControllerUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Portal.Consultoras.Web.Controllers;
+using Portal.Consultoras.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.UnitTest.Controllers
+{
+    [TestClass]
+    public class BaseLanzamientosControllerUnitTest
+    {
+        BaseLanzamientosController controller;
+
+        [TestClass]
+        public class Detalle : Base
+        {
+            BaseLanzamientosController controller;
+
+            public virtual BaseLanzamientosController GetLanzamientosController()
+            {
+                return new BaseLanzamientosController(sessionManager.Object, logManager.Object);
+            }
+
+            [TestInitialize]
+            public override void Test_Initialize()
+            {
+                base.Test_Initialize();
+                controller = GetLanzamientosController();
+            }
+
+            [TestCleanup]
+            public override void Test_Cleanup()
+            {
+                base.Test_Cleanup();
+                controller.Dispose();
+            }
+
+            [TestMethod]
+            public void Detalle_RevistaDigitalEsNula_DebeEscribirEnLog()
+            {
+                controller.RevistaDigital = null;
+                ActionResult result;
+
+                try
+                {
+                    result = controller.Detalle(string.Empty, 0);
+                }
+                catch
+                {
+                }
+
+                logManager.Verify(x => x.LogErrorWebServicesBusWrap(
+                    It.Is<Exception>(e=> e.Message.Contains("revistaDigital no puede ser nulo.")),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.Is<string>(str => str.Contains("BaseLanzamientosController.Detalle"))
+                    ));
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(Exception))]
+            public void Detalle_RevistaDigitalEsNula_DebeLanzarExcepcion()
+            {
+                controller.RevistaDigital = null;
+
+                var result = controller.Detalle(string.Empty, 0);
+            }
+
+            [TestMethod]
+            public void Detalle_ConsultoraNoTieneRevistaDigitalYEsNoActiva_DebeRedireccionarABienvenida()
+            {
+                controller.RevistaDigital = new RevistaDigitalModel
+                {
+                    TieneRDC = false,
+                    EsActiva = false
+                };
+
+                var result = controller.Detalle(string.Empty, 0) as RedirectToRouteResult;
+
+                Assert.AreEqual("Bienvenida", result.RouteValues["controller"]);
+                Assert.AreEqual("Index", result.RouteValues["action"]);
+            }
+        }
+    }
+}
--- app/Portal.Consultoras.Web/Controllers/BaseLanzamientosController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseLanzamientosController.cs
@@ -1,5 +1,7 @@
 ﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.LogManager;
 using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.SessionManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,58 +12,100 @@ namespace Portal.Consultoras.Web.Controllers
 {
     public class BaseLanzamientosController : BaseEstrategiaController
     {
+        public BaseLanzamientosController()
+            : base()
+        {
+            //
+        }
+
+        public BaseLanzamientosController(ISessionManager sessionManager)
+            : base(sessionManager)
+        {
+        }
+
+        public BaseLanzamientosController(ISessionManager sessionManager, ILogManager logManager)
+            : base(sessionManager, logManager)
+        {
+
+        }
+
+        public RevistaDigitalModel RevistaDigital
+        {
+            get
+            {
+                return revistaDigital;
+            }
+            set
+            {
+                revistaDigital = value;
+            }
+        }
+
         public virtual ActionResult Detalle(string cuv, int campaniaId)
         {
-            if (!(revistaDigital.TieneRevistaDigital() && revistaDigital.EsActiva))
+            try
             {
-                return RedirectToAction("Index", "Bienvenida", new { area = IsMobile() ? "Mobile" : "" });
-            }
-
-            var modelo = (EstrategiaPersonalizadaProductoModel)Session[Constantes.ConstSession.ProductoTemporal];
-            if (modelo == null || modelo.EstrategiaID == 0 || modelo.CUV2 != cuv || modelo.CampaniaID != campaniaId)
-            {
-                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
-            }
-            if (EsCampaniaFalsa(modelo.CampaniaID))
-            {
-                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
-            }
-            if (modelo.EstrategiaID <= 0)
-            {
-                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
-            }
-
-            modelo.TipoEstrategiaDetalle = modelo.TipoEstrategiaDetalle ?? new EstrategiaDetalleModelo();
-            modelo.ListaDescripcionDetalle = modelo.ListaDescripcionDetalle ?? new List<string>();
-
-            var EstrategiaDetalle = EstrategiaGetDetalle(modelo.EstrategiaID);
-            if (EstrategiaDetalle.Hermanos != null)
-            {
-                modelo.Hermanos = EstrategiaDetalle.Hermanos;
-            }
-
-            if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.IndividualConTonos)
-            {
-                if ((modelo.Hermanos.Any()))
+                if(revistaDigital == null)
                 {
-                    modelo.ClaseBloqueada = "btn_desactivado_general";
+                    throw new Exception("revistaDigital no puede ser nulo.");
                 }
-            }
-            else if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaFija || modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaVariable)
-            {
-                if (modelo.Hermanos != null && modelo.Hermanos.Any())
+
+                if (!revistaDigital.TieneRevistaDigital() || !revistaDigital.EsActiva)
                 {
-                    if (modelo.Hermanos[0].Digitable == 1 && modelo.Hermanos[0].Hermanos.Any())
+                    return RedirectToAction("Index", "Bienvenida", new { area = IsMobile() ? "Mobile" : "" });
+                }
+
+                var modelo = (EstrategiaPersonalizadaProductoModel)Session[Constantes.ConstSession.ProductoTemporal];
+                if (modelo == null || modelo.EstrategiaID == 0 || modelo.CUV2 != cuv || modelo.CampaniaID != campaniaId)
+                {
+                    return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+                }
+                if (EsCampaniaFalsa(modelo.CampaniaID))
+                {
+                    return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+                }
+                if (modelo.EstrategiaID <= 0)
+                {
+                    return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+                }
+
+                modelo.TipoEstrategiaDetalle = modelo.TipoEstrategiaDetalle ?? new EstrategiaDetalleModelo();
+                modelo.ListaDescripcionDetalle = modelo.ListaDescripcionDetalle ?? new List<string>();
+
+                var EstrategiaDetalle = EstrategiaGetDetalle(modelo.EstrategiaID);
+                if (EstrategiaDetalle.Hermanos != null)
+                {
+                    modelo.Hermanos = EstrategiaDetalle.Hermanos;
+                }
+
+                if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.IndividualConTonos)
+                {
+                    if ((modelo.Hermanos.Any()))
                     {
                         modelo.ClaseBloqueada = "btn_desactivado_general";
                     }
                 }
-            }
+                else if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaFija || modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaVariable)
+                {
+                    if (modelo.Hermanos != null && modelo.Hermanos.Any())
+                    {
+                        if (modelo.Hermanos[0].Digitable == 1 && modelo.Hermanos[0].Hermanos.Any())
+                        {
+                            modelo.ClaseBloqueada = "btn_desactivado_general";
+                        }
+                    }
+                }
 
-            ViewBag.EstadoSuscripcion = revistaDigital.SuscripcionModel.EstadoRegistro;
-            ViewBag.Campania = campaniaId;
-            
-            return View(modelo);
+                ViewBag.EstadoSuscripcion = revistaDigital.SuscripcionModel.EstadoRegistro;
+                ViewBag.Campania = campaniaId;
+
+                return View(modelo);
+            }
+            catch (Exception ex)
+            {
+                logManager.LogErrorWebServicesBusWrap(ex, userData.CodigoConsultora, userData.CodigoISO, "BaseLanzamientosController.Detalle");
+                throw;
+            }
         }
     }
 }
