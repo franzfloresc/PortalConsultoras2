99f233dbf8df2a2c57c6b4bc208aa39dffab996e
ruben9261 <ruben9261@gmail.com>
2018-04-24 12:18:21 -0500

    EPM - 868

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/detalle-lanzamiento.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/detalle-lanzamiento.js
@@ -98,30 +98,36 @@
 
         $(_elements.divCarruselSetsProductosRelacionados + '.slick-initialized').slick('unslick');
         $(_elements.divCarruselSetsProductosRelacionados).not('.slick-initialized').slick({
+            dots: false,
             infinite: true,
-            vertical: false,
-            centerMode: false,
-            centerPadding: '0px',
+            speed: 260,
             slidesToShow: 4,
             slidesToScroll: 1,
-            autoplay: false,
-            speed: 270,
-            variableWidth: true,
-            pantallaPedido: false,
             prevArrow: slickArrows[platform].prev,
             nextArrow: slickArrows[platform].next,
             responsive: [
                 {
-                    breakpoint: 1200,
-                    settings: { slidesToShow: 3, slidesToScroll: 1 }
-                },
-                {
-                    breakpoint: 900,
-                    settings: { slidesToShow: 2, slidesToScroll: 1 }
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
                 },
                 {
                     breakpoint: 600,
-                    settings: { slidesToShow: 1, slidesToScroll: 1 }
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
             ]
         });
