1f451ed29282b4f2ee9c419af0d75849be1a5a9b
Alan <alanaupe.trabajo@gmail.com>
2018-04-25 10:35:15 -0500

    BPT-Sprint-22 => LAN Detalle Desktop - Carrusel generar

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/detalle-lanzamiento.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Lanzamientos/detalle-lanzamiento.js
@@ -106,34 +106,9 @@
             slidesToShow: 4,
             slidesToScroll: 1,
             variableWidth: widthDimamico,
+            infinite: true,
             prevArrow: slickArrows[platform].prev,
-            nextArrow: slickArrows[platform].next,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        infinite: true
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        infinite: true
-                    }
-                }
-            ]
+            nextArrow: slickArrows[platform].next
         });
 
         $(_elements.divSetsProductosRelacionados).fadeIn();
