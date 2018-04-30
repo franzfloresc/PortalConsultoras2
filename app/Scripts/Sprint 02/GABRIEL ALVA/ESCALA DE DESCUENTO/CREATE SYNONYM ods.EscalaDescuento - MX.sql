a11a8ab1579f455168ef4959192fc3915ce324a7
Alan <alanaupe.trabajo@gmail.com>
2018-04-30 10:19:42 -0500

    BPT-Sprint-24-Mejoras => app/Scripts - Eliminar archivos sql

--- app/Scripts/Sprint 02/GABRIEL ALVA/ESCALA DE DESCUENTO/CREATE SYNONYM ods.EscalaDescuento - MX.sql
+++
@@ -1,5 +0,0 @@
-USE [BelcorpMexico_SB2]
-GO
-
-CREATE SYNONYM [ods].[EscalaDescuento] FOR [ODS_MX].[dbo].[EscalaDescuento]
-GO
--- app/Scripts/Sprint 04/PE/ALTER TABLE PEDIDOWEBDETALLE +1.sql
+++
@@ -1,5 +0,0 @@
-if (select COUNT(*) from dbo.sysobjects inner join dbo.syscolumns on SYSOBJECTS.ID = SYSCOLUMNS.ID 
- where sysobjects.id = object_id('dbo.pedidowebdetalle') and SYSCOLUMNS.NAME = N'OrdenPedidoWD') = 0
-		ALTER TABLE dbo.PedidoWebDetalle 
-		ADD OrdenPedidoWD int
-go
