e01163ff03aa1160e3f836f21bc9c63c7509ee6c
ipndeveloper <i_nav_31_8@hotmail.com>
2018-05-04 16:31:56 -0500

    Rename "obj" as this name is already used in declaration at line 119.
    Remove this useless assignment to local variable "urlAccion".
    Rename "Cantidad" as this name is already used in declaration at line 357.
    Rename "obj" as this name is already used in declaration at line 404.
    Remove this useless assignment to local variable "descripcionMarca".
    Rename "Cantidad" as this name is already used in declaration at line 647.
    Rename "j" as this name is already used in declaration at line 812.
    Remove this useless assignment to local variable "mensajePedidoCheckout".
    Rename "tipoMensaje" as this name is already used in declaration at line 868.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/PedidoFIC/Detalle.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Mobile/PedidoFIC/Detalle.js
@@ -116,8 +116,8 @@ function GetProductoEntidad(id) {
 
 function UpdateLiquidacionEvento(evento) {
 
-    var obj = $(evento.currentTarget);
-    var id = $.trim(obj.attr("data-pedidodetalleid")) || "0";
+    var obj0 = $(evento.currentTarget);
+    var id = $.trim(obj0.attr("data-pedidodetalleid")) || "0";
     if (isNaN(id) || parseInt(id, 10) <= 0) {
         return false;
     }
@@ -135,13 +135,7 @@ function UpdateLiquidacionEvento(evento) {
 }
 
 function UpdateLiquidacionSegunTipoOfertaSis(CampaniaID, PedidoID, PedidoDetalleID, TipoOfertaSisID, CUV, FlagValidacion, CantidadModi, EsBackOrder, PrecioUnidad) {
-    var urlAccion = TipoOfertaSisID == ofertaLiquidacion
-        ? urlValidarUnidadesPermitidasPedidoProducto
-        : TipoOfertaSisID == ofertaShowRoom
-            ? urlValidarUnidadesPermitidasPedidoProductoShowRoom
-            : TipoOfertaSisID == ofertaAccesorizate
-                ? urlValidarUnidadesPermitidasPedidoProducto2
-                : "";
+
 
     var urls = new Object();
     if (TipoOfertaSisID == ofertaLiquidacion) {
@@ -377,9 +371,9 @@ function UpdateConCantidad(CampaniaID, PedidoID, PedidoDetalleID, FlagValidacion
         CliID = 0;
     }
 
-    var Cantidad = CantidadModi;
+    var Cantidad_ = CantidadModi;
     var PrecioUnidad = $('#PrecioUnidad_' + PedidoDetalleID).val();
-    var Total = DecimalToStringFormat(parseFloat(Cantidad * PrecioUnidad));
+    var Total = DecimalToStringFormat(parseFloat(Cantidad_ * PrecioUnidad));
     $('#ImporteTotal_' + PedidoDetalleID).html(Total);
     $('#ImporteTotalMinimo_' + PedidoDetalleID).html(Total);
 
@@ -401,8 +395,8 @@ function UpdateConCantidad(CampaniaID, PedidoID, PedidoDetalleID, FlagValidacion
 
 function EliminarPedidoEvento(evento, esBackOrder) {
     
-    var obj = $(evento.currentTarget);
-    var id = $.trim(obj.attr("data-pedidodetalleid")) || "0";
+    var obj0 = $(evento.currentTarget);
+    var id = $.trim(obj0.attr("data-pedidodetalleid")) || "0";
     if (isNaN(id) || parseInt(id, 10) <= 0) {
         return false;
     }
@@ -453,7 +447,7 @@ function EliminarPedido(CampaniaID, PedidoID, PedidoDetalleID, TipoOfertaSisID, 
 
                 ActualizarGanancia(data.DataBarra);
                 CargarPedido();
-                var descripcionMarca = GetDescripcionMarca(MarcaID);
+               
                 TrackingJetloreRemove(Cantidad, $("#hdCampaniaCodigo").val(), CUV);
                 dataLayer.push({
                     'event': 'removeFromCart',
@@ -644,22 +638,22 @@ function Update(CampaniaID, PedidoID, PedidoDetalleID, FlagValidacion, CUV, EsBa
 
     var CliID = $('#ClienteID_' + PedidoDetalleID).val();
     var CliDes = $('#ClienteNombre_' + PedidoDetalleID).val();
-    var Cantidad = $('#Cantidad_' + PedidoDetalleID).val();
+    var Cantidad_ = $('#Cantidad_' + PedidoDetalleID).val();
     var CantidadAnti = $('#CantidadTemporal_' + PedidoDetalleID).val();
     var DesProd = $('#DescripcionProducto_' + PedidoDetalleID).html();
 
-    if (Cantidad.length == 0) {
+    if (Cantidad_.length == 0) {
         messageInfoMalo('Por favor ingrese una cantidad válida.');
         return false;
     }
 
-    if (Cantidad == 0) {
+    if (Cantidad_ == 0) {
         messageInfoMalo('Por favor ingrese una cantidad mayor a cero.');
         return false;
     }
 
     if (FlagValidacion == "1") {
-        if (CantidadAnti == Cantidad)
+        if (CantidadAnti == Cantidad_)
             return false;
     } else {
         if (ClienteAnti == CliDes)
@@ -801,6 +795,7 @@ function TagManagerClickEliminarProducto(descripcionProd, cuv, precioUnidad, des
 }
 
 function SeparadorMiles(pnumero) {
+    debugger;
     var resultado = "";
     var numero = pnumero.replace(/\,/g, '');
     var nuevoNumero = numero.replace(/\./g, '');
@@ -809,7 +804,7 @@ function SeparadorMiles(pnumero) {
 
     if (numero.indexOf(",") >= 0) nuevoNumero = nuevoNumero.substring(0, nuevoNumero.indexOf(","));
 
-    for (var j, i = nuevoNumero.length - 1, j = 0; i >= 0; i--, j++)
+    for (var  i = nuevoNumero.length - 1, j = 0; i >= 0; i--, j++)
         resultado = nuevoNumero.charAt(i) + ((j > 0) && (j % 3 == 0) ? "." : "") + resultado;
 
     if (numero.indexOf(",") >= 0) resultado += numero.substring(numero.indexOf(","));
@@ -835,6 +830,7 @@ function EjecutarServicioPROLSinOfertaFinal() {
 }
 
 function RespuestaEjecutarServicioPROL(response, inicio) {
+    var tipoMensaje;
     inicio = inicio == null || inicio == undefined ? true : inicio;
     var model = response.data;
 
@@ -849,7 +845,7 @@ function RespuestaEjecutarServicioPROL(response, inicio) {
     $("hdfPROLSinStock").val(model.ProlSinStock == true ? "1" : "0");
     $("hdfModificaPedido").val(model.EsModificacion == true ? "1" : "0");
     
-    var mensajePedidoCheckout = ConstruirObservacionesPROL(model);
+
     
     $('#btnGuardarPedido').text(model.Prol);
     var tooltips = model.ProlTooltip.split('|');
@@ -865,7 +861,7 @@ function RespuestaEjecutarServicioPROL(response, inicio) {
 
     if (model.Reserva != true) {
         if (inicio) {
-            var tipoMensaje = codigoMensajeProl == "00" ? 1 : 2;
+             tipoMensaje = codigoMensajeProl == "00" ? 1 : 2;
             cumpleOferta = CumpleOfertaFinalMostrar(montoPedido, montoEscala, tipoMensaje, codigoMensajeProl, response.data.ListaObservacionesProl);
         }
         if (!cumpleOferta.resultado) {
@@ -913,7 +909,7 @@ function RespuestaEjecutarServicioPROL(response, inicio) {
         }
 
         if (inicio) {
-            var tipoMensaje = codigoMensajeProl == "00" ? 1 : 2;
+            tipoMensaje = codigoMensajeProl == "00" ? 1 : 2;
             cumpleOferta = CumpleOfertaFinalMostrar(montoPedido, montoEscala, tipoMensaje, codigoMensajeProl, response.data.ListaObservacionesProl);
         }
 
