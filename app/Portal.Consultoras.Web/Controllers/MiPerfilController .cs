7faa7a54050b25783f98831f559491726eec898f
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-05-23 14:11:07 -0500

    Avance HD-2450 - Actualización de datos con doble optin (Desktop). Vistas Mi Perfil, Actualizar Correo, Actualizar Celular y Cambiar Contraseña

---
+++ app/Portal.Consultoras.Web/Controllers/MiPerfilController .cs
@@ -0,0 +1,33 @@
+﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServiceAsesoraOnline;
+using System;
+using System.Configuration;
+using System.ServiceModel;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class MiPerfilController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CambiarContrasenia()
+        {
+            return View();
+        }
+
+        public ActionResult ActualizarCorreo()
+        {
+            return View();
+        }
+
+        public ActionResult ActualizarCelular()
+        {
+            return View();
+        }
+    }
+}
