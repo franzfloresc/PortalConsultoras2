95c0c6dc82c479a9cf208f0e4d49179f980af24e
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-05-23 18:48:30 -0500

    Se crea Vista Mi Perfil (Mobile) así como sus hojas de estilo (Ésika y L'bel)

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/MiPerfilController.cs
@@ -0,0 +1,26 @@
+﻿using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Portal.Consultoras.Web.Models.PagoEnLinea;
+using System.ServiceModel;
+using System.Text;
+using Portal.Consultoras.Common.PagoEnLinea;
+using System.Net;
+using System.IO;
+using System.Web;
+using Portal.Consultoras.Web.ServicePedido;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class MiPerfilController : BaseMobileController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
