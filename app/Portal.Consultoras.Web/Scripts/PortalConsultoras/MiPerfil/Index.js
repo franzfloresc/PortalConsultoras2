6f2c017b5044a693affb05ee1f7ad9e8303adc21
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-05-23 19:14:07 -0500

    Se sube csproj actualizado con la vista Mi Perfil (Mobile) agregada.

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/MiPerfil/Index.js
@@ -0,0 +1,30 @@
+﻿var MiPerfil;
+
+$(document).ready(function () {
+    'use strict';
+
+    var vistaMiPerfil;
+
+    vistaMiPerfil = function () {
+        var me = this;
+
+        me.Globals = {
+
+        },
+        me.Funciones = {
+            InicializarEventos: function () {
+
+            }
+        },
+        me.Eventos = {
+
+        },
+        me.Inicializar = function () {
+            me.Funciones.InicializarEventos();
+        }
+    }
+
+    MiPerfil = new vistaMiPerfil();
+    MiPerfil.Inicializar();
+
+});
