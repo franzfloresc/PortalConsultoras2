262aa3536dae67838ae22d7fbf41f093cb36cb63
Hugo Roca <hugo.rock20@hotmail.com>
2018-05-25 15:44:03 -0500

    Rollback para todos los paises en scripts BD

--- app/Scripts/MejoraContinua/HD-2375/1_ProductoSugeridoPadre_MC.sql
+++ app/Scripts/MejoraContinua/HD-2375/1_ProductoSugeridoPadre_MC.sql
@@ -1,129 +1,4 @@
-USE BelcorpBolivia
 GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpChile
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpColombia
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpCostaRica
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpDominicana
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpEcuador
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpGuatemala
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpMexico
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
-USE BelcorpPanama
-GO
-IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
-   DROP TABLE dbo.ProductoSugeridoPadre
-GO
-CREATE TABLE dbo.ProductoSugeridoPadre(
-	CampaniaID INT,
-	CUV VARCHAR(20),
-	UsuarioRegistro VARCHAR(50),
-	FechaRegistro DATETIME,
-	UsuarioModificacion VARCHAR(50),
-	FechaModificacion DATETIME
-)
-
 USE BelcorpPeru
 GO
 IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
@@ -138,6 +13,52 @@ CREATE TABLE dbo.ProductoSugeridoPadre(
 	FechaModificacion DATETIME
 )
 
+GO
+USE BelcorpMexico
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpColombia
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpSalvador
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
 USE BelcorpPuertoRico
 GO
 IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
@@ -152,7 +73,8 @@ CREATE TABLE dbo.ProductoSugeridoPadre(
 	FechaModificacion DATETIME
 )
 
-USE BelcorpSalvador
+GO
+USE BelcorpPanama
 GO
 IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
    DROP TABLE dbo.ProductoSugeridoPadre
@@ -165,3 +87,95 @@ CREATE TABLE dbo.ProductoSugeridoPadre(
 	UsuarioModificacion VARCHAR(50),
 	FechaModificacion DATETIME
 )
+
+GO
+USE BelcorpGuatemala
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpEcuador
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpDominicana
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpCostaRica
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpChile
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
+USE BelcorpBolivia
+GO
+IF EXISTS (SELECT 1 FROM SYSOBJECTS WHERE ID = OBJECT_ID('dbo.ProductoSugeridoPadre') AND TYPE = 'U')
+   DROP TABLE dbo.ProductoSugeridoPadre
+GO
+CREATE TABLE dbo.ProductoSugeridoPadre(
+	CampaniaID INT,
+	CUV VARCHAR(20),
+	UsuarioRegistro VARCHAR(50),
+	FechaRegistro DATETIME,
+	UsuarioModificacion VARCHAR(50),
+	FechaModificacion DATETIME
+)
+
+GO
