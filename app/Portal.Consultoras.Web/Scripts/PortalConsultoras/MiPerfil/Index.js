7ddaf253be599a2dd58de2616b0e9eada0ddf5a5
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-05-25 17:53:27 -0500

    Se agrega scripts en vistas Mobile de Mi Perfil

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/MiPerfil/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/MiPerfil/Index.js
@@ -8,16 +8,23 @@ $(document).ready(function () {
     vistaMiPerfil = function () {
         var me = this;
 
-        me.Globals = {
+        //me.Globals = {
 
-        },
+        //},
         me.Funciones = {
             InicializarEventos: function () {
-
+                $('body').on('blur', '.grupo_form_cambio_datos input', me.Eventos.LabelActivo);
             }
         },
         me.Eventos = {
-
+            LabelActivo: function () {
+                var campoDatos = $(this).val();
+                if(campoDatos != ''){
+                    $(this).addClass('campo_con_datos');
+                } else {
+                    $(this).removeClass('campo_con_datos');
+                }
+            }
         },
         me.Inicializar = function () {
             me.Funciones.InicializarEventos();
