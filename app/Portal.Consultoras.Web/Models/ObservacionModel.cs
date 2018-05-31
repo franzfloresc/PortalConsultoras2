0c5900e06779cea2116fcd7b200c8a426483fc63
Victor Vilela <victor.vilela@avantica.net>
2018-05-31 12:58:26 -0500

    Split PedidoModel.cs into ProductoModel.cs, PedidoDetalleModel.cs y ObservacionModel.cs

---
+++ app/Portal.Consultoras.Web/Models/ObservacionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.Models
+{
+    [Serializable]
+    public class ObservacionModel
+    {
+        public string CUV { get; set; }
+        public int Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public int Caso { get; set; }
+    }
+}
