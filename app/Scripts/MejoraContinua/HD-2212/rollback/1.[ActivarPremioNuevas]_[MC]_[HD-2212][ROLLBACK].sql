d25b6e9f517f386abcc56a48213778531375593e
HUNDRED\lennyn.muchotrigo <lennyn.muchotrigo@hundred.com.pe>
2018-06-04 15:46:09 -0500

    script Programa de nuevas: Mostrar imagen del regalo en la barra de descuentos  [HD-2212]

---
+++ app/Scripts/MejoraContinua/HD-2212/rollback/1.[ActivarPremioNuevas]_[MC]_[HD-2212][ROLLBACK].sql
@@ -0,0 +1,81 @@
+USE [BelcorpPeru]  
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpBolivia]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpChile]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpColombia]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpCostaRica]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpDominicana]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpEcuador]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpGuatemala]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpMexico]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpPanama]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpPuertoRico]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpSalvador]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+USE [BelcorpVenezuela]
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.ActivarPremioNuevas') AND type in (N'U')) 
+	DROP TABLE dbo.ActivarPremioNuevas
+GO
+
+
+
+
