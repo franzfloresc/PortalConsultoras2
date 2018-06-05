454fd36a26d59250eedcbea4165d54ce01cc4e72
Felix Sueldo <fsueldov@gmail.com>
2018-06-05 12:38:54 -0500

    Unificación Estrategia oferta
    Unificación estrategia oferta.

---
+++ app/Portal.Consultoras.BizLogic/Estrategia/BLEstrategiaOferta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Portal.Consultoras.Data;
+using Portal.Consultoras.Entities.ShowRoom;
+
+namespace Portal.Consultoras.BizLogic.Estrategia
+{
+    public class BLEstrategiaOferta
+    {
+        public BEShowRoomEvento GetShowRoomEventoByCampaniaID(int paisID, int campaniaID)
+        {
+            BEShowRoomEvento entidad = null;
+            var daPedidoWeb = new DAShowRoomEvento(paisID);
+
+            using (IDataReader reader = daPedidoWeb.GetShowRoomEventoByCampaniaID(campaniaID))
+            {
+                if (reader.Read())
+                {
+                    entidad = new BEShowRoomEvento(reader);
+                }
+            }   
+            return entidad;
+        }
+
+
+    }
+}
---
+++ app/Portal.Consultoras.Web/CustomHelpers/CustomFilters/ClearSessionMobileAppAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Portal.Consultoras.Web.Areas.Mobile.Models;
+using Portal.Consultoras.Web.Helpers;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.CustomFilters
+{
+    /// <summary>
+    /// Clean session based on MobileAppConfiguracionModel
+    /// </summary>
+    public class ClearSessionMobileAppAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Route identifier
+        /// </summary>
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// Session Key Name
+        /// </summary>
+        public string SessionName { get; set; }
+
+        public string StartSessionName { get; set; }
+
+        public ClearSessionMobileAppAttribute(string routeIdentifier, string sessionName, string startSessionName)
+        {
+            Identifier = routeIdentifier;
+            SessionName = sessionName;
+            StartSessionName = startSessionName;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var uniqueKey = filterContext.RouteData.GetUniqueRoute(Identifier);
+
+            var configuracionApp = filterContext.HttpContext.Session.GetUniqueSession<MobileAppConfiguracionModel>(uniqueKey, SessionName);
+            if (configuracionApp == null)
+                return;
+
+            if (filterContext.HttpContext.Session[StartSessionName] == null)
+                return;
+
+            var datetimeStartSession = (DateTime)filterContext.HttpContext.Session[StartSessionName];
+
+            if (datetimeStartSession.AddMinutes(configuracionApp.TimeOutSession) < DateTime.Now)
+            {
+                var sessionKeysToDelete = (from object key in filterContext.HttpContext.Session.Keys
+                                           where !key.ToString().Contains(uniqueKey)
+                                           select key.ToString()).ToList();
+
+                sessionKeysToDelete.ForEach(k => filterContext.HttpContext.Session.Remove(k));
+                filterContext.HttpContext.Session[StartSessionName] = DateTime.Now;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
