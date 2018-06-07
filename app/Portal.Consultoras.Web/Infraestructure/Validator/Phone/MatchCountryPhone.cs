590c628ca398cdc42ff589a9ce06f85538e978d3
Giancarlos Salas <giansalex@gmail.com>
2018-06-07 11:54:43 -0500

    add validators impl

---
+++ app/Portal.Consultoras.Web/Infraestructure/Validator/Phone/MatchCountryPhone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Portal.Consultoras.Common.Validator;
+
+namespace Portal.Consultoras.Web.Infraestructure.Validator.Phone
+{
+    public class MatchCountryPhone : IPhoneValidator
+    {
+        public string IsoPais { get; set; }
+
+        public Task<SimpleResult> Valid(string number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ app/Portal.Consultoras.Web/Infraestructure/Validator/Phone/NotExistingPhone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Portal.Consultoras.Common.Validator;
+
+namespace Portal.Consultoras.Web.Infraestructure.Validator.Phone
+{
+    public class NotExistingPhone : IPhoneValidator
+    {
+        public Task<SimpleResult> Valid(string number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
