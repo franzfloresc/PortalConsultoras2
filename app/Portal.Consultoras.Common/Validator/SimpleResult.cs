03259c54b2bf72282a5be0b74945f11a199f5297
Giancarlos Salas <giansalex@gmail.com>
2018-06-07 11:54:28 -0500

    add validator definition

---
+++ app/Portal.Consultoras.Common/Validator/SimpleResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Consultoras.Common.Validator
+{
+    public class SimpleResult
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
