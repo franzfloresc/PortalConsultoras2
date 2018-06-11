56ae1c24454a56f5b5d686e553b0a92b9499673b
Felix Sueldo <fsueldov@gmail.com>
2018-06-11 14:47:34 -0500

    Se traslada llamamientos de palancas en BaseEstrategiaController a OfertaSerivce
    Se traslada llamamientos de palancas en BaseEstrategiaController a OfertaSerivce

--- app/Portal.Consultoras.Web/Controllers/BaseEstrategiaController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseEstrategiaController.cs
@@ -85,10 +85,11 @@ namespace Portal.Consultoras.Web.Controllers
             return listEstrategia;
         }
 
-        //protected virtual List<BEEstrategia> ConsultarEstrategiasPorTipo(string tipo, int campaniaId = 0)
 =======
         private List<BEEstrategia> ConsultarEstrategiasPorTipo(string tipo, int campaniaId = 0)
 >>>>>>> 22170ac250743ce87b09ec26065532a7ed362d97
+        //private List<BEEstrategia> ConsultarEstrategiasPorTipo(string tipo, int campaniaId = 0)
+        private List<ServiceOferta.BEEstrategia> ConsultarEstrategiasPorTipo(string tipo, int campaniaId = 0)
         {
             //var listEstrategia = new List<BEEstrategia>();
             var listEstrategia = new List<ServiceOferta.BEEstrategia>();
