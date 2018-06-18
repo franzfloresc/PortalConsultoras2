5178eeac5a1816cfd69954dd02e1b710808ffb95
Carlos Jave <jave.carlos@gmail.com>
2018-06-18 17:41:32 -0500

    restringiendo descripciones de errores

--- app/Portal.Consultoras.Web/Scripts/LogError.js
+++ app/Portal.Consultoras.Web/Scripts/LogError.js
@@ -117,6 +117,19 @@ function registrarLogError(objError) {
         console.log(objError);
     }
 
+    var listaErroresExcluidos = new Array();
+    listaErroresExcluidos.push("200-OK");
+    listaErroresExcluidos.push("Script error");
+    listaErroresExcluidos.push("Error no especificado."); 
+
+    var esMensajeValido = true;
+    $.each(listaErroresExcluidos, function(index, value) {
+        if (value == objError.Mensaje)
+            esMensajeValido = false;
+    });
+
+    if (!esMensajeValido) return;
+
     if (!urlLogDynamo) return;
 
     var urlLogError = urlLogDynamo + "Api/LogError";
