7b12899728fa17c94759582f2c2c8932a3f1fd19
Mauro Pelaez <mauro.pelaez@hundred.com.pe>
2018-06-18 13:14:15 -0500

    INSERTS OpcionesVerificacion

--- app/Scripts/MejoraContinua/HD-2500/2_Insert_OpcionesVerificacion_MC_HD2500.sql
+++ app/Scripts/MejoraContinua/HD-2500/2_Insert_OpcionesVerificacion_MC_HD2500.sql
@@ -4,7 +4,6 @@ GO
 delete from FiltrosOpcionesVerificacion
 DBCC CHECKIDENT(FiltrosOpcionesVerificacion, RESEED, 0)
 delete from ZonasOpcionesVerificacion
-DBCC CHECKIDENT(ZonasOpcionesVerificacion, RESEED, 0)
 delete from OpcionesVerificacion
 
 go
@@ -15,11 +14,14 @@ insert into OpcionesVerificacion values (1, 'Olvide Contrasenia', 1, 1, 1, 1, 0,
 
 GO
 
-insert into ZonasOpcionesVerificacion values (15, 1, 2),
-											 (81, 1, 2),
-											 (31, 1, 2),
-											 (148, 1, 2),
-											 (48, 1, 2)
+insert into ZonasOpcionesVerificacion values (12029, 15, 0, 1, 0, 0)
+											,(12029, 31, 0, 1, 0, 0)
+											,(12029, 148, 0, 1, 0, 0)
+											,(12029, 149, 0, 1, 0, 0)
+											,(12029, 144, 0, 1, 0, 0)
+											,(12029, 13, 0, 1, 0, 0)
+											,(12029, 155, 0, 1, 0, 0)
+											,(12029, 152, 0, 1, 0, 0)
 GO
 
 
