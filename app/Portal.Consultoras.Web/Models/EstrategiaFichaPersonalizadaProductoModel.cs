31928d946f40d8803d1584727b69c6091e7c7d76
Jorge Nizama <jorgenizamarios@gmail.com>
2018-06-20 11:20:54 -0500

    New objects to model in detail landing

---
+++ app/Portal.Consultoras.Web/Models/EstrategiaFichaPersonalizadaProductoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.Models
+{
+    public class EstrategiaFichaPersonalizadaProductoModel : EstrategiaPersonalizadaProductoModel
+    {
+       //hacer el seguimiento.
+    }
+}
