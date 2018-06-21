6b8985d54ae01596cdfafc935f2da92956868ce9
Felix Sueldo <fsueldov@gmail.com>
2018-06-21 17:23:58 -0500

    Se crea BaseViewController
    RDController, LANController, GNDController, HVController ahora heredan de BaseViewController.

---
+++ app/Portal.Consultoras.Web/Controllers/BaseViewController.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Linq;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.LogManager;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.SessionManager;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Portal.Consultoras.Web.Providers;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class BaseViewController : BaseEstrategiaController
+    {
+        private readonly IssuuProvider _issuuProvider;
+
+        public BaseViewController() : base() {
+            _issuuProvider = new IssuuProvider();
+        }
+
+        public BaseViewController(ISessionManager sesionManager) : base(sesionManager)
+        {
+
+        }
+
+        public BaseViewController(ISessionManager sesionManager, ILogManager logManager) : base(sesionManager, logManager)
+        {
+
+        }
+
+        #region RevistaDigital
+
+        public ActionResult RDIndexModel()
+        {
+            if (revistaDigital.TieneRDI)
+                return View("template-informativa-rdi");
+
+            var esMobile = IsMobile();
+            if (!revistaDigital.TieneRDC && !revistaDigital.TieneRDS)
+                return RedirectToAction("Index", "Ofertas", new { area = esMobile ? "Mobile" : "" });
+
+            var modelo = _revistaDigitalProvider.InformativoModel(IsMobile());
+            return View("template-informativa", modelo);
+        }
+
+        public ActionResult RDViewLanding(int tipo)
+        {
+            var id = tipo == 1 ? userData.CampaniaID : Util.AddCampaniaAndNumero(userData.CampaniaID, 1, userData.NroCampanias);
+
+            var model = new RevistaDigitalLandingModel
+            {
+                CampaniaID = id,
+                IsMobile = IsMobile(),
+                FiltersBySorting = _ofertasViewProvider.GetFiltersBySorting(IsMobile()),
+                FiltersByBrand = _ofertasViewProvider.GetFiltersByBrand(),
+                Success = true,
+                MensajeProductoBloqueado = _ofertasViewProvider.MensajeProductoBloqueado(IsMobile()),
+                CantidadFilas = 15
+            };
+
+            var dato = _ofertasViewProvider.ObtenerPerdioTitulo(model.CampaniaID, IsMobile());
+            model.ProductosPerdio = dato.Estado;
+            model.PerdioTitulo = dato.Valor1;
+            model.PerdioSubTitulo = dato.Valor2;
+
+            model.MostrarFiltros = !model.ProductosPerdio && !(revistaDigital.TieneRDC && !revistaDigital.EsActiva);
+
+            return PartialView("template-landing", model);
+        }
+
+        public ActionResult RDDetalleModel(string cuv, int campaniaId)
+        {
+            var modelo = sessionManager.GetProductoTemporal();
+            if (modelo == null || modelo.EstrategiaID == 0 || modelo.CUV2 != cuv || modelo.CampaniaID != campaniaId)
+            {
+                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+            }
+
+            if (!revistaDigital.TieneRevistaDigital())
+            {
+                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+            }
+
+            if (_ofertaPersonalizadaProvider.EsCampaniaFalsa(modelo.CampaniaID))
+            {
+                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+            }
+            if (modelo.EstrategiaID <= 0)
+            {
+                return RedirectToAction("Index", "Ofertas", new { area = IsMobile() ? "Mobile" : "" });
+            }
+
+            modelo.TipoEstrategiaDetalle = modelo.TipoEstrategiaDetalle ?? new EstrategiaDetalleModelo();
+            modelo.ListaDescripcionDetalle = modelo.ListaDescripcionDetalle ?? new List<string>();
+
+            ViewBag.EstadoSuscripcion = revistaDigital.SuscripcionModel.EstadoRegistro;
+
+            var dato = _ofertasViewProvider.ObtenerPerdioTitulo(modelo.CampaniaID, IsMobile());
+            ViewBag.TieneProductosPerdio = dato.Estado;
+            ViewBag.PerdioTitulo = dato.Valor1;
+            ViewBag.PerdioSubTitulo = dato.Valor2;
+
+            ViewBag.Campania = campaniaId;
+            return View(modelo);
+
+        }
+
+        #endregion
+
+        #region Lanzamiento
+
+        public virtual ActionResult LANDetalle(string cuv, int campaniaId)
+        {
+            try
+            {
+                var esMobile = IsMobile();
+
+                if (!revistaDigital.TieneRevistaDigital() || !revistaDigital.EsActiva)
+                {
+                    return RedirectToAction("Index", "Ofertas", new { area = esMobile ? "Mobile" : "" });
+                }
+
+                var modelo = sessionManager.GetProductoTemporal();
+                if (modelo == null || modelo.EstrategiaID == 0 || _ofertaPersonalizadaProvider.EsCampaniaFalsa(modelo.CampaniaID) ||
+                    modelo.CUV2 != cuv || modelo.CampaniaID != campaniaId)
+                {
+                    return RedirectToAction("Index", "Ofertas", new { area = esMobile ? "Mobile" : "" });
+                }
+
+                modelo.TipoEstrategiaDetalle = modelo.TipoEstrategiaDetalle ?? new EstrategiaDetalleModelo();
+                modelo.ListaDescripcionDetalle = modelo.ListaDescripcionDetalle ?? new List<string>();
+
+                var EstrategiaDetalle = EstrategiaGetDetalle(modelo.EstrategiaID);
+                if (EstrategiaDetalle.Hermanos != null)
+                {
+                    modelo.Hermanos = EstrategiaDetalle.Hermanos;
+                }
+
+                if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.IndividualConTonos)
+                {
+                    if ((modelo.Hermanos.Any()))
+                    {
+                        modelo.ClaseBloqueada = "btn_desactivado_general";
+                    }
+                }
+                else if (modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaFija || modelo.CodigoVariante == Constantes.TipoEstrategiaSet.CompuestaVariable)
+                {
+                    if (modelo.Hermanos != null && modelo.Hermanos.Any())
+                    {
+                        if (modelo.Hermanos[0].Digitable == 1 && modelo.Hermanos[0].Hermanos.Any())
+                        {
+                            modelo.ClaseBloqueada = "btn_desactivado_general";
+                        }
+                    }
+                }
+
+                modelo.MensajeProductoBloqueado = _ofertasViewProvider.MensajeProductoBloqueado(esMobile);
+
+                ViewBag.EstadoSuscripcion = revistaDigital.SuscripcionModel.EstadoRegistro;
+                ViewBag.Campania = campaniaId;
+
+                return View(modelo);
+            }
+            catch (Exception ex)
+            {
+                logManager.LogErrorWebServicesBusWrap(ex, userData.CodigoConsultora, userData.CodigoISO, "BaseLanzamientosController.Detalle");
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Guia Negocio
+
+        public virtual ActionResult GNDViewLanding()
+        {
+            ViewBag.CodigoRevistaActual = _issuuProvider.GetRevistaCodigoIssuu(userData.CampaniaID.ToString(), revistaDigital.TieneRDCR, userData.CodigoISO, userData.CodigoZona);
+            ViewBag.CodigoRevistaAnterior = _issuuProvider.GetRevistaCodigoIssuu(Util.AddCampaniaAndNumero(userData.CampaniaID, -1, userData.NroCampanias).ToString(), revistaDigital.TieneRDCR, userData.CodigoISO, userData.CodigoZona);
+            ViewBag.CodigoRevistaSiguiente = _issuuProvider.GetRevistaCodigoIssuu(Util.AddCampaniaAndNumero(userData.CampaniaID, 1, userData.NroCampanias).ToString(), revistaDigital.TieneRDCR, userData.CodigoISO, userData.CodigoZona);
+
+            var model = new RevistaDigitalLandingModel
+            {
+                CampaniaID = userData.CampaniaID,
+                IsMobile = IsMobile(),
+                FiltersBySorting = _ofertasViewProvider.GetFiltersBySorting(IsMobile()),
+                FiltersByBrand = _ofertasViewProvider.GetFiltersByBrand(),
+                Success = true,
+                MensajeProductoBloqueado = new MensajeProductoBloqueadoModel(),
+                CantidadFilas = 10
+            };
+
+            return PartialView("Index", model);
+        }
+
+        #endregion
+
+        #region Herramienta Venta
+
+        public ActionResult HVViewLanding(int tipo)
+        {
+            var id = tipo == 1 ? userData.CampaniaID : Util.AddCampaniaAndNumero(userData.CampaniaID, 1, userData.NroCampanias);
+
+            var model = new RevistaDigitalLandingModel
+            {
+                CampaniaID = id,
+                IsMobile = IsMobile(),
+                FiltersBySorting = _ofertasViewProvider.GetFiltersBySorting(IsMobile()),
+                FiltersByBrand = _ofertasViewProvider.GetFiltersByBrand(),
+                Success = true,
+                MensajeProductoBloqueado = HVMensajeProductoBloqueado(),
+                CantidadFilas = 10
+            };
+
+            return PartialView("template-landing", model);
+        }
+
+        #endregion
+
+        #region Show Room
+
+
+
+        #endregion
+    }
+}
