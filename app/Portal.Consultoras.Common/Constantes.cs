905b9d6cf5ff140cce1fb6e8a4a190459ea23a83
Kike <lead123456@hotmail.com>
2018-06-22 12:50:34 -0500

    Prol3-Sprint5: Registrar en log de errores, cuando los servicios de Prol2 o Prol3 no responden nada o responden error.

--- app/Portal.Consultoras.Common/Constantes.cs
+++ app/Portal.Consultoras.Common/Constantes.cs
@@ -1137,6 +1137,8 @@ namespace Portal.Consultoras.Common
             public const string Reserva_SinDetalle = "No tiene productos que reservar esta campaña.";
             public const string DeleteAllPedido_Error = "Hubo un problema al intentar eliminar el pedido. Por favor inténtelo nuevamente.";
             public const string Reserva_ObsHuerfanas = "Reserva_ObsHuerfanas: Se obtuvieron observaciones al reservar que no se encuentran en el detalle.";
+            public const string Reserva_Prol2 = "Reserva_Prol2: El servicio externo de reserva de Prol2 retornó vacio.";
+            public const string Reserva_Prol3 = "Reserva_Prol3: El servicio externo de reserva de Sicc retornó vacio o error.";
             public const string ErrorGenerico = "Ocurrio un error, vuelva ha intentarlo.";
         }
 
