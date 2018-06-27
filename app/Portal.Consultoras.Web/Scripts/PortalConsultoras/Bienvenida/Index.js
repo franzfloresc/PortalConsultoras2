f28766a9aaa651f0d63deceb04d4799fbc219f2b
Giancarlos Salas <giansalex@gmail.com>
2018-06-27 18:36:38 -0500

    remove Webcam photo invalid

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Bienvenida/Index.js
@@ -52,8 +52,6 @@ $(document).ready(function () {
         $(this).toggleClass('check_intriga');
     });
 
-   // $('.contenedor_img_perfil').on('click', CargarCamara);
-
     $('#salvavidaTutorial').show();
 
     $("#salvavidaTutorial").click(function () {
@@ -497,93 +495,6 @@ function limitarMinimo(contenido, caracteres, a) {
         return false;
     }
     return true;
-}
-
-function CargarCamara() {
-    Webcam.set({
-        width: 300,
-        height: 300,
-        crop_width: 300,
-        crop_height: 300,
-        image_format: 'jpeg',
-        jpeg_quality: 90,
-        flip_horiz: true
-    });
-    Webcam.attach('#my_camera');
-
-    PopupMostrar('CamaraIntroductoria');
-}
-
-function CerrarCamara() {
-    Webcam.reset();
-    $('#imgFotoTomada').attr('src', '');
-    $('#demo').removeClass('croppie-container').html('');
-
-    PopupCerrar('CamaraIntroductoria');
-}
-
-function CortarFoto() {
-    $('#demo').croppie('result', {
-        type: 'canvas',
-        format: 'png'
-    }).then(function (resp) {
-        waitingDialog();
-        $.ajax({
-            type: 'POST',
-            url: baseUrl + 'Bienvenida/SubirImagen',
-            data: JSON.stringify({ data: resp }),
-            dataType: 'Json',
-            contentType: 'application/json; charset=utf-8',
-            success: function (data) {
-                if (checkTimeout(data)) {
-                    alert_msg(data.message);
-                    if (data.success) {
-                        $('#imgFotoUsuario').show();
-                        $('#imgFotoUsuarioDefault').hide();
-                        $('#imgFotoUsuario').attr('src', data.imagen + '?' + Math.random());
-                    }
-                }
-            },
-            error: function (data, error) { },
-            complete: closeWaitingDialog
-        });
-    });
-}
-
-function TomarFoto() {
-    Webcam.snap(function (data_uri) {
-        $('#imgFotoTomada').attr('src', data_uri);
-        $('#demo').croppie({
-            viewport: {
-                width: 150,
-                height: 150,
-                type: 'circle'
-            },
-            url: data_uri
-        });
-    });
-}
-function SubirFoto() {
-    waitingDialog();
-    $.ajax({
-        type: 'POST',
-        url: baseUrl + 'Bienvenida/SubirImagen',
-        data: JSON.stringify({ data: $('#imgFotoTomada').attr('src') }),
-        dataType: 'Json',
-        contentType: 'application/json; charset=utf-8',
-        success: function (data) {
-            if (checkTimeout(data)) {
-                alert_msg(data.message);
-                if (data.success) {
-                    $('#imgFotoUsuario').show();
-                    $('#imgFotoUsuarioDefault').hide();
-                    $('#imgFotoUsuario').attr('src', data.imagen + '?' + Math.random());
-                }
-            }
-        },
-        error: function (data, error) { },
-        complete: closeWaitingDialog
-    });
 }
 
 function animacionFlechaScroll() {
