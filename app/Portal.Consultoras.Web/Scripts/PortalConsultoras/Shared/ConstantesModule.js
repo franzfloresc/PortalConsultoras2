5af93523598000414bfbbac6ce75f45d88221ed8
Jorge Nizama <jorgenizamarios@gmail.com>
2018-06-28 19:47:59 -0500

    Ajuste para la lectura de constantes a nivel superior (/Shared/ConstantesModule.js) al formar la URL a la ficha detalle.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/ConstantesModule.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/ConstantesModule.js
@@ -1,4 +1,5 @@
-﻿var ConstantesModule = (function() {
+﻿
+var ConstantesModule = (function () {
   
     var _codigosPalanca = {
         OfertaParaTi: "OfertaParaTi",
@@ -32,11 +33,31 @@
         ComuestaFija: "2002",
         CompuestaVariable: "2003"
     }
-    
+
+    var _constantesPalanca = {
+        OfertaParaTi: "001",
+        PackNuevas: "002",
+        OfertaWeb: "003",
+        Lanzamiento: "005",
+        OfertasParaMi: "007",
+        PackAltoDesembolso: "008",
+        RevistaDigital: "101",
+        LosMasVendidos: "020",
+        IncentivosProgramaNuevas: "021",
+        OfertaDelDia: "009",
+        GuiaDeNegocioDigitalizada: "010",
+        Incentivos: "022",
+        ShowRoom: "030",
+        HerramientasVenta: "011",
+        ProgramaNuevasRegalo: "044",
+        ParticipaProgramaNuevas: "1",
+        NotParticipaProgramaNuevas: "0"
+    }
     return {
         CodigosPalanca: _codigosPalanca,
         KeysLocalStorage: _keysLocalStorage,
-        CodigoVariedad: _codigoVariedad
+        CodigoVariedad: _codigoVariedad,
+        ConstantesPalanca: _constantesPalanca
     }
     
 })();
