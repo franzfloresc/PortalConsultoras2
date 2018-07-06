e57fbc306dca6037a0dd8a8347887054705864eb
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-07-06 16:20:03 -0500

    fix bug EPM-1073

--- app/Portal.Consultoras.Web/Controllers/ShowRoomController.cs
+++ app/Portal.Consultoras.Web/Controllers/ShowRoomController.cs
@@ -6,7 +6,6 @@ using Portal.Consultoras.Web.Models.Estrategia.ShowRoom;
 using Portal.Consultoras.Web.ServicePedido;
 using Portal.Consultoras.Web.ServiceUsuario;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Web.Mvc;
