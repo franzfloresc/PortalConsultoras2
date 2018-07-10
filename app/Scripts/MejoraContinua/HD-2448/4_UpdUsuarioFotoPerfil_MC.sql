92ffec2da8d1ae658a31e45558b08a6f01ebddb9
Hugo Roca <hugo.rock20@hotmail.com>
2018-07-10 15:42:33 -0500

    se arreglo un script que tomaba el campo codigousuario en la condición, ahora toma el campo codigoconsultora

---
+++ app/Scripts/MejoraContinua/HD-2448/4_UpdUsuarioFotoPerfil_MC.sql
@@ -0,0 +1,170 @@
+
+GO
+USE BelcorpPeru
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpMexico
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpColombia
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpSalvador
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpPuertoRico
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpPanama
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpGuatemala
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpEcuador
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpDominicana
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpCostaRica
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpChile
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
+USE BelcorpBolivia
+GO
+alter PROCEDURE dbo.UpdUsuarioFotoPerfil
+@CodigoUsuario VARCHAR(20),
+@FotoPerfil VARCHAR(255)
+AS
+BEGIN
+	UPDATE dbo.Usuario
+	SET FotoPerfil = @FotoPerfil
+	WHERE CodigoConsultora = @CodigoUsuario
+END
+
+
+GO
