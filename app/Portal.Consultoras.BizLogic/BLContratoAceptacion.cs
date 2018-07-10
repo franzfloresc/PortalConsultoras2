d57f30a06491f1e7bb8e31a58883b66e33cb9d7a
jose-ponte <jose.ponte@hundred.com.pe>
2018-07-10 18:47:25 -0500

    Se elimino condicion para Colombia HD2705

--- app/Portal.Consultoras.BizLogic/BLContratoAceptacion.cs
+++ app/Portal.Consultoras.BizLogic/BLContratoAceptacion.cs
@@ -7,10 +7,7 @@ namespace Portal.Consultoras.BizLogic
         public int AceptarContratoAceptacion(int paisID, long consultoraid, string codigoConsultora, string origen, string direccionIP, string InformacionSOMobile)
         {
             var daContratoAceptacion = new DAContratoAceptacion(paisID);
-            if (paisID == 4)
-                return daContratoAceptacion.AceptarContratoAceptacionColombia(consultoraid, codigoConsultora, origen, direccionIP, InformacionSOMobile);
-            else
-                return daContratoAceptacion.AceptarContratoAceptacion(consultoraid, codigoConsultora, origen);
+            return daContratoAceptacion.AceptarContratoAceptacion(consultoraid, codigoConsultora, origen, direccionIP, InformacionSOMobile);
         }
     }
 }
