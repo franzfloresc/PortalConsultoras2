3ddb1444bd9482fed691b5039f2da9df391f1beb
Giancarlos Salas <giansalex@gmail.com>
2018-07-12 10:23:59 -0500

    scripts pase

---
+++ app/Scripts/Pases/20180712/1.Script/1_AlterTable_Contrato_MC_HD2705.sql
@@ -0,0 +1,157 @@
+﻿GO
+USE BelcorpPeru
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpMexico
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpColombia
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpSalvador
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpPuertoRico
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpPanama
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpGuatemala
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpEcuador
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpDominicana
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpCostaRica
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpChile
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
+USE BelcorpBolivia
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='DireccionIP')
+BEGIN
+	ALTER TABLE Contrato ADD DireccionIP varchar(15);
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='dbo' and table_name='Contrato' and column_name='InformacionSOMobile')
+BEGIN
+	ALTER TABLE Contrato ADD InformacionSOMobile varchar(500);
+END
+
+GO
