342dfcf653e3e6f50a48e8e2d0a42c3643a0911c
ecangahuala <ecangahuala@gmail.com>
2018-07-24 11:09:54 -0500

    G++ Estabilización de código fuente

---
+++ app/Portal.Consultoras.Web/Models/EstadoProductoComentarioModel.cs
@@ -0,0 +1,19 @@
+﻿using Portal.Consultoras.Common;
+
+namespace Portal.Consultoras.Web.Models
+{
+    public class EstadoProductoComentarioModel
+    {
+        readonly Enumeradores.EstadoProductoComentario _estado;
+
+        public EstadoProductoComentarioModel(Enumeradores.EstadoProductoComentario estado)
+        {
+            this._estado = estado;
+            EstadoComentarioProductoId = (int)this._estado;
+            Nombre = this._estado.ToString();
+        }
+
+        public int EstadoComentarioProductoId { get; private set; }
+        public string Nombre { get; private set; }
+    }
+}
