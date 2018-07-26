2efe3381aeb7593754ffeea26190b76d81cf670d
pcamachog <cgonzalesp@outlook.com>
2018-07-26 10:08:31 -0500

    Se agrega icono para buscar producto en mobile

---
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/BuscadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
+{
+    public class BuscadorController : Controller
+    {
+        // GET: Mobile/Buscador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
