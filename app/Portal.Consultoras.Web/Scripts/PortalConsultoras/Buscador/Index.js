20cea3235f41901b03050db7d90a1825e985417c
HugoRoca <hugo.rock20@hotmail.com>
2018-07-31 16:58:00 -0500

    cambios en buscador/Index.js

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/Index.js
@@ -59,6 +59,7 @@ $(document).ready(function () {
                 if (cantidadCaracteresParaMostrarSugerenciasBusqueda > 0) {
                     me.Funciones.CampoDeBusquedaConCaracteres($('#CampoBuscadorProductos'));
                     if (cantidadCaracteresParaMostrarSugerenciasBusqueda >= CaracteresBuscador) {
+
                         var service = $.ajax({
                             url: baseUrl + "Buscador/BusquedaProductos",
                             method: 'POST',
@@ -68,9 +69,43 @@ $(document).ready(function () {
                         });
 
                         var successBusqueda = function (r) {
-                            console.log(r);
-                        }
+                            var resultados = "";
+                            if (r.Productos.length > 0) {
+                                for (var i = 0; i < r.Productos.length; i++) {
+                                    resultados += '<div class="resultado_busqueda_producto displayBlock text-left">';
+                                    resultados += ' <div class="resultado_busqueda_img_prod displayInlineBlock">';
+                                    resultados += '     <img src="' + r.Productos[i].Imagen + '" alt="Imagen no disponible" class="imagen_no_disponible" onerror="this.onerror=null;this.src="' + baseUrl + 'Content/Images/imagen_prod_no_disponible.png' + '""/>';
+                                    resultados += ' </div>';
+                                    resultados += ' <div class="resultado_busqueda_descrip_prod displayInlineBlock">';
+                                    resultados += '     <div class="resultado_busqueda_nom_producto">' + r.Productos[i].Descripcion + '</div>';
+                                    resultados += '     <div class="resultado_busqueda_precio_prod">';
+                                    resultados += '         <span class="resultado_busqueda_precio_normal displayInlineBlock">' + r.Productos[i].Valorizado + '</span>';
+                                    resultados += '         <span class="resultado_busqueda_precio_oferta displayInlineBlock">' + r.Productos[i].Precio + '</span>';
+                                    resultados += '     </div>';
+                                    resultados += '     <div class="resultado_busqueda_tipo_oferta_prod displayBlock text-uppercase">CATALOGO</div>';
+                                    resultados += ' </div>';
+                                    //condicion
+                                    if (r.Productos[i].CodigoEstrategia == "2003") {
+                                        resultados += '<div class="resultado_busqueda_btn_elegir_opcion_wrapper displayInlineBlock text-center">';
+                                        resultados += ' <a class="btn_elegir_opcion displayBlock text-center text-uppercase">Elige tu opción</a>';
+                                        resultados += '</div>';
+                                    } else {
+                                        resultados += ' <div class="resultado_busqueda_cantidad_prod displayInlineBlock js-no-popup" data-cantidad-contenedor="">';
+                                        resultados += '     <a class="cantidad_menos_home js-no-popup" onclick="javascript:EstrategiaAgregarModule.DisminuirCantidad(event)"><img src = "' + baseUrl + 'Content/Images/Mobile/Esika/menos.png' + '" alt = "" ></a>';
+                                        resultados += '     <input type="tel" value="1" size="2" maxlength="2" id="txtCantidad" data-input="cantidad" class="rango_cantidad_pedido text-center ValidaNumeral ValidaPasteNumeral js-no-popup" />';
+                                        resultados += '     <a class="cantidad_mas_home js-no-popup" onclick="javascript:EstrategiaAgregarModule.AdicionarCantidad(event)"><img src = "' + baseUrl + 'Content/Images/Mobile/Esika/mas.png' + '" alt = "" ></a>';
+                                        resultados += '     <div class="clear"></div>';
+                                        resultados += ' </div>';
+                                        resultados += ' <div class="resultado_busqueda_btn_agregar_wrapper displayInlineBlock text-center">';
+                                        resultados += '     <a class="boton_Agregalo_home displayBlock text-center text-uppercase">Agrégalo</a>';
+                                        resultados += ' </div>';
+                                    }
 
+                                    resultados += '</div>';
+                                }
+                            }
+                            $('#ResultadoBuscador').html(resultados);
+                        }
                         service.then(successBusqueda, function (e) {
                             console.log(e);
                         });
