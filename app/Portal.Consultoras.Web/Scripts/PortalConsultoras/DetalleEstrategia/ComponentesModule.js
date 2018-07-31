2e964026296f0947b340dccdde23c94547908604
ruben9261 <ruben9261@gmail.com>
2018-07-31 17:15:21 -0500

    BPT-PL50-Sprint4-Ficha => EPM-1167

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/ComponentesModule.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/ComponentesModule.js
@@ -36,7 +36,16 @@ var ComponentesModule = (function () {
         //
         opcionesEvents.applyChanges("onComponentSelected", componente);
         //
-        $("#elegir-opciones-modal").modal("show");
+        if (isMobile()) {
+            $("#elegir-opciones-modal").modal("show");
+        } else {
+            $('body').addClass("modal_activado");
+            $('.modal-fondo').css('opacity', '.7');
+            $('.modal-fondo').show();
+            $('.contenedor_seleccion').show();
+            $('.contenedor_seleccion').css('margin-right', '0');
+            $('.contenedor_seleccion').css('opacity', '1');
+        }
 
     }
 
