f9319ea0dcda1a0fb0d2ddd8790b9417183253e2
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-08-06 11:05:24 -0500

    Se agrego los Scripts para el pase a PRD del dia 08/08/2018

---
+++ app/Scripts/Pases/20180808/Rollback/Rollback_4_RegistrarLogOfertaFinal_SB2_BPT-PL50.sql
@@ -0,0 +1,385 @@
+﻿GO
+USE BelcorpPeru
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpMexico
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpColombia
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpSalvador
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpPuertoRico
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpPanama
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpGuatemala
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpEcuador
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpDominicana
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpCostaRica
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpChile
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
+USE BelcorpBolivia
+GO
+GO
+
+ALTER PROCEDURE [dbo].[registrarLogOfertaFinal_SB2]
+	@CampaniaID int,
+	@CodigoConsultora varchar(20),
+	@CUV varchar(20),
+	@Cantidad int,
+	@TipoOfertaFinal varchar(4),
+	@GAP decimal(18,2),
+	@TipoRegistro int = 1,
+	@DesTipoRegistro varchar(150) = null
+AS
+BEGIN
+	declare @CantidadInsertar int
+	if @Cantidad = 0
+	begin
+		set @CantidadInsertar = null
+	end
+	else
+	begin
+		set @CantidadInsertar = @Cantidad
+	end
+
+	INSERT INTO dbo.OfertaFinalConsultora_Log(campaniaID, codigoConsultora, CUV, cantidad, Fecha, TipoOfertaFinal, GAP, TipoRegistro, DesTipoRegistro)
+	VALUES(@CampaniaID, @CodigoConsultora, @CUV, @CantidadInsertar, GETDATE(), @TipoOfertaFinal, @GAP, @TipoRegistro, @DesTipoRegistro)
+END
+GO
+
+
+GO
