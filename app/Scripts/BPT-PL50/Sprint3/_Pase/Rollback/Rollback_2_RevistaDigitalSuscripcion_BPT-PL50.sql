a5500ebbb3bbae2430d99d501698ede07348c879
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-08-06 11:23:18 -0500

    fix Scrips Sprint3

--- app/Scripts/BPT-PL50/Sprint3/_Pase/Rollback/Rollback_2_RevistaDigitalSuscripcion_BPT-PL50.sql
+++ app/Scripts/BPT-PL50/Sprint3/_Pase/Rollback/Rollback_2_RevistaDigitalSuscripcion_BPT-PL50.sql
@@ -3,15 +3,14 @@ go
 UPDATE
 	revistadigitalsuscripcion
 SET
-	Origen = 'NUEVA', SubOrigen = 'RDR'
+	Origen = 'RD', SubOrigen = ''
 WHERE
-	Origen = 'RD' and CodigoZona in 
+	Origen = 'NUEVA' and SubOrigen = 'RDR' and CodigoZona in 
 	(
 	'5011', '5012', '5013', '5014', '5031', '5032', '5033', '5034', '5035', '5036', '5042', '5043', '5044',
 	'5045', '5046', '5051', '5052', '5053', '5054',
 	'8012', '8015', '8016', '8021', '8022', '8026', '8029', '8030'
 	);
-
 go
 
 use BelcorpColombia
@@ -19,9 +18,9 @@ go
 UPDATE
 	revistadigitalsuscripcion
 SET
-	Origen = 'NUEVA', SubOrigen = 'RDR'
+	Origen = 'RD', SubOrigen = ''
 WHERE
-	Origen = 'RD' and CodigoZona in 
+	Origen = 'NUEVA' and SubOrigen = 'RDR' and CodigoZona in 
 	(
 	'0101', '0102', '0103', '0104', '0108', '0123', '0125'
 	);
