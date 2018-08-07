89f574427478a100bc6f9ecdf535d11b938b7639
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-08-07 11:34:27 -0500

    BPT-PL50, se agrego Script pendientes para el pase

---
+++ app/Scripts/Pases/20180808/Script/2_ConfiguracionPais_BPT-PL50.sql
@@ -0,0 +1,7 @@
+
+USE BelcorpChile
+GO
+
+UPDATE ConfiguracionPais SET Activo = 0 WHERE Codigo = 'OFC'
+UPDATE ConfiguracionPais SET Activo = 0 WHERE Codigo = 'OFCGM'
+UPDATE ConfiguracionPais SET Activo = 1 WHERE Codigo = 'OFR'
