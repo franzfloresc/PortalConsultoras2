6af299c45da53fcde5e659d7d46233e771566995
Kike <lead123456@hotmail.com>
2018-08-06 20:09:33 -0500

    HD-2819: Se adjuntan scripts de BD.

---
+++ app/Scripts/MejoraContinua/HD-2819 Fix ProgramaNuevas Campaña/1.New/4_GetConsultoraProgramaNueva_MC_2819.sql
@@ -0,0 +1,238 @@
+﻿USE BelcorpBolivia
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpChile
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpColombia
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpCostaRica
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpDominicana
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpEcuador
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpGuatemala
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpMexico
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpPanama
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpPeru
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpPuertoRico
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
+/*end*/
+
+USE BelcorpSalvador
+GO
+ALTER PROCEDURE dbo.GetConsultoraProgramaNueva
+	@ConsultoraID BIGINT,
+	@Campania VARCHAR(6)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.ConsecutivoNueva, 
+		CP.CodigoPrograma
+	FROM ods.Consultora C WITH(NOLOCK)
+	LEFT JOIN ods.ConsultorasProgramaNuevas CP WITH(NOLOCK) ON
+		C.Codigo = CP.CodigoConsultora AND Campania = @Campania AND CP.Participa = 1
+	WHERE C.ConsultoraID = @ConsultoraID;
+END
+GO
