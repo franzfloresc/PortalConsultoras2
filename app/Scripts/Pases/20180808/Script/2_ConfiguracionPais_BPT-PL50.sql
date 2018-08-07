832c9361b686d26ab094bca2ca65e9b271964b32
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-08-07 11:43:29 -0500

    fix Scripts EPM-1139 Upselling

--- app/Scripts/Pases/20180808/Script/2_ConfiguracionPais_BPT-PL50.sql
+++ app/Scripts/Pases/20180808/Script/2_ConfiguracionPais_BPT-PL50.sql
@@ -2,6 +2,6 @@
 USE BelcorpChile
 GO
 
-UPDATE ConfiguracionPais SET Activo = 0 WHERE Codigo = 'OFC'
-UPDATE ConfiguracionPais SET Activo = 0 WHERE Codigo = 'OFCGM'
-UPDATE ConfiguracionPais SET Activo = 1 WHERE Codigo = 'OFR'
+UPDATE ConfiguracionPais SET Estado = 0 WHERE Codigo = 'OFC'
+UPDATE ConfiguracionPais SET Estado = 0 WHERE Codigo = 'OFCGM'
+UPDATE ConfiguracionPais SET Estado = 1, Excluyente = 0 WHERE Codigo = 'OFR'
