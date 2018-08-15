776b6b30f1f617f4ba6e2e29ca99c59be662b6c5
Jorge Nizama <jorgenizamarios@gmail.com>
2018-08-15 18:44:05 -0500

    UPDATE para la historia EPM-1310

---
+++ app/Scripts/BPT-PL50/Sprint4/Script/2.0.BPT-PL50_ConfiguracionTextosPalancasEnPasePedido.sql
@@ -0,0 +1,16 @@
+﻿
+--script para TODOS los paises
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
