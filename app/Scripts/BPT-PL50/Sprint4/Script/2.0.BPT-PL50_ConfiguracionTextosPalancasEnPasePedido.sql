4d38325ff1fbcf47e3faa8da3dad6e78265dc0f0
Jorge Nizama <jorgenizamarios@gmail.com>
2018-08-16 11:47:53 -0500

    Update EPM-1310 (ultimas notas)

--- app/Scripts/BPT-PL50/Sprint4/Script/2.0.BPT-PL50_ConfiguracionTextosPalancasEnPasePedido.sql
+++ app/Scripts/BPT-PL50/Sprint4/Script/2.0.BPT-PL50_ConfiguracionTextosPalancasEnPasePedido.sql
@@ -1,5 +1,6 @@
 ﻿
---script para TODOS los paises
+
+USE	[BelcorpBolivia]
 --SR
 UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
 --ODD
@@ -14,3 +15,192 @@ UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionE
 UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
 --LAN
 UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE	[BelcorpChile]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE	[BelcorpColombia]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpCostaRica]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpEcuador]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpSalvador]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO
+USE [BelcorpGuatemala]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpMexico]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpPanama]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE	[BelcorpPeru] 
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE [BelcorpPuertoRico]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
+USE  [BelcorpDominicana]
+--SR
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS ESPECIALES' WHERE DescripcionEstrategia = 'ShowRoom'
+--ODD
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTA ¡SOLO HOY!' WHERE DescripcionEstrategia = 'Oferta del Día'
+--OPT
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Oferta para ti'
+--Ofertas Simples
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Simples'
+--Ofertas Niveles
+UPDATE TipoEstrategia SET NombreComercial = 'OFERTAS PARA TI' WHERE DescripcionEstrategia = 'Mis Ofertas Niveles'
+--HV
+UPDATE TipoEstrategia SET NombreComercial = 'HERRAMIENTAS DE VENTA' WHERE DescripcionEstrategia = 'Herramientas de Venta'
+--LAN
+UPDATE TipoEstrategia SET NombreComercial = 'LO NUEVO ¡NUEVO!' WHERE DescripcionEstrategia = 'Lanzamiento'
+
+GO 
