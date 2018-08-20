940ae55279618dd450b74ef637535f986ff3ed57
Victor Marino <vmarinoalvarez@gmail.com>
2018-08-20 12:39:27 -0500

    Update 2_MenuApp_APPCONS-2759.sql

--- app/Scripts/Pases/20180822/Scripts/2_MenuApp_APPCONS-2759.sql
+++ app/Scripts/Pases/20180822/Scripts/2_MenuApp_APPCONS-2759.sql
@@ -1,14 +1,6 @@
 USE BelcorpPeru
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -40,14 +32,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpMexico
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -79,14 +63,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpColombia
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -118,14 +94,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpSalvador
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -157,14 +125,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpPuertoRico
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -196,14 +156,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpPanama
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -235,14 +187,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpGuatemala
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -274,14 +218,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpEcuador
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -313,14 +249,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpDominicana
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -352,14 +280,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpCostaRica
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -391,14 +311,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpChile
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
@@ -430,14 +342,6 @@ WHERE [Codigo] = 'MEN_TOP_PEDIDO'  AND [VersionMenu] = 4;
 USE BelcorpBolivia
 GO
 
-/* UPDATE REGISTROS */
-UPDATE MenuApp 
-SET 
-	[Descripcion] = 'OFERTAS DIGITALES',
-	[Descripcion2] = 'CLUB GANA +',
-	[Descripcion3] = 'OFERTAS DIGITALES'
-WHERE [Codigo] IN ('MEN_TOP_OFERTAS', 'MEN_LAT_OFERTAS') 
-	AND [VersionMenu] = 4;
 /* OCULTAR MENU TOP PEDIDO  */
 UPDATE MenuApp
 	SET [Visible] = 0
