37ceb0199171e2f8577a59409fbedecdb8c6c77e
jose-ponte <jose.ponte@hundred.com.pe>
2018-08-24 11:35:47 -0500

    Se agrego el mensaje de texto para verificacion autenticidad HD-2875

--- app/Scripts/MejoraContinua/HD-2875 Activar Sms Chile/2_TablaLogicaDatos_MC_HD-2875.sql
+++ app/Scripts/MejoraContinua/HD-2875 Activar Sms Chile/2_TablaLogicaDatos_MC_HD-2875.sql
@@ -5,4 +5,6 @@ Insert Into TablaLogicaDatos values (13300, 133, 'USUARIO', 'Usuario para el pro
 									(13301, 133, 'CLAVE', 'Clave para el proveedor, en caso lo requiera', 'ofG2Xo7dJo9/z8tCzQZdyg=='),
 									(13302, 133, 'URL', 'URL de Solicitud', 'http://www.belcorpchile.cl'),
 									(13303, 133, 'RECURSO', 'Ruta del recurso', '/ServiciosRest/sms/enviar'),
-									(13304, 133, 'MENSAJE', 'Mensaje de Texto Verificacion autenticiad', 'Mensaje de prueba')
+									(13304, 133, 'MENSAJE', 'Mensaje de Texto Verificacion autenticiad', '{0} Es su PIN de verificacion para ingresar a Somos Belcorp')
+
+
