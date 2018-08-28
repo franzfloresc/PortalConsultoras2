c1f500bfc2b3fb2524c7dadde0e11b945f2f7462
Victor Vilela <victor.vilela@avantica.net>
2018-08-28 16:37:25 -0500

    add DetalleEstrategiaControllerUnitTests

--- app/Portal.Consultoras.Web.UnitTest/Controllers/DetalleEstrategiaControllerUnitTests.cs
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/DetalleEstrategiaControllerUnitTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Portal.Consultoras.Web.Controllers;
+using Portal.Consultoras.Web.UnitTest.Extensions;
 
 namespace Portal.Consultoras.Web.UnitTest.Controllers
 {
@@ -9,17 +13,32 @@ namespace Portal.Consultoras.Web.UnitTest.Controllers
         [TestClass]
         public class Ficha : Base
         {
+            [TestInitialize]
             public override void Test_Initialize()
             {
                 base.Test_Initialize();
             }
 
+            [TestCleanup]
             public override void Test_Cleanup()
             {
                 base.Test_Cleanup();
             }
 
-            
+            [TestMethod]
+            public void Ficha_parameterPalancaIsNotValid_RedirectsToOfertas()
+            {
+                // Arrange
+                var controller = new DetalleEstrategiaController();
+
+                // Act
+                var actualResult = controller.Ficha(null, 0, null, null) as RedirectToRouteResult;
+
+                // Assert
+                Assert.AreEqual("Ofertas", actualResult.GetControllerName());
+                Assert.AreEqual("Index", actualResult.GetActionName());
+                Assert.AreEqual("", actualResult.GetAreaName());
+            }
         }
     }
 }
