3cc8153dceae89aa69815dca6febbdb2e0a8a22a
Victor Vilela <victor.vilela@avantica.net>
2018-08-28 10:50:19 -0500

    add DetalleEstrategiaControllerUnitTests class

---
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/DetalleEstrategiaControllerUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Portal.Consultoras.Web.UnitTest.Controllers
+{
+    [TestClass]
+    public class DetalleEstrategiaControllerUnitTests 
+    {
+        [TestClass]
+        public class Ficha : Base
+        {
+            public override void Test_Initialize()
+            {
+                base.Test_Initialize();
+            }
+
+            public override void Test_Cleanup()
+            {
+                base.Test_Cleanup();
+            }
+
+            
+        }
+    }
+}
