2d4e6f7a1c8f678dddd047dc1d3568c224f92cec
Victor Marino <victor.marino@hundred.com.pe>
2018-08-30 11:59:05 -0500

    Add Scripts Pase App Ver. 1.2.0 - 3

---
+++ app/Scripts/Pases/20180905/2.Rollback/ROLLBACK_1_MenuApp_APPCONS-3025.sql
@@ -0,0 +1,84 @@
+﻿USE BelcorpPeru
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpMexico
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpColombia
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpSalvador
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpPuertoRico
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpPanama
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpGuatemala
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpEcuador
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpDominicana
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpCostaRica
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpChile
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
+USE BelcorpBolivia
+GO
+
+UPDATE dbo.MenuApp SET Visible = 1 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDO')
+UPDATE dbo.MenuApp SET Visible = 0 WHERE VersionMenu = 4 AND Codigo IN ('MEN_TOP_PEDIDOSNAT')
+GO
+
