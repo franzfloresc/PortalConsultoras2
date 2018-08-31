7bb91a5f6d62bacd4887c6a284ea5f2558f36a21
Oscar Gaspar <oscar.gaspar@zoluxiones.com>
2018-08-31 11:17:07 -0500

    Mejoras HD-2846

---
+++ app/Scripts/MejoraContinua/MC-HD2846/3_CONTRATO_MC_HD2846.sql
@@ -0,0 +1,36 @@
+﻿use belcorpColombia
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[ObtenerReporteContratoAceptacion]') 
+	AND type in (N'P', N'PC')
+) 
+DROP PROCEDURE [dbo].[ObtenerReporteContratoAceptacion]
+GO
+CREATE procedure ObtenerReporteContratoAceptacion
+(
+ @CodigoConsultora varchar(25)=null,
+ @Cedula varchar(20)=null,
+ @fechaInicio datetime=null,
+ @fechaFin datetime=null
+)
+as
+begin
+select ROW_NUMBER() OVER(ORDER BY a.CodigoConsultora ASC) AS Registro
+       ,a.CodigoConsultora
+	   ,concat(concat(concat(concat(b.PrimerNombre,' '),b.PrimerApellido),' '),b.SegundoApellido) as NombreConsultora	  
+	   ,case a.AceptoContrato when 1 then 'Acepto' else 'No Acepto' end AceptoContrato
+	   ,a.FechaAceptacion
+	   ,isnull(a.origen,'') as Origen
+	   ,isnull(a.DireccionIP,'') as DireccionIP
+	   ,isnull(a.InformacionSOMobile,'') as InformacionSOMobile
+	   from contrato a
+	   inner join ods.consultora b on a.CodigoConsultora=b.Codigo
+	   inner join ods.identificacion c on c.ConsultoraID=b.ConsultoraID
+	   where (@CodigoConsultora is null or @CodigoConsultora='' or a.CodigoConsultora=@CodigoConsultora) and
+	         (@Cedula is null  or @Cedula=''  or c.Numero=@Cedula) and
+			 ( cast(a.FechaAceptacion as date)>= @fechaInicio and  cast(a.FechaAceptacion as date)<= @fechaFin or(@fechaInicio is null and @fechaFin is null)) 
+end
+go
---
+++ app/Scripts/MejoraContinua/MC-HD2846/ROOLLBACK/1_CONTRATO_MC_HD2846 -ROOLLBACK.sql
@@ -0,0 +1,10 @@
+﻿use belcorpColombia
+go
+
+DELETE FROM Permiso
+WHERE PermisoID=1070
+
+DELETE FROM RolPermiso
+WHERE RolID=3 AND PermisoID=1070
+  
+GO
