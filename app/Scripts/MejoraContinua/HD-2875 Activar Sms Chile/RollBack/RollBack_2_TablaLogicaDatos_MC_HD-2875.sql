10ecce4170b3e251486ab0e98fe581d5e235fa09
jose-ponte <jose.ponte@hundred.com.pe>
2018-09-03 11:46:27 -0500

    modificacion de Script SMS Chile HD-2875

--- app/Scripts/MejoraContinua/HD-2875 Activar Sms Chile/RollBack/RollBack_2_TablaLogicaDatos_MC_HD-2875.sql
+++ app/Scripts/MejoraContinua/HD-2875 Activar Sms Chile/RollBack/RollBack_2_TablaLogicaDatos_MC_HD-2875.sql
@@ -1,3 +1,6 @@
+USE BelcorpChile
+GO
 Delete from TablaLogicaDatos where TablaLogicaID = 133
 Insert Into TablaLogicaDatos values (13300,	133, 'USUARIO', 'Usuario para el proveedor sms, en caso lo requiera', ''),
 									(13301,	133, 'CLAVE', 'Clave para el proveedor, en caso lo requiera', '')
+GO
