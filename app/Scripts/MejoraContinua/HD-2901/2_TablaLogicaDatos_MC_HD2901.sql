f14805dbc9f45408c34ff382a82b4bba2ed255fa
Giancarlos Salas <giansalex@gmail.com>
2018-09-06 18:51:38 -0500

    update scripts HD-2901

--- app/Scripts/MejoraContinua/HD-2901/2_TablaLogicaDatos_MC_HD2901.sql
+++ app/Scripts/MejoraContinua/HD-2901/2_TablaLogicaDatos_MC_HD2901.sql
@@ -1,6 +1,9 @@
 USE BelcorpMexico;
 GO
-Update tablalogicadatos SET Codigo='2.4' where tablalogicaDatosid=12206
+Update tablalogicadatos SET Codigo='2.4' where tablalogicaDatosid=12206;
 -- codigo banco
-Update tablalogicadatos SET Codigo='20154026' where tablalogicaDatosid=12212
+Update tablalogicadatos SET Codigo='20154026' where tablalogicaDatosid=12212;
+
+UPDATE PagoEnLineaMedioPagoDetalle SET ExpresionRegularTarjeta = '^(4)(\d{12}|\d{15})$|^(606374\d{10}$)' WHERE PagoEnLineaMedioPagoId = 1 AND TipoTarjeta = 'ALL';
+UPDATE PagoEnLineaMedioPagoDetalle SET ExpresionRegularTarjeta = '^(5[1-5]\d{14}$)|^(2(?:2(?:2[1-9][3-9]\d)|[3-6]\d\d|7(?:[01]\d|20))\d{12}$)' WHERE PagoEnLineaMedioPagoId = 2 AND TipoTarjeta = 'ALL';
 GO
