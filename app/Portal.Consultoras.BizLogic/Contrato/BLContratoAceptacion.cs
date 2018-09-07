0dc66b2023df583825cd48432bb86a7f1102708e
Carlos Jave <jave.carlos@gmail.com>
2018-09-07 13:49:00 -0500

    ajuste por integracion

--- app/Portal.Consultoras.BizLogic/Contrato/BLContratoAceptacion.cs
+++ app/Portal.Consultoras.BizLogic/Contrato/BLContratoAceptacion.cs
@@ -1,6 +1,7 @@
 ﻿using Portal.Consultoras.Common;
 using Portal.Consultoras.Data;
 using Portal.Consultoras.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data;
 
@@ -19,6 +20,38 @@ namespace Portal.Consultoras.BizLogic
             var daContratoAceptacion = new DAContratoAceptacion(paisID);
             using (IDataReader reader = daContratoAceptacion.GetContratoAceptacion(consultoraid))
                 return reader.MapToCollection<BEContrato>();
+        }
+
+        public List<BeReporteContrato> ReporteContratoAceptacion(int paisID, string codigoconsultora, string cedula, DateTime? fechaInicio, DateTime? FechaFin)
+        {
+            List<BeReporteContrato> listsBeReporteContrato = new List<BeReporteContrato>();
+
+            //if (string.IsNullOrEmpty(codigoUsuario) && fechaIngreso == DateTime.MinValue && string.IsNullOrEmpty(codigoConsultora) && string.IsNullOrEmpty(codigoZona) && campaniaID == 0)
+            //    return pedidosIngresados;
+
+            var daContratoAc = new DAContratoAceptacion(paisID);
+
+            using (IDataReader reader = daContratoAc.ReporteContratoAceptacion(codigoconsultora, cedula, fechaInicio, FechaFin))
+            {
+                var columns = ((IDataRecord)reader).GetAllNames();
+
+                while (reader.Read())
+                {
+                    listsBeReporteContrato.Add(new BeReporteContrato()
+                    {
+                        Registro = Convert.ToInt32(reader[0]),
+                        CodigoConsultora = reader[1].ToString(),
+                        NombreConsultora = reader[2].ToString(),
+                        AceptoContrato = reader[3].ToString(),
+                        FechaAceptacion = reader[4].ToString(),
+                        Origen = reader[5].ToString(),
+                        DireccionIP = reader[6].ToString(),
+                        InformacionSOMobile = reader[7].ToString()
+                    });
+                }
+
+            }
+            return listsBeReporteContrato;
         }
     }
 }
--- app/Portal.Consultoras.BizLogic/Contrato/IContratoAceptacionBusinessLogic.cs
+++ app/Portal.Consultoras.BizLogic/Contrato/IContratoAceptacionBusinessLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Portal.Consultoras.Entities;
+using System;
 using System.Collections.Generic;
 
 namespace Portal.Consultoras.BizLogic
@@ -7,5 +8,6 @@ namespace Portal.Consultoras.BizLogic
     {
         int AceptarContratoAceptacion(int paisID, long consultoraid, string codigoConsultora, string origen, string direccionIP, string InformacionSOMobile, string imei, string deviceID);
         List<BEContrato> GetContratoAceptacion(int paisID, long consultoraid);
+        List<BeReporteContrato> ReporteContratoAceptacion(int paisID, string codigoconsultora, string cedula, DateTime? fechaInicio, DateTime? FechaFin);
     }
 }
