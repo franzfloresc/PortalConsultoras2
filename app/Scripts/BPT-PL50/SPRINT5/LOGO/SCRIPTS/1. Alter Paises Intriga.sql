628ade6dfc70ee99e8723eec513cfce5a3fd52a1
Felix Sueldo <fsueldov@gmail.com>
2018-09-12 17:34:22 -0500

    fix Historia EPM-1345

---
+++ app/Scripts/BPT-PL50/SPRINT5/LOGO/SCRIPTS/1. Alter Paises Intriga.sql
@@ -0,0 +1,424 @@
+
+use [BelcorpBolivia]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpEcuador]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpSalvador]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpGuatemala]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpMexico]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpPanama]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpPuertoRico]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
+
+use [BelcorpDominicana]
+go
+UPDATE
+	[dbo].[ConfiguracionPais]
+SET
+	DesktopFondoBanner = 'banner-ganamas.jpg', MobileLogoBanner = 'oferta_digital_logo_normal.png'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1;
+
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	valor1 = 'gifofertasdigitales.gif'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'LogoMenuOfertas';
+
+/* HOME  INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DBienvenidaIntriga', 'MBienvenidaIntriga');
+/* HOME FIN */
+
+/* PASE PEDIDO INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DPedidoIntriga', 'MPedidoIntriga');
+/* PASE PEDIDO FIN */
+
+/* CATALOGOS Y REVISTAS INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, encuentra aquí las promociones de campaña'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo in ('DCatalogoIntriga', 'MCatalogoIntriga');
+/* CATALOGOS Y REVISTAS FIN */
+
+/* BANNER CONTENEDOR INICIO */
+UPDATE
+	[dbo].[ConfiguracionPaisDatos]
+SET
+	Valor1 = '#NOMBRE, BIENVENIDA A TU ESPACIO EXCLUSIVO'
+WHERE
+	ConfiguracionPaisID = 17 and Estado = 1 and Codigo = 'DLandingBannerIntriga';
+/* BANNER CONTENEDOR FIN */
+go
