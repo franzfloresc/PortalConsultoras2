1ad30f38ae9775f751b13517d165facae75af758
Victor Vilela <victor.vilela@avantica.net>
2018-09-14 12:21:52 -0500

    EPM-1445 : change Club Gana+ logo by Gana+ logo in Contenedor [Desktop]

---
+++ app/Scripts/PI2-EMP/Sprint1/CambioExperiencia/Scripts/2.1.ConfiguracionPaisDatos.sql
@@ -0,0 +1,64 @@
+﻿
+
+use BelcorpPeru_BPT
+go
+
+print  'BD : ' + db_name()
+
+declare @RevistaDigital_Codigo varchar(50) = 'RD'
+declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
+declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+
+print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialFondoActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+go
+
+use BelcorpChile_BPT
+go
+
+print  'BD : ' + db_name()
+
+declare @RevistaDigital_Codigo varchar(50) = 'RD'
+declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
+declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+
+print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialFondoActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+go
+
+use BelcorpCostaRica_BPT
+go
+
+print  'BD : ' + db_name()
+
+declare @RevistaDigital_Codigo varchar(50) = 'RD'
+declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
+declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+
+print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialFondoActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+go
