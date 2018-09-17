dc61b4002aa194fa9cdd7cb948522062a63fd6b3
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-09-17 14:04:28 -0500

    Se integran cambios de antigua rama de Buscador y Filtros Sprint 4.

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayoutResponsive.js
@@ -0,0 +1,50 @@
+﻿var Layout;
+
+$(document).ready(function () {
+    'use strict';
+
+    var mainLR;
+
+    mainLR = function () {
+        var me = this;
+
+        me.globals = {
+            iconoMenuMobile: $('.header__icono__menu'),
+            iconoCerrarMenuMobile: $('.icono__cerrar'),
+            menuPrincipal: $('.header__menu--lateralIzquierdo'),
+            isMobile: window.matchMedia('(max-width:991px)').matches
+        },
+
+        me.Funciones = {
+            InicializarEventos: function () {
+                if (me.globals.isMobile) {
+                    $(me.globals.iconoMenuMobile).on('click', me.Eventos.MostrarMenuMobile);
+                    $(me.globals.iconoCerrarMenuMobile).on('click', me.Eventos.CerrarMenuMobile);
+                }
+            }
+        },
+
+        me.Eventos = {
+            MostrarMenuMobile: function (e) {
+                e.preventDefault();
+                $(me.globals.menuPrincipal).animate({
+                    left: 0 + '%'
+                }, 200);
+            },
+            CerrarMenuMobile: function (e) {
+                e.preventDefault();
+                $(me.globals.menuPrincipal).animate({
+                    left: -100 + '%'
+                }, 200);
+            }
+        },
+
+        me.Inicializar = function () {
+            me.Funciones.InicializarEventos();
+        }
+    }
+
+    Layout = new mainLR();
+    Layout.Inicializar();
+
+});
