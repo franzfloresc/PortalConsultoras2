7bee3040c83f001b5ebc7b2f34dbe3fac3e35f9d
MIJAEL888 <mijael888@gmail.com>
2018-09-17 15:17:45 -0500

    EPM-1443 cambio de nombre a Gana+ en Bienvenida/Pedido/Catálogo

--- app/Scripts/PI2-EMP/Sprint1/CambioExperiencia/Scripts/2.1.ConfiguracionPaisDatos.sql
+++ app/Scripts/PI2-EMP/Sprint1/CambioExperiencia/Scripts/2.1.ConfiguracionPaisDatos.sql
@@ -1,6 +1,4 @@
-﻿
-
-use BelcorpPeru_BPT
+﻿use BelcorpPeru_BPT
 go
 
 print  'BD : ' + db_name()
@@ -8,6 +6,14 @@ print  'BD : ' + db_name()
 declare @RevistaDigital_Codigo varchar(50) = 'RD'
 declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
 declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+declare @LogoMenuOfertasActiva_Codigo varchar(50) = 'LogoMenuOfertasActiva'
+declare @LogoMenuOfertasActiva_Valor1 varchar(800) = 'gif-club-ganamas2.gif'
+declare @PopupImagenEtiqueta_Codigo varchar(50) = 'PopupImagenEtiqueta'
+declare @PopupImagenEtiqueta_Valor1 varchar(800) = 'ClubGanaMas-etiqueta-dorado2.png'
+declare @PopupBotonTexto_Codigo varchar(50) = 'PopupBotonTexto'
+declare @PopupBotonTexto_Valor1 varchar(800) = 'Conoce Gana+'
+declare @LogoComercialActiva_Codigo varchar(50) = 'LogoComercialActiva'
+declare @LogoComercialActiva_Valor1 varchar(800) = 'logotipo-club-ganamaplus-blanco2.svg'
 
 print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
 
@@ -18,6 +24,47 @@ from ConfiguracionPais cp
 		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
 where cp.codigo = @RevistaDigital_Codigo
 and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+print  'Actualizando ''LogoMenuOfertasActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoMenuOfertasActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoMenuOfertasActiva_Codigo
+
+print  'Actualizando ''PopupImagenEtiqueta'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupImagenEtiqueta_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupImagenEtiqueta_Codigo
+
+print  'Actualizando ''PopupBotonTexto'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupBotonTexto_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupBotonTexto_Codigo
+
+print  'Actualizando ''LogoComercialActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialActiva_Valor1,
+	cpd.Valor2 =@LogoComercialActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialActiva_Codigo
 
 go
 
@@ -29,6 +76,14 @@ print  'BD : ' + db_name()
 declare @RevistaDigital_Codigo varchar(50) = 'RD'
 declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
 declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+declare @LogoMenuOfertasActiva_Codigo varchar(50) = 'LogoMenuOfertasActiva'
+declare @LogoMenuOfertasActiva_Valor1 varchar(800) = 'gif-club-ganamas2.gif'
+declare @PopupImagenEtiqueta_Codigo varchar(50) = 'PopupImagenEtiqueta'
+declare @PopupImagenEtiqueta_Valor1 varchar(800) = 'ClubGanaMas-etiqueta-dorado2.png'
+declare @PopupBotonTexto_Codigo varchar(50) = 'PopupBotonTexto'
+declare @PopupBotonTexto_Valor1 varchar(800) = 'Conoce Gana+'
+declare @LogoComercialActiva_Codigo varchar(50) = 'LogoComercialActiva'
+declare @LogoComercialActiva_Valor1 varchar(800) = 'logotipo-club-ganamaplus-blanco2.svg'
 
 print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
 
@@ -39,6 +94,47 @@ from ConfiguracionPais cp
 		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
 where cp.codigo = @RevistaDigital_Codigo
 and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+print  'Actualizando ''LogoMenuOfertasActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoMenuOfertasActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoMenuOfertasActiva_Codigo
+
+print  'Actualizando ''PopupImagenEtiqueta'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupImagenEtiqueta_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupImagenEtiqueta_Codigo
+
+print  'Actualizando ''PopupBotonTexto'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupBotonTexto_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupBotonTexto_Codigo
+
+print  'Actualizando ''LogoComercialActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialActiva_Valor1,
+	cpd.Valor2 =@LogoComercialActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialActiva_Codigo
 
 go
 
@@ -50,6 +146,14 @@ print  'BD : ' + db_name()
 declare @RevistaDigital_Codigo varchar(50) = 'RD'
 declare @LogoComercialFondoActiva_Codigo varchar(50) = 'LogoComercialFondoActiva'
 declare @LogoComercialFondoActiva_Valor1 varchar(800) = 'logo-club-ganamas-dorado2.png'
+declare @LogoMenuOfertasActiva_Codigo varchar(50) = 'LogoMenuOfertasActiva'
+declare @LogoMenuOfertasActiva_Valor1 varchar(800) = 'gif-club-ganamas2.gif'
+declare @PopupImagenEtiqueta_Codigo varchar(50) = 'PopupImagenEtiqueta'
+declare @PopupImagenEtiqueta_Valor1 varchar(800) = 'ClubGanaMas-etiqueta-dorado2.png'
+declare @PopupBotonTexto_Codigo varchar(50) = 'PopupBotonTexto'
+declare @PopupBotonTexto_Valor1 varchar(800) = 'Conoce Gana+'
+declare @LogoComercialActiva_Codigo varchar(50) = 'LogoComercialActiva'
+declare @LogoComercialActiva_Valor1 varchar(800) = 'logotipo-club-ganamaplus-blanco2.svg'
 
 print  'Actualizando ''LogoComercialFondoActiva'' en ''ConfiguracionPaisDatos'''
 
@@ -60,5 +164,46 @@ from ConfiguracionPais cp
 		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
 where cp.codigo = @RevistaDigital_Codigo
 and cpd.Codigo = @LogoComercialFondoActiva_Codigo
+
+print  'Actualizando ''LogoMenuOfertasActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoMenuOfertasActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoMenuOfertasActiva_Codigo
+
+print  'Actualizando ''PopupImagenEtiqueta'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupImagenEtiqueta_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupImagenEtiqueta_Codigo
+
+print  'Actualizando ''PopupBotonTexto'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @PopupBotonTexto_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @PopupBotonTexto_Codigo
+
+print  'Actualizando ''LogoComercialActiva'' en ''ConfiguracionPaisDatos'''
+
+update cpd
+set cpd.Valor1 = @LogoComercialActiva_Valor1,
+	cpd.Valor2 =@LogoComercialActiva_Valor1
+from ConfiguracionPais cp
+	join ConfiguracionPaisDatos cpd 
+		on cp.ConfiguracionPaisID = cpd.ConfiguracionPaisID 
+where cp.codigo = @RevistaDigital_Codigo
+and cpd.Codigo = @LogoComercialActiva_Codigo
 
 go
