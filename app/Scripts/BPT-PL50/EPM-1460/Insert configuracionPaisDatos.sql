23912d1b448b45991339d06202a0bf8249b12025
jhonattanalarcon <jhonattan_alarcon@hotmail.com>
2018-09-20 17:37:28 -0500

    https://jira.belcorp.biz/browse/EPM-1460 menu horizotnal

--- app/Scripts/BPT-PL50/EPM-1460/Insert configuracionPaisDatos.sql
+++ app/Scripts/BPT-PL50/EPM-1460/Insert configuracionPaisDatos.sql
@@ -1,15 +1,5 @@
 use belcorpperu_bpt
 go
-select * from configuracionpaisdatos where configuracionpaisid=3 and codigo like '%logom%'
-
--- select * from configuracionpais where configuracionpaisid=3
-
-
-
---select * from configuracionpais where codigo='RD'
-
-
-
 
 
 update configuracionpaisdatos set valor1='gana_mas_seleccionado.svg', valor2='gana_mas_seleccionado.svg' where codigo='LogoMenuInicioActiva';
@@ -21,30 +11,28 @@ go
 if(not(exists(select 1 from configuracionpaisdatos where codigo='LogoMenuInicioNoSuscrita' )))
 begin
 
-
-
-INSERT INTO [dbo].[ConfiguracionPaisDatos]
-           ( 
-		   configuracionpaisid,
-           [Codigo]
-           ,[CampaniaID]
-           ,[Valor1]
-           ,[Valor2]
-           ,[Valor3]
-           ,[Descripcion]
-           ,[Estado]
-           ,[Componente])
-     VALUES
-           ( 
-		  ( select configuracionpaisid from configuracionpais where codigo='RD'),
-           'LogoMenuInicioNoSuscrita'
-           ,0
-           ,'ofertas_digitales_seleccionado.svg'
-           ,'ofertas_digitales_seleccionado.svg'
-           ,null
-           ,'Logo del menu inicio para desktop y mobile para la consultora Activa'
-           ,1
-           ,null)
+		INSERT INTO [dbo].[ConfiguracionPaisDatos]
+				   ( 
+				   configuracionpaisid,
+				   [Codigo]
+				   ,[CampaniaID]
+				   ,[Valor1]
+				   ,[Valor2]
+				   ,[Valor3]
+				   ,[Descripcion]
+				   ,[Estado]
+				   ,[Componente])
+			 VALUES
+				   ( 
+				  ( select configuracionpaisid from configuracionpais where codigo='RD'),
+				   'LogoMenuInicioNoSuscrita'
+				   ,0
+				   ,'ofertas_digitales_seleccionado.svg'
+				   ,'ofertas_digitales_seleccionado.svg'
+				   ,null
+				   ,'Logo del menu inicio para desktop y mobile para la consultora Activa'
+				   ,1
+				   ,null)
 
 
 end
@@ -56,28 +44,28 @@ begin
 
 
 
-INSERT INTO [dbo].[ConfiguracionPaisDatos]
-           ( 
-		   configuracionpaisid,
-           [Codigo]
-           ,[CampaniaID]
-           ,[Valor1]
-           ,[Valor2]
-           ,[Valor3]
-           ,[Descripcion]
-           ,[Estado]
-           ,[Componente])
-     VALUES
-           ( 
-		     ( select configuracionpaisid from configuracionpais where codigo='RD'),
-           'LogoMenuInicioNoActivaNoSuscrita'
-           ,0
-           ,'ofertas_digitales_no_seleccionado.svg'
-           ,'ofertas_digitales_no_seleccionado.svg'
-           ,null
-           ,'Logo del menu inicio para desktop y mobile para la consultora Activa'
-           ,1
-           ,null)
+			INSERT INTO [dbo].[ConfiguracionPaisDatos]
+					   ( 
+					   configuracionpaisid,
+					   [Codigo]
+					   ,[CampaniaID]
+					   ,[Valor1]
+					   ,[Valor2]
+					   ,[Valor3]
+					   ,[Descripcion]
+					   ,[Estado]
+					   ,[Componente])
+				 VALUES
+					   ( 
+						 ( select configuracionpaisid from configuracionpais where codigo='RD'),
+					   'LogoMenuInicioNoActivaNoSuscrita'
+					   ,0
+					   ,'ofertas_digitales_no_seleccionado.svg'
+					   ,'ofertas_digitales_no_seleccionado.svg'
+					   ,null
+					   ,'Logo del menu inicio para desktop y mobile para la consultora Activa'
+					   ,1
+					   ,null)
 
 end
 GO
