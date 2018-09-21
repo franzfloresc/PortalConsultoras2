1599c991e992a2e5c2be6b643e53bde3fe978485
Lennyn <lennyn.muchotrigo@hundred.com.pe>
2018-09-21 12:52:11 -0500

    Activar Flag para Activar o desactivar URL [HD-2971]

--- app/Portal.Consultoras.Common/Constantes.cs
+++ app/Portal.Consultoras.Common/Constantes.cs
@@ -174,6 +174,10 @@ namespace Portal.Consultoras.Common
                 public static readonly int ColorFondoBanner = 9301;
                 public static readonly int ColorFondoDisplay = 9302;
             }
+
+            public const int ActualizaEscalaDescuentoDestokp = 14514;
+            public const int ActualizaEscalaDescuentoMobile = 14515;
+
         }
 
         public static class ParametrosNames
@@ -1609,6 +1613,9 @@ namespace Portal.Consultoras.Common
             public const short HabilitarChatEmtelco = 144;
             public const short OrdenamientoShowRoom = 99;
             public const short NuevaDescripcionProductos = 145;
+
+            public const short EscalaDescuentoDestokp = 72;
+            public const short EscalaDescuentoMobile = 73;
         }
 
         public struct MensajesCDRExpress
