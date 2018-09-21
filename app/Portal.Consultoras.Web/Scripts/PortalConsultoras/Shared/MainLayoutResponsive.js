aab76ddeac8dfcaf72cfb87ea9b95e94b324de29
Giovanni <giovanni.benvenutto@zoluxiones.com>
2018-09-21 17:27:37 -0500

    Ajustes marca país (Ésika y L'bel) (Header)

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayoutResponsive.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayoutResponsive.js
@@ -44,9 +44,10 @@ $(document).ready(function () {
                 e.preventDefault();
                 var subMenuMobile = $(this).next();
                 $('.menu__link--conSubmenuMobile').next().slideUp(80);
-                if(subMenuMobile.css('display') == 'block'){
+                if(subMenuMobile.css('display') == 'flex'){
                     subMenuMobile.slideUp(80);
                 } else {
+                    subMenuMobile.css('display', 'flex');
                     subMenuMobile.slideDown(130);
                 }
             }
