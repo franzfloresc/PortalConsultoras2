3106a35eb2dbeaaaeaa11b0a7e9cc3906db5094c
pcamachog <cgonzalesp@outlook.com>
2018-09-24 14:12:34 -0500

    update

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayoutResponsive.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayoutResponsive.js
@@ -9,9 +9,7 @@
         itemFooter: ".layout__footer--mobile .footer__item__title"
     },
     me.Config = {
-        isMobile: window.matchMedia("(max-width:991px)").matches
-
-                    document.getElementById('pUrlProductosPedido').href = '/Mobile/Pedido/Detalle';
+        isMobile: window.matchMedia("(max-width:991px)").matches                    
     },
     me.Eventos = {
         MostrarMenuMobile: function(e) {
@@ -55,6 +53,10 @@
     //Public functions
     function Inicializar() {
         me.Funciones.InicializarEventos();
+        if (me.Config.isMobile) {
+            document.getElementById('pUrlProductosPedido').href = '/Mobile/Pedido/Detalle';
+        }
+
     }
     
     return {
