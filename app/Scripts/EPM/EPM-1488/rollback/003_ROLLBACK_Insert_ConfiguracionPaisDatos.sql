c28f78eb8459517104147f8f32c8f81c04428522
Alexander Valladares <alexander.valladares@hundred.com.pe>
2018-10-01 10:12:32 -0500

    Ajuste y scripts

---
+++ app/Scripts/EPM/EPM-1488/rollback/003_ROLLBACK_Insert_ConfiguracionPaisDatos.sql
@@ -0,0 +1,72 @@
+﻿USE BelcorpPeru
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpMexico
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpColombia
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpSalvador
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpPuertoRico
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpPanama
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpGuatemala
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpEcuador
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpDominicana
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpCostaRica
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpChile
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
+USE BelcorpBolivia
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO
+
---
+++ app/Scripts/EPM/EPM-1488/scripts/003_Insert_ConfiguracionPaisDatos.sql
@@ -0,0 +1,96 @@
+﻿USE BelcorpPeru
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpMexico
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpColombia
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpSalvador
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpPuertoRico
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpPanama
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpGuatemala
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpEcuador
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpDominicana
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpCostaRica
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpChile
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
+USE BelcorpBolivia
+GO
+
+DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
+GO 
+INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (8, 'BloqueoProductoDigital', 0, '0', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+GO
+
