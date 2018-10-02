61ca170b75a2887aad3148da61c75b3127cc046b
pcamachog <cgonzalesp@outlook.com>
2018-10-02 18:01:21 -0500

    funcionalidad opcion ordenar

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/BusquedaProductoModule.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/BusquedaProductoModule.js
@@ -1,24 +1,29 @@
 ﻿var BusquedaProductoModule = (function () {
 
-   var _elementos = {
-        body: "body"
-        
+    var _elementos = {
+        body: "body",
+        opcionOrdenar: "#dpw-ordenar",
+        desplegado: "opcion__ordenamiento__dropdown--desplegado"
     };
     var _config = {
         isMobile: window.matchMedia("(max-width:991px)").matches,
-        
+
     };
     var _funciones = { //Funciones privadas
         InicializarEventos: function () {
-         
+            $(document).on("click", _elementos.opcionOrdenar, _eventos.Ordenar);
         },
-        CargarProductos: function() {
-            
+        CargarProductos: function () {
+
         }
     };
     var _eventos = {
         Ordenar: function () {
-           
+            var element = document.getElementById('dpw-ordenar');
+            element.classList.toggle(_elementos.desplegado);
+
+            var element2 = document.getElementById('ul-ordenar');
+            element2.classList.toggle('d-none');
         }
     };
 
@@ -27,7 +32,7 @@
     function Inicializar() {
         _funciones.InicializarEventos();
     }
-    
+
     function MostrarProductos() {
         console.log("Scroll page");
     }
