128f2796d2f67364b64f01b0e2a540bc6468afde
Alexander Valladares <alexander.valladares@hundred.com.pe>
2018-10-02 10:14:52 -0500

    Modificacion script para activar busqueda solo en colombia y costa rica

--- app/Scripts/EPM/EPM-1488/rollback/003_ROLLBACK_Insert_ConfiguracionPaisDatos.sql
+++ app/Scripts/EPM/EPM-1488/rollback/003_ROLLBACK_Insert_ConfiguracionPaisDatos.sql
@@ -1,72 +1,216 @@
 ﻿USE BelcorpPeru
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpMexico
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpColombia
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpSalvador
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpPuertoRico
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpPanama
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpGuatemala
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpEcuador
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpDominicana
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpCostaRica
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpChile
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
 USE BelcorpBolivia
 GO
 
-DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = 8 and Codigo = 'BloqueoProductoDigital';
-GO
+BEGIN
+	DECLARE @configPaisSR as int
+	DECLARE @configPaisRD as int
+
+	SELECT @configPaisSR = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'SR';
+
+	SELECT @configPaisRD = ConfiguracionPaisId FROM ConfiguracionPais WHERE Codigo = 'RD';
+	
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisSR and Codigo = 'BloqueoProductoDigital';
+	 
+	DELETE ConfiguracionPaisDatos WHERE ConfiguracionPaisID = @configPaisRD and Codigo = 'BloqueoProductoDigital';
+
+	INSERT ConfiguracionPaisDatos (ConfiguracionPaisID, Codigo, CampaniaID, Valor1, Valor2, Valor3, Descripcion, Estado, Componente) VALUES (@configPaisRD, 'BloqueoProductoDigital', 0, '1', NULL, NULL, 'Bloquear productos expuestos por el portal en pase pedido, 1 - Activa,  0 - Desactiva', 1, NULL)
+END
 
