83a2d2dc411d4e924639b2154bfae25cb3a63e92
MIJAEL888 <mijael888@gmail.com>
2018-10-02 16:48:45 -0500

    Avance BusquedaProductoModule

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/BusquedaProductoModule.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Buscador/BusquedaProductoModule.js
@@ -8,12 +8,61 @@
         isMobile: window.matchMedia("(max-width:991px)").matches,
         
     };
+    var _provider = {
+        BusquedaProductoPromise: function (params) {
+            var dfd = jQuery.Deferred();
+
+            jQuery.ajax({
+                type: "POST",
+                url: baseUrl + "Buscador/BusquedaProductos",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(params),
+                async: true,
+                cache: false,
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data, error) {
+                    dfd.reject(data, error);
+                }
+            });
+
+            return dfd.promise();
+        }
+    }
     var _funciones = { //Funciones privadas
         InicializarEventos: function () {
          
         },
-        CargarProductos: function() {
-            
+        ConstruirModeloBusqueda: function() {
+            var modelo = {
+                TextoBusqueda: "lab",
+                Paginacion: {
+                    Cantidad: 20,
+                    NuemroPagina: 0
+                },
+                Orden: {
+                    Campo: "Orden",
+                    Tipo: "Desc"
+                }
+            }
+            return modelo;
+        },
+        CargarProductos: function () {
+            var modelo = _funciones.ConstruirModeloBusqueda();
+            _provider.BusquedaProductoPromise(modelo)
+            .done(function(data) {
+                $.each(data, function (index, item) {
+                    item.posicion = index + 1;
+                    //if (item.Descripcion.length > TotalCaracteresEnListaBuscador) {
+                    //    item.Descripcion = item.Descripcion.substring(0, TotalCaracteresEnListaBuscador) + "...";
+                    //}
+                    console.log(item.toString());
+                });
+            }).fail(function(data, error) {
+                    console.error(error.toString());
+            });
         }
     };
     var _eventos = {
@@ -26,6 +75,7 @@
     //Public functions
     function Inicializar() {
         _funciones.InicializarEventos();
+        _funciones.CargarProductos();
     }
     
     function MostrarProductos() {
