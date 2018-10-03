8c02bab15ac5d9726c9909494f0bbeede3cc0425
Alan <alanaupe.trabajo@gmail.com>
2018-10-03 18:09:19 -0500

    PI2-EPM-Sprint1-Pase01102018 => Script del sprint 4 y 5 para el pase

---
+++ app/Scripts/EPM/PI2-Sprint1/Pase01102018/Rollback/Rollback_2_ConfiguracionPaisDatos_EPM_CambioExperienciaS4.sql
@@ -0,0 +1,337 @@
+﻿GO
+USE BelcorpPeru
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpMexico
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpColombia
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpSalvador
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpPuertoRico
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpPanama
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpGuatemala
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpEcuador
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpDominicana
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpCostaRica
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpChile
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
+USE BelcorpBolivia
+GO
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = '#NOMBRE, TODAS <b>TUS OFERTAS EN UN SOLO LUGAR</b>' where Codigo IN
+('DBienvenidaNoInscritaActiva','DBienvenidaNoInscritaNoActiva','MBienvenidaNoInscritaActiva',
+'MBienvenidaNoInscritaNoActiva','MPedidoNoInscritaActiva','MPedidoNoInscritaNoActiva')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'APROVECHA OFERTAS HECHAS A TU MEDIDA EN' where Codigo IN
+('DLandingBannerNoActivaNoSuscrita','DLandingBannerActivaNoSuscrita',
+'MLandingBannerNoActivaNoSuscrita','MLandingBannerActivaNoSuscrita')
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor1='#NOMBRE, TODAS', valor2 = 'TUS OFERTAS EN UN<br/> SOLO LUGAR' where Codigo = 'DPedidoNoInscritaNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set Valor2 = 'inicio-ganamas-mobile_normal.svg' where Codigo = 'LogoMenuInicioNoActiva'
+GO
+
+update [dbo].[ConfiguracionPaisDatos] set valor1 = 'gif-ganamas.gif' where Codigo = 'LogoMenuOfertasNoActiva'
+GO
+
+
+GO
