252ba30b9a4595a23c42ba113ed68258f905ed42
Omar Diaz <omardiaze@gmail.com>
2018-10-12 14:42:28 -0500

    Se agregó script de inserción a TablaLogica, opciones de ordenamiento

---
+++ app/Scripts/BuscadorYFiltros/sprint5/3_TablaLogica_OpcionOrdenamiento_Insert.sql
@@ -0,0 +1,109 @@
+GO
+USE BelcorpPeru
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpMexico
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpColombia
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpSalvador
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpPuertoRico
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpPanama
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpGuatemala
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpEcuador
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpDominicana
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpCostaRica
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpChile
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
+USE BelcorpBolivia
+GO
+IF NOT EXISTS (SELECT 1 FROM TablaLogica WHERE TablaLogicaID = 147)
+BEGIN
+	INSERT INTO TablaLogica (TablaLogicaID, Descripcion)
+	VALUES (147, 'B&F Opciones de ordenamiento')
+END
+
+GO
