91b89b6109d5f25168790fca2f64843038a491ac
Juan Jose Lucero Goicochea <juanjolucerog@gmail.com>
2018-10-15 11:26:58 -0500

    EPM-1476: Script ConfiguracionOfertasHome

--- app/Scripts/EPM/PI2-Sprint2/LasMasGanadoras/Script/2_02_ConfiguracionOfertasHome_EPM_1476.sql
+++ app/Scripts/EPM/PI2-Sprint2/LasMasGanadoras/Script/2_02_ConfiguracionOfertasHome_EPM_1476.sql
@@ -13,11 +13,11 @@ begin
 	where 
 		not desktoporden in(0,1);
 		
-	declare @ConfiguracionPaisID int;
+	declare @ConfiguracionPaisPeruID int;
 	
-	set @ConfiguracionPaisID = (select top 1 ConfiguracionPaisID from dbo.configuracionpais where codigo = 'MG');
+	set @ConfiguracionPaisPeruID = (select top 1 ConfiguracionPaisID from dbo.configuracionpais where codigo = 'MG');
 	
-	delete from configuracionofertashome where ConfiguracionPaisID = @ConfiguracionPaisID
+	delete from configuracionofertashome where ConfiguracionPaisID = @ConfiguracionPaisPeruID;
 
 	-- inserta configuracionofertashome
 	insert into dbo.configuracionofertashome(
@@ -33,7 +33,107 @@ begin
 		,UrlSeccion
 		,DesktopUsarImagenFondo,MobileUsarImagenFondo
 		) values  (
-		@ConfiguracionPaisID
+		@ConfiguracionPaisPeruID
+		,'201801'
+		,'2','2','2','2'
+		,'','banner_ganadoras_mobile.jpg'
+		,'LAS MÁS GANADORAS','LAS MÁS GANADORAS'
+		,'1','9'
+		,'007','007'
+		,'5','5'
+		,'1','1'
+		,''
+		,'0','0'
+		);
+end;
+go
+
+use [BelcorpChile_BPT];
+go
+
+begin
+	-- actualiza configuracionofertashome
+	update 
+		dbo.configuracionofertashome 
+	set 
+		 desktoporden		= desktoporden		+ 1
+		,mobileorden		= mobileorden		+ 1
+		,desktopordenbpt	= desktopordenbpt	+ 1
+		,mobileordenbpt		= mobileordenbpt	+ 1
+	where 
+		not desktoporden in(0,1);
+		
+	declare @ConfiguracionPaisChileID int;
+	
+	set @ConfiguracionPaisChileID = (select top 1 ConfiguracionPaisID from dbo.configuracionpais where codigo = 'MG');
+	
+	delete from configuracionofertashome where ConfiguracionPaisID = @ConfiguracionPaisChileID;
+
+	-- inserta configuracionofertashome
+	insert into dbo.configuracionofertashome(
+		ConfiguracionPaisID
+		,CampaniaID
+		,DesktopOrden,MobileOrden,DesktopOrdenBpt,MobileOrdenBpt
+		,DesktopImagenFondo,MobileImagenFondo
+		,DesktopTitulo,MobileTitulo
+		,DesktopTipoPresentacion,MobileTipoPresentacion
+		,DesktopTipoEstrategia,MobileTipoEstrategia
+		,DesktopCantidadProductos,MobileCantidadProductos
+		,DesktopActivo,MobileActivo
+		,UrlSeccion
+		,DesktopUsarImagenFondo,MobileUsarImagenFondo
+		) values  (
+		@ConfiguracionPaisChileID
+		,'201801'
+		,'2','2','2','2'
+		,'','banner_ganadoras_mobile.jpg'
+		,'LAS MÁS GANADORAS','LAS MÁS GANADORAS'
+		,'1','9'
+		,'007','007'
+		,'5','5'
+		,'1','1'
+		,''
+		,'0','0'
+		);
+end;
+go
+
+use [BelcorpCostaRica_BPT];
+go
+
+begin
+	-- actualiza configuracionofertashome
+	update 
+		dbo.configuracionofertashome 
+	set 
+		 desktoporden		= desktoporden		+ 1
+		,mobileorden		= mobileorden		+ 1
+		,desktopordenbpt	= desktopordenbpt	+ 1
+		,mobileordenbpt		= mobileordenbpt	+ 1
+	where 
+		not desktoporden in(0,1);
+		
+	declare @ConfiguracionPaisCostaRicaID int;
+	
+	set @ConfiguracionPaisCostaRicaID = (select top 1 ConfiguracionPaisID from dbo.configuracionpais where codigo = 'MG');
+	
+	delete from configuracionofertashome where ConfiguracionPaisID = @ConfiguracionPaisCostaRicaID;
+
+	-- inserta configuracionofertashome
+	insert into dbo.configuracionofertashome(
+		ConfiguracionPaisID
+		,CampaniaID
+		,DesktopOrden,MobileOrden,DesktopOrdenBpt,MobileOrdenBpt
+		,DesktopImagenFondo,MobileImagenFondo
+		,DesktopTitulo,MobileTitulo
+		,DesktopTipoPresentacion,MobileTipoPresentacion
+		,DesktopTipoEstrategia,MobileTipoEstrategia
+		,DesktopCantidadProductos,MobileCantidadProductos
+		,DesktopActivo,MobileActivo
+		,UrlSeccion
+		,DesktopUsarImagenFondo,MobileUsarImagenFondo
+		) values  (
+		@ConfiguracionPaisCostaRicaID
 		,'201801'
 		,'2','2','2','2'
 		,'','banner_ganadoras_mobile.jpg'
