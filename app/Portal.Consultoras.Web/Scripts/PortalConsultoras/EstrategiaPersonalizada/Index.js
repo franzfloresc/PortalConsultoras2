43f45fbd4fcee1f3cf9aee00aca71eb1e7b647c4
Alan <alanaupe.trabajo@gmail.com>
2018-10-16 16:38:16 -0500

    PI2-EPM-Sprint2_LasMasGanadoras => Jira 1569 - Quitar banner dorado mobile

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaPersonalizada/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaPersonalizada/Index.js
@@ -357,6 +357,10 @@ function SeccionMostrarProductos(data) {
     else if (data.Seccion.Codigo === CONS_CODIGO_SECCION.HV || data.Seccion.Codigo === CONS_CODIGO_SECCION.MG)
     {
         console.log('SeccionMostrarProductos -if ' + data.Seccion.Codigo);
+        if (data.Seccion.Codigo === CONS_CODIGO_SECCION.MG) {
+            CarruselCiclico = false;
+        }
+
         if (data.lista.length > 0) {
             $("#" + data.Seccion.Codigo).find(".seccion-content-contenedor").fadeIn();
             var cantidadAMostrar = parseInt($("#" + data.Seccion.Codigo).find("[data-productos-info] [data-productos-mostrar]").html());
@@ -368,13 +372,9 @@ function SeccionMostrarProductos(data) {
             }
             else {
                 if (data.Seccion.Codigo === CONS_CODIGO_SECCION.MG) {
-                    CarruselCiclico = false;
-                    //console.log('objBannerCajaProducto', data);
-                    
                     if (data.objBannerCajaProducto != undefined) {
                         data.lista.push(data.objBannerCajaProducto);
                     }
-                    
                 }
             }
             $("#" + data.Seccion.Codigo).find("[data-productos-info] [data-productos-total]").html(data.cantidadTotal);
@@ -393,7 +393,6 @@ function SeccionMostrarProductos(data) {
         $.each(data.lista, function (i, item) {
             item.EsBanner = false;
             item.EsLanzamiento = false;
-            item.TipoObjeto = item.TipoObjeto || 0;
         });
     }
 
@@ -418,6 +417,8 @@ function SeccionMostrarProductos(data) {
         RenderCarruselIndividuales(htmlSeccion);
     }
     else if (data.Seccion.TipoPresentacion == CONS_TIPO_PRESENTACION.carruselIndividualesv2) {
+
+        console.log('objBannerCajaProducto - data.Seccion.TipoPresentacion ', CONS_TIPO_PRESENTACION.carruselIndividualesv2, CarruselCiclico);
         RenderCarruselSimpleV2(htmlSeccion, CarruselCiclico, true);
     }
 }
