d2fdb70b16b04506e92f6bc934553447a5d3912c
HugoRoca <hugo.rock20@hotmail.com>
2018-10-19 11:48:46 -0500

    [GANAMAS-396] - Problemas del pase del sprint 5

--- app/Portal.Consultoras.Web/Scripts/General.js
+++ app/Portal.Consultoras.Web/Scripts/General.js
@@ -770,6 +770,7 @@ function IsValidUrl(value) {
 function isMobile() {
     var isUrlMobile = $.trim(location.href.replace("#", "/") + "/").toLowerCase().indexOf("/mobile/") > 0 ||
         $.trim(location.href).toLowerCase().indexOf("/g/") > 0;
+    if (!isUrlMobile) isUrlMobile = (window.matchMedia("(max-width:991px)").matches);
     return isUrlMobile;
 }
 
