67a369551acf7e85eba0fe93249ae105e17d3d96
Victor Marino <victor.marino@hundred.com.pe>
2018-10-19 17:20:11 -0500

    Scripts y configs Pase app Ver. 1.2.3_2

---
+++ app/Scripts/Pases/20181025/2.RollBack/ROLLBACK_2.0.APP_MenuApp_APPCONS-3370.sql
@@ -0,0 +1,120 @@
+﻿USE BelcorpPeru
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpMexico
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpColombia
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpSalvador
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpPuertoRico
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpPanama
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpGuatemala
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpEcuador
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpDominicana
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpCostaRica
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpChile
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
+USE BelcorpBolivia
+GO
+
+UPDATE dbo.MenuApp
+SET Descripcion = UPPER(Descripcion), 
+	Descripcion2 = UPPER(Descripcion2), 
+	Descripcion3 = UPPER(Descripcion3)
+WHERE Posicion = 1
+GO
+
