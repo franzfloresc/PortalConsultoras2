7e591f314ee738e9d448fc76c4544032c0639fac
Alan <alanaupe.trabajo@gmail.com>
2018-10-24 16:26:55 -0500

    QAS => Script Pase Gana +

---
+++ app/Scripts/Pases/20181025/2.RollBack/Rollback_2_05_ConfiguracionPais_EPM_Ordenar.sql
@@ -0,0 +1,265 @@
+﻿GO
+USE BelcorpPeru
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpMexico
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpColombia
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpSalvador
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpPuertoRico
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpPanama
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpGuatemala
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpEcuador
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpDominicana
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpCostaRica
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpChile
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
+USE BelcorpBolivia
+GO
+go
+
+begin
+	if(exists(select 1 from dbo.configuracionpais where codigo = 'MG'))
+	begin
+		update
+			dbo.configuracionpais
+		set
+			 orden			= orden			 - 1
+			,ordenbpt		= ordenbpt		 - 1
+			,mobileorden	= mobileorden	 - 1
+			,mobileordenbpt = mobileordenbpt - 1
+		where
+			not orden in(0,1);
+
+	end;
+end;
+go
+
+GO
