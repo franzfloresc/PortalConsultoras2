7b66f603281b3e5a26c346dcba7cd39e16676d2c
AlquimistaBinario <ecangahuala@gmail.com>
2018-11-05 11:50:51 -0500

    PROCEDIMIENTO ALMACENADO(UPDATED) Se realiza ajustes los SP para el pase a QAS

---
+++ app/Scripts/Gana++/GANA-218/Rollback/Rollback_3_Eliminarusp_UpdateEstrategiaProductoMasiva_Gana_218.sql
@@ -0,0 +1,110 @@
+USE BelcorpPeru
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpMexico
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpColombia
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpSalvador
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpPanama
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpGuatemala
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpDominicana
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpCostaRica
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpChile
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
+USE BelcorpBolivia
+GO
+
+IF EXISTS(SELECT 1 FROM sys.procedures 
+          WHERE Name = 'usp_UpdateEstrategiaProductoMasiva')
+BEGIN
+    DROP PROCEDURE dbo.usp_UpdateEstrategiaProductoMasiva
+END
+GO
+
