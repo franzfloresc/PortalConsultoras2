9c095e368f3ddc614c567b0cc42fe76a11c7b553
Omar Diaz <omardiaze@gmail.com>
2018-11-12 10:26:57 -0500

    Creación de scripts filtros: inserción de marcas, inserción de precios

---
+++ app/Scripts/BuscadorYFiltros/Sprint8/1.1 Insertar marca.sql
@@ -0,0 +1,253 @@
+GO
+USE BelcorpPeru
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpMexico
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpColombia
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpSalvador
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpPuertoRico
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpPanama
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpGuatemala
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpEcuador
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpDominicana
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpCostaRica
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpChile
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
+USE BelcorpBolivia
+GO
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '201')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '201','Ésika','Marca Ésika')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '202')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '202','L''bel','Marca L''bel')
+END
+
+IF NOT EXISTS (SELECT 1 FROM FiltroBuscador WHERE Codigo = '203')
+BEGIN
+	INSERT INTO FiltroBuscador (TablaLogicaDatosID, Estado, Codigo, Nombre, Descripcion)
+	VALUES (14802, 1, '203','Cyzone','Marca Cyzone')
+END
+
+GO
