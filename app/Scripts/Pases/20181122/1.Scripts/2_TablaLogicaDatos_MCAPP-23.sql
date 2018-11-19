5d909e80cab797ba56aec72a98e4cd63e3c5f016
Jonathan L <jhon.dev2014@gmail.com>
2018-11-19 18:05:32 -0500

    MCAPP-23 : Pago En Linea Ajuste de Servicios
    MCAPP-27 : Pago En Linea Reporte

---
+++ app/Scripts/Pases/20181122/1.Scripts/2_TablaLogicaDatos_MCAPP-23.sql
@@ -0,0 +1,12 @@
+﻿USE BelcorpPeru
+GO
+
+/* Habilitar el Pago en Linea para la App */
+INSERT INTO [TablaLogicaDatos] ([TablaLogicaDatosID], [TablaLogicaID], [Codigo], [Descripcion], [Valor])
+VALUES (12213, 122, 'FLAG_ENABLE_APP', 'Flag para habilitar Pago en Linea en el App de Consultoras', '1');
+
+/* Habilitar la carga de Aplicaciones Externas en Pago de Banca por Internet */
+INSERT INTO [TablaLogicaDatos] ([TablaLogicaDatosID], [TablaLogicaID], [Codigo], [Descripcion], [Valor])
+VALUES (12214, 122, 'FLAG_PBI_EXTERNAL_APP', 'Flag para habilitar la Carga de las Apps de los Bancos', '1');
+
+GO
