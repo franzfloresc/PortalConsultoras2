df907bd0dcc92f704b2ab751b261f82469479a82
Victor Marino <victor.marino@hundred.com.pe>
2018-11-19 16:33:34 -0500

    Scripts y configs pase App Ver. 1.2.4

---
+++ app/Scripts/Pases/20181123/2.Rollback/ROLLBACK_2.1.APP_MenuApp_APPCONS-3447.sql
@@ -0,0 +1,84 @@
+﻿USE BelcorpPeru
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpMexico
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpColombia
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpSalvador
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpPuertoRico
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpPanama
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpGuatemala
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpEcuador
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpDominicana
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpCostaRica
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpChile
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
+USE BelcorpBolivia
+GO
+
+DELETE FROM dbo.MenuApp
+WHERE VersionMenu = 7
+GO
+
