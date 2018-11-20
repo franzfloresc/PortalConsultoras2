9b46a60ffc88e53dc481d325d8913a3d4bd74be3
Jonathan L <jhon.dev2014@gmail.com>
2018-11-20 18:01:07 -0500

    Revert "APPCONS-3631 VerificacionChile : Forzar el envio de SMS a celulares de Perú"
    This reverts commit d6cb64ef3b7505e3e03442c538b5cb360258bf60.

--- app/Portal.Consultoras.BizLogic/Usuario/BLUsuario.cs
+++ app/Portal.Consultoras.BizLogic/Usuario/BLUsuario.cs
@@ -2875,8 +2875,7 @@ namespace Portal.Consultoras.BizLogic
             {
                 try
                 {
-                    //BEEnviarSms oCredencial = GetCredencialesSmsCache(paisID);
-                    BEEnviarSms oCredencial = GetCredencialesSmsCache(Constantes.PaisID.Peru); //PRUEBA
+                    BEEnviarSms oCredencial = GetCredencialesSmsCache(paisID);
                     string codGenerado = Common.Util.GenerarCodigoRandom();
                     oCredencial.Mensaje = string.Format(
                         tipoMensaje == Constantes.EnviarSMS.CredencialesProvedoresSMS.Bolivia.MENSAJE_OPTIN
@@ -2895,8 +2894,7 @@ namespace Portal.Consultoras.BizLogic
                         CampaniaID = oUsu.campaniaID,
                         NroCelular = oUsu.Celular,
                         Mensaje = oCredencial.Mensaje,
-                        //CodigoIso = oUsu.CodigoIso,
-                        CodigoIso = Constantes.CodigosISOPais.Peru, //PRUEBA
+                        CodigoIso = oUsu.CodigoIso,
                         EsMobile = oUsu.EsMobile,
                         RequestUrl = oCredencial.RequestUrl,
                         RecursoApi = oCredencial.RecursoApi
