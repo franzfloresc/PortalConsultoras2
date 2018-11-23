4ae1d3e187da7af9a87aa2be87efb631c6c59272
Alan <alanaupe.trabajo@gmail.com>
2018-11-23 17:36:49 -0500

    PI2-EPM-Sprint3-Pase22112018 => Mejora Popup Sin Scroll

--- app/Portal.Consultoras.Web/Scripts/General.js
+++ app/Portal.Consultoras.Web/Scripts/General.js
@@ -559,10 +559,18 @@ function ImgUrlRender(imgOriginal, tipo) {
     return urlRender;
 }
 
-function showDialog(dialogId) {
-    $("#" + dialogId).dialog("open");
-    $("#ui-datepicker-div").css("z-index", "9999");
+function showDialogSinScroll(dialogId) {
     $("body").css("overflow", "hidden");
+    showDialog(dialogId);
+    return false;
+}
+
+function showDialog(dialogId) {
+    dialogId = (dialogId || "").trim();
+    dialogId = dialogId[0] == "#" ? dialogId : ("#" + dialogId);
+    $(dialogId).dialog("open");
+    $("#ui-datepicker-div").css("z-index", "9999");
+    //$("body").css("overflow", "hidden");
     //$("body").removeClass("overflow_hidden");
     console.log(dialogId);
     setTimeout(function () {
