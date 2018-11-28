04e941a062737573a06a7df595c615d4f0ba9c3a
RodolfoPerez6 <44589561+RodolfoPerez6@users.noreply.github.com>
2018-11-28 15:41:19 -0500

    (UPDATED)AdmContenido(Lan)-2
    Actualización Lan en portal consultoras

--- app/Portal.Consultoras.Web/Providers/OfertaBaseProvider.cs
+++ app/Portal.Consultoras.Web/Providers/OfertaBaseProvider.cs
@@ -79,21 +79,28 @@ namespace Portal.Consultoras.Web.Providers
                         TipoEstrategiaID = Convert.ToInt32(item.tipoEstrategiaId),
                         TipoEstrategiaImagenMostrar = 6,
                         EsSubCampania = Convert.ToBoolean(item.esSubCampania) ? 1 : 0,
-                        //Lan
-                        FlagIndividual = Convert.ToBoolean(item.flagIndividual ?? false),
-                        Slogan = item.slogan,
-                        ImgHomeDesktop = item.imgHomeDesktop,
-                        ImgHomeMobile = item.imgHomeMobile,
-                        ImgFondoDesktop = item.imgFondoDesktop,
-                        ImgFondoMobile = item.imgFondoMobile,
-                        ImgFichaDesktop = item.imgFichaDesktop,
-                        ImgFichaFondoDesktop = item.imgFichaFondoDesktop,
-                        ImgFichaMobile = item.imgFichaMobile,
-                        ImgFichaFondoMobile = item.imgFichaFondoMobile,
-                        UrlVideoDesktop = item.urlVideoDesktop,
-                        UrlVideoMobile = item.urlVideoMobile,
                     };
                     estrategia.TipoEstrategia = new ServiceOferta.BETipoEstrategia { Codigo = item.codigoTipoEstrategia };
+
+                    if (estrategia.TipoEstrategia.Codigo == Constantes.TipoEstrategiaCodigo.Lanzamiento)
+                    {
+                        estrategia.EstrategiaDetalle = new ServiceOferta.BEEstrategiaDetalle
+                        {
+                            EstrategiaID = estrategia.EstrategiaID,
+                            FlagIndividual = Convert.ToBoolean(item.flagIndividual ?? false),
+                            Slogan = item.slogan,
+                            ImgHomeDesktop = item.imgHomeDesktop,
+                            ImgHomeMobile = item.imgHomeMobile,
+                            ImgFondoDesktop = item.imgFondoDesktop,
+                            ImgFondoMobile = item.imgFondoMobile,
+                            ImgFichaDesktop = item.imgFichaDesktop,
+                            ImgFichaFondoDesktop = item.imgFichaFondoDesktop,
+                            ImgFichaMobile = item.imgFichaMobile,
+                            ImgFichaFondoMobile = item.imgFichaFondoMobile,
+                            UrlVideoDesktop = item.urlVideoDesktop,
+                            UrlVideoMobile = item.urlVideoMobile,
+                        };
+                    }
 
                     if (estrategia.Precio2 > 0)
                     {
