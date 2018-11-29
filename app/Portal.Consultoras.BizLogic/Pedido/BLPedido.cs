67a42eb57915e755a6fba98161376c647d6e649b
pcabrerahundred <paul.cabrera@hundred.com.pe>
2018-11-29 16:22:38 -0500

    fix bugs GANAZG-20, GANAZG-24

--- app/Portal.Consultoras.BizLogic/Pedido/BLPedido.cs
+++ app/Portal.Consultoras.BizLogic/Pedido/BLPedido.cs
@@ -1990,7 +1990,7 @@ namespace Portal.Consultoras.BizLogic.Pedido
                         case Constantes.PedidoAccion.INSERT:
                             {
                                 _pedidoWebDetalleBusinessLogic.InsertPedidoWebSetTransaction(usuario.PaisID, usuario.CampaniaID, pedidoWebDetalles[0].PedidoID, estrategia.Cantidad, estrategia.CUV2
-                                        , usuario.ConsultoraID, string.Empty, cuvlist, estrategia.EstrategiaID, pedidoDetalle.ClienteID, pedidoWebDetalles[0].TipoEstrategiaID);
+                                        , usuario.ConsultoraID, usuario.CodigoConsultora, cuvlist, estrategia.EstrategiaID, pedidoDetalle.ClienteID, pedidoWebDetalles[0].TipoEstrategiaID);
                                 
                             }
                             break;
