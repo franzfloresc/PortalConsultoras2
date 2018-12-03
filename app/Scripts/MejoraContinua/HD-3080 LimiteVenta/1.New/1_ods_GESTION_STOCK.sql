e1bd8a6f5bc4c504e82714238d1bd9ce95567a1b
Kike <lead123456@hotmail.com>
2018-12-03 14:47:32 -0500

    HD-3080: Actualización de scripts de BD.

--- app/Scripts/MejoraContinua/HD-3080 LimiteVenta/1.New/1_ods_GESTION_STOCK.sql
+++ app/Scripts/MejoraContinua/HD-3080 LimiteVenta/1.New/1_ods_GESTION_STOCK.sql
@@ -1,95 +1,95 @@
 USE BelcorpBolivia
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_BO.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_BO.dbo.GestionStock
 end
 GO
 
 USE BelcorpChile
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_CL.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_CL.dbo.GestionStock
 end
 GO
 
 USE BelcorpColombia
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_CO.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_CO.dbo.GestionStock
 end
 GO
 
 USE BelcorpCostaRica
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_CR.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_CR.dbo.GestionStock
 end
 GO
 
 USE BelcorpDominicana
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_DO.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_DO.dbo.GestionStock
 end
 GO
 
 USE BelcorpEcuador
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_EC.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_EC.dbo.GestionStock
 end
 GO
 
 USE BelcorpGuatemala
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_GT.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_GT.dbo.GestionStock
 end
 GO
 
 USE BelcorpMexico
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_MX.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_MX.dbo.GestionStock
 end
 GO
 
 USE BelcorpPanama
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_PA.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_PA.dbo.GestionStock
 end
 GO
 
 USE BelcorpPeru
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_PE.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_PE.dbo.GestionStock
 end
 GO
 
 USE BelcorpPuertoRico
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_PR.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_PR.dbo.GestionStock
 end
 GO
 
 USE BelcorpSalvador
 GO
-if not exists (select 1 from sys.synonyms where name = N'GESTION_STOCK' AND schema_id = schema_id(N'ods'))
+if not exists (select 1 from sys.synonyms where name = N'GestionStock' AND schema_id = schema_id(N'ods'))
 begin
-	create synonym ods.GESTION_STOCK FOR ODS_SV.dbo.GESTION_STOCK
+	create synonym ods.GestionStock FOR ODS_SV.dbo.GestionStock
 end
 GO
