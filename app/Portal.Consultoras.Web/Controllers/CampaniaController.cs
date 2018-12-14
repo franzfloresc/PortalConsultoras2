90bc9153e0aad5634106440678beab0c3b7c6020
Alan <alanaupe.trabajo@gmail.com>
2018-12-14 17:10:55 -0500

    PI2-EPM-Mejora => BaseAdmController - mover ListarCampanias a ObtenerCampaniasPorUsuario

--- app/Portal.Consultoras.Web/Controllers/CampaniaController.cs
+++ app/Portal.Consultoras.Web/Controllers/CampaniaController.cs
@@ -6,12 +6,12 @@ namespace Portal.Consultoras.Web.Controllers
 {
     public class CampaniaController : BaseAdmController
     {
-        [HttpGet]
-        public JsonResult ListarCampanias()
-        {
-            IEnumerable<CampaniaModel> listaCampanias = _zonificacionProvider.GetCampanias(userData.PaisID);
-
-            return Json(new { listaCampanias = listaCampanias }, JsonRequestBehavior.AllowGet);
-        }
+        //movido BaseAdm/ObtenerCampaniasPorUsuario
+        //[HttpGet]
+        //public JsonResult ListarCampanias()
+        //{
+        //    IEnumerable<CampaniaModel> listaCampanias = _zonificacionProvider.GetCampanias(userData.PaisID);
+        //    return Json(new { listaCampanias = listaCampanias }, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
