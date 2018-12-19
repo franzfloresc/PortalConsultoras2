6459c420b9c36c235be34096bfb7c0927fc405a0
Abel Reynaltte <abelreynalte@gmail.com>
2018-12-19 11:49:01 -0500

    HD-3155: Opciones de usuarios dinámico

--- app/Portal.Consultoras.Web/Providers/MiPerfilProvider.cs
+++ app/Portal.Consultoras.Web/Providers/MiPerfilProvider.cs
@@ -3,10 +3,7 @@ using Portal.Consultoras.Web.Models;
 using Portal.Consultoras.Web.ServiceUnete;
 using Portal.Consultoras.Web.SessionManager;
 using Portal.Consultoras.Web.ServiceUsuario;
-using Portal.Consultoras.Web.SessionManager;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Portal.Consultoras.Web.Providers
 {
@@ -39,14 +36,6 @@ namespace Portal.Consultoras.Web.Providers
                 var datos = usuario.GetUsuarioOpciones(paisId, codigoUsuario);
                 return Mapper.Map<IEnumerable<BEUsuarioOpciones>, List<UsuarioOpcionesModel>>(datos);
             }
-            List<ParametroUneteBE> result;
-            Int32 param = 0;
-            using (var sv = new PortalServiceClient())
-            {
-                result = sv.ObtenerParametrosUnete(CodigoISO, EnumsTipoParametro.LugarNivel1, param);
-            }
-
-            return result;
         }
         public ParametroUneteCollection ObtenerUbigeoDependiente(string CodigoISO , int Nivel , int IdPadre)
         {
