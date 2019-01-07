4199b749fbde2b6a4ce32575ca221f69ec97b64a
Alan <alanaupe.trabajo@gmail.com>
2019-01-07 16:17:14 -0500

    PI2-EPM-Mejora => CatalogoService - optimizar codigo

--- app/Portal.Consultoras.Service/CatalogoService.cs
+++ app/Portal.Consultoras.Service/CatalogoService.cs
@@ -66,26 +66,27 @@ namespace Portal.Consultoras.Service
             //if (marcaId == 0) throw new Exception("El Servicio no recibió el parámetro marcaID correcto");
 
             int idPais = Util.GetPaisID(codigoPais);
-            List<BETablaLogicaDatos> vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 58);
-            BETablaLogicaDatos longitudUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 5801);
-            if (longitudUbigeo != null)
-            {
-                int limiteInferior;
-                int.TryParse(longitudUbigeo.Codigo, out limiteInferior);
-                vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 67);
-                BETablaLogicaDatos configFactorUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6701);
-                int factorUbigeo;
-                int.TryParse(configFactorUbigeo.Codigo, out factorUbigeo);
-                limiteInferior *= factorUbigeo;
-                if (codigoUbigeo.Length < limiteInferior)
-                {
-                    string mensajeValidacion = string.Format("La longitud del parámetro CodigoUbigeo debe tener como valor mínimo {0}", limiteInferior);
-                    throw new Exception(mensajeValidacion);
-                }
-            }
-            int tipoFiltroUbigeo;
-            vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 66);
+            ValidarCodigoUbigeo(idPais, codigoUbigeo);
+            //List<BETablaLogicaDatos> vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 58);
+            //BETablaLogicaDatos longitudUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 5801);
+            //if (longitudUbigeo != null)
+            //{
+            //    int limiteInferior;
+            //    int.TryParse(longitudUbigeo.Codigo, out limiteInferior);
+            //    vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 67);
+            //    BETablaLogicaDatos configFactorUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6701);
+            //    int factorUbigeo;
+            //    int.TryParse(configFactorUbigeo.Codigo, out factorUbigeo);
+            //    limiteInferior *= factorUbigeo;
+            //    if (codigoUbigeo.Length < limiteInferior)
+            //    {
+            //        string mensajeValidacion = string.Format("La longitud del parámetro CodigoUbigeo debe tener como valor mínimo {0}", limiteInferior);
+            //        throw new Exception(mensajeValidacion);
+            //    }
+            //}
+            var vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 66);
             BETablaLogicaDatos filtroUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6601);
+            int tipoFiltroUbigeo;
             int.TryParse(filtroUbigeo.Codigo, out tipoFiltroUbigeo);
 
             BEListaConsultoraCatalogo bEListaConsultoraCatalogo = new BEListaConsultoraCatalogo();
@@ -108,49 +109,50 @@ namespace Portal.Consultoras.Service
             //if (string.IsNullOrEmpty(codigoUbigeo)) throw new Exception("El Servicio no recibió el parámetro CodigoUbigeo correcto");
             //if (marcaId == 0) throw new Exception("El Servicio no recibió el parámetro marcaID correcto");
 
-            BEListaConsultoraCatalogo bEListaConsultoraCatalogo = new BEListaConsultoraCatalogo();
             int idPais = Util.GetPaisID(codigoPais);
-
-            int tipoFiltroUbigeo;
             List<BETablaLogicaDatos> vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 66);
             BETablaLogicaDatos filtroUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6601);
+
+            int tipoFiltroUbigeo;
             int.TryParse(filtroUbigeo.Codigo, out tipoFiltroUbigeo);
 
-            if (codigoPais == Constantes.CodigosISOPais.Peru || codigoPais == Constantes.CodigosISOPais.Ecuador || codigoPais == Constantes.CodigosISOPais.Bolivia)
-            {
-                if (codigoUbigeo.Length == 18) bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
-                else if (codigoUbigeo.Length == 12) bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeo12AndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
-                else throw new Exception("La longitud del parámetro CodigoUbigeo debe ser 12 o 18");
-            }
-            else
-            {
-                vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 58);
-                BETablaLogicaDatos longitudUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 5801);
-                if (longitudUbigeo != null)
-                {
-                    int limiteInferior;
-                    int.TryParse(longitudUbigeo.Codigo, out limiteInferior);
-                    vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 67);
-                    BETablaLogicaDatos configFactorUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6701);
-                    int factorUbigeo;
-                    int.TryParse(configFactorUbigeo.Codigo, out factorUbigeo);
-                    limiteInferior *= factorUbigeo;
-                    if (codigoUbigeo.Length < limiteInferior)
-                    {
-                        string mensajeValidacion = string.Format("La longitud del parámetro CodigoUbigeo debe tener como valor mínimo {0}", limiteInferior);
-                        throw new Exception(mensajeValidacion);
-                    }
-                }
+            BEListaConsultoraCatalogo bEListaConsultoraCatalogo = new BEListaConsultoraCatalogo();
+            bEListaConsultoraCatalogo.ConsultorasCatalogos = GetConsultorasCatalogos(codigoPais, codigoUbigeo, idPais, marcaId, nombres, apellidos, tipoFiltroUbigeo);
 
-                bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
-            }
+            //if (codigoPais == Constantes.CodigosISOPais.Peru || codigoPais == Constantes.CodigosISOPais.Ecuador || codigoPais == Constantes.CodigosISOPais.Bolivia)
+            //{
+            //    if (codigoUbigeo.Length == 18) bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+            //    else if (codigoUbigeo.Length == 12) bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeo12AndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+            //    else throw new Exception("La longitud del parámetro CodigoUbigeo debe ser 12 o 18");
+            //}
+            //else
+            //{
+            //    vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 58);
+            //    BETablaLogicaDatos longitudUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 5801);
+            //    if (longitudUbigeo != null)
+            //    {
+            //        int limiteInferior;
+            //        int.TryParse(longitudUbigeo.Codigo, out limiteInferior);
+            //        vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 67);
+            //        BETablaLogicaDatos configFactorUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6701);
+            //        int factorUbigeo;
+            //        int.TryParse(configFactorUbigeo.Codigo, out factorUbigeo);
+            //        limiteInferior *= factorUbigeo;
+            //        if (codigoUbigeo.Length < limiteInferior)
+            //        {
+            //            string mensajeValidacion = string.Format("La longitud del parámetro CodigoUbigeo debe tener como valor mínimo {0}", limiteInferior);
+            //            throw new Exception(mensajeValidacion);
+            //        }
+            //    }
+            //    bEListaConsultoraCatalogo.ConsultorasCatalogos = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+            //}
 
             bEListaConsultoraCatalogo.NumeroRegistros = bEListaConsultoraCatalogo.ConsultorasCatalogos.Count;
             if (bEListaConsultoraCatalogo.NumeroRegistros > 10) bEListaConsultoraCatalogo.ConsultorasCatalogos = null;
             else bEListaConsultoraCatalogo.ConsultorasCatalogos.Update(x => x.Pais = codigoPais);
             return bEListaConsultoraCatalogo;
         }
-
+        
         public List<BEConsultoraCatalogo> GetConsultorasPorCodigoTerritorioGeo(string codigoPais, string codigoTerritorioGeo)
         {
             try
@@ -223,8 +225,7 @@ namespace Portal.Consultoras.Service
                     nombreCliente, emailCliente, telefonoCliente, nuevaConsultora);
             return result;
         }
-
-
+        
         private string GetConsultoraCatalogoValidarMensaje(string paisIso, string codigoConsultora, int paisId)
         {
             var respuesta = "";
@@ -247,5 +248,46 @@ namespace Portal.Consultoras.Service
 
             return respuesta;
         }
+
+        private List<BEConsultoraCatalogo> GetConsultorasCatalogos(string codigoPais, string codigoUbigeo, int idPais, int marcaId, string nombres, string apellidos, int tipoFiltroUbigeo)
+        {
+            var lista = new List<BEConsultoraCatalogo>();
+
+            if (codigoPais == Constantes.CodigosISOPais.Peru || codigoPais == Constantes.CodigosISOPais.Ecuador || codigoPais == Constantes.CodigosISOPais.Bolivia)
+            {
+                if (codigoUbigeo.Length == 18) lista = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+                else if (codigoUbigeo.Length == 12) lista = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeo12AndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+                else throw new Exception("La longitud del parámetro CodigoUbigeo debe ser 12 o 18");
+            }
+            else
+            {
+                ValidarCodigoUbigeo(idPais, codigoUbigeo);
+                lista = new BLConsultoraCatalogo().GetConsultorasCatalogosPorUbigeoAndNombresAndApellidos(idPais, codigoUbigeo, nombres, apellidos, marcaId, tipoFiltroUbigeo);
+            }
+
+            return lista;
+        }
+
+        private void ValidarCodigoUbigeo(int idPais, string codigoUbigeo)
+        {
+            var vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 58);
+            BETablaLogicaDatos longitudUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 5801);
+            if (longitudUbigeo != null)
+            {
+                int limiteInferior;
+                int.TryParse(longitudUbigeo.Codigo, out limiteInferior);
+                vListaTablaLogicaDatos = new BLTablaLogicaDatos().GetList(idPais, 67);
+                BETablaLogicaDatos configFactorUbigeo = vListaTablaLogicaDatos.Find(x => x.TablaLogicaDatosID == 6701);
+                int factorUbigeo;
+                int.TryParse(configFactorUbigeo.Codigo, out factorUbigeo);
+                limiteInferior *= factorUbigeo;
+                if (codigoUbigeo.Length < limiteInferior)
+                {
+                    var mensaje = string.Format("La longitud del parámetro CodigoUbigeo debe tener como valor mínimo {0}", limiteInferior);
+                    throw new Exception(mensaje);
+                }
+            }
+        }
+
     }
 }
