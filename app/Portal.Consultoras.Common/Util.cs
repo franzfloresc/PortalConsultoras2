f57a387b9ef8427b30251f7d47512b0a883a81cf
Alan <alanaupe.trabajo@gmail.com>
2019-01-09 10:34:41 -0500

    PI2-EPM-Mejora => Sonar

--- app/Portal.Consultoras.Common/Util.cs
+++ app/Portal.Consultoras.Common/Util.cs
@@ -3401,39 +3401,45 @@ namespace Portal.Consultoras.Common
                     limiteMinimoTelef = 7;
                     limiteMaximoTelef = 9;
                     break;
-                case Constantes.PaisID.Mexico:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 10;
-                    break;
-                case Constantes.PaisID.Ecuador:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 10;
-                    break;
+                //case Constantes.PaisID.Mexico:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 10;
+                //    break;
+                //case Constantes.PaisID.Ecuador:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 10;
+                //    break;
                 case Constantes.PaisID.Chile:
                     limiteMinimoTelef = 5;
                     limiteMaximoTelef = 9;
                     break;
+                case Constantes.PaisID.Mexico:
+                case Constantes.PaisID.Ecuador:
                 case Constantes.PaisID.RepublicaDominicana:
                 case Constantes.PaisID.Colombia:
                     limiteMinimoTelef = 5;
                     limiteMaximoTelef = 10;
                     break;
+                //case Constantes.PaisID.Bolivia:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 8;
+                //    break;
+                //case Constantes.PaisID.Guatemala:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 8;
+                //    break;
+                //case Constantes.PaisID.ElSalvador:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 8;
+                //    break;
+                //case Constantes.PaisID.Panama:
+                //    limiteMinimoTelef = 5;
+                //    limiteMaximoTelef = 8;
+                //    break;
                 case Constantes.PaisID.Bolivia:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 8;
-                    break;
                 case Constantes.PaisID.Guatemala:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 8;
-                    break;
                 case Constantes.PaisID.ElSalvador:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 8;
-                    break;
                 case Constantes.PaisID.Panama:
-                    limiteMinimoTelef = 5;
-                    limiteMaximoTelef = 8;
-                    break;
                 case Constantes.PaisID.CostaRica:
                     limiteMinimoTelef = 5;
                     limiteMaximoTelef = 8;
