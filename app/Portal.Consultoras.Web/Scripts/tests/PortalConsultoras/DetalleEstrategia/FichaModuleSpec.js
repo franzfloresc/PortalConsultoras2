28c6221ad94e4eaa82f39c7242dedc4383afca8a
Victor Vilela <victor.vilela@avantica.net>
2019-01-09 17:51:56 -0500

    add jasmine framework and gulp script to invoque unit tests

---
+++ app/Portal.Consultoras.Web/Scripts/tests/PortalConsultoras/DetalleEstrategia/FichaModuleSpec.js
@@ -0,0 +1,77 @@
+///// <reference path="../../../PortalConsultoras/EstrategiaPersonalizada/LocalStorage.js" />
+///// <reference path="../../../PortalConsultoras/DetalleEstrategia/FichaModule.js" />
+
+describe("FichaModule", function () {
+    describe("Inicializar", function () {
+
+        beforeAll(function () {
+            //
+        });
+        afterAll(function () {
+            //
+        });
+
+        beforeEach(function () {
+            //
+        });
+        afterEach(function () {
+            //
+        });
+
+        it("test", function () {
+            expect(true).toBe(true);
+        });
+
+        //        it("throw an exception when config is undefined", function () {
+        //            var errorMsg = '';
+
+        //            try {
+        //                var ficha = FichaModule(undefined);
+        //            } catch (error) {
+        //                errorMsg = error;
+        //            }
+
+        //            expect(errorMsg).toContain("config is null or undefined");
+        //        });
+
+        //        it("throw an exception when config is null", function () {
+        //            var errorMsg = '';
+
+        //            try {
+        //                var ficha = FichaModule(null);
+        //            } catch (error) {
+        //                errorMsg = error;
+        //            }
+
+        //            expect(errorMsg).toContain("config is null or undefined");
+        //        });
+
+        //        it("throw an exception when localStorage is undefined", function () {
+        //            var errorMsg = '';
+
+        //            try {
+        //                var ficha = FichaModule({ 
+        //                    localStorageModule : undefined
+        //                });
+        //            } catch (error) {
+        //                errorMsg = error;
+        //            }
+
+        //            expect(errorMsg).toContain("config.localStorageModule is null or undefined");
+        //        });
+
+        //        it("throw an exception when localStorage is null", function () {
+        //            var errorMsg = '';
+
+        //            try {
+        //                var ficha = FichaModule({ 
+        //                    localStorageModule : null
+        //                });
+        //            } catch (error) {
+        //                errorMsg = error;
+        //            }
+
+        //            expect(errorMsg).toContain("config.localStorageModule is null or undefined");
+        //        });
+    });
+});
