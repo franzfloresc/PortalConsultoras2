d103601df74503da341449db8e46a78237b59913
Victor Vilela <victor.vilela@avantica.net>
2019-01-10 17:32:35 -0500

    config mocha reporter and coverage reporter

--- app/Portal.Consultoras.Web/karma.conf.js
+++ app/Portal.Consultoras.Web/karma.conf.js
@@ -43,14 +43,22 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            'Scripts/PortalConsultoras/**/*.js': ['coverage']
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: [
+            //'progress',
+            'mocha',
+            'coverage',
+        ],
 
+        coverageReporter: {
+            type: 'text'
+        },      
 
         // web server port
         port: 9876,
@@ -71,7 +79,10 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: [
+            //'Chrome'
+            'PhantomJS'
+        ],
 
 
         // Continuous Integration mode
