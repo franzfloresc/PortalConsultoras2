a130ef65848b09968a07b1fb2dff57755a8f0439
jose-ponte <jose.ponte@hundred.com.pe>
2019-01-15 10:38:35 -0500

    Script HD-3180

---
+++ app/Scripts/MejoraContinua/HD-3180/script/2_OpcionesUsuario_MC_HD-3180.sql
@@ -0,0 +1,84 @@
+USE BelcorpPeru
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpMexico
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpColombia
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpSalvador
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpPuertoRico
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpPanama
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpGuatemala
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpEcuador
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpDominicana
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpCostaRica
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
+USE BelcorpChile
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 1),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', 1, 2, 'Opcion para compartir a WhatsApp', 1)
+GO
+
+USE BelcorpBolivia
+GO
+
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
+																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
+GO
+
