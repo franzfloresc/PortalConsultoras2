704d5d1cf098f80b6fb14b2107de0fbd19a53fad
Abel Reynaltte <abelreynalte@gmail.com>
2019-01-16 12:16:21 -0500

    Homologar los nombres de los archivos: Index.cshtml, index.js Catálogo Digital.

---
+++ app/Scripts/MejoraContinua/HD-3255 CatalogoDigital/2_CatalogoDigital_MC.sql
@@ -0,0 +1,217 @@
+﻿USE BelcorpPeru_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
+USE BelcorpMexico_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
+USE BelcorpColombia_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
+USE BelcorpSalvador_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '0')
+end
+go
+
+USE BelcorpPuertoRico_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 22 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '0')
+end
+go
+
+USE BelcorpPanama_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '0')
+end
+go
+
+USE BelcorpGuatemala_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '0')
+end
+go
+
+USE BelcorpEcuador_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
+USE BelcorpDominicana_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '0')
+end
+go
+
+USE BelcorpCostaRica_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
+USE BelcorpChile_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+
+end
+go
+
+USE BelcorpBolivia_MC
+GO
+
+update menumobile set urlitem = 'MisCatalogosRevistas/Index' where menumobileid = 21 --Mobile/Catalogo
+
+declare @TablaLogicaID int = 155, @TablaLogicaDatosID int = 15302
+
+if not exists (select 1 from TablaLogica where TablaLogicaID = 155)
+begin
+	insert into TablaLogica values(@TablaLogicaID, 'Piloto Catalogo Digital');
+end
+
+if not exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15302)
+begin
+	insert into TablaLogicaDatos values(@TablaLogicaDatosID, @TablaLogicaID, '01', 'Piloto', '1')
+end
+go
+
