cede858a742e43b2ab54517e61320f44f10be270
jose-ponte <jose.ponte@hundred.com.pe>
2019-01-23 10:38:43 -0500

    Se ha desecho el cambio Validacion Venta Maxima solo para Peru. HD-3253

--- app/Portal.Consultoras.Web/Controllers/PedidoController.cs
+++ app/Portal.Consultoras.Web/Controllers/PedidoController.cs
@@ -4459,9 +4459,7 @@ namespace Portal.Consultoras.Web.Controllers
                 {
                     respValidar = sv.CuvTieneLimiteVenta(userData.PaisID, userData.CampaniaID, userData.CodigorRegion, userData.CodigoZona, cuv, cantidad, cantidadActual);
                 }
-                if (!respValidar.TieneLimite) return mensaje;
-                if (userData.CodigoISO == "PE") return string.Format(Constantes.MensajesError.ExcedioLimiteVentaPE, cuv, descripcion, respValidar.UnidadesMaximas);
-                mensaje = string.Format(Constantes.MensajesError.ExcedioLimiteVenta, respValidar.UnidadesMaximas);
+                if (respValidar.TieneLimite) mensaje = string.Format(Constantes.MensajesError.StockLimiteVenta, cuv, descripcion, respValidar.UnidadesMaximas);
             }
             catch (Exception ex)
             {
