d2d37dd2725569acf4b6b5a32b1b3efb5f284472
Victor Vilela <victor.vilela@avantica.net>
2019-01-24 10:56:45 -0500

    sql scripts :  to add row into TablaLogica  : Pantallas Responsive

---
+++ app/Scripts/EPM/PI3/Sprint1/FichaResumida/Rollbacks/2_1_Rollback_TablaLogica.sql
@@ -0,0 +1,24 @@
+use BelcorpPeru_BPT
+go
+
+print db_name()
+
+declare @TablaLogicaId_FeatureFlags int = 157
+declare @TablaLogicaDatosId_MisClientesResponsive int = @TablaLogicaId_FeatureFlags * 100 + 1
+
+if  exists (	select 1
+				from TablaLogica tl
+				where tl.TablaLogicaID = @TablaLogicaId_FeatureFlags)
+begin
+	print 'Rollback insert into TablaLogica : 157 - Pantallas Responsive'
+	
+	delete tld
+	from TablaLogicaDatos tld 
+	where tld.TablaLogicaDatosID = @TablaLogicaDatosId_MisClientesResponsive
+	and tld.TablaLogicaID = @TablaLogicaId_FeatureFlags
+
+	delete tl
+	from TablaLogica tl
+	where tl.TablaLogicaID = @TablaLogicaId_FeatureFlags
+end
+
---
+++ app/Scripts/EPM/PI3/Sprint1/FichaResumida/Scripts/2_1_TablaLogica.sql
@@ -0,0 +1,43 @@
+use BelcorpPeru_BPT
+go
+
+print db_name()
+
+declare @TablaLogicaId_FeatureFlags int = 157
+declare @TablaLogicaDescripcion_FeatureFlags varchar(30) = 'Pantallas Responsive'
+--
+declare @TablaLogicaDatosId_MisClientesResponsive int = @TablaLogicaId_FeatureFlags * 100 + 1
+
+if not exists (	select 1
+				from TablaLogica tl
+				where tl.TablaLogicaID = @TablaLogicaId_FeatureFlags)
+begin
+	print 'insert into TablaLogica : 157 - Pantallas Responsive'
+	insert into TablaLogica(
+	TablaLogicaID
+	,Descripcion
+	)
+	values
+	(
+	@TablaLogicaId_FeatureFlags
+	,@TablaLogicaDescripcion_FeatureFlags
+	)
+
+	print 'insert into TablaLogicaDatos : MisClientes'
+	insert into TablaLogicaDatos(
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+	values
+	(
+	@TablaLogicaDatosId_MisClientesResponsive
+	,@TablaLogicaId_FeatureFlags
+	,'MisClientes'
+	,'0 : Habilitar, 1: Deshabilitar'
+	,'0'
+	)
+end
+
