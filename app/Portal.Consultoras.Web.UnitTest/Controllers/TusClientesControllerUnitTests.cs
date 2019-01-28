7a1924c2bd925aaaf3f90d2dee3e3476ddc9b474
Victor Vilela <victor.vilela@avantica.net>
2019-01-28 15:58:01 -0500

    add unit test to MisClientesController.Consultar, verifying client name filter-

--- app/Portal.Consultoras.Web.UnitTest/Controllers/TusClientesControllerUnitTests.cs
+++ app/Portal.Consultoras.Web.UnitTest/Controllers/TusClientesControllerUnitTests.cs
@@ -9,6 +9,7 @@ using Portal.Consultoras.Web.Models.TusClientes;
 using Portal.Consultoras.Web.Providers;
 using Portal.Consultoras.Web.SessionManager;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Web.Mvc;
 
@@ -27,6 +28,14 @@ namespace Portal.Consultoras.Web.UnitTest.Controllers
             {
                 base.Test_Initialize();
                 clienteProvider = new Mock<ClienteProvider>();
+                clienteProvider
+                    .Setup(x => x.SelectByConsultora(
+                        It.IsAny<int>(),
+                        It.IsAny<long>()))
+                    .Returns( new List<ClienteModel>() {
+                        new ClienteModel{ NombreCliente = "xyz" },
+                        new ClienteModel{ NombreCliente = "abcabc" },
+                    });
                 ConfigureUserDataWithCampaniaActualAndConsultoraId(201901, 999888777);
             }
 
@@ -58,6 +67,42 @@ namespace Portal.Consultoras.Web.UnitTest.Controllers
             }
 
             [TestMethod]
+            public void Consultar_textIsNotEmpty_shouldCallServiceAllClientes()
+            {
+                //Arrange
+                var controller = new TusClientesController(clienteProvider.Object
+                    , SessionManager.Object
+                    , LogManager.Object);
+
+
+                //Act
+                controller.Consultar("abc");
+
+                //Assert    
+                clienteProvider.Verify(x => x.SelectByConsultora(
+                        It.Is<int>(p => p.Equals(11)),
+                        It.Is<long>(p => p.Equals(999888777))
+                        )
+                    );
+            }
+
+            [TestMethod]
+            public void Consultar_textIsNotEmpty_shouldFilterClients()
+            {
+                //Arrange
+                var controller = new TusClientesController(clienteProvider.Object
+                    , SessionManager.Object
+                    , LogManager.Object);
+
+
+                //Act
+                var result = (ConsultarResult)controller.Consultar("abc").Data;
+
+                //Assert    
+                Assert.AreEqual(1, result.miData.Count);
+            }
+
+            [TestMethod]
             public void Consultar_CallToProviderThrowException_shouldLogManager()
             {
                 //Arrange
@@ -79,5 +124,103 @@ namespace Portal.Consultoras.Web.UnitTest.Controllers
                 VerifyCallLogManager("TusClientesController.Consultar");
             }
         }
+
+
+        //[TestClass]
+        //public class Mantener : Base
+        //{
+        //    [TestInitialize]
+        //    public override void Test_Initialize()
+        //    {
+        //        base.Test_Initialize();
+        //        clienteProvider = new Mock<ClienteProvider>();
+        //        ConfigureUserDataWithCampaniaActualAndConsultoraId(201901, 999888777);
+        //    }
+
+        //    [TestCleanup]
+        //    public override void Test_Cleanup()
+        //    {
+        //        base.Test_Cleanup();
+        //        clienteProvider = null;
+        //    }
+
+        //    [TestMethod]
+        //    public void ConsultaMantener_textIsEmpty_shouldCallServiceAllClientes()
+        //    {
+        //        //Arrange
+        //        var controller = new TusClientesController(clienteProvider.Object
+        //            , SessionManager.Object
+        //            , LogManager.Object);
+
+
+        //        //Act
+        //        controller.Consultar(string.Empty);
+
+        //        //Assert    
+        //        clienteProvider.Verify(x => x.SelectByConsultora(
+        //                It.Is<int>(p => p.Equals(11)),
+        //                It.Is<long>(p => p.Equals(999888777))
+        //                )
+        //            );
+        //    }
+
+        //    //[TestMethod]
+        //    //public void Consultar_textIsNotEmpty_shouldCallServiceAllClientes()
+        //    //{
+        //    //    //Arrange
+        //    //    var controller = new TusClientesController(clienteProvider.Object
+        //    //        , SessionManager.Object
+        //    //        , LogManager.Object);
+
+
+        //    //    //Act
+        //    //    controller.Consultar("abc");
+
+        //    //    //Assert    
+        //    //    clienteProvider.Verify(x => x.SelectByConsultora(
+        //    //            It.Is<int>(p => p.Equals(11)),
+        //    //            It.Is<long>(p => p.Equals(999888777))
+        //    //            )
+        //    //        );
+        //    //}
+
+        //    //[TestMethod]
+        //    //public void Consultar_textIsNotEmpty_shouldFilterClients()
+        //    //{
+        //    //    //Arrange
+        //    //    var controller = new TusClientesController(clienteProvider.Object
+        //    //        , SessionManager.Object
+        //    //        , LogManager.Object);
+
+
+        //    //    //Act
+        //    //    var result = (ConsultarResult)controller.Consultar("abc").Data;
+
+        //    //    //Assert    
+        //    //    Assert.AreEqual(1, result.miData.Count);
+        //    //}
+
+        //    //[TestMethod]
+        //    //public void Consultar_CallToProviderThrowException_shouldLogManager()
+        //    //{
+        //    //    //Arrange
+        //    //    clienteProvider
+        //    //        .Setup(x => x.SelectByConsultora(
+        //    //            It.IsAny<int>(),
+        //    //            It.IsAny<long>()))
+        //    //        .Throws<Exception>();
+
+        //    //    var controller = new TusClientesController(clienteProvider.Object
+        //    //        , SessionManager.Object
+        //    //        , LogManager.Object);
+
+
+        //    //    //Act
+        //    //    controller.Consultar(string.Empty);
+
+        //    //    //Assert    
+        //    //    VerifyCallLogManager("TusClientesController.Consultar");
+        //    //}
+        //}
     }
 }
