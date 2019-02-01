73ffdf03554b718c61ce376f94361f6c5c7b1dec
ipndeveloper <i_nav_31_8@hotmail.com>
2019-02-01 11:49:36 -0500

    quitar logs: apiSB y tvt.

--- app/Portal.Consultoras.Common/LogManager/LogManager.cs
+++ app/Portal.Consultoras.Common/LogManager/LogManager.cs
@@ -77,8 +77,7 @@ namespace Portal.Consultoras.Common
                     logError.IsoPais = Util.GetPaisISO(int.Parse(logError.IsoPais));
                 }
 
-                RegistrarArchivoTexto(logError, pathFile);
-                RegistrarDynamoDB(logError);
+                
                 RegistrarElastic(logError);
             }
             catch (Exception)
@@ -121,7 +120,7 @@ namespace Portal.Consultoras.Common
             }
             catch (Exception ex)
             {
-                EventLog.WriteEntry("SomosBelcorp - LogManager", string.Format("Mensaje: {0} \nTrace: {1}", ex.Message, ex.StackTrace), EventLogEntryType.Error);
+                //EventLog.WriteEntry("SomosBelcorp - LogManager", string.Format("Mensaje: {0} \nTrace: {1}", ex.Message, ex.StackTrace), EventLogEntryType.Error);
             }
         }
 
