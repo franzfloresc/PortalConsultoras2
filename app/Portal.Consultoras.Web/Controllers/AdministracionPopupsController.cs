4b6391d2178cdd57f47126c1072078e3278fd48a
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-02-01 18:16:02 -0500

    Avance HD-3344 - Herramienta Administración de Popups (Diseño).

---
+++ app/Portal.Consultoras.Web/Controllers/AdministracionPopupsController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServicePedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class AdministracionPopupsController : BaseAdmController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }   
+}
