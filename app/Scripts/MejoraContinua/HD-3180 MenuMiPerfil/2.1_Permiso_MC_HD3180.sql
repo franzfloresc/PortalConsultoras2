6ff3557b5a8c8fa7edd3d8a1f2784a631a7c3309
Abel Reynaltte <abelreynalte@gmail.com>
2019-02-01 11:33:06 -0500

    HD-3180: Scripts

---
+++ app/Scripts/MejoraContinua/HD-3180 MenuMiPerfil/2.1_Permiso_MC_HD3180.sql
@@ -0,0 +1,287 @@
+USE BelcorpPeru
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpMexico
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpColombia
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpSalvador
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpPuertoRico
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpPanama
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpGuatemala
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpEcuador
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpDominicana
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpCostaRica
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpChile
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
+
+USE BelcorpBolivia
+GO
+
+declare @IdPadre int = 1091
+
+if not exists(select 1 from Permiso where PermisoID = 1091)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1091,'MI PERFIL',0,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1092)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1092,'MIS DATOS',@IdPadre,1,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+
+if not exists(select 1 from Permiso where PermisoID = 1093)
+begin
+	insert into Permiso(PermisoID, Descripcion, IdPadre, OrdenItem, UrlItem, PaginaNueva, Posicion, UrlImagen, EsSoloImagen, EsMenuEspecial, EsServicios, EsPrincipal, Codigo)
+	values(1093,'DIRECCIÓN DE ENTREGA',@IdPadre,2,'',0,'Body','',0,0,0,1,'MiPerfil')
+end
+go
---
+++ app/Scripts/MejoraContinua/HD-3180 MenuMiPerfil/ROLLBACK_2.2_Permiso_MC_HD3180.sql
@@ -0,0 +1,95 @@
+USE BelcorpPeru
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpMexico
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpColombia
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpSalvador
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpPuertoRico
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpPanama
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpGuatemala
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpEcuador
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpDominicana
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpCostaRica
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpChile
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
+
+USE BelcorpBolivia
+GO
+
+delete from Permiso where PermisoID = 1091
+delete from Permiso where PermisoID = 1092
+delete from Permiso where PermisoID = 1093
+go
