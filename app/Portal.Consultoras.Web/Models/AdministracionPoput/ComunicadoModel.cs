bbf4d17b3a1043ad2fb2c368df5d96d712dae5a8
Paquirrikurlang <spaquirri@gmail.com>
2019-02-05 15:24:47 -0500

    cambio 2 del gestor de contenido

---
+++ app/Portal.Consultoras.Web/Models/AdministracionPoput/ComunicadoModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.Models.AdministracionPoput
+{
+    public class ComunicadoModel
+    {
+        public int ComunicadoId { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+        public string CodigoCampania { get; set; }
+        public string Accion { get; set; }
+        public string DescripcionAccion { get; set; }
+        public int SegmentacionID { get; set; }
+        public string UrlImagen { get; set; }
+        public string Titulo { get; set; }
+        public int Orden { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int TipoDispositivo { get; set; }
+        public bool SegmentacionConsultora { get; set; }
+        public bool SegmentacionRegionZona { get; set; }
+        public bool SegmentacionEstadoActividad { get; set; }
+        public int CampaniaID { get; set; }
+        public int Numero { get; set; }
+        public string FechaInicio_ { get; set; }
+        public string FechaFin_ { get; set; }
+        public IEnumerable<SegmentacionComunicadoModel> ListaSegmentacionComunicado { set; get; }
+        public IEnumerable<CampaniaModel> listaCampania { get; set; }
+
+    }
+   
+}
---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AdministraciónPoput/administracion-popups.js
@@ -0,0 +1,36 @@
+﻿/*
+ 1 -->  Nuevo
+ 2 -->  MOdificar
+ */
+
+jQuery(document).ready(function () {
+
+    var valorAccion = $("hdValueAction").val();
+    if (valorAccion == 2) {
+        CargarCamposEdicion();
+    }
+
+});
+
+
+function CargarCamposEdicion() {
+
+    var comunicadoId = $("hdComunicadoId").val();
+    $.ajax({
+        async: false,
+        type: 'post',
+        url: 'GestionPopup/GetCargaEdicionPoput',
+        data: { comunicadoId: comunicadoId },
+        success: function (data) {
+            if (!data) {
+                alert(data);
+                return;
+            }
+            todoMenus = data.menu;
+        },
+        error: function (request, status, error) {
+            alert(jQuery.parseJSON(request.responseText).Message);
+        }
+    });
+
+}
