d10cb5c2e441c92d491680f5543803409cb90f8e
jose-ponte <jose.ponte@hundred.com.pe>
2019-02-05 14:54:11 -0500

    Se actualizo la ruta tabla Banner HD-3378

--- app/Scripts/MejoraContinua/HD-3378/script/2_Banner_MC_HD-3378.sql
+++ app/Scripts/MejoraContinua/HD-3378/script/2_Banner_MC_HD-3378.sql
@@ -1,4 +1,4 @@
 USE BelcorpPeru
 GO
-Update Banner set TituloComentario = 'Comparte tu catálogo digital y gana más' where CampaniaId = 201903 and bannerID in(5,6)
+Update Banner set TituloComentario = 'Comparte tu catálogo digital y gana más', URL = 'https://www.somosbelcorp.com/MisCatalogosRevistas' where CampaniaId = 201903 and bannerID in(5,6)
 GO
