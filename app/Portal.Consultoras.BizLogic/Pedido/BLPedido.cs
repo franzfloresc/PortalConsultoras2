507199e57b0075e54f1c22f9a24cccbfbc453b58
Paul Cabrera Perez <paul.cabrera@hundred.com.pe>
2019-02-06 11:41:57 -0500

    GANAZG-123 - "caso RDI" Fixed Up

--- app/Portal.Consultoras.BizLogic/Pedido/BLPedido.cs
+++ app/Portal.Consultoras.BizLogic/Pedido/BLPedido.cs
@@ -2757,7 +2757,7 @@ namespace Portal.Consultoras.BizLogic.Pedido
                 CampaniaId = usuario.CampaniaID,
                 ConsultoraId = usuario.ConsultoraID,
                 Consultora = usuario.Nombre,
-                EsBpt = usuario.RevistaDigital.EsOpt() == 1,
+                //EsBpt = usuario.RevistaDigital.EsOpt() == 1,
                 CodigoPrograma = usuario.CodigoPrograma,
                 NumeroPedido = usuario.ConsecutivoNueva,
                 AgruparSet = true
@@ -3133,16 +3133,18 @@ namespace Portal.Consultoras.BizLogic.Pedido
             var usuario = pedidoDetalle.Usuario;
 
             #region ConfiguracionPais
-            var taskRevistaDigital = Task.Run(() => _usuarioBusinessLogic.ConfiguracionPaisUsuario(usuario, Constantes.ConfiguracionPais.RevistaDigital));
-            var taskValidacionMontoMaximo = Task.Run(() => _usuarioBusinessLogic.ConfiguracionPaisUsuario(usuario, Constantes.ConfiguracionPais.ValidacionMontoMaximo));
+            //var taskRevistaDigital = Task.Run(() => _usuarioBusinessLogic.ConfiguracionPaisUsuario(usuario, Constantes.ConfiguracionPais.RevistaDigital));
+            //var taskValidacionMontoMaximo = Task.Run(() => _usuarioBusinessLogic.ConfiguracionPaisUsuario(usuario, Constantes.ConfiguracionPais.ValidacionMontoMaximo));
 
-            Task.WaitAll(taskRevistaDigital, taskValidacionMontoMaximo);
+            //Task.WaitAll(taskRevistaDigital, taskValidacionMontoMaximo);
 
             //Configuracion pais - RevistaDigital
-            usuario.RevistaDigital = taskRevistaDigital.Result.RevistaDigital;
+            //suario.RevistaDigital = taskRevistaDigital.Result.RevistaDigital;
 
             //Configuracion pais - ValidacionMontoMaximo
-            usuario.TieneValidacionMontoMaximo = taskValidacionMontoMaximo.Result.TieneValidacionMontoMaximo;
+            //usuario.TieneValidacionMontoMaximo = taskValidacionMontoMaximo.Result.TieneValidacionMontoMaximo;
+            usuario.TieneValidacionMontoMaximo = _usuarioBusinessLogic.ConfiguracionPaisUsuario(usuario, 
+                Constantes.ConfiguracionPais.ValidacionMontoMaximo).TieneValidacionMontoMaximo;
             #endregion
 
             #region HorarioRestringido
