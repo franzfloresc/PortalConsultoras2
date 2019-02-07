078d028d203c2abaf27af5fcb22217f0a3af9184
Enrique Inca <inca.quispe.e@outlook.com>
2019-02-07 11:02:34 -0500

    Agregnado los script de inserción de los mensajes de importante

---
+++ app/Scripts/MejoraContinua/HD-3309/rollback/TablaLogicaDatos_MC_HD-3309_rollback.sql
@@ -0,0 +1,139 @@
+﻿USE BelcorpPeru
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpBolivia
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+USE BelcorpChile
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+USE BelcorpColombia
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+USE BelcorpGuatemala
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpMexico
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpCostaRica
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+USE BelcorpEcuador
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpSalvador
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+USE BelcorpPanama
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpPuertoRico
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+
+USE BelcorpDominicana
+GO
+
+DELETE
+FROM dbo.TablaLogicaDatos
+WHERE TablaLogicaID = 160;
+
+DELETE
+FROM dbo.TablaLogica
+WHERE TablaLogicaID = 160;
+GO
+
+
---
+++ app/Scripts/MejoraContinua/HD-3309/script/TablaLogicaDatos_MC_HD-3309.sql
@@ -0,0 +1,334 @@
+﻿USE BelcorpPeru
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo pedido tendrá recargo por flete dependiendo del monto del pedido, peso y distancia. En Lima entre S/9.50 y S16.00 en Provincias entre S/13.50 y S/19.00 Amazonas, San Martín, Ucayali y Loreto entre S16.50 y S/24.00. Evita el cobro de flete adicional pasando tu pedido en fecha.
+Si pasa tu pedido en fecha extemporánea de facturación se cobrará un adicional de S/5.00 en Lima S/7.00 en Provincias y S/10 .00 en Zonas Alejadas.'
+	);
+GO
+
+USE BelcorpBolivia
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo pedido tendrá un recargo por flete.'
+	);
+GO
+
+USE BelcorpChile
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo lo que vendas suma para alcanzar tu escala de comisión. Todo el pedido tendrá recargo por flete: Santiago $. 1.800 y Provincia $. 2.900. No se aceptarán pedidos por un monto menor a $ 45.000.'
+	);
+GO
+
+USE BelcorpColombia
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo pedido tendrá recargo por flete.'
+	);
+GO
+
+USE BelcorpGuatemala
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'No se aceptarán pedidos por un monto menor de Q. 570.00. Todo pedido tendrá un recargo por flete, en la Capital será de Q. 29.79 y en Provincia de Q.38.99.'
+	);
+GO
+
+USE BelcorpMexico
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo pedido tendrá recargo por flete.'
+	);
+GO
+
+USE BelcorpCostaRica
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'No se aceptarán pedidos por un monto menor de ₡. 55,000. Todo pedido tendrá un recargo por flete. Si tu pedido es hasta ₡. 75,999.99 se te cargará ₡. 2,499 en la Capital y ₡. 2,799 en Provincia.'
+	);
+GO
+
+USE BelcorpEcuador
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'Todo pedido tendrá un recargo por flete.'
+	);
+GO
+
+USE BelcorpSalvador
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,'No se aceptarán pedidos por un monto menor de $. 75.00. Todo pedido tendrá un recargo por flete, en la Capital será de $. 3.19 y en Departamento $. 4.09'
+	);
+GO
+
+USE BelcorpPanama
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,''
+	);
+
+USE BelcorpPuertoRico
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,''
+	);
+GO
+
+USE BelcorpDominicana
+GO
+
+INSERT INTO dbo.TablaLogica (
+	TablaLogicaID
+	,Descripcion
+	)
+VALUES (
+	160
+	,'Msj Importante Escala DSCTO'
+	)
+
+INSERT INTO dbo.TablaLogicaDatos (
+	TablaLogicaDatosID
+	,TablaLogicaID
+	,Codigo
+	,Descripcion
+	,Valor
+	)
+VALUES (
+	15400
+	,160
+	,'01'
+	,'Mensaje importante escala descuento'
+	,''
+	);
