92edaf1cf0490c3a4cd51e922078c3f102358c98
jose-ponte <jose.ponte@hundred.com.pe>
2019-02-07 12:18:05 -0500

    script HD-3180

---
+++ app/Scripts/MejoraContinua/HD-3180/script/2_ConfiguracionPais_MC_HD-3180.sql
@@ -0,0 +1,84 @@
+USE BelcorpPeru
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpMexico
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpColombia
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpSalvador
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpPuertoRico
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpPanama
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpGuatemala
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpEcuador
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpDominicana
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpCostaRica
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpChile
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 1, 0, 201903, 0, 0, 0, 0)
+GO
+
+USE BelcorpBolivia
+GO
+
+Insert ConfiguracionPais (Codigo, Excluyente, Descripcion, Estado, TienePerfil, DesdeCampania, Orden, OrdenBpt, MobileOrden, MobileOrdenBPT)
+						values('DIRECCION_ENTREGA', 0, 'Dirección de entrega en perfil', 0, 0, 201903, 0, 0, 0, 0)
+GO
+
--- app/Scripts/MejoraContinua/HD-3180/script/2_OpcionesUsuario_MC_HD-3180.sql
+++ app/Scripts/MejoraContinua/HD-3180/script/2_OpcionesUsuario_MC_HD-3180.sql
@@ -1,84 +1,72 @@
 USE BelcorpPeru
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpMexico
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpColombia
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpSalvador
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpPuertoRico
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpPanama
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpGuatemala
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpEcuador
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpDominicana
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpCostaRica
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
 USE BelcorpChile
 GO
 
 insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 1),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', 1, 2, 'Opcion para compartir a WhatsApp', 1)
-GO
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 1)
 
 USE BelcorpBolivia
 GO
 
-insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', NULL, NULL, NULL, 0),
-																							   (2,'Quiero que se comuniquen conmigo por WhatsApp', 'chkComunicacionWhatsApp', NULL, NULL, NULL, 0)
-GO
+insert into OpcionesUsuario(OpcionesUsuarioId, Opcion, Codigo, Marcacion, Orden, Descripcion, Activo) values(1,'Quiero recibir mis boletas impresas', 'chkBoletasImpresas', 1, 1, 'Si desmarcas esta opción podras descargar tus boletas electronicas desde SomosBelcorp', 0),
+																							   (2,'Quiero recibir mi paquete documentario impreso', 'chkComunicacionWhatsApp', 1, 2, 'Si desmarcas esta opción tu paquete documentario podrás tenerlo solo desde SomosBelcorp', 0)
 
