ae3eb30571779664e08145da4477e630d9db1ff0
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-02-07 12:38:35 -0500

    Se agrega scripts para cambiar el idioma a calendarios.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AdminPoput/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AdminPoput/Index.js
@@ -18,6 +18,27 @@ $(document).ready(function () {
                 $('body').on('click', '.btn__modal--guardar, .btn__modal--descartar', me.Eventos.CerrarPopup);
             },
             CargarDatePicker: function() {
+                // Cambia el idioma a calendario
+                $.datepicker.regional['es'] = {
+                    closeText: 'Cerrar',
+                    prevText: '<Ant',
+                    nextText: 'Sig>',
+                    currentText: 'Hoy',
+                    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                    monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+                    dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+                    dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+                    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+                    weekHeader: 'Sm',
+                    dateFormat: 'dd/mm/yy',
+                    firstDay: 1,
+                    isRTL: false,
+                    showMonthAfterYear: false,
+                    yearSuffix: ''
+                };
+                $.datepicker.setDefaults($.datepicker.regional['es']);
+                // Fin - Cambia el idioma a calendario
+
                 $('#fechaMin').datepicker();
                 $('#fechaMax').datepicker();
             }
