476dd864d981224ce7b684c54da67222d9d12354
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-02-07 11:55:57 -0500

    Se agrega scripts para cargar calendario para seleccionar fecha mínima y máxima de actividad de popup.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AdminPoput/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/AdminPoput/Index.js
@@ -16,26 +16,31 @@ $(document).ready(function () {
             InicializarEventos: function () {
                 $('body').on('click', '.btn__agregar, .enlace__editar', me.Eventos.AbrirPopup);
                 $('body').on('click', '.btn__modal--guardar, .btn__modal--descartar', me.Eventos.CerrarPopup);
+            },
+            CargarDatePicker: function() {
+                $('#fechaMin').datepicker();
+                $('#fechaMax').datepicker();
             }
         },
-            me.Eventos = {
-                AbrirPopup: function (e) {
-                    e.preventDefault();
-                    $('#modalTitulo').html($(this).attr('title'));
-                    $('#AgregarPopup').fadeIn(100);
-                    $('#AgregarPopup').scrollTop(0);
-                    $('#AgregarPopup').css('display', 'flex');
-                    $('body').css('overflow-y', 'hidden');
-                },
-                CerrarPopup: function (e) {
-                    e.preventDefault();
-                    $('#AgregarPopup').fadeOut(100);
-                    $('body').css('overflow-y', '');
-                }
+        me.Eventos = {
+            AbrirPopup: function (e) {
+                e.preventDefault();
+                $('#modalTitulo').html($(this).attr('title'));
+                $('#AgregarPopup').fadeIn(100);
+                $('#AgregarPopup').scrollTop(0);
+                $('#AgregarPopup').css('display', 'flex');
+                $('body').css('overflow-y', 'hidden');
             },
-            me.Inicializar = function () {
-                me.Funciones.InicializarEventos();
+            CerrarPopup: function (e) {
+                e.preventDefault();
+                $('#AgregarPopup').fadeOut(100);
+                $('body').css('overflow-y', '');
             }
+        },
+        me.Inicializar = function () {
+            me.Funciones.InicializarEventos();
+            me.Funciones.CargarDatePicker();
+        }
     }
 
     VistaAdministracionPopups = new vistaAdPop();
