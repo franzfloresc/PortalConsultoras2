a941db596755071ea0dd0a7036c865a77f5e9721
César Cárdenas <cesar.cardenas@hundred.com.pe>
2019-02-19 13:10:31 -0500

    pull dummy

--- app/Portal.Consultoras.Web/Scripts/AdminContenido/AdministrarEstrategia.js
+++ app/Portal.Consultoras.Web/Scripts/AdminContenido/AdministrarEstrategia.js
@@ -1,5 +1,5 @@
 ﻿var AdministrarEstrategia = (function (config) {
- 
+
     var _config = {
         actualizarMatrizComercialAction: config.actualizarMatrizComercialAction || "",
         getImagesBySapCodeAction: config.getImagesBySapCodeAction || "",
