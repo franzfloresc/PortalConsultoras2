e22510868705d4b491fd681a55753b5ef90f1f2e
Kike <lead123456@hotmail.com>
2019-02-26 17:48:54 -0500

    qas: Agregar scripts de BD de HD-3180 a carpeta de pase.

---
+++ app/Scripts/Pases/20190301/2.RollBack/RollBack_2_ConfiguracionPais_MC_HD-3180.sql
@@ -0,0 +1,72 @@
+USE BelcorpPeru
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpMexico
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpColombia
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpSalvador
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpPuertoRico
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpPanama
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpGuatemala
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpEcuador
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpDominicana
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpCostaRica
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpChile
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
+USE BelcorpBolivia
+GO
+
+delete from ConfiguracionPais where Codigo = 'DIRECCION_ENTREGA'
+GO
+
