6c11bcedf32ad9e26f05724e58e047135dfa7d38
Kike <lead123456@hotmail.com>
2019-02-27 14:45:07 -0500

    qas: Ajuste en scripts 1_AddCampaniaFin_SPD.

--- app/Scripts/Pases/20190301/2.RollBack/AppCatalogo/RollBack_1_AddCampaniaFin_SPD.sql
+++ app/Scripts/Pases/20190301/2.RollBack/AppCatalogo/RollBack_1_AddCampaniaFin_SPD.sql
@@ -2,7 +2,7 @@
 GO
 IF EXISTS(SELECT 1 FROM sys.columns 
           WHERE Name = 'CampaniaFin'
-          AND Object_ID = Object_ID('AppCatalogo.ProductoCampanaTemporalSB'))
+          AND Object_ID = Object_ID('dbo.ProductoCampanaTemporalSB'))
 BEGIN
    ALTER TABLE [dbo].[ProductoCampanaTemporalSB]
    DROP COLUMN CampaniaFin
