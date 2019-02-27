71fdeaf2031d8ef61df73dcae0315dfe1896ea13
Victor Marino <victor.marino@hundred.com.pe>
2019-02-27 16:55:32 -0500

    Upload Fix Scripts App

---
+++ app/Scripts/Pases/20190301/2.RollBack/BelcorpPais/Rollback_2_1_TablaLogicaDatos_UX-1.sql
@@ -0,0 +1,120 @@
+﻿USE BelcorpPeru
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpMexico
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpColombia
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpSalvador
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpPuertoRico
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpPanama
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpGuatemala
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpEcuador
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpDominicana
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpCostaRica
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpChile
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
+USE BelcorpBolivia
+GO
+
+if exists (select 1 from TablaLogicaDatos where TablaLogicaDatosID = 15901)
+begin
+	delete from TablaLogicaDatos where TablaLogicaDatosID = 15901
+end
+
+GO
+
