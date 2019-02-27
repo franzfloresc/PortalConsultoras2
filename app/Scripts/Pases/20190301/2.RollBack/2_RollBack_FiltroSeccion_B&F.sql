f3ce9b66d31e9e832c67d8ffd8f853721eec5c73
MIJAEL888 <mijael888@gmail.com>
2019-02-27 11:43:41 -0500

    Correccion Scripts

--- app/Scripts/Pases/20190301/2.RollBack/2_RollBack_FiltroSeccion_B&F.sql
+++ app/Scripts/Pases/20190301/2.RollBack/2_RollBack_FiltroSeccion_B&F.sql
@@ -1,133 +1,96 @@
-GO
 USE BelcorpPeru
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpMexico
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpColombia
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpSalvador
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpPuertoRico
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpPanama
 GO
-IF EXISTS (SELECT * FROM filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpGuatemala
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpEcuador
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpDominicana
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpCostaRica
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpChile
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
 USE BelcorpBolivia
 GO
-IF EXISTS (SELECT * FROM   filtroseccion WHERE  campoes = 'seccion.keyword')
-BEGIN
-	DECLARE @Id int = (SELECT TablaLogicaDatosID from  filtroseccion where campoes = 'seccion.keyword')
-	delete from filtrobuscador where tablalogicadatosid = @Id
-	delete from filtroseccion where tablalogicadatosid = @Id
-	delete from TablaLogicaDatos where TablaLogicaDatosID = @Id
-END
 
+BEGIN 
+	DELETE FROM filtroseccion WHERE tablalogicadatosid = 14804
+END
 GO
+
