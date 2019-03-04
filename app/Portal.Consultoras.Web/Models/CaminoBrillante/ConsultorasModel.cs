d010a615939dc648cb9ae38e14d55ee2e1ac0c21
ricjimenez <ricardo.jimenez@hundred.com.pe>
2019-03-04 17:11:27 -0500

    - Creación controller/view "Camino Brillante"
    - Creación modelos [Consultores, ofertas, periodos y niveles]

---
+++ app/Portal.Consultoras.Web/Models/CaminoBrillante/ConsultorasModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Portal.Consultoras.Web.Models.CaminoBrillante
+{
+    public class ConsultorasModel
+    {
+        public string Isopais { get; set; }
+        public string PeriodoCae { get; set; }
+        public string Campana { get; set; }
+        public string NivelActual { get; set; }
+        public decimal MontoPedido { get; set; }
+        public string FechaIngreso { get; set; }
+        public decimal GananciaCampaña { get; set; }
+        public decimal GananciaPeriodo { get; set; }
+        public decimal GananciaAnual { get; set; }
+    }
+}
