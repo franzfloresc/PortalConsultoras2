1efe727a08bce467031aefcdb7ddf958cb39451e
ricjimenez <ricardo.jimenez@hundred.com.pe>
2019-03-04 17:31:16 -0500

    Creación de los metodos que obtendrá la información del WS de comercial.

--- app/Portal.Consultoras.Web/Providers/CaminoBrillanteProvider.cs
+++ app/Portal.Consultoras.Web/Providers/CaminoBrillanteProvider.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿
+using Portal.Consultoras.Web.SessionManager;
+using Portal.Consultoras.Web.Models.CaminoBrillante;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Portal.Consultoras.Web.Providers
 {
     public class CaminoBrillanteProvider
     {
+        public ConsultorasModel GetDatosConsultora(string consultoraID)
+        {
+            return null;
+        }
 
+        public List<NivelesModel> GetNiveles(string isopais)
+        {
+            return null;
+        }
+
+        public List<PeriodosModel> GetPeriodos(string isopais)
+        {
+            return null;
+        }
+
+        public List<OfertasModel> GetOfertas(string isopais, string campana)
+        {
+            return null;
+        }
     }
 }
