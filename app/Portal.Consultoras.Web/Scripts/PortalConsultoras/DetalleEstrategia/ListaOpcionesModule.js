409ce92dd8a3c45b02d527d2677bb9b8aec9e219
Victor Vilela <victor.vilela@avantica.net>
2019-03-06 15:48:35 -0500

    extracting DetalleEstrategiaProvider from FichaModule.js into DetalleEstrategiaProvider .js

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/ListaOpcionesModule.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/ListaOpcionesModule.js
@@ -14,6 +14,7 @@
 /// <reference path="../../../Scripts/PortalConsultoras/RevistaDigital/RevistaDigital.js" />
 /// <reference path="../../../Scripts/PortalConsultoras/Shared/ConstantesModule.js" />
 /// <reference path="../../../Scripts/PortalConsultoras/DetalleEstrategia/OpcionesSeleccionadasModule.js" />
+/// <reference path="../../../Scripts/PortalConsultoras/DetalleEstrategia/DetalleEstrategiaProvider.js" />
 /// <reference path="../../../Scripts/PortalConsultoras/DetalleEstrategia/FichaModule.js" />
 
 var opcionesEvents = opcionesEvents || {};
--- app/Portal.Consultoras.Web/karma.conf.js
+++ app/Portal.Consultoras.Web/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function (config) {
             'Scripts/PortalConsultoras/TusClientes/PanelListaModule.js',
             'Scripts/PortalConsultoras/TusClientes/PanelMantenerModule.js',
             'Scripts/PortalConsultoras/TusClientes/ClientePanelModule.js',
+            'Scripts/PortalConsultoras/DetalleEstrategia/DetalleEstrategiaProvider.js',
             'Scripts/PortalConsultoras/DetalleEstrategia/FichaModule.js',
 
             // TusClientes
