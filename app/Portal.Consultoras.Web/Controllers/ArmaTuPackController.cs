59104941283d8480d530d118179df2c95aa0ddae
Victor Vilela <victor.vilela@avantica.net>
2019-03-06 14:40:36 -0500

    rename ArmaTupack/Index to ArmaTuPacK/Detalle

--- app/Portal.Consultoras.Web/Controllers/ArmaTuPackController.cs
+++ app/Portal.Consultoras.Web/Controllers/ArmaTuPackController.cs
@@ -1,13 +1,22 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace Portal.Consultoras.Web.Controllers
 {
+    [RoutePrefix("ArmaTuPack")]
     public class ArmaTuPackController : BaseController
     {
         public ActionResult Index()
         {
+            throw new NotImplementedException();
+        }
+        [HttpGet()]
+        [Route("Detalle/{cuv:int}")]
+        public ActionResult Detalle(string cuv)
+        {
+            if (string.IsNullOrEmpty(cuv)) throw new ArgumentNullException("cuv", "is null or empty.");
+
             return View();
         }
-        
     }
 }
