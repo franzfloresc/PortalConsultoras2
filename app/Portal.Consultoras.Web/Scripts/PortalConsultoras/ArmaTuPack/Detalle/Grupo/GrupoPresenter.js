e627ad575d486f94ba2dfa23971ad9a39b0aff0b
Victor Vilela <victor.vilela@avantica.net>
2019-03-07 09:44:35 -0500

    adding js classes for ArmaTuPack/Detalle

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupo/GrupoPresenter.js
@@ -0,0 +1,18 @@
+﻿var GrupoPresenter = function (config) {
+    if (typeof config === "undefined" || config === null) throw "config is null";
+    if (typeof config.grupoView === "undefined" || config.grupoView === null) throw "config is null";
+    if (typeof config.detalleEstrategiaProvider === "undefined" || config.detalleEstrategiaProvider === null) throw "config is null";
+
+    var _config = {
+        grupoView: config.grupoView,
+        detalleEstrategiaProvider: config.detalleEstrategiaProvider,
+    };
+
+    var _init = function () {
+
+    };
+
+    return {
+        init : _init
+    };
+};
