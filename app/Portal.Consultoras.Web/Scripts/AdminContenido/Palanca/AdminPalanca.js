a9fa9d8de2281bec64d0c1fedf96fb6dc7a263ca
EAAR85 <44047823+EAAR85@users.noreply.github.com>
2019-03-07 11:28:29 -0500

    AGANA 159
    Validaciones preliminares.

--- app/Portal.Consultoras.Web/Scripts/AdminContenido/Palanca/AdminPalanca.js
+++ app/Portal.Consultoras.Web/Scripts/AdminContenido/Palanca/AdminPalanca.js
@@ -95,13 +95,26 @@ function Modificar(idConfiguracionPais, event) {
                 UploadFilePalanca("icono"), UploadFilePalanca("desktop-fondo-banner"), UploadFilePalanca("desktop-logo-banner"),
                 UploadFilePalanca("mobile-fondo-banner"), UploadFilePalanca("mobile-logo-banner")
             );
+
+          
             showDialog("DialogMantenimientoPalanca");
 
             var esTrueAncla = $.trim($("#UrlMenu").val()) == "#";
+        
             if (esTrueAncla) {
                 $("#cbAncla").prop("checked", true);
                 $("#UrlMenu").attr("disabled", "disabled");
             }
+            
+            /*INIT Agana 159*/
+            //var esATP = $.trim($("#Codigo").val()) === "ATP";
+            
+            var criterioCss = $.trim($("#Codigo").val()) === "ATP" ? 'none' : 'block';
+          
+            $("#divUrlMenu").css("display", criterioCss);
+            $("#divUrlMenuTxt").css("display", criterioCss);
+        
+            /*END Agana 159*/
         },
         error: function (request, status, error) { }
     });
@@ -159,7 +172,7 @@ function IniDialogs() {
         draggable: false,
         title: "Configurar Contenedor Menú",
         open: function (event, ui) {
-            (".ui-dialog-titlebar-close", ui.dialog).hide();
+            //(".ui-dialog-titlebar-close", ui.dialog).hide(); //eaar tiene bug
         },
         close: function () {
             HideDialog("DialogMantenimientoPalanca");
@@ -251,9 +264,9 @@ function IniDialogs() {
                     $(this).ColorPickerSetColor(this.value);
                 }
             })
-            .bind("keyup", function () {
-                $(this).ColorPickerSetColor(this.value);
-            });
+                .bind("keyup", function () {
+                    $(this).ColorPickerSetColor(this.value);
+                });
 
             if ($("#DesktopColorFondo").val() === "") {
                 $("#DesktopColorFondo").val("#000000");
