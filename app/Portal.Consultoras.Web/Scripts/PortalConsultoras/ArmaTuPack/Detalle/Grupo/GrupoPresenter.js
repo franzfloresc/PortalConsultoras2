eff846a2781e8217518df71b8b667af90d179ee5
Victor Vilela <victor.vilela@avantica.net>
2019-03-07 14:34:50 -0500

    AGANA-326 : redirect to Ofertas when client can not get data from backend

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupo/GrupoPresenter.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupo/GrupoPresenter.js
@@ -3,10 +3,12 @@
     //
     if (typeof config.grupoView === "undefined" || config.grupoView === null) throw "config.grupoView is null or undefined";
     if (typeof config.armaTuPackProvider === "undefined" || config.armaTuPackProvider === null) throw "config.armaTuPackProvider is null or undefined";
+    if (typeof config.generalModule === "undefined" || config.generalModule === null) throw "config.generalModule is null or undefined";
 
     var _config = {
         grupoView: config.grupoView,
         armaTuPackProvider: config.armaTuPackProvider,
+        generalModule: config.generalModule,
     };
 
     var _init = function () {
@@ -14,8 +16,11 @@
         _config
             .armaTuPackProvider
             .getPackComponentsPromise(cuv)
-            .done(function(){
-
+            .done(function(data){
+                _config.grupoView.grupos(data);
+            })
+            .fail(function(data,error){
+                _config.generalModule.redirectTo("/ofertas");
             });
     };
 
