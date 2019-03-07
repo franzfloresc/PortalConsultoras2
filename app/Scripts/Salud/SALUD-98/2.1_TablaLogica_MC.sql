28aba7ced2b3f2635a3523264db843cc02b092d1
Abel Reynaltte <abelreynalte@gmail.com>
2019-03-07 15:51:15 -0500

    SALUD-98: Scripts

---
+++ app/Scripts/Salud/SALUD-98/2.1_TablaLogica_MC.sql
@@ -0,0 +1,169 @@
+
+USE BelcorpPeru
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpMexico
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpColombia
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpSalvador
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpPuertoRico
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpPanama
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpGuatemala
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpEcuador
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpDominicana
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpCostaRica
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpChile
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
+USE BelcorpBolivia
+GO
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 163)
+begin
+	insert into TablaLogica values(163, '# intentos logueo de usuario')
+end
+
+if not exists(select 1 from TablaLogica where TablaLogicaID = 164)
+begin
+	insert into TablaLogica values(164, 'Tiempo espera de logueo')
+end
+go
+
