c5f33f2680525855fc675463cb12dff2179632b3
Victor Vilela <victor.vilela@avantica.net>
2019-03-08 13:56:01 -0500

    add unit test to SeleccionadosPresenter's constructor

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Seleccionados/SeleccionadosPresenter.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Seleccionados/SeleccionadosPresenter.js
@@ -1,8 +1,11 @@
 ﻿var SeleccionadosPresenter = function (config) {
-    if (typeof config === "undefined" || config === null) throw "config is null";
+    if (typeof config === "undefined" || config === null) throw "config is null or undefined";
+    if (typeof config.seleccionadosView === "undefined" || config.seleccionadosView === null) throw "config.seleccionadosView is null or undefined";
+    if (typeof config.armaTuPackDetalleEvents === "undefined" || config.armaTuPackDetalleEvents === null) throw "config.armaTuPackDetalleEvents is null or undefined";
 
     var _config = {
-
+        seleccionadosView: config.seleccionadosView,
+        armaTuPackDetalleEvents: config.armaTuPackDetalleEvents
     };
 
     return {
--- app/Portal.Consultoras.Web/Scripts/tests/PortalConsultoras/ArmaTuPack/Detalle/Seleccionados/SeleccionadosPresenterSpec.js
+++ app/Portal.Consultoras.Web/Scripts/tests/PortalConsultoras/ArmaTuPack/Detalle/Seleccionados/SeleccionadosPresenterSpec.js
@@ -1,1 +1,109 @@
-﻿
+﻿describe("ArmaTuPack - Detalle - Seleccionados", function () {
+    describe("Constructor", function () {
+        var errorMsg = '';
+
+        beforeEach(function () {
+            errorMsg = '';
+        });
+
+        it("throw an exception when config is undefined", function () {
+
+            try {
+                SeleccionadosPresenter(undefined);
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config is null or undefined");
+        });
+
+        it("throw an exception when config is null", function () {
+
+            try {
+                SeleccionadosPresenter(null);
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config is null or undefined");
+        });
+
+        it("throw an exception when config.seleccionadosView is undefined", function () {
+
+            try {
+                SeleccionadosPresenter({
+                    seleccionadosView: undefined
+                });
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config.seleccionadosView is null or undefined");
+        });
+
+        it("throw an exception when config.seleccionadosView is null", function () {
+
+            try {
+                SeleccionadosPresenter({
+                    seleccionadosView: null
+                });
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config.seleccionadosView is null or undefined");
+        });
+
+        it("throw an exception when config.armaTuPackDetalleEvents is undefined", function () {
+
+            try {
+                SeleccionadosPresenter({
+                    seleccionadosView : {},
+                    armaTuPackDetalleEvents : undefined
+                });
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config.armaTuPackDetalleEvents is null or undefined");
+        });
+
+        it("throw an exception when config.armaTuPackDetalleEvents is null", function () {
+
+            try {
+                SeleccionadosPresenter({
+                    seleccionadosView : {},
+                    armaTuPackDetalleEvents : null
+                });
+            } catch (error) {
+                errorMsg = error;
+            }
+
+            expect(errorMsg).to.have.string("config.armaTuPackDetalleEvents is null or undefined");
+        });
+    });
+
+    describe("OnGruposLoaded", function () {
+        var errorMsg = '';
+        //
+        var armaTuPackDetalleEvents = null;
+        var seleccionadosView = null;
+        var seleccionadosPresenter = null;
+
+        beforeEach(function () {
+            errorMsg = '';
+            //
+            armaTuPackDetalleEvents = sinon.stub(ArmaTuPackDetalleEvents());
+            seleccionadosView = sinon.stub(SeleccionadosView());
+            //
+            seleccionadosPresenter = SeleccionadosPresenter({
+                seleccionadosView:seleccionadosView,
+                armaTuPackDetalleEvents:armaTuPackDetalleEvents
+            });
+        });
+
+        afterEach(function () {
+            sinon.restore();
+        });
+    });
+});
