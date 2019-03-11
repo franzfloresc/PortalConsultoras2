46e6afa33ce641bffcf6e1dc15fc17520ff0a9c1
Victor Vilela <victor.vilela@avantica.net>
2019-03-10 21:41:56 -0500

    add uni test to GrupoPresenter's constructor

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupos/GruposPresenter.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupos/GruposPresenter.js
@@ -4,6 +4,7 @@
     if (typeof config.gruposView === "undefined" || config.gruposView === null) throw "config.gruposView is null or undefined";
     if (typeof config.armaTuPackProvider === "undefined" || config.armaTuPackProvider === null) throw "config.armaTuPackProvider is null or undefined";
     if (typeof config.generalModule === "undefined" || config.generalModule === null) throw "config.generalModule is null or undefined";
+    if (typeof config.armaTuPackDetalleEvents === "undefined" || config.armaTuPackDetalleEvents === null) throw "config.armaTuPackDetalleEvents is null or undefined";
 
     var _config = {
         gruposView: config.gruposView,
@@ -12,12 +13,27 @@
         armaTuPackDetalleEvents: config.armaTuPackDetalleEvents
     };
 
+    var _packComponentsModel = null;
 
-    var _onGruposLoaded = function (PackComponents) {
-        _config.gruposView.renderGrupos(PackComponents);
+    var _packComponents = function (value) {
+        if (typeof value === "undefined") {
+            return _packComponentsModel;
+        } else if (value !== null) {
+            _packComponentsModel = value;
+        }
+    };
+
+    var _onGruposLoaded = function (packComponents) {
+        _packComponents(packComponents);
+        _config.gruposView.renderGrupos(packComponents);
+    };
+
+    var _addComponente = function (cuvGrupo, cuvComponente) {
+
     };
 
     return {
-        onGruposLoaded: _onGruposLoaded
+        onGruposLoaded: _onGruposLoaded,
+        addComponente: _addComponente
     };
 };
