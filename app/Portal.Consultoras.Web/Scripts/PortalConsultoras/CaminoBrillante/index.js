7ac3676543bd3e76f13e5125578463708916a2dd
ricjimenez <ricardo.jimenez@hundred.com.pe>
2019-03-13 18:11:08 -0500

    Estilos niveles

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/index.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var _nivel = 6;
+    var _medalla = (_nivel === 6) ? _nivel - 1 : _nivel;
+    $("#indicadorNivel").addClass("medal-" + _medalla);
+    for (var i = 1; i <= _nivel; i++)
+        $(".pt" + i).addClass("activo");
+    $(".pt" + _nivel).addClass("brillante");
+});
