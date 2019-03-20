de27020d4e8f4519b05795565689b05cce8305a5
ricjimenez <ricardo.jimenez@hundred.com.pe>
2019-03-19 19:52:04 -0500

    correción model CaminoBrillante.css

--- app/Portal.Consultoras.Web/Models/CaminoBrillante/BeneficiosNivelCaminoBrillanteModel.cs
+++ app/Portal.Consultoras.Web/Models/CaminoBrillante/BeneficiosNivelCaminoBrillanteModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Portal.Consultoras.Web.Models.CaminoBrillante
+﻿namespace Portal.Consultoras.Web.Models.CaminoBrillante
 {
     public class BeneficiosNivelCaminoBrillanteModel
     {
