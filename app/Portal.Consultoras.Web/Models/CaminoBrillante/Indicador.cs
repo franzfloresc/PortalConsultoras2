7d50607f1fb3648b2ba228582f80c7275084a8a7
ricjimenez <ricardo.jimenez@hundred.com.pe>
2019-03-20 18:55:12 -0500

    ok

--- app/Portal.Consultoras.Web/Models/CaminoBrillante/Indicador.cs
+++ app/Portal.Consultoras.Web/Models/CaminoBrillante/Indicador.cs
@@ -1,9 +1,14 @@
-﻿namespace Portal.Consultoras.Web.Models.CaminoBrillante
+﻿using System.Runtime.Serialization;
+namespace Portal.Consultoras.Web.Models.CaminoBrillante
 {
+    [DataContract]
     public class Indicador
     {
+        [DataMember]
         public string Titulo { get; set; }
+        [DataMember]
         public string Valor { get; set; }
+        [DataMember]
         public string UrlImagen { get; set; }
     }
 }
--- app/Portal.Consultoras.Web/Models/CaminoBrillante/NivelesCaminoBrillanteModel.cs
+++ app/Portal.Consultoras.Web/Models/CaminoBrillante/NivelesCaminoBrillanteModel.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 namespace Portal.Consultoras.Web.Models.CaminoBrillante
 {
+    [DataContract]
     public class NivelesCaminoBrillanteModel
     {
+        [DataMember]
         public string IsoPais { get; set; }
+        [DataMember]
         public string CodigoNivel { get; set; }
+        [DataMember]
         public string DescripcionNivel { get; set; }
+        [DataMember]
         public string MontoMinimo { get; set; }
+        [DataMember]
         public string MontoMaximo { get; set; }
+        [DataMember]
         public string Beneficio1 { get; set; }
+        [DataMember]
         public string Beneficio2 { get; set; }
+        [DataMember]
         public string Beneficio3 { get; set; }
+        [DataMember]
         public string Beneficio4 { get; set; }
+        [DataMember]
         public string Beneficio5 { get; set; }
+        [DataMember]
         public string UrlImagenNivel { get; set; }
+        [DataMember]
         public List<BeneficiosNivelCaminoBrillanteModel> BeneficiosNivel { get; set; }
     }
 }
