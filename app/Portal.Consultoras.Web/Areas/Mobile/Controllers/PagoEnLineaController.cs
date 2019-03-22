f782cfef9bbf502c89c87713da3b6d6d43563df3
Abel Reynaltte <abelreynalte@gmail.com>
2019-03-21 19:22:28 -0500

    SALUD-101: Utilizar misma lógica de web a mobile para mostrar bancos.

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/PagoEnLineaController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/PagoEnLineaController.cs
@@ -38,6 +38,7 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 
         public ActionResult MetodoPago()
         {
+            string bancos = string.Empty;
             if (!userData.TienePagoEnLinea)
                 return RedirectToAction("Index", "Bienvenida");
 
@@ -45,22 +46,30 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
             {
                 ListaMetodoPago = _pagoEnLineaProvider.ObtenerListaMetodoPago()
             };
+
+            using (var ps = new PedidoServiceClient())
+            {
+                bancos = ps.ObtenerPagoEnLineaURLPaginasBancos(userData.PaisID);
+
+            }
+
+            model.Bancos = bancos;
             SessionManager.SetDatosPagoVisa(model);
 
             return View(model);
         }
 
 
-        [HttpPost]
-        public string ObtenerBancos()
-        {
-            string bancos = "";
-            using (var ps = new PedidoServiceClient())
-            {
-                bancos = ps.ObtenerPagoEnLineaURLPaginasBancos(userData.PaisID);
-            }
-            return bancos;
-        }
+        //[HttpPost]
+        //public string ObtenerBancos()
+        //{
+        //    string bancos = "";
+        //    using (var ps = new PedidoServiceClient())
+        //    {
+        //        bancos = ps.ObtenerPagoEnLineaURLPaginasBancos(userData.PaisID);
+        //    }
+        //    return bancos;
+        //}
 
 
         [HttpGet]
