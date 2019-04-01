a14a98ec25ade9d67d650dc7f44a2014d801b353
Alan <alanaupe.trabajo@gmail.com>
2019-04-01 17:55:32 -0500

    BPT/PI3/Sprint3/ArmaTuPack => JS - Eliminar codigo comentado

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupos/GruposPresenter.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Grupos/GruposPresenter.js
@@ -1,8 +1,6 @@
 ﻿var GruposPresenter = function (config) {
     if (typeof config === "undefined" || config === null) throw "config is null or undefined";
-    //
     if (typeof config.gruposView === "undefined" || config.gruposView === null) throw "config.gruposView is null or undefined";
-    //if (typeof config.armaTuPackProvider === "undefined" || config.armaTuPackProvider === null) throw "config.armaTuPackProvider is null or undefined";
     if (typeof config.generalModule === "undefined" || config.generalModule === null) throw "config.generalModule is null or undefined";
     if (typeof config.armaTuPackDetalleEvents === "undefined" || config.armaTuPackDetalleEvents === null) throw "config.armaTuPackDetalleEvents is null or undefined";
 
