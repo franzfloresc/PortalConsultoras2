22a60f3ee4c938b64ea2850519d7906ce3255c1c
Victor Vilela <victor.vilela@avantica.net>
2019-04-02 12:21:12 -0500

    extracting code from Ficha.cshtml views (desktop and mobile) to Ficha.js

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/Ficha/Ficha.js
@@ -0,0 +1,35 @@
+﻿var fichaModule;
+var componenteDetalleModule;
+
+$(document).ready(function () {
+    AbrirLoad();
+    
+    componenteDetalleModule = ComponenteDetalleModule({
+        localStorageModule: LocalStorageModule(),
+        analyticsPortalModule: AnalyticsPortalModule,
+        palanca: modelPalanca,
+        campania: modelCampania,
+        cuv: modelCuv,
+        origen: modelOrigen
+    });
+
+    fichaModule = FichaModule({
+        localStorageModule: LocalStorageModule(),
+        analyticsPortalModule: AnalyticsPortalModule,
+        componenteDetalleModule: componenteDetalleModule,
+
+        generalModule: GeneralModule,
+        palanca: modelPalanca,
+        campania: modelCampania,
+        cuv: modelCuv,
+        origen: modelOrigen
+    });
+
+    fichaModule.Inicializar();
+
+    CerrarLoad();
+});
+
+$(document).ajaxStop(function () {
+    //
+});
