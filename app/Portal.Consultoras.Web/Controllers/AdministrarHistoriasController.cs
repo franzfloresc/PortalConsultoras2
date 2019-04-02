fb9eae571c7741fd1eb18816f23fb612c4dc64b4
Alexis Basilio <alexisb1v1@gmail.com>
2019-04-02 10:28:14 -0500

    Avance

---
+++ app/Portal.Consultoras.Web/Controllers/AdministrarHistoriasController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class AdministrarHistoriasController : BaseAdmController
+    {
+        // GET: AdministrarHistorias
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
