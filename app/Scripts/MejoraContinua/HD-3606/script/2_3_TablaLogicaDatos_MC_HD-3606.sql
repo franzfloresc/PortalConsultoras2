802116539b75ee9094313473b2e795808b500bf7
Enrique Inca <inca.quispe.e@outlook.com>
2019-04-08 13:25:29 -0500

    Corrección de scripts de base de datos

---
+++ app/Scripts/MejoraContinua/HD-3606/script/2_3_TablaLogicaDatos_MC_HD-3606.sql
@@ -0,0 +1,323 @@
+USE BelcorpBolivia
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		) 
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpChile
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpColombia
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpCostaRica
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpDominicana
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpEcuador
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpGuatemala
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpMexico
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpPanama
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpPeru
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'ACTIVO'
+		);
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
+
+USE BelcorpSalvador
+GO
+
+IF NOT EXISTS (
+		SELECT 1
+		FROM TablaLogicaDatos
+		WHERE TablaLogicaID = 200
+			AND Descripcion LIKE '%Activación versión nueva trama YOBEL%'
+		)
+BEGIN
+	INSERT INTO dbo.TablaLogicaDatos (
+		TablaLogicaDatosID
+		,TablaLogicaID
+		,Codigo
+		,Descripcion
+		,Valor
+		)
+	VALUES (
+		20001
+		,200
+		,'01'
+		,'Activación versión nueva trama YOBEL'
+		,'INACTIVO'
+		);
+END
+GO
