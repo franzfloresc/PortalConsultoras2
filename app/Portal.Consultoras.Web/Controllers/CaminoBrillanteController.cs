4e7134687d1bec5c5307198fb1ce1ad42bbb9d19
jose-ponte <jose.ponte@hundred.com.pe>
2019-04-09 14:21:37 -0500

    Se agrego imagen no disponible estandar

--- app/Portal.Consultoras.Web/Controllers/CaminoBrillanteController.cs
+++ app/Portal.Consultoras.Web/Controllers/CaminoBrillanteController.cs
@@ -66,7 +66,7 @@ namespace Portal.Consultoras.Web.Controllers
             int cantKit = lstKit.Count();
             int cantDemo = lstDemo.Count();
             ViewBag.Moneda = userData.Simbolo;
-            ViewBag.RutaImagenNoDisponible = _configuracionManagerProvider.GetConfiguracionManager(Constantes.ConfiguracionManager.rutaImagenNotFoundAppCatalogo);
+            ViewBag.RutaImagenNoDisponible = _configuracionManagerProvider.GetConfiguracionManager(Constantes.ConfiguracionManager.urlSinImagenTiposyTonos);
 
             if (lstKit != null || lstDemo != null)
             {
