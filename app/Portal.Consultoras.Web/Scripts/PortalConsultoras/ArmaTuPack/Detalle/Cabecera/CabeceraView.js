f425e460d0b65ca788aecd6f64f2d6fbb2129892
Victor Vilela <victor.vilela@avantica.net>
2019-04-16 14:40:47 -0500

    fix effects to show groups and header

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Cabecera/CabeceraView.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Cabecera/CabeceraView.js
@@ -18,7 +18,7 @@
 
     var _renderTitle = function (packComponents) {
         SetHandlebars(_elements.cabecera.templateId, packComponents, _elements.cabecera.id);
-        $(_elements.cabecera.id).show();
+        $(_elements.cabecera.id).show("fade", 1000);
     };
 
     return {
