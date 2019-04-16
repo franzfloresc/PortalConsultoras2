404f2f9a82b75089f1243ecc1d3e38c2c677d93b
Juan Jose Lucero Goicochea <juan.lucero@PE-LPALQ00610>
2019-04-16 15:33:41 -0500

    AGANA-52: se agrega diseño a cabecera en landing ATP

--- app/Portal.Consultoras.Web/Models/EstrategiaFichaPersonalizadaProductoModel.cs
+++ app/Portal.Consultoras.Web/Models/EstrategiaFichaPersonalizadaProductoModel.cs
@@ -45,4 +45,12 @@ namespace Portal.Consultoras.Web.Models
 
         public bool MostrarFichaEnriquecida { get; set; }
     }
+
+    public class DetalleEstrategiaFichaDisenoModel : DetalleEstrategiaFichaModel
+    {
+        public string ImagenFondo { get; set; }
+        public string ColorFondo { get; set; }
+        public string SubTitulo { get; set; }
+        public string ColorTexto { get; set; }
+    }
 }
