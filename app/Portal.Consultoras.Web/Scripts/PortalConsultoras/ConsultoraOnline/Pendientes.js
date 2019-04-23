a74bd5109e30055239acca1b533f8be7562fd07c
Juan Alfaro <juan.alfaro@hundred.com.pe>
2019-04-23 17:41:49 -0500

    Cambios Vista Desktop

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
@@ -150,7 +150,10 @@ function DetallePedidoPendiente(ids) {
         async: true,
         success: function (response) {
             if (response.success) {
-                console.log(response)
+                debugger;
+                console.log(response);
+                SetHandlebars("#template-paso-1-Producto", response.data, "#Paso1-Productos");
+                $('#Paso1-Productos').show();
             }
         },
         error: function (error) {
@@ -235,7 +238,7 @@ function RechazarSolicitudClientePorCuv(cuv) {
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(obj),
         success: function (response) {
-                    /*CloseLoading*/();
+                    /*CloseLoading*/
             if (response.success) {
                 //document.location.href = '/ConsultoraOnline/Pendientes';
             }
