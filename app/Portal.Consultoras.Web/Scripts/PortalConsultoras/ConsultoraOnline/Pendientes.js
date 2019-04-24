1137996ecd490e7540eec3b98bc400e50767579b
Juan Alfaro <juan.alfaro@hundred.com.pe>
2019-04-24 13:32:44 -0500

    Cambios desto

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
@@ -212,6 +212,8 @@ function RechazarSolicitudCliente(pedidoId) {
             //CloseLoading();
 
             if (response.success) {
+                $('#rechazarTodop').addClass('hide');
+                $('#Paso1-Productos').hide();
                 //document.location.href = '/ConsultoraOnline/Pendientes';
             }
             else {
@@ -240,6 +242,8 @@ function RechazarSolicitudClientePorCuv(cuv) {
         success: function (response) {
                     /*CloseLoading*/
             if (response.success) {
+                $('#rechazarTodo').addClass('hide');
+                $('#Paso1-Clientes').hide();
                 //document.location.href = '/ConsultoraOnline/Pendientes';
             }
             else {
@@ -260,7 +264,7 @@ function ContinuarPedido() {
 
     $('.pedidos').each(function () {
 
-        if ($(this).find('a[id*="aceptar_"]').hasClass('active') == false) {
+        if ($(this).find('a[id*="aceptar_"]').hasClass('ghost') == false) {
             //$(aceptado).addClass('active');
             var pedidoId = $(this).find(".pedidoId").val();
             var cuv = $(this).find(".cuv").val();
@@ -276,6 +280,8 @@ function ContinuarPedido() {
         }
     });
 
+    debugger;
+
     if (lstDetalle.length > 0) {
         $.ajax({
             type: "POST",
@@ -284,7 +290,7 @@ function ContinuarPedido() {
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(lstDetalle),
             success: function (response) {
-                CloseLoading();
+                //CloseLoading();
                 if (response.success) {
                     //document.location.href = '/ConsultoraOnline/PendientesMedioDeCompra';
                 }
@@ -293,7 +299,7 @@ function ContinuarPedido() {
                 }
             },
             error: function (err) {
-                CloseLoading();
+                //CloseLoading();
                 console.log(err);
             }
         });
