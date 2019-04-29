791b1be9ef708012050041bdcc179c222c2d4c25
Enrique Inca <inca.quispe.e@outlook.com>
2019-04-29 11:07:01 -0500

    En caso de que no tenga fecha, mostrar vacío

--- app/Portal.Consultoras.Web/Areas/Mobile/Controllers/SeguimientoPedidoController.cs
+++ app/Portal.Consultoras.Web/Areas/Mobile/Controllers/SeguimientoPedidoController.cs
@@ -202,8 +202,8 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
 
             if (desde != null && hasta != null)
             {
-                if (desde.Fecha.HasValue) horaEstimadaEntregaDesde = desde.Fecha.Value.TimeOfDay.TotalHours.Equals(0) ? desde.Fecha.Value.ToString() : desde.Fecha.Value.ToString("HH:mm tt");
-                if (hasta.Fecha.HasValue) horaEstimadaEntregaHasta = hasta.Fecha.Value.TimeOfDay.TotalHours.Equals(0) ? hasta.Fecha.Value.ToString() : hasta.Fecha.Value.ToString("HH:mm tt");
+                if (desde.Fecha.HasValue) horaEstimadaEntregaDesde = desde.Fecha.Value.TimeOfDay.TotalHours.Equals(0) ? string.Empty : desde.Fecha.Value.ToString("HH:mm tt");
+                if (hasta.Fecha.HasValue) horaEstimadaEntregaHasta = hasta.Fecha.Value.TimeOfDay.TotalHours.Equals(0) ? string.Empty : hasta.Fecha.Value.ToString("HH:mm tt");
             }
 
             foreach (var item in listaSeguimientoSecundario)
@@ -250,10 +250,10 @@ namespace Portal.Consultoras.Web.Areas.Mobile.Controllers
                         break;
                 }
 
-                var flag = (horaEstimadaEntregaDesde != string.Empty && horaEstimadaEntregaHasta != string.Empty) ? true : false;
+                var flag = (horaEstimadaEntregaDesde != string.Empty && horaEstimadaEntregaHasta != string.Empty);
 
                 if (item.Etapa == Constantes.SegPedidoSituacion.FechaEstimadaEntrega && flag)
-                    item.HoraEstimadaDesdeHasta = ValidarZonaRegion() == true ? string.Format("{0} - {1}", horaEstimadaEntregaDesde, horaEstimadaEntregaHasta) : "";
+                    item.HoraEstimadaDesdeHasta = ValidarZonaRegion() ? string.Format("{0} - {1}", horaEstimadaEntregaDesde, horaEstimadaEntregaHasta) : "";
 
                 listaEstadoSeguimiento.Add(item);
             }
