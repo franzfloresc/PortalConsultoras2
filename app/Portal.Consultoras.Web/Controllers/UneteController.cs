cef1679f651126ca255e9cc45c508b0504047225
edwinceslev <edwin.cespedes@tismart.com>
2019-05-02 18:36:17 -0500

    Codigo popup

--- app/Portal.Consultoras.Web/Controllers/UneteController.cs
+++ app/Portal.Consultoras.Web/Controllers/UneteController.cs
@@ -1586,6 +1586,12 @@ namespace Portal.Consultoras.Web.Controllers
             return PartialView("_ConsultarEstadoCrediticia");
         }
 
+        public ActionResult ConsultarEstadoTelefonico(int id)
+        {
+            ViewBag.HTMLSACUnete = getHTMLSACUnete("ConsultarEstadoTelefonico", "&id=" + id);
+            return PartialView("_ConsultarEstadoTelefonico");
+        }
+
         public ActionResult MostrarMensajeBuro(string respuestaBuro) {
             ViewBag.HTMLSACUnete = getHTMLSACUnete("MensajeRespuestaBuro", "&respuestaBuro=" + respuestaBuro);
             return PartialView("~/Views/Unete/_MensajeRespuestaBuro.cshtml");
@@ -1600,6 +1606,18 @@ namespace Portal.Consultoras.Web.Controllers
                 actualizado = sv.ActualizarEstado(CodigoISO, id, EnumsTipoParametro.EstadoBurocrediticio, idEstado);
             }
             RegistrarLogGestionSacUnete(id.ToString(), "CONSULTA CREDITICIA", "ASIGNAR");
+            return Json(actualizado, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult ConsultarEstadoTelefonico(int id, int idEstado)
+        {
+            bool actualizado;
+            using (var sv = new PortalServiceClient())
+            {
+                actualizado = sv.ActualizarEstado(CodigoISO, id, EnumsTipoParametro.EstadoTelefonico, idEstado);
+            }
+            RegistrarLogGestionSacUnete(id.ToString(), "CONSULTA TELEFONICA", "ASIGNAR");
             return Json(actualizado, JsonRequestBehavior.AllowGet);
         }
 
