fc4ff4e918d1da6dc17290c8c424e7ba77e18dbd
Giancarlos Salas <giansalex@gmail.com>
2019-05-10 11:52:24 -0500

    add HMACSHA1 Algorithm

---
+++ app/Portal.Consultoras.Common.UnitTest/Cryptography/HmacShaAlgorithmTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Portal.Consultoras.Common.Cryptography;
+
+namespace Portal.Consultoras.Common.UnitTest.Cryptography
+{
+    [TestClass]
+    public class HmacShaAlgorithmTests
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("36882", "654321ba", DisplayName = "Esika Hash")]
+        [DataRow("36947", "765432dc", DisplayName = "Lbel Hash")]
+        public void GetHashTest(string textTohash, string key)
+        {
+            var hash = HmacShaAlgorithm.GetHash(textTohash, key);
+
+            Assert.IsNotNull(hash);
+            Assert.AreEqual(40, hash.Length);
+        }
+    }
+}
