cee6339af7f8fe30a43ef58adc68a81a1e7c9f71
eiq21 <inca.quispe.e@outlook.com>
2019-05-14 17:03:24 -0500

    Agregando cambio

--- app/Portal.Consultoras.Web/Controllers/MisReclamosController.cs
+++ app/Portal.Consultoras.Web/Controllers/MisReclamosController.cs
@@ -436,48 +436,57 @@ namespace Portal.Consultoras.Web.Controllers
 
         public JsonResult BuscarCuvCambiar(MisReclamosModel model)
         {
-            List<ServiceODS.BEProducto> olstProducto;
             List<ProductoModel> olstProductoModel = new List<ProductoModel>();
-
-            using (ODSServiceClient sv = new ODSServiceClient())
+            try
             {
-                olstProducto = sv.SelectProductoByCodigoDescripcionSearchRegionZona(userData.PaisID, model.CampaniaID, model.CUV, userData.RegionID, userData.ZonaID, userData.CodigorRegion, userData.CodigoZona, 1, 1, false).ToList();
+                List<ServiceODS.BEProducto> olstProducto;
+                
+
+                using (ODSServiceClient sv = new ODSServiceClient())
+                {
+                    olstProducto = sv.SelectProductoByCodigoDescripcionSearchRegionZona(userData.PaisID, model.CampaniaID, model.CUV, userData.RegionID, userData.ZonaID, userData.CodigorRegion, userData.CodigoZona, 1, 1, false).ToList();
+                }
+
+                if (olstProducto.Count == 0)
+                {
+                    olstProductoModel.Add(new ProductoModel() { MarcaID = 0, CUV = "El producto solicitado no existe.", TieneSugerido = 0 });
+                    return Json(olstProductoModel, JsonRequestBehavior.AllowGet);
+                }
+
+                olstProductoModel.Add(new ProductoModel()
+                {
+                    CUV = olstProducto[0].CUV.Trim(),
+                    Descripcion = olstProducto[0].Descripcion.Trim(),
+                    PrecioCatalogo = olstProducto[0].PrecioCatalogo,
+                    MarcaID = olstProducto[0].MarcaID,
+                    EstaEnRevista = olstProducto[0].EstaEnRevista,
+                    TieneStock = olstProducto[0].TieneStock,
+                    EsExpoOferta = olstProducto[0].EsExpoOferta,
+                    CUVRevista = olstProducto[0].CUVRevista.Trim(),
+                    CUVComplemento = olstProducto[0].CUVComplemento.Trim(),
+                    IndicadorMontoMinimo = olstProducto[0].IndicadorMontoMinimo.ToString().Trim(),
+                    TipoOfertaSisID = olstProducto[0].TipoOfertaSisID,
+                    ConfiguracionOfertaID = olstProducto[0].ConfiguracionOfertaID,
+                    MensajeCUV = "",
+                    ObservacionCUV = "",
+                    DesactivaRevistaGana = 0,
+                    DescripcionMarca = olstProducto[0].DescripcionMarca,
+                    DescripcionEstrategia = olstProducto[0].DescripcionEstrategia,
+                    DescripcionCategoria = olstProducto[0].DescripcionCategoria,
+                    FlagNueva = olstProducto[0].FlagNueva,
+                    TipoEstrategiaID = olstProducto[0].TipoEstrategiaID,
+                    TieneSugerido = olstProducto[0].TieneSugerido,
+                    CodigoProducto = olstProducto[0].CodigoProducto,
+                    LimiteVenta = 99
+                });
+                return Json(olstProductoModel, JsonRequestBehavior.AllowGet);
+
             }
-
-            if (olstProducto.Count == 0)
+            catch (Exception ex)
             {
-                olstProductoModel.Add(new ProductoModel() { MarcaID = 0, CUV = "El producto solicitado no existe.", TieneSugerido = 0 });
+                olstProductoModel.Add(new ProductoModel() { MarcaID = 0, CUV = ex.Message, TieneSugerido = 0 });
                 return Json(olstProductoModel, JsonRequestBehavior.AllowGet);
             }
-
-            olstProductoModel.Add(new ProductoModel()
-            {
-                CUV = olstProducto[0].CUV.Trim(),
-                Descripcion = olstProducto[0].Descripcion.Trim(),
-                PrecioCatalogo = olstProducto[0].PrecioCatalogo,
-                MarcaID = olstProducto[0].MarcaID,
-                EstaEnRevista = olstProducto[0].EstaEnRevista,
-                TieneStock = olstProducto[0].TieneStock,
-                EsExpoOferta = olstProducto[0].EsExpoOferta,
-                CUVRevista = olstProducto[0].CUVRevista.Trim(),
-                CUVComplemento = olstProducto[0].CUVComplemento.Trim(),
-                IndicadorMontoMinimo = olstProducto[0].IndicadorMontoMinimo.ToString().Trim(),
-                TipoOfertaSisID = olstProducto[0].TipoOfertaSisID,
-                ConfiguracionOfertaID = olstProducto[0].ConfiguracionOfertaID,
-                MensajeCUV = "",
-                ObservacionCUV = "",
-                DesactivaRevistaGana = 0,
-                DescripcionMarca = olstProducto[0].DescripcionMarca,
-                DescripcionEstrategia = olstProducto[0].DescripcionEstrategia,
-                DescripcionCategoria = olstProducto[0].DescripcionCategoria,
-                FlagNueva = olstProducto[0].FlagNueva,
-                TipoEstrategiaID = olstProducto[0].TipoEstrategiaID,
-                TieneSugerido = olstProducto[0].TieneSugerido,
-                CodigoProducto = olstProducto[0].CodigoProducto,
-                LimiteVenta = 99
-            });
-
-            return Json(olstProductoModel, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult BuscarMotivo(MisReclamosModel model)
