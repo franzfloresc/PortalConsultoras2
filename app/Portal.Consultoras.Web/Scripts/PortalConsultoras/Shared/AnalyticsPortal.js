6979bcaaa7f40a88488395eca5da7c62efc3d024
Alan <alanaupe.trabajo@gmail.com>
2019-05-15 15:29:57 -0500

    feature/GA-4 => AnalyticsPortal - metodos autoMapperEstrategia - centralizar la creacion de listado del model para productImpression

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/AnalyticsPortal.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/AnalyticsPortal.js
@@ -479,6 +479,34 @@ var AnalyticsPortalModule = (function () {
     // Ini - Analytics Evento Product Impression
     ////////////////////////////////////////////////////////////////////////////////////////
 
+    var autoMapperEstrategia = function (lista, cantidadMostrar, parametroList) {
+
+        lista = lista[];
+        cantidadMostrar = cantidadMostrar || 0;
+        parametroList = parametroList || "";
+
+        var impressions = [];
+
+        $.each(lista, function (index, item) {
+            if (index < cantidadMostrar) {
+                var impression = {
+                    'name': item.DescripcionCompleta,
+                    'id': item.CUV2 || item.CUV,
+                    'price': item.PrecioVenta || item.PrecioString,
+                    'brand': item.DescripcionMarca || _getMarca(item.MarcaId || item.MarcaID),
+                    'category': _texto.notavaliable,
+                    'variant': _texto.estandar,
+                    'list': parametroList,
+                    'position': (item.Position == undefined ? index : item.Position) + 1
+                };
+
+                impressions.push(impression);
+            }
+        });
+
+        return impressions;
+    };
+
     var marcaGenericaLista = function (seccion, data, pos) {
         try {
             console.log('marca Generica Lista- ini', seccion, data, pos);
@@ -496,28 +524,30 @@ var AnalyticsPortalModule = (function () {
             if (_constantes.isTest)
                 alert("Marcación product impression.");
 
+            data = data || {};
+
             var parametroList = _getParametroListSegunOrigen(data.Origen);
 
-            var lista = data.lista;
+            var lista = data.lista || [];
             var cantidadMostrar = lista.length == 1 ? 1 : data.CantidadMostrar;
-            var impressions = [];
-            $.each(lista, function (index) {
-                if (index < cantidadMostrar) {
-                    var item = lista[index];
-                    var impression = {
-                        'name': item.DescripcionCompleta,
-                        'id': item.CUV2,
-                        'price': item.PrecioVenta,
-                        'brand': item.DescripcionMarca,
-                        'category': _texto.notavaliable,
-                        'variant': _texto.estandar,
-                        'list': parametroList,
-                        'position': (item.Position == undefined ? index : item.Position) + 1
-                    };
+            var impressions = autoMapperEstrategia(lista, cantidadMostrar, parametroList);
 
-                    impressions.push(impression);
-                }
-            });
+            //$.each(lista, function (index) {
+            //    if (index < cantidadMostrar) {
+            //        var item = lista[index];
+            //        var impression = {
+            //            'name': item.DescripcionCompleta,
+            //            'id': item.CUV2,
+            //            'price': item.PrecioVenta,
+            //            'brand': item.DescripcionMarca,
+            //            'category': _texto.notavaliable,
+            //            'variant': _texto.estandar,
+            //            'list': parametroList,
+            //            'position': (item.Position == undefined ? index : item.Position) + 1
+            //        };
+            //        impressions.push(impression);
+            //    }
+            //});
 
             _marcarImpresionSetProductos(impressions);
 
@@ -564,28 +594,29 @@ var AnalyticsPortalModule = (function () {
     var marcaProductImpressionRecomendaciones = function (data, isMobile) {
         try {
 
+            data = data || {};
             var cantidadMostrar = isMobile ? 1 : (data.Total >= 3) ? 3 : data.Total;
 
             var lista = data.Productos;
 
-            var impressions = [];
+            var impressions = autoMapperEstrategia(lista, cantidadMostrar, 'Pedido - Ofertas Relacionadas');
 
-            $.each(lista, function (index) {
-                if (index == cantidadMostrar)
-                    return false;
-                var item = lista[index];
-                var impression = {
-                    'name': item.DescripcionCompleta,
-                    'id': item.CUV,
-                    'price': parseFloat(item.Precio).toFixed(2).toString(),
-                    'brand': _getMarca(item.MarcaId),
-                    'category': _texto.notavaliable,
-                    'variant': _texto.estandar,
-                    'list': 'Pedido - Ofertas Relacionadas',
-                    'position': index + 1
-                };
-                impressions.push(impression);
-            });
+            //$.each(lista, function (index) {
+            //    if (index == cantidadMostrar)
+            //        return false;
+            //    var item = lista[index];
+            //    var impression = {
+            //        'name': item.DescripcionCompleta,
+            //        'id': item.CUV,
+            //        'price': parseFloat(item.Precio).toFixed(2).toString(),
+            //        'brand': _getMarca(item.MarcaId),
+            //        'category': _texto.notavaliable,
+            //        'variant': _texto.estandar,
+            //        'list': 'Pedido - Ofertas Relacionadas',
+            //        'position': index + 1
+            //    };
+            //    impressions.push(impression);
+            //});
 
             dataLayer.push({
                 'event': _evento.productImpression,
@@ -604,25 +635,27 @@ var AnalyticsPortalModule = (function () {
     var marcaProductImpressionViewRecomendaciones = function (data, index) {
         try {
 
-            var lista = data.Productos;
-
-            var impressions = [];
-
+            var lista = data.Productos || [];
             var item = lista[index];
 
-            var impression = {
-                'name': item.DescripcionCompleta,
-                'id': item.CUV,
-                'price': parseFloat(item.Precio).toFixed(2).toString(),
-                'brand': _getMarca(item.MarcaId),
-                'category': _texto.notavaliable,
-                'variant': _texto.estandar,
-                'list': 'Pedido - Ofertas Relacionadas',
-                'position': index + 1
-            };
-            impressions.push(impression);
+            var listaMarca = [];
+            listaMarca.push(item);
+            var impressions = autoMapperEstrategia(listaMarca, 1, 'Pedido - Ofertas Relacionadas');
 
-
+            //var impressions = [];
+            //var item = lista[index];
+            //var impression = {
+            //    'name': item.DescripcionCompleta,
+            //    'id': item.CUV,
+            //    'price': parseFloat(item.Precio).toFixed(2).toString(),
+            //    'brand': _getMarca(item.MarcaId),
+            //    'category': _texto.notavaliable,
+            //    'variant': _texto.estandar,
+            //    'list': 'Pedido - Ofertas Relacionadas',
+            //    'position': index + 1
+            //};
+            //impressions.push(impression);
+            
             dataLayer.push({
                 'event': _evento.productImpression,
                 'ecommerce': {
