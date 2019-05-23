94003d18a2671d18fe2df5b2413773457bc9aeeb
Richard Aguirre <richardjap@gmail.com>
2019-05-23 15:07:24 -0500

    Update BannerController.cs

--- app/Portal.Consultoras.Web/Controllers/BannerController.cs
+++ app/Portal.Consultoras.Web/Controllers/BannerController.cs
@@ -685,6 +685,11 @@ namespace Portal.Consultoras.Web.Controllers
             });
 
             lstFinalModel = lstFinalModel.Where(m => m.Clase != "oculto").ToList();
+            //SOPORTEPD-252 ELIMINAR EL TENER EL DESARROLLO CONFIGURABLE
+            var bannerHVList= lstFinalModel.Where(x => x.BannerID == 35);
+            if (bannerHVList != null && bannerHVList.Count() > 0)
+                bannerHVList.FirstOrDefault().Archivo = "https://cdn1-prd.somosbelcorp.com/Banners/Herramientas_ventas_2019352583.png";
+            //SOPORTEPD-252
 
             return Json(new
             {
