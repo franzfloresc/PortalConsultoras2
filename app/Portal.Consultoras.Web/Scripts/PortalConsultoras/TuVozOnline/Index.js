4b7ee031750beda243f7447b4829723536cb0af7
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-05-28 12:37:58 -0500

    Se agrega sección Confirmación Correo para Vista Tu Voz Online (Responsive) y se agrega js respectivo.

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/TuVozOnline/Index.js
@@ -0,0 +1,35 @@
+﻿var TuVozOnlineView = (function () {
+
+    var _elementos = {
+        campoCorreoElectronico: '#CorreoElectronicoCampo'
+    };
+    var _funciones = {
+        InicializarEventos: function() {
+            $(document).on("keyup", _elementos.campoCorreoElectronico, _eventos.CampoConDatos);
+        }
+    };
+    var _eventos = {
+        CampoConDatos: function (e) {
+            var campoDatos = $(this).val();
+            if (campoDatos) {
+                $(this).addClass('text__field__sb--withContent');
+            } else {
+                $(this).removeClass('text__field__sb--withContent');
+            }
+        }
+    }
+
+    //Public functions
+    function Inicializar() {
+        _funciones.InicializarEventos();
+    }
+
+    return {
+        Inicializar: Inicializar
+    };
+
+})();
+
+$(document).ready(function () {
+    TuVozOnlineView.Inicializar();
+});
