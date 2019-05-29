06b1d98488e87d0e1448a6e9b95544620f91f3ab
mijael888 <mijael888@gmail.com>
2019-05-29 16:18:12 -0500

    GA-84 Avance carrusel responsive

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Carrusel/CarruselModel.js
@@ -0,0 +1,27 @@
+﻿class CarruselModel
+{
+    constructor(palanca,
+        campania,
+        cuv,
+        urlDataCarrusel,
+        OrigenPedidoWeb,
+        pantalla,
+        tituloCarrusel,
+        cantidadPack,
+        codigoProducto,
+        precioProducto,
+        productosHermanos)
+    {
+        this.palanca = palanca || "";
+        this.campania = campania || "";
+        this.cuv = cuv || "";
+        this.urlDataCarrusel = urlDataCarrusel || "/Estrategia/FichaObtenerProductosUpSellingCarrusel";
+        this.OrigenPedidoWeb = OrigenPedidoWeb || "";
+        this.pantalla = pantalla;
+        this.tituloCarrusel = tituloCarrusel;
+        this.cantidadPack = cantidadPack;
+        this.codigoProducto = codigoProducto;
+        this.precioProducto = precioProducto;
+        this.productosHermanos = productosHermanos;
+    }
+}
