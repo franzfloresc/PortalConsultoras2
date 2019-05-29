2875d9b07652b028eb4088eed91a690708e687ed
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-05-29 14:46:44 -0500

    Se agrega funcionalidad para cuando campos de popup Recepción Pedido tengan datos.

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Pedido/PopupRecepcionPedido.js
@@ -0,0 +1,35 @@
+﻿var PopupRecepcionPedido = (function () {
+
+    var _elementos = {
+        camposPopupRecepcionPedido: '.text__field__sb'
+    };
+    var _funciones = {
+        InicializarEventos: function () {
+            $(document).on("blur", _elementos.camposPopupRecepcionPedido, _eventos.CampoConDatos);
+        }
+    };
+    var _eventos = {
+        CampoConDatos: function (e) {
+            var campoDatos = $(this).val();
+            if (campoDatos) {
+                $(this).addClass('text__field__sb--withContent');
+            } else {
+                $(this).removeClass('text__field__sb--withContent');
+            }
+        }
+    }
+
+    //Public functions
+    function Inicializar() {
+        _funciones.InicializarEventos();
+    }
+
+    return {
+        Inicializar: Inicializar
+    };
+
+})();
+
+$(document).ready(function () {
+    PopupRecepcionPedido.Inicializar();
+});
