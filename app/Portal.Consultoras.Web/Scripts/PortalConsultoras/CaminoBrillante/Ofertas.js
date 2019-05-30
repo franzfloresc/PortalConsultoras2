4b2867b9e51d2b342604b8e42111d601b688806f
giancarlogcp <giancarlogcp@hotmail.com>
2019-05-30 12:18:05 -0500

    marcaciones

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/Ofertas.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/Ofertas.js
@@ -273,7 +273,7 @@ function AgregarProducto(data, cantidad, contenedor, tab, isKit) {
                 'ecommerce': {
                     'currencyCode': moneda,
                     'add': {
-                        'actionField': 'list Ofertas-CaminoBrillante : ' + categoria ,
+                        'actionField': 'list : ' + categoria ,
                         'products': [{
                             'name': nombre_producto,
                             'price': precio_producto,
