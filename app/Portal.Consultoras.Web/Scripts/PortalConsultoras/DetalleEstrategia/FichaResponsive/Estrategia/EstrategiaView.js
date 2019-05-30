4450e0a7c87124ab77d6dbe69728781563a54c55
joaopercycr <joaopercy.cr@gmail.com>
2019-05-30 11:21:28 -0500

    GA-16: Correcion titulos capitalize, videos y tabs acordeon

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Estrategia/EstrategiaView.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Estrategia/EstrategiaView.js
@@ -62,11 +62,7 @@
                     "</a>",
                 responsive: [
                     {
-                        breakpoint: 1024,
-                        settings: { slidesToShow: 3, slidesToScroll: 3 }
-                    },
-                    {
-                        breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 }
+                        breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1, prevArrow: null, nextArrow:null }
                     }
                 ]
             });
