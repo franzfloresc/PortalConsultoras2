b6d4d669132cec178a0dd0e5d8fb3c85813c9048
Victor Vilela <victor.vilela@avantica.net>
2019-06-04 12:04:40 -0500

    renombra variables gruposDesktopView  y gruposDesktopPresenter
    - de gruposDesktopView   a gruposView
    - de gruposDesktopPresenter a gruposPresenter

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Detalle.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ArmaTuPack/Detalle/Detalle.js
@@ -36,17 +36,17 @@ var cabeceraPresenter = CabeceraPresenter({
 });
 //cabeceraView.setPresenter(cabeceraPresenter);
 
-var gruposDesktopView = GruposView({
+var gruposView = GruposView({
     generalModule: generalModule,
     gruposContainerId: "#grupos"
 });
-var gruposDesktopPresenter = GruposPresenter({
-    gruposView: gruposDesktopView,
+var gruposPresenter = GruposPresenter({
+    gruposView: gruposView,
     armaTuPackProvider: armaTuPackProvider,
     generalModule: generalModule,
     armaTuPackDetalleEvents: armaTuPackDetalleEvents
 });
-gruposDesktopView.setPresenter(gruposDesktopPresenter);
+gruposView.setPresenter(gruposPresenter);
 
 var seleccionadosView = SeleccionadosView({
     seleccionadosContainerId: "#seleccionados"
@@ -66,15 +66,15 @@ $(document).ready(function () {
 
 armaTuPackDetalleEvents.subscribe(armaTuPackDetalleEvents.eventName.onGruposLoaded, function (packComponents) {
     cabeceraPresenter.onGruposLoaded(packComponents);
-    gruposDesktopPresenter.onGruposLoaded(packComponents);
+    gruposPresenter.onGruposLoaded(packComponents);
     seleccionadosPresenter.onGruposLoaded(packComponents);
 });
 
 armaTuPackDetalleEvents.subscribe(armaTuPackDetalleEvents.eventName.onSelectedComponentsChanged, function (packComponents) {
-    gruposDesktopPresenter.onSelectedComponentsChanged(packComponents);
+    gruposPresenter.onSelectedComponentsChanged(packComponents);
     seleccionadosPresenter.onSelectedComponentsChanged(packComponents);
 });
 
 armaTuPackDetalleEvents.subscribe(armaTuPackDetalleEvents.eventName.onShowWarnings, function (packComponents) {
-    gruposDesktopPresenter.onShowWarnings(packComponents);
+    gruposPresenter.onShowWarnings(packComponents);
 });
