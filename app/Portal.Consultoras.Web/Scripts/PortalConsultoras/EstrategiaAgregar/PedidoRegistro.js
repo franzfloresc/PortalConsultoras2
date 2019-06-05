fa8b1d985fc16f33c942f511bff3c6637838ddb2
Abel Reynaltte <abelreynalte@gmail.com>
2019-06-05 14:25:26 -0500

    SALUD-211: Error al digitar el cuv del regalo ProgNuevas

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaAgregar/PedidoRegistro.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaAgregar/PedidoRegistro.js
@@ -1105,7 +1105,6 @@ var PedidoRegistroModule = function () {
                     return false;
                 }
                 showPopupNivelSuperado(data.DataBarra, prevTotal);
-                CargarDetallePedido();
                 $('#divMensajeCUV').hide();
                 $("#divProductoObservaciones").html("");
                 $("#divProductoMantenedor").hide();
