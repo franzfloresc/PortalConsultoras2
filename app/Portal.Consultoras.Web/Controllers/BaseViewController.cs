8c04951ced4a6e8c0d97f5031bb8337b9ca9a9a3
giancarlogcp <giancarlogcp@hotmail.com>
2019-06-06 12:02:02 -0500

    Ficha Producto

--- app/Portal.Consultoras.Web/Controllers/BaseViewController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseViewController.cs
@@ -828,7 +828,7 @@ namespace Portal.Consultoras.Web.Controllers
                     }
                 }
                 
-                else if (palanca == "CaminoBrillanteDemostradores") {
+                else if (palanca == "CaminoBrillanteDemostradores" || palanca == "CaminoBrillanteKits") {
                     var demostradores = _caminoBrillanteProvider.GetDesmostradoresCaminoBrillante(0,0,"","").LstDemostradores ?? new List<Models.CaminoBrillante.DemostradorCaminoBrillanteModel>();
                     modelo = demostradores.Where(e => e.CUV == cuv).Select(e => new DetalleEstrategiaFichaModel()
                     { }).FirstOrDefault() ?? modelo;
