2dd77359275b74072fed43596b6001c38e4e5069
Jorge Nizama <jorgenizamarios@gmail.com>
2019-06-06 11:30:53 -0500

    Tesla28 - Cálculo Ganancia ofertas Catálogo (Experiencia UX) - Div Ganancia

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaAgregar/PedidoRegistro.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EstrategiaAgregar/PedidoRegistro.js
@@ -1719,6 +1719,7 @@ function UpdateTransaction(CantidadActual, CampaniaID, PedidoID, PedidoDetalleID
             CargarDetallePedido();
             var prevTotal = mtoLogroBarra;
             MostrarBarra(data);
+            ActualizaGanancias(data);  //TESLA-28
             showPopupNivelSuperado(data.DataBarra, prevTotal);
 
             if (data.modificoBackOrder) {
@@ -1743,7 +1744,15 @@ function UpdateTransaction(CantidadActual, CampaniaID, PedidoID, PedidoDetalleID
     });
 }
 
-
+function ActualizaGanancias(data) {
+    data = ActualizaGanancias || "";
+    if (data !== "") {
+        $('#div-ganancia-totalGananciaCatalogo').text('GG');
+        $('#div-ganancia-totalGananciaRevista').text('GG');
+        $('#div-ganancia-totalGananciaWeb').text('GG');
+        $('#div-ganancia-totalGananciaOtros').text('GG');
+    }
+}
 function UpdateLiquidacion(event, CampaniaID, PedidoID, PedidoDetalleID, TipoOfertaSisID, CUV, FlagValidacion, CantidadModi, setId, esCuponNuevas) {
 	
     var rowElement = $(event.target).closest(".contenido_ingresoPedido");
