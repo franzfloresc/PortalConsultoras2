c63f1989872b04a020b873d481f12a8f06162ef1
Giancarlos Salas <giansalex@gmail.com>
2019-06-12 14:24:25 -0500

    quitar scripts antiguos

--- app/Scripts/Pases/20180524/Rollback/Database/2.MC/002_RB_MC_HD_2360_CREATE_ValidarLoginJsonWebToken.sql
+++
@@ -1,6 +0,0 @@
-﻿USE BelcorpPeru
-go
-
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ValidarLoginJsonWebToken]') AND type in (N'P', N'PC')) 
-	DROP PROCEDURE [dbo].[ValidarLoginJsonWebToken]
-GO
