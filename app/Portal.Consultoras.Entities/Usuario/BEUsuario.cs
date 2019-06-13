9ee381603ee822f5e15cba94817137611964e11a
alexis.basilio <alexisb1v1@gmail.com>
2019-06-13 15:46:06 -0500

    Link tu voz online

--- app/Portal.Consultoras.Entities/Usuario/BEUsuario.cs
+++ app/Portal.Consultoras.Entities/Usuario/BEUsuario.cs
@@ -524,6 +524,7 @@ namespace Portal.Consultoras.Entities
         public string PrimerNombre { get; set; }
 
         [DataMember]
+        [Column("PrimerApellido")]
         public string PrimerApellido { get; set; }
 
         [DataMember]
