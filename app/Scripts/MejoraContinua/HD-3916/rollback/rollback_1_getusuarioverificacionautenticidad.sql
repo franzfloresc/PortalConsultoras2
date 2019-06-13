f59782d75e19254619d02624698b004f359793cc
Carlos Ramos <carlosesteban943@gmail.com>
2019-06-13 18:19:50 -0500

    SP

---
+++ app/Scripts/MejoraContinua/HD-3916/rollback/rollback_1_getusuarioverificacionautenticidad.sql
@@ -0,0 +1,97 @@
+GO
+USE BelcorpPeru
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpMexico
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpColombia
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpSalvador
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpPuertoRico
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpPanama
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpGuatemala
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpEcuador
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpDominicana
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpCostaRica
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpChile
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
+USE BelcorpBolivia
+GO
+IF (EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  delete from FiltrosOpcionesVerificacion  WHERE IdEstadoActividad = 4 AND OrigenID = 2
+END
+
+GO
---
+++ app/Scripts/MejoraContinua/HD-3916/script/1_INSERT_INTO_FiltrosOpcionesVerificacion.sql
@@ -0,0 +1,97 @@
+GO
+USE BelcorpPeru
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpMexico
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpColombia
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpSalvador
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpPuertoRico
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpPanama
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpGuatemala
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpEcuador
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpDominicana
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpCostaRica
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpChile
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
+USE BelcorpBolivia
+GO
+IF (NOT EXISTS(select existe = 1 FROM FiltrosOpcionesVerificacion WHERE IdEstadoActividad = 4 AND OrigenID = 2) )
+BEGIN
+  INSERT INTO FiltrosOpcionesVerificacion VALUES(4,0,0,'¡Nos alegras que hayas vuelto! Como hace tiempo que no sabíamos de ti, por tu seguridad vamos a verificar tus datos.',0,2)
+END
+
+GO
