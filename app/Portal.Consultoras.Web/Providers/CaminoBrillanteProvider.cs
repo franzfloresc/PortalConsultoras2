48acd9ef7a45b10ba7a244659f4f284115d71eaa
Victor Vilela <victor.vilela@avantica.net>
2019-06-13 10:57:37 -0500

    TESLA-32 : actualiza la version de automapper del proyecto web de 2.2.0 a 4.2.1

--- app/Portal.Consultoras.Web/Providers/CaminoBrillanteProvider.cs
+++ app/Portal.Consultoras.Web/Providers/CaminoBrillanteProvider.cs
@@ -10,6 +10,7 @@ using Portal.Consultoras.Common;
 using Portal.Consultoras.Web.Models.CaminoBrillante;
 using Portal.Consultoras.Web.HojaInscripcionODS;
 using Portal.Consultoras.Web.ServiceODS;
+using AutoMapper.Internal;
 
 namespace Portal.Consultoras.Web.Providers
 {
