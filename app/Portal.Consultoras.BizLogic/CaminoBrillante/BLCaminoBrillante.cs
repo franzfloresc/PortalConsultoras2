dec8243bb84f2eea5dfb29bc0296a463b229cb1f
Jonathan L <jhon.dev2014@gmail.com>
2019-06-17 17:10:54 -0500

    Fix Integracion

--- app/Portal.Consultoras.BizLogic/CaminoBrillante/BLCaminoBrillante.cs
+++ app/Portal.Consultoras.BizLogic/CaminoBrillante/BLCaminoBrillante.cs
@@ -1317,16 +1317,11 @@ namespace Portal.Consultoras.BizLogic.CaminoBrillante
                 MarcaID = demostrador.MarcaID,
                 CodigoMarca = demostrador.CodigoMarca,
                 DescripcionMarca = demostrador.DescripcionMarca,
-                //CodigoNivel = e.cod
-                //DescripcionNivel { get; set; }
                 PrecioValorizado = demostrador.PrecioValorizado,
                 PrecioCatalogo = demostrador.PrecioCatalogo,
                 FotoProductoSmall = demostrador.FotoProductoSmall,
                 FotoProductoMedium = demostrador.FotoProductoMedium,
                 FlagSeleccionado = demostrador.FlagSeleccionado,
-                //FlagDigitable = e.flag
-                //FlagHabilitado = e.fla
-                ///FlagHistorico = e.
                 EsCatalogo = demostrador.EsCatalogo,
                 Detalle = loadDetalle ? new List<BEOfertaCaminoBrillante>() { ToBEOfertaCaminoBrillante(demostrador, false) } : null
             };
@@ -1343,7 +1338,6 @@ namespace Portal.Consultoras.BizLogic.CaminoBrillante
                 DescripcionCUV = kit.DescripcionCUV,
                 DescripcionCortaCUV = kit.DescripcionCortaCUV,
                 MarcaID = kit.MarcaID,
-                //CodigoMarca = e.CodigoMarca,
                 DescripcionMarca = kit.DescripcionMarca,
                 CodigoNivel = kit.CodigoNivel,
                 DescripcionNivel = kit.DescripcionNivel,
@@ -1356,7 +1350,6 @@ namespace Portal.Consultoras.BizLogic.CaminoBrillante
                 FlagDigitable = kit.FlagDigitable,
                 FlagHabilitado = kit.FlagHabilitado,
                 FlagHistorico = kit.FlagHistorico,
-                //EsCatalogo = e.EsCatalogo
                 Detalle = (loadDetale && kit.Detalle != null) ? kit.Detalle.Select(e => ToBEOfertaCaminoBrillante(e, false)).ToList() : new List<BEOfertaCaminoBrillante>()
             };
         }
--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/index.js
@@ -473,3 +473,13 @@ $(window).load(function () {
     $("#overlayer").delay(200).fadeOut("slow");
 })
 
+$(".tog-vermas").click(function () {
+    $(".boxtom").slideToggle();
+    $(this).toggleClass("tog-vermenos");
+    if ($(".tog-vermas").hasClass("tog-vermenos")) {
+        $(this).text("Ver menos");
+    }
+    else {
+        $(this).text("Ver más");
+    }
+});
