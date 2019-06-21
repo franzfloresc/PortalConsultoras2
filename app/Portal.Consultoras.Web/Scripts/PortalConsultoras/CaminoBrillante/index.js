0e9b802d4979129c2d86c7fecfb2a52588f062d3
wilfredobelcorp <takian_99@hotmail.com>
2019-06-21 10:23:26 -0500

    carrusel infinito, vermas mejoras, barra de arriba alinear

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/CaminoBrillante/index.js
@@ -245,7 +245,7 @@ function ArmarMisGanancias(data) {
         serie.push(item.ValorSerie);
         titles.push(item.ValorSerieFormat);
         backgroundColors.push(colorBar);
-        hoverBackgrounds.push(colorBarSelected);
+        //hoverBackgrounds.push(colorBarSelected);
         if (item.FlagSeleccionMisGanancias) {
             indexSeleccion = x;
         }
@@ -256,7 +256,7 @@ function ArmarMisGanancias(data) {
         serie.push(0);
         titles.push("");
         backgroundColors.push(colorBar);
-        hoverBackgrounds.push(colorBarSelected);
+        //hoverBackgrounds.push(colorBarSelected);
     }
 
     Chart.pluginService.register({
@@ -313,7 +313,7 @@ function ArmarMisGanancias(data) {
                     borderWidth: 0,
                    
                     backgroundColor: backgroundColors,
-                    hoverBackgroundColor: hoverBackgrounds,
+                    //hoverBackgroundColor: hoverBackgrounds,
                     data: serie
                 }
             ]
@@ -330,9 +330,9 @@ function ArmarMisGanancias(data) {
                     // Reset old state
                     dataset = myBar.data.datasets[datasetIndex];
                     dataset.backgroundColor = backgroundColors.slice();
-                    dataset.hoverBackgroundColor = hoverBackgrounds.slice();
+                    //dataset.hoverBackgroundColor = hoverBackgrounds.slice();
                     dataset.backgroundColor[index] = colorBarSelected;
-                    dataset.hoverBackgroundColor[index] = colorBarSelected;
+                    //dataset.hoverBackgroundColor[index] = colorBarSelected;
                     // 
                 } else {
                     // remove hover styles
@@ -462,7 +462,7 @@ function ArmarCarrusel(data) {
     $('#carrusel').show();
 
     $(".regular").slick({
-        infinite: false,
+        infinite: true,
         slidesToShow: 4,
         centerMode: false,
         centerPadding: "0px",
@@ -475,7 +475,7 @@ function ArmarCarrusel(data) {
         responsive: [
             {
                 breakpoint: 426,
-                settings: { slidesToShow: 2, slidesToScroll:1, centerPadding: "25px",  infinite: false}
+                settings: { slidesToShow: 2, slidesToScroll: 2, centerPadding: "25px",  infinite: true}
             }
         ]
     });
