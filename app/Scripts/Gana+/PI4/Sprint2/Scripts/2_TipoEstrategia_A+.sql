baf678658c92bf3221fb184f69b465c499087c23
MIJAEL888 <mijael.palomino@hundred.com.pe>
2019-06-24 18:46:44 -0500

    GA-261 Crear tipo estrategia las mas ganadoras

---
+++ app/Scripts/Gana+/PI4/Sprint2/Scripts/2_TipoEstrategia_A+.sql
@@ -0,0 +1,588 @@
+USE BelcorpPeru
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpMexico
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpColombia
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpSalvador
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpPuertoRico
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpPanama
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpGuatemala
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpEcuador
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpDominicana
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpCostaRica
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpChile
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
+USE BelcorpBolivia
+GO
+
+DELETE FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+INSERT INTO [dbo].[TipoEstrategia]
+           ([DescripcionEstrategia]
+           ,[ImagenEstrategia]
+           ,[Orden]
+           ,[FlagActivo]
+           ,[UsuarioRegistro]
+           ,[FechaRegistro]
+           ,[UsuarioModificacion]
+           ,[FechaModificacion]
+           ,[flagNueva]
+           ,[flagRecoProduc]
+           ,[flagRecoPerfil]
+           ,[CodigoPrograma]
+           ,[FlagMostrarImg]
+           ,[Codigo]
+           ,[MostrarImgOfertaIndependiente]
+           ,[ImagenOfertaIndependiente]
+           ,[FlagValidarImagen]
+           ,[PesoMaximoImagen]
+           ,[NombreComercial]
+           ,[MensajeValidacion])
+     VALUES
+           ('Las mas ganadoras'
+           ,''
+           ,9
+           ,0
+           ,''
+           ,GETDATE()
+           ,''
+           ,GETDATE()
+           ,0
+           ,0
+           ,1
+           ,''
+           ,NULL
+           ,'LMG'
+           ,0
+           ,''
+           ,0
+           ,0
+           ,'Las más ganadoras'
+           ,'Este producto es una oferta digital. Te invitamos a que revises tu sección de ofertas.')
+--SELECT * FROM [dbo].[TipoEstrategia] WHERE Codigo = 'LMG'
+GO
+
