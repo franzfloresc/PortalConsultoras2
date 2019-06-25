8d75576c448b39e5a40ca65f9d05909860549457
César Cárdenas <cesar.cardenas@hundred.com.pe>
2019-06-25 11:48:27 -0500

    Se adecua condiciones para mostrar botón volver en ficha resumida mobile

--- app/Portal.Consultoras.Web/Controllers/BaseViewController.cs
+++ app/Portal.Consultoras.Web/Controllers/BaseViewController.cs
@@ -579,7 +579,7 @@ namespace Portal.Consultoras.Web.Controllers
         {
             var tipo = Constantes.TipoAccionNavegar.SinBoton;
 
-            if (esMobile && origen.ToString().StartsWith(Constantes.IngresoExternoOrigen.App))
+            if (!esEditar && esMobile && origen.ToString().StartsWith(Constantes.IngresoExternoOrigen.App))
             {
                 return tipo;
             }
