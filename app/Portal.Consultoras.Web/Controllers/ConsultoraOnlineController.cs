5a3fbd0ec16cf7b12e7965d6ccbf6155d566f81d
Carlos Ramos <carlosesteban943@gmail.com>
2019-06-25 14:51:38 -0500

    fix vilneravilidad

--- app/Portal.Consultoras.Web/Controllers/ConsultoraOnlineController.cs
+++ app/Portal.Consultoras.Web/Controllers/ConsultoraOnlineController.cs
@@ -6,6 +6,7 @@ using Portal.Consultoras.Web.Models;
 using Portal.Consultoras.Web.Providers;
 using Portal.Consultoras.Web.ServiceODS;
 using Portal.Consultoras.Web.ServicePedido;
+using Portal.Consultoras.Web.ServiceSAC;
 using Portal.Consultoras.Web.ServiceUsuario;
 using System;
 using System.Collections.Generic;
@@ -1389,36 +1390,6 @@ namespace Portal.Consultoras.Web.Controllers
 
                     }
                 }
-                //else
-                //{
-                //    String titulo = "(" + userData.CodigoISO + ") Consultora que atenderá tu pedido de " +
-                //                    HttpUtility.HtmlDecode(marcaPedido);
-                //    StringBuilder mensaje = new StringBuilder();
-                //    mensaje.AppendFormat("<p>Hola {0},</br><br /><br />", HttpUtility.HtmlDecode(pedidoAux.Cliente));
-                //    mensaje.AppendFormat("{0}</p><br/>", mensajeaCliente);
-                //    mensaje.Append("<br/>Saludos,<br/><br />");
-                //    mensaje.Append("<table><tr><td><img src=\"cid:{0}\" /></td>");
-                //    mensaje.AppendFormat(
-                //        "<td><p style='text-align: center;'><strong>{0}<br/>{1}<br/>Consultora</strong></p></td></tr></table>",
-                //        userData.NombreConsultora, userData.EMail);
-
-                //    try
-                //    {
-                //        if (pedido.Accion == 1)
-                //        {
-                //            Util.EnviarMail3(emailDe, pedidoAux.Email, titulo, mensaje.ToString(), true, string.Empty);
-                //        }
-                //        else
-                //        {
-                //            Util.EnviarMail3Mobile(emailDe, pedidoAux.Email, titulo, mensaje.ToString(), true,
-                //                string.Empty);
-                //        }
-                //    }
-                //    catch (Exception ex)
-                //    {
-                //        LogManager.LogManager.LogErrorWebServicesBus(ex, userData.CodigoConsultora, userData.CodigoISO);
-                //    }
-                //}
 
                 return Json(new
                 {
@@ -1484,13 +1455,7 @@ namespace Portal.Consultoras.Web.Controllers
 
                 if (NumIteracion == numIteracionMaximo)
                 {
-                    sv.RechazarSolicitudCliente(paisId, SolicitudId, true, OpcionRechazo, RazonMotivoRechazo);
-
-
-
-                    //if (pedido.FlagMedio == "01")
-                    //{
-                    double totalPedido = 0;
+                    sv.RechazarSolicitudCliente(paisId, SolicitudId, true, OpcionRechazo, RazonMotivoRechazo);                   
 
                     String titulocliente = "Tu pedido ha sido RECHAZADO por " + userData.PrimerNombre + " " +
                                            userData.PrimerApellido + " - " + medio;
@@ -2389,28 +2354,9 @@ namespace Portal.Consultoras.Web.Controllers
                     }, JsonRequestBehavior.AllowGet);
                 }
 
-                string medio = String.Empty;
-                switch (pedido.FlagMedio)
-                {
-                    case Constantes.SolicitudCliente.FlagMedio.AppCatalogos:
-                        medio = "App Catálogo";
-                        break;
-                    case Constantes.SolicitudCliente.FlagMedio.WebMarcas:
-                        medio = "Web Marcas";
-                        break;
-                    case Constantes.SolicitudCliente.FlagMedio.CatalogoDigital:
-                        medio = "Catálogo Digital";
-                        break;
-                    case Constantes.SolicitudCliente.FlagMedio.MaquilladorVirtual:
-                        medio = "Maquillador Virtual";
-                        break;
-                    default:
-                        break;
-                }
-
                 using (SACServiceClient svc = new SACServiceClient())
                 {
-                    svc.UpdSolicitudClienteRechazar(userData.PaisID, pedido.PedidoId, motivoRechazoId, motivoRechazoTexto);
+                    svc.UpdSolicitudClienteRechazar(userData.PaisID, pedido.PedidoId);
                 }
 
                 MisPedidosModel model = GetPendientes();
@@ -2592,8 +2538,7 @@ namespace Portal.Consultoras.Web.Controllers
                         det.Elegido = false;
                     }
                 }
-
-                //var flgMedio = Constantes.SolicitudCliente.FlagMedio.AppCatalogos;
+                
                 foreach (var det in lstDetalle)
                 {
                     foreach (var cab in pedidos.ListaPedidos)
