a619b12deaae5739c7aa444b85baa0650d763124
eiq21 <inca.quispe.e@outlook.com>
2019-06-26 16:02:56 -0500

    Corrección de scripts

--- app/Scripts/MejoraContinua/HD-4017/rollback/4_4_UpdCDRWebDetalle_MC_HD-4017_rollback.sql
+++ app/Scripts/MejoraContinua/HD-4017/rollback/4_4_UpdCDRWebDetalle_MC_HD-4017_rollback.sql
@@ -7,48 +7,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpChile
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
 BEGIN
 	 DROP PROC UpdCDRWebDetalle
 END
-GO
-
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
 GO
 
 USE BelcorpColombia
@@ -59,48 +23,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpCostaRica
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
 BEGIN
 	 DROP PROC UpdCDRWebDetalle
 END
-GO
-
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
 GO
 
 USE BelcorpDominicana
@@ -111,48 +39,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpEcuador
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
 BEGIN
 	 DROP PROC UpdCDRWebDetalle
 END
-GO
-
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
 GO
 
 USE BelcorpGuatemala
@@ -163,48 +55,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpMexico
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
 BEGIN
 	 DROP PROC UpdCDRWebDetalle
 END
-GO
-
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
 GO
 
 USE BelcorpPanama
@@ -215,48 +71,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpPeru
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
 BEGIN
 	 DROP PROC UpdCDRWebDetalle
 END
-GO
-
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
 GO
 
 USE BelcorpPuertoRico
@@ -267,24 +87,6 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
-
 USE BelcorpSalvador
 GO
 IF  EXISTS(SELECT * FROM sys.procedures WHERE name  = N'UpdCDRWebDetalle')
@@ -293,20 +95,3 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdCDRWebDetalle] (
-		@CDRWebDetalleID INT = 0	
-		,@DetalleXML XML  = '' 
-		,@RetornoID INT OUTPUT
-		)
-	AS
-	BEGIN
-		SET NOCOUNT ON;
-		SET @RetornoID = 0
-		 UPDATE CDRWebDetalle
-		 SET
-		 DetalleReemplazo = ISNULL(@DetalleXML,DetalleReemplazo)
-		 WHERE CDRWebDetalleID = @CDRWebDetalleID;
-		 SET @RetornoID = 1	
-		 SET NOCOUNT OFF;
-	END
-GO
