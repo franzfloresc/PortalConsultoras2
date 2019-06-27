e8a5d34b7e221d8001e77ab0576a82dd960a573c
Fernando Antezana <fernando.antezana@hundred.com.pe>
2019-06-27 16:11:26 -0500

    Tesla-179 : Modal Promociones CSS

---
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Promociones/PromocionesView.js
@@ -0,0 +1,43 @@
+﻿const _promoModal = document.getElementById('modal_promociones')
+
+
+//document.addEventListener('click', function (event) {
+//    if (event.target.matches('a[href], a[href] *')) {
+//        event.preventDefault();
+//        console.log('works fine')
+//    }
+//}, false);
+
+document.addEventListener('click', function (event) {
+    if (event.target.closest('[data-selector]')) {
+        
+        _showModal('#modal_promociones')
+        console.log('abre modal')
+    }
+    if (event.target.parentElement.matches("#cerrar-promociones-modal")) {
+        _hideModal('#modal_promociones')
+        console.log('cierra modal')
+    }
+}, false);
+
+var _showModal = function (idModal) {
+    if ($("body").find(".modal-fondo").length == 0) {
+        $("body").append('<div class="modal-fondo" style="opacity: 0.8; display:none"></div>');
+    }
+
+    $("body").css('overflow', 'hidden');
+    $('.modal-fondo').css('opacity', '.8');
+    $('.modal-fondo').show();
+    $(idModal).addClass("popup_active");
+
+    return true;
+};
+
+var _hideModal = function (idModal) {
+    $(idModal).removeClass('popup_active');
+    $('.modal-fondo').css('opacity', '0');
+    $('.modal-fondo').hide();
+    $("body").css('overflow', 'auto');
+
+    return true;
+};
