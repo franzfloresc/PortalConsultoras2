a2f6dfd1969e58966f0098abb374a1862f0b0b35
Victor Vilela <victor.vilela@avantica.net>
2019-07-02 18:27:13 -0500

    TESLA-177 : muestra banner de promociones
    - Muestra el banner de promociones en la ficha de producto
    - Muestra el popup de promociones
    - Creo PromocionesPresenter y PromocionesView

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Promociones/PromocionesView.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/DetalleEstrategia/FichaResponsive/Promociones/PromocionesView.js
@@ -1,43 +1,71 @@
-﻿const _promoModal = document.getElementById('modal_promociones')
+﻿var PromocionesView = function () {
+    var _presenter = null;
 
+    var _setPresenter = function (presenter) {
+        _presenter = presenter;
+    };
 
-//document.addEventListener('click', function (event) {
-//    if (event.target.matches('a[href], a[href] *')) {
-//        event.preventDefault();
-//        console.log('works fine')
-//    }
-//}, false);
+    var _elements = {
+        banner: {
+            all: "[data-selector]",
+        },
+        promocionesModal: {
+            id: "#modal-promociones",
+            btnCerrar: {
+                id: "#cerrar-promociones-modal"
+            }
+        }
+    };
 
-document.addEventListener('click', function (event) {
-    if (event.target.closest('[data-selector]')) {
-        
-        _showModal('#modal_promociones')
-        console.log('abre modal')
-    }
-    if (event.target.parentElement.matches("#cerrar-promociones-modal")) {
-        _hideModal('#modal_promociones')
-        console.log('cierra modal')
-    }
-}, false);
+    var _showModal = function (idModal) {
+        if ($("body").find(".modal-fondo").length == 0) {
+            $("body").append('<div class="modal-fondo" style="opacity: 0.8; display:none"></div>');
+        }
 
-var _showModal = function (idModal) {
-    if ($("body").find(".modal-fondo").length == 0) {
-        $("body").append('<div class="modal-fondo" style="opacity: 0.8; display:none"></div>');
-    }
+        $("body").css('overflow', 'hidden');
+        $('.modal-fondo').css('opacity', '.8');
+        $('.modal-fondo').show();
+        $(idModal).addClass("popup_active");
 
-    $("body").css('overflow', 'hidden');
-    $('.modal-fondo').css('opacity', '.8');
-    $('.modal-fondo').show();
-    $(idModal).addClass("popup_active");
+        return true;
+    };
 
-    return true;
-};
+    var _hideModal = function (idModal) {
+        $(idModal).removeClass('popup_active');
+        $('.modal-fondo').css('opacity', '0');
+        $('.modal-fondo').hide();
+        $("body").css('overflow', 'auto');
 
-var _hideModal = function (idModal) {
-    $(idModal).removeClass('popup_active');
-    $('.modal-fondo').css('opacity', '0');
-    $('.modal-fondo').hide();
-    $("body").css('overflow', 'auto');
+        return true;
+    };
 
-    return true;
+    var _hideBanner = function () {
+        $(_elements.banner.all).hide();
+    };
+
+    var _showBanner = function () {
+        $(_elements.banner.all).show();
+
+        $("body").on("click", _elements.banner.all, function (e) {
+            e.preventDefault();
+            _presenter.onShowModalPromocionesClicked();
+            
+        });
+
+        $(_elements.promocionesModal.btnCerrar.id).click(function (e) {
+            e.preventDefault();
+            _hideModal(_elements.promocionesModal.id);
+        });
+    };
+
+    var _showModalPromociones = function () {
+        _showModal(_elements.promocionesModal.id);
+    };
+
+    return {
+        setPresenter: _setPresenter,
+        hideBanner: _hideBanner,
+        showBanner: _showBanner,
+        showModalPromociones: _showModalPromociones,
+    };
 };
