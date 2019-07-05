5aeb85692f3098b3823e161fb4a461cf2010a3a8
Paquirri <spaquirri@gmail.com>
2019-07-05 12:17:52 -0500

    Pase de scripts

---
+++ app/Scripts/MejoraContinua/HD-4220/RollBack_3_VerificarConsultoraDigitalRecibe_MC_HD-4288.sql
@@ -0,0 +1,153 @@
+﻿use [BelcorpBolivia]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+use [BelcorpChile]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpColombia]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpCostaRica]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpDominicana]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpEcuador]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpGuatemala]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpMexico]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpPanama]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpPeru]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpPuertoRico]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
+
+
+use [BelcorpSalvador]	
+go
+
+IF EXISTS (
+	SELECT * FROM sys.objects 
+	WHERE object_id = 
+	OBJECT_ID(N'[dbo].[VerificarConsultoraDigitalRecibe]') 
+	AND type in (N'P', N'PC')
+) 
+	DROP PROCEDURE [dbo].[VerificarConsultoraDigitalRecibe]
+GO
