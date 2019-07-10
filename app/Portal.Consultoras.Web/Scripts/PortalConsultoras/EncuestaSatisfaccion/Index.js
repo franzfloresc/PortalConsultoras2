a16bb7f06761c4aa02e6ebcc94b3988ecfa68007
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-07-10 15:38:45 -0500

    Se agrega clase para desactivar estados que no hayan sido seleccionados.

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EncuestaSatisfaccion/Index.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/EncuestaSatisfaccion/Index.js
@@ -26,25 +26,18 @@
             $(_elementos.enlaceCerrarEncuestaSatisfaccion).fadeOut(100);
             $(_elementos.enlaceVolverAtras).fadeIn(100);
             $(this).parents(_elementos.encuestaSatisfaccion).addClass('seccion__encuesta__satisfaccion__expandir');
-            $(_elementos.estadoEncuestaSatisfaccion).parents('.encuesta__satisfaccion__estado').addClass('encuesta__satisfaccion__disabled');
-            $(this).parents('.encuesta__satisfaccion__estado').removeClass('encuesta__satisfaccion__disabled');
+            $(_elementos.estadoEncuestaSatisfaccion).addClass('encuesta__satisfaccion__disabled');
+            $(this).removeClass('encuesta__satisfaccion__disabled');
             if (!$(_elementos.motivosCalificacion).is(':visible')) {
                 $(_elementos.motivosCalificacion).show();
-                $(_elementos.motivosCalificacion).delay(280);
-                $(_elementos.motivosCalificacion).animate({
-                    'opacity': 1,
-                    'top': 0
-                }, 300);
+                $(_elementos.motivosCalificacion).addClass('seccion__encuesta__satisfaccion__motivosCalificacion--mostrar');
             }
         },
         volverAtras: function (e) {
             e.preventDefault();
-            $(_elementos.motivosCalificacion).animate({
-                'opacity': 0,
-                'top': -30
-            }, 350);
+            $(_elementos.motivosCalificacion).removeClass('seccion__encuesta__satisfaccion__motivosCalificacion--mostrar');
             $(_elementos.motivosCalificacion).hide();
-            $(_elementos.estadoEncuestaSatisfaccion).parents('.encuesta__satisfaccion__estado').removeClass('encuesta__satisfaccion__disabled');
+            $(_elementos.estadoEncuestaSatisfaccion).removeClass('encuesta__satisfaccion__disabled');
             $(_elementos.enlaceVolverAtras).fadeOut(100);
             $(this).parents(_elementos.encuestaSatisfaccion).removeClass('seccion__encuesta__satisfaccion__expandir');
             $(_elementos.enlaceCerrarEncuestaSatisfaccion).fadeIn(100);
