b08c2dbc7703f59e8048b4a4ffc8b3080bbdebbe
melissadrrs <melissadrrs@gmail.com>
2019-07-11 16:30:57 -0500

    .

---
+++ app/Portal.Consultoras.Web/Controllers/ReporteEncuestaSatisfaccionController.cs
@@ -0,0 +1,53 @@
+﻿using ClosedXML.Excel;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.ServiceSAC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class ReporteEncuestaSatisfaccionController : BaseAdmController
+    {
+        // GET: EncuestaSatisfaccion
+        public ActionResult Index()
+        {
+            int paisId = userData.PaisID;
+            try
+            {
+                if (!UsuarioModel.HasAcces(ViewBag.Permiso, "ReporteEncuestaSatisfaccionController/Index"))
+                    return RedirectToAction("Index", "Bienvenida");
+
+
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, userData.CodigoConsultora, userData.CodigoISO);
+            }
+
+
+            var model = new ReporteEncuestaSatisfaccionModel()
+            {
+                listaCampanias = _zonificacionProvider.GetCampanias(paisId),
+                listaPaises = DropDowListPaises(),
+                listaRegiones = _zonificacionProvider.GetRegiones(paisId),
+                listaZonas = _zonificacionProvider.GetZonas(paisId),
+                PaisID = paisId
+
+
+            };
+            return View(model);
+        }
+
+
+
+
+    }
+}
---
+++ app/Portal.Consultoras.Web/Models/ReporteEncuestaSatisfaccionModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.Models
+{
+    public class ReporteEncuestaSatisfaccionModel
+    {
+        public int PaisID { get; set; }
+        public int RegionID { get; set; }
+        public int ZonaID { get; set; }
+        public IEnumerable<CampaniaModel> listaCampanias { set; get; }
+        public IEnumerable<RegionModel> listaRegiones { set; get; }
+        public IEnumerable<ZonaModel> listaZonas { set; get; }
+        public IEnumerable<PaisModel> listaPaises { set; get; }
+
+        public int ConsultoraID { get; set; }
+        public string CodigoConsultora { get; set; }
+        public string CodigoCampania { set; get; }
+        public string Consultora { get; set; }
+        public int MotivoID { get; set; }
+        public string Calificacion { get; set; }
+        public string Motivo { get; set; }
+
+    }
+}
