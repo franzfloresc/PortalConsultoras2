646cf8b484e6d21f208ce73c8d53ac89d0659945
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-07-11 17:54:19 -0500

    Vista Fechas de Facturación (Mobile).

---
+++ app/Portal.Consultoras.Web/Controllers/FechasFacturacionController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Portal.Consultoras.Common;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class FechasFacturacionController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
