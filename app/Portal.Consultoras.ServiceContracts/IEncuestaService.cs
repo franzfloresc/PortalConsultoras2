0901a555af708cdb5401d7bb40a8c1b42a3a0bd6
eiq21 <inca.quispe.e@outlook.com>
2019-07-12 13:15:15 -0500

    Avance creación de tablas y métodos

---
+++ app/Portal.Consultoras.ServiceContracts/IEncuestaService.cs
@@ -0,0 +1,14 @@
+﻿using Portal.Consultoras.Entities.Encuesta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Consultoras.ServiceContracts
+{
+    public interface IEncuestaService
+    {
+        List<BEDataConfigEncuesta> ObtenerDataEncuesta(int paisId, int encuestaId);
+    }
+}
