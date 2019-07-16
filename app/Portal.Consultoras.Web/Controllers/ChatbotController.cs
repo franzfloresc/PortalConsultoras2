c3d717342cf8d494351ec130201efb636cf14dbe
Omar <omar.calderon@hundred.com.pe>
2019-07-16 12:08:51 -0500

    creacion rama HD-4507

---
+++ app/Portal.Consultoras.Web/Controllers/ChatbotController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.Providers;
+using Portal.Consultoras.Web.ServiceSAC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Web.Mvc;
+
+namespace Portal.Consultoras.Web.Controllers
+{
+    public class ChatbotController : BaseAdmController
+    {
+        protected TablaLogicaProvider _tablaLogica;
+        public ChatbotController()
+        {
+            _tablaLogica = new TablaLogicaProvider();
+        }
+
+        public ActionResult Index()
+        {
+            
+            return View(model);
+
+        }
+
+
+    }
+}
