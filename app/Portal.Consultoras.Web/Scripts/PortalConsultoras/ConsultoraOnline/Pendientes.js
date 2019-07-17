c238e34d97e1d66456a3ceb00f2190a092503698
Giovanni <giovanni.benvenutto@zoluxiones.com>
2019-07-17 16:44:40 -0500

    HD-4638 - Botón Aceptación Pedido Pendiente (Desktop).

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/ConsultoraOnline/Pendientes.js
@@ -15,18 +15,18 @@ function bindElments() {
 
         var btn = $(this).find('.dark-color');
         if (btn) {
-            if (!btn.hasClass('ghost')) {
-                $('.btnAccion').find('a').removeClass('ghost');
+            if (!btn.hasClass('ghost--estadoAceptado')) {
+                $('.btnAccion').find('a').removeClass('ghost--estadoAceptado');
                 $('.btnAccion').find('a').html('Elegir');
-                btn.addClass('ghost');
+                btn.addClass('ghost--estadoAceptado');
                 btn.html('Elegido');
             } else {
-                btn.removeClass('ghost');
+                btn.removeClass('ghost--estadoAceptado');
                 btn.html('Elegir');
             }
         }
 
-        if ($('.btnAccion a.ghost').length == $('.ghost a').length) {
+        if ($('.btnAccion a.ghost--estadoAceptado').length == $('.ghost--estadoAceptado a').length) {
             $('#btnAceptarPedido span').removeClass('second-color');
             $('#btnAceptarPedido span').addClass('disabled');
         }
@@ -42,7 +42,7 @@ function bindElments() {
 
 function AceptarPedidoPendiente() {
 
-    var btn = $('.btnAccion a.ghost')[0];
+    var btn = $('.btnAccion a.ghost--estadoAceptado')[0];
     var accionTipo = $(btn).parent().data('accion');
 
     if (btn) {
@@ -304,13 +304,13 @@ function OcultarMensajedeRechazoPedido(cuv) {
 
 function AceptarPedidoProducto(id) {
     var aceptado = '#aceptar_' + id;
-    if ($(aceptado).hasClass("ghost")) {
-        $(aceptado).removeClass('ghost');
+    if ($(aceptado).hasClass("ghost--estadoAceptado")) {
+        $(aceptado).removeClass('ghost--estadoAceptado');
         $(aceptado).text('Aceptar');
     }
     else {
-        $(aceptado).addClass('ghost');
-        $(aceptado).text('Aceptado');
+        $(aceptado).addClass('ghost--estadoAceptado');
+        $(aceptado).text('Está aceptado');
 
         MarcaAnalyticsClienteProducto("Aceptado");
     }
@@ -483,7 +483,7 @@ function ContinuarPedido() {
 
     $paso1.find('.pedidos').each(function () {
 
-        if ($(this).find('a[id*="aceptar_"]').hasClass('ghost')) {
+        if ($(this).find('a[id*="aceptar_"]').hasClass('ghost--estadoAceptado')) {
             var pedidoId = $(this).find(".pedidoId").val();
             var cuv = $(this).find(".cuv").val();
             var cantNew = $(this).find('[data-cantNew]').val();
