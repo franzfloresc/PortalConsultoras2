ed27fabde5824eccce3f8bdad9966f0c37c4215b
Carlos Ramos <carlosesteban943@gmail.com>
2019-07-17 10:07:45 -0500

    agregando case de numero numino y maximo de celular puerto rico

--- app/Portal.Consultoras.Common/Util.cs
+++ app/Portal.Consultoras.Common/Util.cs
@@ -3616,6 +3616,10 @@ namespace Portal.Consultoras.Common
                     limiteMinimoTelef = 5;
                     limiteMaximoTelef = 8;
                     break;
+                case Constantes.PaisID.PuertoRico:
+                    limiteMinimoTelef = 10;
+                    limiteMaximoTelef = 10;
+                    break;
                 default:
                     limiteMinimoTelef = 5;
                     limiteMaximoTelef = 15;
