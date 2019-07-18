873612f021214b8691adc6a0e6afe611c6b67cc3
Carlos Ramos <carlosesteban943@gmail.com>
2019-07-18 12:08:01 -0500

    fix observaciones sonarcube

--- app/Portal.Consultoras.BizLogic/Usuario/BLUsuario.cs
+++ app/Portal.Consultoras.BizLogic/Usuario/BLUsuario.cs
@@ -4245,8 +4245,7 @@ namespace Portal.Consultoras.BizLogic
             {
                 try
                 {
-                    string paisISO = Portal.Consultoras.Common.Util.GetPaisISO(paisID);
-                    string paisesEsika = ConfigurationManager.AppSettings["PaisesEsika"] ?? "";                    
+                    string paisISO = Portal.Consultoras.Common.Util.GetPaisISO(paisID);                                       
                     string emailFrom = "no-responder@somosbelcorp.com";
                     string emailTo = oUsu.Correo;
                     string titulo = "(" + paisISO + ") Cambio de clave de Somosbelcorp";                    
@@ -4279,8 +4278,7 @@ namespace Portal.Consultoras.BizLogic
             {
                 try
                 {
-                    string paisISO = Portal.Consultoras.Common.Util.GetPaisISO(paisID);
-                    string paisesEsika = ConfigurationManager.AppSettings["PaisesEsika"] ?? "";                    
+                    string paisISO = Portal.Consultoras.Common.Util.GetPaisISO(paisID);                                 
                     string emailFrom = "no-responder@somosbelcorp.com";
                     string emailTo = oUsu.Correo;
                     string titulo = "(" + paisISO + ") Cambio de clave de Somosbelcorp";
