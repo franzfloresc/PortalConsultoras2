c97ddab93f223ac856dae5d0403d21e69f9b42f9
jose-ponte <jose.ponte@hundred.com.pe>
2019-07-25 10:06:01 -0500

    Creacion de Vista Monto Exigencia

--- app/Portal.Consultoras.Web/Controllers/CaminoBrillanteAdministrarController.cs
+++ app/Portal.Consultoras.Web/Controllers/CaminoBrillanteAdministrarController.cs
@@ -1,4 +1,8 @@
-﻿using Portal.Consultoras.Web.Models;
+﻿using AutoMapper;
+using Portal.Consultoras.Common;
+using Portal.Consultoras.Web.Models;
+using Portal.Consultoras.Web.Models.CaminoBrillante;
+using Portal.Consultoras.Web.ServicePedido;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,8 @@ namespace Portal.Consultoras.Web.Controllers
 {
     public class CaminoBrillanteAdministrarController: BaseAdmController
     {
+        static List<BEConfiguracionOferta> lstConfiguracion = new List<BEConfiguracionOferta>();
+
         public ActionResult AdministrarBeneficios(TipoEstrategiaModel model)
         {
             try
@@ -22,5 +28,47 @@ namespace Portal.Consultoras.Web.Controllers
             }
             return View(model);
         }
+
+        #region Administrar Monto Exigencia
+        public ActionResult AdministrarMontoExigencia(AdministrarMontoExigenciaModel model)
+        {
+            try
+            {
+                model.listaPaises = DropDowListPaises();
+                model.listaCampania = new List<CampaniaModel>();
+            }
+            catch (FaultException ex)
+            {
+                LogManager.LogManager.LogErrorWebServicesPortal(ex, userData.CodigoConsultora, userData.CodigoISO);
+            }
+            return View(model);
+        }
+
+        public JsonResult ObtenerCampaniasYConfiguracionPorPais(int PaisID)
+        {
+            IEnumerable<CampaniaModel> lst = _zonificacionProvider.GetCampanias(PaisID);
+            IEnumerable<ConfiguracionOfertaModel> lstConfig = DropDowListConfiguracion(PaisID);
+            string habilitarNemotecnico = _tablaLogicaProvider.GetTablaLogicaDatoCodigo(PaisID, ConsTablaLogica.Plan20.TablaLogicaId, ConsTablaLogica.Plan20.BusquedaNemotecnicoOfertaLiquidacion);
+
+            return Json(new
+            {
+                lista = lst,
+                lstConfig = lstConfig,
+                habilitarNemotecnico = habilitarNemotecnico == "1"
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<ConfiguracionOfertaModel> DropDowListConfiguracion(int paisId)
+        {
+            List<BEConfiguracionOferta> lst;
+            using (PedidoServiceClient sv = new PedidoServiceClient())
+            {
+                lstConfiguracion = sv.GetTipoOfertasAdministracion(paisId, Constantes.ConfiguracionOferta.Liquidacion).ToList();
+                lst = lstConfiguracion;
+            }
+
+            return Mapper.Map<IList<BEConfiguracionOferta>, IEnumerable<ConfiguracionOfertaModel>>(lst);
+        }
+        #endregion
     }
 }
---
+++ app/Portal.Consultoras.Web/Models/CaminoBrillante/AdministrarMontoExigenciaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Consultoras.Web.Models.CaminoBrillante
+{
+    public class AdministrarMontoExigenciaModel
+    {
+        public int PaisID { set; get; }
+        public IEnumerable<PaisModel> listaPaises { set; get; }
+        public int CampaniaID { get; set; }
+        public IEnumerable<CampaniaModel> listaCampania { set; get; }
+    }
+}
