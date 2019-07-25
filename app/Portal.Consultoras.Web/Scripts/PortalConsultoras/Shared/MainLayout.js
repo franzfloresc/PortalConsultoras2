b1cdf125a5fb15840aa3630891222e6275f1995b
Giancarlos Salas <giansalex@gmail.com>
2019-07-25 11:24:16 -0500

    fix invalid close bracket

--- app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayout.js
+++ app/Portal.Consultoras.Web/Scripts/PortalConsultoras/Shared/MainLayout.js
@@ -529,9 +529,6 @@ function alert_msg(message, titulo, funcion) {
     showDialogSinScroll("alertDialogMensajes");
 }
 
-
-}
-
 function ValidarCorreo(correo) {
     var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+[a-zA-Z0-9]{2,4}$/;
     return expr.test(correo);
