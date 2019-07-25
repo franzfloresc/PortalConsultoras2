04432c47a72b09fd6f5ca6536e07dfda1499320b
Jonathan L <jhon.dev2014@gmail.com>
2019-07-25 17:55:21 -0500

    Revert "Revert "Ajustes Varios""
    This reverts commit 5f60577b0d3d6bfc2e211578c86e241ad2a434d3.

--- app/Portal.Consultoras.Service/UsuarioService.cs
+++ app/Portal.Consultoras.Service/UsuarioService.cs
@@ -995,6 +995,26 @@ namespace Portal.Consultoras.Service
         {
             return _caminoBrillanteBusinessLogic.GetCaminoBrillanteConfiguracion(paisID, esApp);
         }
+
+        public List<BENivelCaminoBrillante> GetNiveles(int paisID)
+        {
+            return _caminoBrillanteBusinessLogic.GetNiveles(paisID);
+        }
+
+        public List<BEBeneficioCaminoBrillante> GetBeneficiosCaminoBrillante(int paisID, string codigoNivel)
+        {
+            return _caminoBrillanteBusinessLogic.GetBeneficiosCaminoBrillante(paisID, codigoNivel);
+        }
+
+        public void InsBeneficioCaminoBrillante(int paisId, BEBeneficioCaminoBrillante entidad)
+        {
+            _caminoBrillanteBusinessLogic.InsBeneficioCaminoBrillante(paisId, entidad);
+        }
+
+        public void DelBeneficioCaminoBrillante(int paisId, string CodigoNivel, string CodigoBeneficio)
+        {
+            _caminoBrillanteBusinessLogic.DelBeneficioCaminoBrillante(paisId, CodigoNivel, CodigoBeneficio);
+        }
         #endregion
 
         public int ActualizarValidacionDatos(bool isMobile, string ipDispositivo, string codigoConsultora, int PaisID, string CodigoUsuario, string tipoEnvio1, string tipoEnvio2)
