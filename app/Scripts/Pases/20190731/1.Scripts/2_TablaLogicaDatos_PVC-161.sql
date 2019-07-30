2186567429d01afa9a61e7cea5ceee7c02486f40
Jonathan L <jhon.dev2014@gmail.com>
2019-07-30 15:03:39 -0500

    PVC-161 Scripts

---
+++ app/Scripts/Pases/20190731/1.Scripts/2_TablaLogicaDatos_PVC-161.sql
@@ -0,0 +1,48 @@
+﻿USE BelcorpColombia
+GO
+
+DELETE FROM TablaLogicaDatos where TablaLogicaDatosID in ( 17609, 17610);
+
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17609, 176, 'sb_granBrillante', 'Gran Brillante', '24300');
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17610, 176, 'app_granBrillante', 'Gran Brillante', '24300');
+
+GO
+
+USE BelcorpCostaRica
+GO
+
+DELETE FROM TablaLogicaDatos where TablaLogicaDatosID in ( 17609, 17610);
+
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17609, 176, 'sb_granBrillante', 'Gran Brillante', '4900');
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17610, 176, 'app_granBrillante', 'Gran Brillante', '4900');
+
+GO
+
+USE BelcorpChile
+GO
+
+DELETE FROM TablaLogicaDatos where TablaLogicaDatosID in ( 17609, 17610);
+
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17609, 176, 'sb_granBrillante', 'Gran Brillante', '5000');
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17610, 176, 'app_granBrillante', 'Gran Brillante', '5000');
+
+GO
+
+USE BelcorpBolivia
+GO
+
+DELETE FROM TablaLogicaDatos where TablaLogicaDatosID in ( 17609, 17610);
+
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17609, 176, 'sb_granBrillante', 'Gran Brillante', '10000');
+INSERT INTO TablaLogicaDatos (TablaLogicaDatosID, TablaLogicaID, Codigo, Descripcion, Valor)
+	VALUES (17610, 176, 'app_granBrillante', 'Gran Brillante', '10000');
+
+GO
+
