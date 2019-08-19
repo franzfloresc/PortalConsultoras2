737ad619ddeefcaab88ca54c1e40d7628ef42ef3
Enrique Alfaro <lead123456@hotmail.com>
2019-08-19 17:10:38 -0500

    qas: Agregar scripts de BD SALUD-123 y SALUD-134

---
+++ app/Scripts/Pases/20190822/1.Scripts/4_GPR_ActualizarIndicadorGPRPedidosRechazados_SALUD_134.sql
@@ -0,0 +1,227 @@
+﻿USE BelcorpBolivia
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpChile
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpColombia
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpCostaRica
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpDominicana
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpEcuador
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpGuatemala
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpMexico
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpPanama
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpPeru
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
+
+USE BelcorpSalvador
+GO
+ALTER PROCEDURE GPR.ActualizarIndicadorGPRPedidosRechazados
+	@Campania int,
+	@PedidoId int
+AS
+BEGIN
+	update dbo.PedidoWeb with(nolock)
+	set
+		PW.IndicadorEnviado = 0, 
+		PW.GPRSB = 2,
+		PW.EstadoPedido = 201,
+		PW.MontoTotalProl = 0,			
+		PW.ValidacionAbierta = 0,
+		PW.ModificaPedidoReservado = 0
+	where CampaniaId = @Campania and PedidoId = @PedidoId;
+END
+GO
---
+++ app/Scripts/Pases/20190822/2.Rollback/Rollback_5_GPR_GetProcesoPedidoRechazadoConfiguracion_SALUD_123.sql
@@ -0,0 +1,287 @@
+﻿USE BelcorpBolivia
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpChile
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpColombia
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpCostaRica
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpDominicana
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpEcuador
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpGuatemala
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpMexico
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpPanama
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpPeru
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpPuertoRico
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
+
+USE BelcorpSalvador
+GO
+IF OBJECT_ID('GPR.GetProcesoPedidoRechazadoConfiguracion') IS NOT NULL
+	DROP PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+GO
+CREATE PROCEDURE GPR.GetProcesoPedidoRechazadoConfiguracion
+	@CodigoProceso varchar(20)
+AS
+BEGIN
+	SELECT TOP 1
+		ProcesoPedidoRechazadoConfiguracionId,
+		CodigoProceso,
+		Activo,
+		RestriccionEjecucion,
+		HoraInicioRestriccion,
+		HoraFinRestriccion,
+		ProlServiceEndPoint,
+		EnvioCorreoTodo,
+		EnvioCorreoError
+	FROM GPR.ProcesoPedidoRechazadoConfiguracion
+	WHERE CodigoProceso = @CodigoProceso
+END
+GO
