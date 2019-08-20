1e7546bd9142fb8c363f56010f20abeafa5d8cd0
Enrique Alfaro <lead123456@hotmail.com>
2019-08-20 18:05:24 -0500

    qas: Ajustes en identación.

--- app/Portal.Consultoras.Web/Providers/ShowRoomProvider.cs
+++ app/Portal.Consultoras.Web/Providers/ShowRoomProvider.cs
@@ -405,24 +405,15 @@ namespace Portal.Consultoras.Web.Providers
 
         public void CargarEventoPersonalizacion(UsuarioModel model)
         {
-
-            if (model == null)
-            {
-                return;
-            }
-
+            if (model == null) return;
             string _codigoConsultora = model.GetCodigoConsultora();
-            if (string.IsNullOrWhiteSpace(_codigoConsultora))
-                return;
+            if (string.IsNullOrWhiteSpace(_codigoConsultora)) return;
+
             ConfigModel configEstrategiaSR= new ConfigModel();
             try
             {
                 configEstrategiaSR = _sessionManager.GetEstrategiaSR() ?? new ConfigModel();
-
-                if (configEstrategiaSR.CargoEntidadEventoPersonalizacion)
-                {
-                    return;
-                }
+                if (configEstrategiaSR.CargoEntidadEventoPersonalizacion) return;
 
                 _sessionManager.SetEsShowRoom("0");
                 _sessionManager.SetMostrarShowRoomProductos("0");
